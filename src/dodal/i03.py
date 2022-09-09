7073d171ccb0a611b9955d52942c501f83cf007a
Dominic Oram <dominic.oram@diamond.ac.uk>
2022-09-09 15:19:25 +0100

    (#2) Add more devices to i03/s03

--- src/dodal/i03.py
+++ src/dodal/i03.py
@@ -7,6 +7,9 @@ from dodal.devices.synchrotron import Synchrotron
 from dodal.devices.slit_gaps import SlitGaps
 from dodal.devices.eiger import EigerDetector
 from dodal.devices.detector import DetectorParams
+from dodal.devices.backlight import Backlight
+from dodal.devices.aperture import Aperture
+from dodal.devices.oav import OAV
 
 BEAMLINE_PREFIX = "BL03I"
 INSERTION_PREFIX = "SR03I"
@@ -31,3 +34,12 @@ def sample_motors() -> I03Smargon:
 
 def eiger(parameters: DetectorParams) -> EigerDetector:
     return EigerDetector(parameters, prefix=f"{BEAMLINE_PREFIX}-EA-EIGER-01:")
+
+def backlight() -> Backlight:
+    return Backlight(name="Backlight", prefix=f"{BEAMLINE_PREFIX}")
+
+def aperture() -> Aperture:
+    return Aperture(name="Aperture", prefix=f"{BEAMLINE_PREFIX}-MO-MAPT-01:")
+
+def oav() -> OAV:
+    return OAV(name="OAV", prefix=f"{BEAMLINE_PREFIX}-DI-OAV-01")
