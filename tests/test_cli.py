b68e83aaac26fdbba6737ba834735ab75efa76ec
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-01-18 16:15:05 +0000

    Remove CLI test

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-from dodal import __version__
-
-
-def test_cli_version():
-    cmd = [sys.executable, "-m", "dodal", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
