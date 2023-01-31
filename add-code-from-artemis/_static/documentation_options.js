4f696cdee8be80b9ffa5cb4eb3b682baa282bd73
noemifrisina <noemifrisina@users.noreply.github.com>
2023-01-31 14:38:01 +0000

    deploy: 3e711c0c010bac75b2767598655f53e58bc0cf8b

--- add-code-from-artemis/_static/documentation_options.js
+++ add-code-from-artemis/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev505+g7c411f2',
+    VERSION: '0.1.dev506+g3e711c0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
