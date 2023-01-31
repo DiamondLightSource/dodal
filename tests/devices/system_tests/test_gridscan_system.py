41f99d95fb49540cc67fe5c91061352e6d82187e
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2023-01-31 15:58:57 +0000

    Add back system test gridscan

--- tests/devices/system_tests/test_gridscan_system.py
+++ tests/devices/system_tests/test_gridscan_system.py
@@ -1,6 +1,5 @@
+import bluesky.plan_stubs as bps
 import pytest
-
-# from artemis.experiment_plans.fast_grid_scan_plan import wait_for_fgs_valid
 from bluesky.run_engine import RunEngine
 
 from dodal.devices.fast_grid_scan import (
@@ -8,6 +7,18 @@ from dodal.devices.fast_grid_scan import (
     GridScanParams,
     set_fast_grid_scan_params,
 )
+
+
+def wait_for_fgs_valid(fgs_motors: FastGridScan, timeout=0.5):
+    SLEEP_PER_CHECK = 0.1
+    times_to_check = int(timeout / SLEEP_PER_CHECK)
+    for _ in range(times_to_check):
+        scan_invalid = yield from bps.rd(fgs_motors.scan_invalid)
+        pos_counter = yield from bps.rd(fgs_motors.position_counter)
+        if not scan_invalid and pos_counter == 0:
+            return
+        yield from bps.sleep(SLEEP_PER_CHECK)
+    raise Exception(f"Scan parameters invalid after {timeout} seconds")
 
 
 @pytest.fixture()
@@ -31,9 +42,9 @@ def test_when_program_data_set_and_staged_then_expected_images_correct(
 def test_given_valid_params_when_kickoff_then_completion_status_increases_and_finishes(
     fast_grid_scan: FastGridScan,
 ):
-    # def set_and_wait_plan(fast_grid_scan: FastGridScan):
-    #     yield from set_fast_grid_scan_params(fast_grid_scan, GridScanParams(3, 3))
-    #     yield from wait_for_fgs_valid(fast_grid_scan)
+    def set_and_wait_plan(fast_grid_scan: FastGridScan):
+        yield from set_fast_grid_scan_params(fast_grid_scan, GridScanParams(3, 3))
+        yield from wait_for_fgs_valid(fast_grid_scan)
 
     prev_current, prev_fraction = None, None
 
@@ -49,8 +60,8 @@ def test_given_valid_params_when_kickoff_then_completion_status_increases_and_fi
                 assert 0 < fraction < 1
                 assert 0 < prev_fraction < 1
 
-    # RE = RunEngine()
-    # RE(set_and_wait_plan(fast_grid_scan))
+    RE = RunEngine()
+    RE(set_and_wait_plan(fast_grid_scan))
     assert fast_grid_scan.position_counter.get() == 0
 
     # S03 currently is giving 2* the number of expected images (see #13)
