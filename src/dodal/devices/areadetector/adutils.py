0bb86478c6d6b80400a648d1d5aa3af77ebfc505
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-02-17 11:12:16 +0000

    Duplicate nonblocking AD functionality from nslsii package

--- src/dodal/devices/areadetector/adutils.py
+++ src/dodal/devices/areadetector/adutils.py
@@ -1,20 +1,34 @@
-from nslsii.ad33 import CamV33Mixin
+from ophyd import Component as Cpt
+from ophyd import EpicsSignal, EpicsSignalRO
 from ophyd.areadetector.cam import AreaDetectorCam
 from ophyd.areadetector.filestore_mixins import FileStoreHDF5, FileStoreIterativeWrite
 from ophyd.areadetector.plugins import HDF5Plugin
 
 
-class SynchronisedAdDriverBase(AreaDetectorCam, CamV33Mixin):
+class SynchronisedAdDriverBase(AreaDetectorCam):
     """
     Base Ophyd device to control an AreaDetector driver and
     syncrhonise it on other AreaDetector plugins, even non-blocking ones.
     """
+
+    adcore_version = Cpt(EpicsSignalRO, "ADCoreVersion_RBV", string=True, kind="config")
+    driver_version = Cpt(EpicsSignalRO, "DriverVersion_RBV", string=True, kind="config")
+    wait_for_plugins = Cpt(EpicsSignal, "WaitForPlugins", string=True, kind="config")
 
     def stage(self, *args, **kwargs):
         # Makes the detector allow non-blocking AD plugins but makes Ophyd use
         # the AcquireBusy PV to determine when an acquisition is complete
         self.ensure_nonblocking()
         return super().stage(*args, **kwargs)
+
+    def ensure_nonblocking(self):
+        self.stage_sigs["wait_for_plugins"] = "Yes"
+        for c in self.parent.component_names:
+            cpt = getattr(self.parent, c)
+            if cpt is self:
+                continue
+            if hasattr(cpt, "ensure_nonblocking"):
+                cpt.ensure_nonblocking()
 
 
 class Hdf5Writer(HDF5Plugin, FileStoreHDF5, FileStoreIterativeWrite):
