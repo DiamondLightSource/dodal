8bfa966d18b2f0d998eb069d20579ac10987574c
callumforrester <callumforrester@users.noreply.github.com>
2023-03-09 11:01:51 +0000

    deploy: bff1d6ff965ab31de130e648f3eaf144c210bcd1

--- testRigFixes/_static/documentation_options.js
+++ testRigFixes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev843+gb9c7a26',
+    VERSION: '0.1.dev844+gbff1d6f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
