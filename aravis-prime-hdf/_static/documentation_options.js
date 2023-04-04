1ecb84ff693ef6722586aca80a2b60c142425aa1
callumforrester <callumforrester@users.noreply.github.com>
2023-04-04 13:22:58 +0000

    deploy: 71c1cb5451807f1d5bb2dfc904a6d5ed14337c0f

--- aravis-prime-hdf/_static/documentation_options.js
+++ aravis-prime-hdf/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev14+g4385e12',
+    VERSION: '0.0.2.dev15+g71c1cb5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
