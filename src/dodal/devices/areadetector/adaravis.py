f7755d1fb64fef0e2f254d78788178faafa13c7e
Callum Forrester <29771545+callumforrester@users.noreply.github.com>
2023-04-05 08:27:58 +0100

    Aravis Detector: Warmup HDF Writer During Stage (#34)
    Make ADAravis detector pass a quick frame through the HDF5 writer before acquisition to ensure that the ROIs are synced

--- src/dodal/devices/areadetector/adaravis.py
+++ src/dodal/devices/areadetector/adaravis.py
@@ -1,5 +1,7 @@
+from typing import Any, Mapping
+
 from ophyd import Component as Cpt
-from ophyd import DetectorBase
+from ophyd import DetectorBase, EpicsSignal, Signal
 from ophyd.areadetector.base import ADComponent as Cpt
 from ophyd.areadetector.detectors import DetectorBase
 
@@ -16,11 +18,13 @@ class AdAravisDetector(SingleTriggerV33, DetectorBase):
         root="",
         write_path_template="",
     )
+    _priming_settings: Mapping[Signal, Any]
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.hdf.kind = "normal"
 
+        # Values for staging
         self.stage_sigs = {
             # Get stage to wire up the plugins
             self.hdf.nd_array_port: self.cam.port_name.get(),
@@ -35,6 +39,27 @@ class AdAravisDetector(SingleTriggerV33, DetectorBase):
             **self.stage_sigs,  # type: ignore
         }
 
+        # Settings to apply when priming plugins during pre-stage
+        self._priming_settings = {
+            self.hdf.enable: 1,
+            self.hdf.nd_array_port: self.cam.port_name.get(),
+            self.cam.array_callbacks: 1,
+            self.cam.image_mode: "Single",
+            self.cam.trigger_mode: "Off",
+            # Take the quickest possible frame
+            self.cam.acquire_time: 6.3e-05,
+            self.cam.acquire_period: 0.003,
+        }
+
+        # Signals that control driver and hdf writer should be put_complete to
+        # avoid race conditions during priming
+        for signal in set(self.stage_sigs.keys()).union(
+            set(self._priming_settings.keys())
+        ):
+            if isinstance(signal, EpicsSignal):
+                signal.put_complete = True
+        self.cam.acquire.put_complete = True
+
     def stage(self, *args, **kwargs):
         # We have to manually set the acquire period bcause the EPICS driver
         # doesn't do it for us. If acquire time is a staged signal, we use the
@@ -46,5 +71,29 @@ class AdAravisDetector(SingleTriggerV33, DetectorBase):
             acquire_time = self.cam.acquire_time.get()
         self.stage_sigs[self.cam.acquire_period] = acquire_time + _ACQUIRE_BUFFER_PERIOD
 
+        # Ensure detector warmed up
+        self._prime_hdf()
+
         # Now calling the super method should set the acquire period
         super(AdAravisDetector, self).stage(*args, **kwargs)
+
+    def _prime_hdf(self) -> None:
+        """
+        Take a single frame and pipe it through the HDF5 writer plugin
+        """
+
+        # Backup state and ensure we are not acquiring
+        reset_to = {signal: signal.get() for signal in self._priming_settings.keys()}
+        self.cam.acquire.set(0).wait(timeout=10)
+
+        # Apply all settings for acquisition
+        for signal, value in self._priming_settings.items():
+            # Ensure that .wait really will wait until the PV is set including its RBV
+            signal.set(value).wait(timeout=10)
+
+        # Acquire a frame
+        self.cam.acquire.set(1).wait(timeout=10)
+
+        # Revert settings to previous values
+        for signal, value in reversed(reset_to.items()):
+            signal.set(value).wait(timeout=10)
