da0f4533862263752ba5487e3099c0df7c6c9f56
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-04-17 13:55:01 +0100

    Added smoke system tests against s03 and i03

---
+++ tests/system_tests/test_i03_system.py
@@ -0,0 +1,15 @@
+from unittest.mock import patch
+
+from dodal import i03
+from dodal.utils import make_all_devices
+
+if __name__ == "__main__":
+    """This test runs against the real beamline and confirms that all the devices connect
+    i.e. that we match the beamline PVs. Obviously this must be run on the DLS network
+    and could be flaky if parts of the beamline are down for maintenance.
+
+    This is not implemented as a normal pytest test as those tests run using the S03
+    EPICS ports and switching ports at runtime is non-trivial
+    """
+    with patch("dodal.i03.BL", "i03"):
+        make_all_devices(i03)
