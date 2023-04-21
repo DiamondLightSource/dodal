c18fab88b785f005dcb5d6386a0cfb2b5cdab4c9
olliesilvester <olliesilvester@users.noreply.github.com>
2023-04-21 09:19:04 +0000

    deploy: 27bc0c633ef975ce5d7ee03315f96c90a0e3dae6

---
+++ artemis_348_replace_point_3d_to_support_arithmetic/_static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.0.2.dev44+g27bc0c6',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
