5c9d9b331cfe4c4a6a866df9785c2deed860ef5c
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-04-25 12:14:38 +0100

    (DiamondLightSource/python-artemis#569) Move more logging to dodal

--- src/dodal/log.py
+++ src/dodal/log.py
@@ -3,32 +3,59 @@ from __future__ import annotations
 import logging
 from os import environ
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple, Union
 
 from bluesky.log import config_bluesky_logging
 from bluesky.log import logger as bluesky_logger
+from graypy import GELFTCPHandler
 from ophyd.log import config_ophyd_logging
 from ophyd.log import logger as ophyd_logger
 
 LOGGER = logging.getLogger("Dodal")
-LOGGER.setLevel(logging.DEBUG)  # default logger to log everything
+LOGGER.setLevel(logging.DEBUG)
 ophyd_logger.parent = LOGGER
 bluesky_logger.parent = LOGGER
 
 
+class BeamlineFilter(logging.Filter):
+    beamline: Union[str, None] = environ.get("BEAMLINE")
+
+    def filter(self, record):
+        record.beamline = self.beamline if self.beamline else "dev"
+        return True
+
+
+beamline_filter = BeamlineFilter()
+
+
+def set_beamline(beamline_name: str):
+    """Set the beamline on all subsequent log messages."""
+    beamline_filter.beamline = beamline_name
+
+
 def set_up_logging_handlers(
-    logging_level: Optional[str] = "INFO", dev_mode: bool = False
+    logging_level: Optional[str] = "INFO",
+    dev_mode: bool = False,
+    logging_path: Optional[Path] = None,
 ) -> List[logging.Handler]:
     """Set up the logging level and instances for user chosen level of logging.
-
-    Mode defaults to production and can be switched to dev with the --dev flag on run.
+    Args:
+        logging_level: The level of logs that should be saved to file/graylog. Defaults to INFO.
+        dev_mode: True if in dev mode, will not log to graylog in dev. Defaults to False.
+        logging_path: The location to store log files, if left as None then puts them in the default location.
     """
+    if not logging_path:
+        logging_path = _get_logging_file_path()
+        print(f"Logging to {logging_path}")
+
     logging_level = logging_level if logging_level else "INFO"
-    file_path = Path(_get_logging_file_path(), "dodal.txt")
+    file_path = Path(logging_path)
+    graylog_host, graylog_port = _get_graylog_configuration(dev_mode)
     formatter = logging.Formatter(
         "[%(asctime)s] %(name)s %(module)s %(levelname)s: %(message)s"
     )
     handlers: list[logging.Handler] = [
+        GELFTCPHandler(graylog_host, graylog_port),
         logging.StreamHandler(),
         logging.FileHandler(filename=file_path),
     ]
@@ -37,10 +64,20 @@ def set_up_logging_handlers(
         handler.setLevel(logging_level)
         LOGGER.addHandler(handler)
 
+    LOGGER.addFilter(beamline_filter)
+
     # for assistance in debugging
     if dev_mode:
         set_seperate_ophyd_bluesky_files(
-            logging_level=logging_level, logging_path=_get_logging_file_path()
+            logging_level=logging_level, logging_path=logging_path.parent
+        )
+
+    # Warn users if trying to run in prod in debug mode
+    if not dev_mode and logging_level == "DEBUG":
+        LOGGER.warning(
+            'STARTING ARTEMIS IN DEBUG WITHOUT "--dev" WILL FLOOD PRODUCTION GRAYLOG'
+            " WITH MESSAGES. If you really need debug messages, set up a"
+            " local graylog instead!\n"
         )
 
     return handlers
@@ -65,7 +102,7 @@ def _get_logging_file_path() -> Path:
         logging_path = Path("./tmp/dev/")
 
     Path(logging_path).mkdir(parents=True, exist_ok=True)
-    return logging_path
+    return logging_path / Path("dodal.txt")
 
 
 def set_seperate_ophyd_bluesky_files(logging_level: str, logging_path: Path) -> None:
@@ -79,3 +116,18 @@ def set_seperate_ophyd_bluesky_files(logging_level: str, logging_path: Path) -> 
 
     config_bluesky_logging(file=str(bluesky_file_path), level=logging_level)
     config_ophyd_logging(file=str(ophyd_file_path), level=logging_level)
+
+
+def _get_graylog_configuration(dev_mode: bool) -> Tuple[str, int]:
+    """Get the host and port for the  graylog interaction.
+
+    If running on dev mode, this switches to localhost. Otherwise it publishes to the
+    dls graylog.
+
+    Returns:
+        (host,port): A tuple of the relevent host and port for graylog.
+    """
+    if dev_mode:
+        return "localhost", 5555
+    else:
+        return "graylog2.diamond.ac.uk", 12218
