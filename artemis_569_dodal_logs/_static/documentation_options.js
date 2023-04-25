d18527ce4f88701ba689e85a144f8f09f421d3f9
DominicOram <DominicOram@users.noreply.github.com>
2023-04-25 12:42:27 +0000

    deploy: 397a449e922b815d65ecb34bd8dbaa060e6a414b

--- artemis_569_dodal_logs/_static/documentation_options.js
+++ artemis_569_dodal_logs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev40+g5c9d9b3',
+    VERSION: '0.0.2.dev43+g397a449',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
