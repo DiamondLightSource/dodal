81510c1bdec336f11986e91c2c7b6124166af66c
DominicOram <DominicOram@users.noreply.github.com>
2023-05-04 18:39:34 +0000

    deploy: a8980133ad487ff0847f845e9f6507cbfa5b51cd

--- artemis560_unlimited_images/_static/documentation_options.js
+++ artemis560_unlimited_images/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev50+g19d75fc',
+    VERSION: '0.0.2.dev51+ga898013',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
