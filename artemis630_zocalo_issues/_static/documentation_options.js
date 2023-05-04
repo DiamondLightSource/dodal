c5afd324675da3e114b7350a086c0f0ade3f407e
DominicOram <DominicOram@users.noreply.github.com>
2023-05-04 17:43:23 +0000

    deploy: 2076fdf06bb6d01046fec0613356fa2027836195

--- artemis630_zocalo_issues/_static/documentation_options.js
+++ artemis630_zocalo_issues/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev40+g2189681',
+    VERSION: '0.0.2.dev41+g2076fdf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
