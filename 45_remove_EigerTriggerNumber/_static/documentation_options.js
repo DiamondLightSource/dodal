bc65364e347def995201c23fd0e482cb5c3f36ac
DominicOram <DominicOram@users.noreply.github.com>
2023-05-04 16:43:55 +0000

    deploy: 0fc304db51bbdfefabc5d6aaa18652f61adc11f4

--- 45_remove_EigerTriggerNumber/_static/documentation_options.js
+++ 45_remove_EigerTriggerNumber/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev40+gedf0b21',
+    VERSION: '0.0.2.dev41+g0fc304d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
