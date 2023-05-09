3134aad2d5a9c0eac7091b075e717939e5ccf380
NeilSmithDLS <NeilSmithDLS@users.noreply.github.com>
2023-05-09 08:21:03 +0000

    deploy: f20e3c2ba2b4f58914dbddf6a3bd08061b276b18

--- I23-Grid-Scans/_static/documentation_options.js
+++ I23-Grid-Scans/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev43+ga50156d',
+    VERSION: '0.0.3.dev13+gf20e3c2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
