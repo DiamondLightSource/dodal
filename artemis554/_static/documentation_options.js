d1a9a8df671c038c304c746764ea69d912ca7b9c
dperl-dls <dperl-dls@users.noreply.github.com>
2023-05-12 09:08:26 +0000

    deploy: 4b4bf94f1d8bd107ad432ae1269e07e179c5c26d

--- artemis554/_static/documentation_options.js
+++ artemis554/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3.dev35+g127a03f',
+    VERSION: '0.0.3.dev36+g4b4bf94',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
