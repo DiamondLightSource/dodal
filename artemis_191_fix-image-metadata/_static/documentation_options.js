7e9d2a5cf4819d24b509922caa7188e43ab87f00
dperl-dls <dperl-dls@users.noreply.github.com>
2023-05-15 16:38:24 +0000

    deploy: 9a382e750bac181b9bc47343171b7e46190e1d03

--- artemis_191_fix-image-metadata/_static/documentation_options.js
+++ artemis_191_fix-image-metadata/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.2.dev40+g447826e',
+    VERSION: '0.0.4.dev3+g9a382e7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
