05e854ffb154bd50f83032173a5fcdf88ac1df30
DominicOram <DominicOram@users.noreply.github.com>
2023-05-15 19:24:27 +0000

    deploy: f71871882710a69a12a4b03495e388af3e2c2a25

--- 51_aperture_with_no_positions/_static/documentation_options.js
+++ 51_aperture_with_no_positions/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev5+gab256e5',
+    VERSION: '0.0.4.dev6+gf718718',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
