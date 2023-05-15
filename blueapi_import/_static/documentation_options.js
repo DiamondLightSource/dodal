508fdd9872ea8216e4711ddcc3b349e1223a1128
abbiemery <abbiemery@users.noreply.github.com>
2023-05-15 09:28:56 +0000

    deploy: ac31d194a4dc3c579a6a4afacba7f9e308a7ce67

--- blueapi_import/_static/documentation_options.js
+++ blueapi_import/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev7+g8ac4f92',
+    VERSION: '0.0.4.dev6+gac31d19',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
