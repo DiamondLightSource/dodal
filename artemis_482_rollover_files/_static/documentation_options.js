67d1c263dcb9fe0caca31a1b5af7d11fa46c26a2
DominicOram <DominicOram@users.noreply.github.com>
2023-05-17 15:35:16 +0000

    deploy: ee6b95a52b132be7b1adb8ac85c34b2c312a29af

--- artemis_482_rollover_files/_static/documentation_options.js
+++ artemis_482_rollover_files/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev7+gcd8e643',
+    VERSION: '0.0.4.dev8+gee6b95a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
