d6f7ee332c451eda74407d6b4bd0e11005e663df
DominicOram <DominicOram@users.noreply.github.com>
2023-05-22 11:52:34 +0000

    deploy: 756cd3d53162068011966f88d14bee23993823aa

--- artemis_661_fix_get_beamline_name/_static/documentation_options.js
+++ artemis_661_fix_get_beamline_name/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev21+g6c63750',
+    VERSION: '0.0.4.dev22+g756cd3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
