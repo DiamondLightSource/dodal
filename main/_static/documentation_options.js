f6924e0f1389d07943e8fb17ee38532a2593df59
DominicOram <DominicOram@users.noreply.github.com>
2023-05-22 09:11:01 +0000

    deploy: 491bb53dfd5eb82c2ae8538c9a45f255c58190eb

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev18+gfa49030',
+    VERSION: '0.0.4.dev20+g491bb53',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
