c66bdb6be8bbc3539d78fb05f5c205fbe016098b
dperl-dls <dperl-dls@users.noreply.github.com>
2023-05-24 07:23:26 +0000

    deploy: aff218d7f10b8c24c532c8618a3842664a76f4bf

--- artemis_678_pydantic_internal_params/_static/documentation_options.js
+++ artemis_678_pydantic_internal_params/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.5.dev8+gdd7beefc',
+    VERSION: '0.0.5.dev9+gaff218d7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
