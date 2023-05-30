679a7cef89f4c02869139af60ef26b71c4751d4e
olliesilvester <olliesilvester@users.noreply.github.com>
2023-05-30 08:06:28 +0000

    deploy: 81a3a38053df9024ef585bb88b5df1403b5fb0d5

--- artemis_262_move_arming_out_of_stage/_static/documentation_options.js
+++ artemis_262_move_arming_out_of_stage/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.5.dev23+g95a8cb4',
+    VERSION: '0.0.4.dev47+g81a3a380',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
