cf0b94f361241604e416f6044fe1d414a86bdbad
dperl-dls <dperl-dls@users.noreply.github.com>
2023-05-31 13:43:18 +0000

    deploy: 947d5dcc04e62c20b82a38eb33432f0396552279

--- 69_add_i24/_static/documentation_options.js
+++ 69_add_i24/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev48+gcef4fa4b',
+    VERSION: '0.0.4.dev49+g947d5dcc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
