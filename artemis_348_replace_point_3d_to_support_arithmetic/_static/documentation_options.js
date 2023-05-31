b3cc323c832a983bfa10a8c701cb52290ba9c5c8
dperl-dls <dperl-dls@users.noreply.github.com>
2023-05-31 13:17:15 +0000

    deploy: 865a71cddc2bafd2378f512f6f2f8f2d1e8769bb

--- artemis_348_replace_point_3d_to_support_arithmetic/_static/documentation_options.js
+++ artemis_348_replace_point_3d_to_support_arithmetic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev10+gf6ca7ce',
+    VERSION: '0.0.4.dev49+g865a71cd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
