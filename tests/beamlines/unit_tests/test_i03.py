a58a81efd599466ec53483003d65f3bff7581ca3
David Perl <david.perl@diamond.ac.uk>
2023-05-31 16:34:00 +0100

    do more device clearing

--- tests/beamlines/unit_tests/test_i03.py
+++ tests/beamlines/unit_tests/test_i03.py
@@ -4,6 +4,7 @@ from dodal.utils import make_all_devices
 
 
 def test_list():
+    beamline_utils.clear_devices()
     i03.zebra(wait_for_connection=False)
     i03.synchrotron(wait_for_connection=False)
     i03.aperture_scatterguard(wait_for_connection=False)
@@ -29,6 +30,7 @@ def test_devices_are_identical():
 
 
 def test_getting_second_aperture_scatterguard_gives_valid_device():
+    beamline_utils.clear_devices()
     test_positions = AperturePositions(
         (0, 1, 2, 3, 4), (5, 6, 7, 8, 9), (10, 11, 12, 13, 14), (15, 16, 17, 18, 19)
     )
