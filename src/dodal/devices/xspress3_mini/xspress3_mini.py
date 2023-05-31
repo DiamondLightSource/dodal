3f5c15c95da1dc64126dd325e679ce3ad2e42ad5
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-05-31 13:52:47 +0100

    cleanup

--- src/dodal/devices/xspress3_mini/xspress3_mini.py
+++ src/dodal/devices/xspress3_mini/xspress3_mini.py
@@ -7,102 +7,19 @@ class Xspress3Mini(Device):
     # Assume only one channel for now
     channel_1 = Component(Xspress3MiniChannel, "C1_")
 
-    pv_erase: EpicsSignal = Component(
-        EpicsSignal, "ERASE"
-    )  # doErase function sets this PV to 1, which does the erase, IntergerFromEnumPV
-    pv_get_max_num_channels = Component(
-        EpicsSignalRO, "MAX_NUM_CHANNELS_RBV"
-    )  # Using get on this PV should give the max number of channels, ReadOnlyIntegerPV
+    pv_erase: EpicsSignal = Component(EpicsSignal, "ERASE")
+    pv_get_max_num_channels = Component(EpicsSignalRO, "MAX_NUM_CHANNELS_RBV")
 
-    # total_time_pvs: list[Component] = [] #These are read only
+    pv_acquire: EpicsSignal = (Component(EpicsSignal, "Acquire"),)
 
-    # # For each channel, there is a pvsSCA5UpdateArraysMini PV, and these are all set to 'acquire_state = done' upon startup
-    # for i in pv_get_max_num_channels.get():
-    #     pv_sca5_update_arrays_mini_list.append(
-    #         Component(EpicsSignalRO, f"C{i}_SCAS:TS:TSAcquire")
-    #     )  # EnumPV, with the enum having ACQUIRE_STATE set...
-    #     # ...to done on startup
+    pv_get_roi_calc_mini: EpicsSignal = Component(EpicsSignal, "MCA1:Enable_RBV")
 
-    # Instead of using this for loop, use other device Xspress3MiniChannel and instantiate one for each channel. Assume one channel
+    NUMBER_ROIS_DEFAULT = 6
 
-    pv_acquire: EpicsSignal = (
-        Component(EpicsSignal, "Acquire"),
-    )  # enumPV, his is set to acquire state = ACQUIRE on startup, this value is set with no wait
+    set_trigger_mode_mini: EpicsSignal = Component(EpicsSignal, "TriggerMode")
 
-    pv_get_roi_calc_mini: EpicsSignal = Component(
-        EpicsSignal, "MCA1:Enable_RBV"
-    )  # only .get() is used on this
-
-    NUMBER_ROIS_DEFAULT = 6  # This might need to be changed
-
-    # one element for each channel
-
-    # Get pv_roi_llm like : channel_1.cls.ROI_1.cls.pv_roi_llm
-
-    # [roi][channel] , need PVs which return the number of rois and the number of detector channels, then fill this up
-    # accordingly... Where roi has NUMBER_ROIS_DEFAULT values, and channel has pv_get_max_num_channels.get() values
-
-    # for roi in range(NUMBER_ROIS_DEFAULT):
-    #     for channel in range(pv_get_max_num_channels.get()):
-    #         pv_roi_llm[roi][channel] = Component(
-    #             EpicsSignal, f"C{channel}_MCA_ROI{roi}_LLM"
-    #         )  # IntegerPV
-    #         pv_roi_size[roi][channel] = Component(
-    #             EpicsSignal,
-    #             f"C{channel}_SCA5_HLM",  # Strange as it doesn't use roi variable, but this does happen in gda code
-    #         )  # IntegerPV
-
-    # Logic of this for loop is unrolled and within Xspress3MiniChannel.py
-
-    set_trigger_mode_mini: EpicsSignal = Component(
-        EpicsSignal, "TriggerMode"
-    )  # enumPV, putwait is used on this within a set_trigger_mode function
-
-    pv_get_trig_mode_mini: EpicsSignalRO = Component(
-        EpicsSignalRO, "TriggerMode_RBV"
-    )  # Only get is used on this
-
-    # roi_llm and roi_size are then used to get the roi limits in a get_roi_limits function
+    pv_get_trig_mode_mini: EpicsSignalRO = Component(EpicsSignalRO, "TriggerMode_RBV")
 
     pv_roi_start_x: EpicsSignal = Component(EpicsSignal, "ROISUM1:MinX")
     pv_roi_size_x: EpicsSignal = Component(EpicsSignal, "ROISUM1:SizeX")
-
-    # A function readout_scalar_values uses the following integer RO pvs arrays:
-    # pv_time: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_reset_ticks: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_reset_count: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_all_event: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_all_good: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_pileup: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-    # pv_total_time: list[EpicsSignalRO] = [None] * pv_get_max_num_channels.get()
-
-    # The names for these variables depend on useTSarrayValueNames in EpicsXspress3ControllerPvProvider.java . Assuming useTSarrayValueNames = True:
-
-    # might need to use SCA_TEMPLATE here instead
-    # for channel in pv_get_max_num_channels.get():
-    #     pv_time[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{0}:TSArrayValue"
-    #     )
-    #     pv_reset_ticks[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{1}:TSArrayValue"
-    #     )
-    #     pv_reset_count[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{2}:TSArrayValue"
-    #     )
-    #     pv_all_event[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{3}:TSArrayValue"
-    #     )
-    #     pv_all_good[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{4}:TSArrayValue"
-    #     )
-    #     pv_pileup[channel] = Component(
-    #         EpicsSignalRO, f"C{self.this_channel}_SCAS:{7}:TSArrayValue"
-    #     )
-
-    # The second digit here is the index of the ScarIndex Enum values
-
-    pv_acquire_time: EpicsSignal = Component(EpicsSignal, "AcquireTime")  # PV<double>
-
-
-my_det = Xspress3Mini(prefix="BL03I-EA-XSP3-01:", name="test_fluorence")
-my_det.wait_for_connection()
+    pv_acquire_time: EpicsSignal = Component(EpicsSignal, "AcquireTime")
--- src/dodal/devices/xspress3_mini/xspress3_mini_channel.py
+++ src/dodal/devices/xspress3_mini/xspress3_mini_channel.py
@@ -1,12 +1,9 @@
 from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 
-# from dodal.devices.xspress3_mini.xspress3_mini import Xspress3Mini
 from dodal.devices.xspress3_mini.xspress3_mini_roi import Xspress3MiniROI
 
 
 class Xspress3MiniChannel(Device):
-    total_channels = 0
-
     # Assume 6 ROI's per channel and one channel. This might need to be changed
     ROI_1 = Component(Xspress3MiniROI, "MCA_ROI1_")
     ROI_2 = Component(Xspress3MiniROI, "MCA_ROI2_")
@@ -17,13 +14,11 @@ class Xspress3MiniChannel(Device):
 
     pv_sca5_update_arrays_mini = Component(EpicsSignalRO, "SCAS:TS:TSAcquire")
 
-    # Access pv_roi_llm list with ROI_1.pv_roi_llm
-
     pv_roi_size = Component(
         EpicsSignal, "SCA5_HLM"
     )  # Strange as it doesn't use roi variable, but this does happen in gda code.
     pv_roi_llm = Component(EpicsSignal, "SCA5_LLM")
-    # GDA code seems dodgy for these two variables. IntegerPVs
+    # GDA code seems dodgy for these two variables.
 
     pv_time = Component(EpicsSignalRO, f"SCAS:{1}:TSArrayValue")
     pv_reset_ticks = Component(EpicsSignalRO, f"SCAS:{2}:TSArrayValue")
