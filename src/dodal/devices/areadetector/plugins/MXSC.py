4d94af5a7c494df54812f2a2a85d1f76e014f38d
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-06-01 14:49:17 +0100

    (DiamondLightSource/python-artemis#683) Add specufuc pin tip detection device

--- src/dodal/devices/areadetector/plugins/MXSC.py
+++ src/dodal/devices/areadetector/plugins/MXSC.py
@@ -1,4 +1,36 @@
-from ophyd import Component, Device, EpicsSignal
+from ophyd import Component, Device, EpicsSignal, Kind, Signal
+from ophyd.status import Status, SubscriptionStatus
+
+
+class PinTipDetect(Device):
+    INVALID_POSITION = (-1, -1)
+    tip_x: EpicsSignal = Component(EpicsSignal, "TipX")
+    tip_y: EpicsSignal = Component(EpicsSignal, "TipY")
+
+    triggered_tip: Signal = Component(Signal, kind=Kind.hinted)
+    validity_timeout: Signal = Component(Signal, value=1)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update_tip_if_valid(self, value, **_):
+        current_value = (value, self.tip_y.get())
+        if current_value != self.INVALID_POSITION:
+            self.triggered_tip.set(current_value)
+            return True
+
+    def trigger(self) -> Status:
+        subscription_status = SubscriptionStatus(self.tip_x, self.update_tip_if_valid)
+
+        def set_to_default_and_finish(_):
+            self.triggered_tip.set(self.INVALID_POSITION)
+            subscription_status.set_finished()
+
+        Status(self, timeout=self.validity_timeout.get()).add_callback(
+            set_to_default_and_finish
+        )
+
+        return subscription_status
 
 
 class MXSC(Device):
@@ -23,8 +55,7 @@ class MXSC(Device):
     sample_detection_min_tip_height: EpicsSignal = Component(
         EpicsSignal, "MinTipHeight"
     )
-    tip_x: EpicsSignal = Component(EpicsSignal, "TipX")
-    tip_y: EpicsSignal = Component(EpicsSignal, "TipY")
+
     top: EpicsSignal = Component(EpicsSignal, "Top")
     bottom: EpicsSignal = Component(EpicsSignal, "Bottom")
     output_array: EpicsSignal = Component(EpicsSignal, "OutputArray")
@@ -32,3 +63,5 @@ class MXSC(Device):
     draw_edges: EpicsSignal = Component(EpicsSignal, "DrawEdges")
     waveform_size_x: EpicsSignal = Component(EpicsSignal, "ArraySize1_RBV")
     waveform_size_y: EpicsSignal = Component(EpicsSignal, "ArraySize2_RBV")
+
+    pin_tip: PinTipDetect = Component(PinTipDetect)
