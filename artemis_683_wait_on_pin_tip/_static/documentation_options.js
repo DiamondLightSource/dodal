543b96cc29c69ab3fa717adac9d7db2e5cc9ecba
DominicOram <DominicOram@users.noreply.github.com>
2023-06-01 13:58:13 +0000

    deploy: 744f5a94d7dde6f7001f42d3a14b239d95400f0e

--- artemis_683_wait_on_pin_tip/_static/documentation_options.js
+++ artemis_683_wait_on_pin_tip/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev40+g4d94af5a',
+    VERSION: '0.0.4.dev58+g744f5a94',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
