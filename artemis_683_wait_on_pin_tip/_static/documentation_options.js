bfb71564a65e10ef851b6f4c17270b9e4bd09ef1
dperl-dls <dperl-dls@users.noreply.github.com>
2023-06-02 12:53:31 +0000

    deploy: abd371d2be6dcf57fdeb1b7bb9171b363fbad96e

--- artemis_683_wait_on_pin_tip/_static/documentation_options.js
+++ artemis_683_wait_on_pin_tip/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev58+g744f5a94',
+    VERSION: '0.0.4.dev72+gabd371d2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
