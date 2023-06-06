79e335a2257eb473224cd8d0ac2d1e72ba42bdd8
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-06-06 15:21:18 +0100

    Revert "recombine test"
    This reverts commit e5d53c956ba7be9bd5d193100fb468962da33592.

--- tests/devices/unit_tests/test_eiger.py
+++ tests/devices/unit_tests/test_eiger.py
@@ -287,7 +287,14 @@ def test_unsuccessful_true_roi_mode_change_results_in_callback_error(
     with pytest.raises(errors.StatusTimeoutError):
         wrap_and_do_funcs(unwrapped_funcs)
 
-    # Test false
+
+@patch("ophyd.status.Status.__and__")
+def test_unsuccessful_false_roi_mode_change_results_in_callback_error(
+    mock_and, fake_eiger
+):
+    dummy_status = Status(timeout=0)
+    mock_and.return_value = dummy_status
+
     unwrapped_funcs = [
         lambda: fake_eiger.change_roi_mode(enable=False),
         lambda: fake_eiger.set_detector_threshold(
