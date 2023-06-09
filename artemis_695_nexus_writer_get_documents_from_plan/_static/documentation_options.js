de7fc14558763cd52a344cc6a2388cdd5de37c15
dperl-dls <dperl-dls@users.noreply.github.com>
2023-06-09 10:12:40 +0000

    deploy: e66b4b50f2de26121027c9606ea8408eac83c7e4

--- artemis_695_nexus_writer_get_documents_from_plan/_static/documentation_options.js
+++ artemis_695_nexus_writer_get_documents_from_plan/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev5+g1ab6f242',
+    VERSION: '0.0.6.dev6+ge66b4b50',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
