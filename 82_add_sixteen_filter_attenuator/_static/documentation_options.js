4b98d72c90a12476b739a485162a0817b63fb98b
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-09 10:29:33 +0000

    deploy: c4b101183882148fc9d22c78d9324081b8955cc1

--- 82_add_sixteen_filter_attenuator/_static/documentation_options.js
+++ 82_add_sixteen_filter_attenuator/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev58+gef57229e',
+    VERSION: '0.0.4.dev59+gc4b10118',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
