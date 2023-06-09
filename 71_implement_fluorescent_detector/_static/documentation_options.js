c2708285b22b40160e4a0ab282fcf360ee4f8af9
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-09 10:17:29 +0000

    deploy: d10d5306bc72f6897df63076f56cfda586e3ef19

--- 71_implement_fluorescent_detector/_static/documentation_options.js
+++ 71_implement_fluorescent_detector/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.dev43+gc425129a',
+    VERSION: '0.0.4.dev44+gd10d5306',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
