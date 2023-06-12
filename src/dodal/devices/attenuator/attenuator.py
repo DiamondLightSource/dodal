2b35cc9b9a9132cae87025c2a2df544eb326642c
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-06-12 13:21:38 +0100

    Fix set_transmission

--- src/dodal/devices/attenuator/attenuator.py
+++ src/dodal/devices/attenuator/attenuator.py
@@ -3,6 +3,7 @@ from typing import Optional
 from ophyd import Arming, Component, Device, EpicsSignal, Signal
 from ophyd.status import SubscriptionStatus
 
+from dodal.devices.attenuator.filter import AtteunatorFilter
 from dodal.devices.detector import DetectorParams
 from dodal.devices.status import await_value
 from dodal.log import LOGGER
@@ -15,41 +16,39 @@ class Attenuator(Device):
 
     do_set_transmission: TransmissionSignal = Component(TransmissionSignal)
 
-    # Could make a separate class for these, but that's potentially pointless as this is the only PV for it
-    calulated_filter_state_0: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B0")
-    calulated_filter_state_1: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B1")
-    calulated_filter_state_2: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B2")
-    calulated_filter_state_3: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B3")
-    calulated_filter_state_4: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B4")
-    calulated_filter_state_5: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B5")
-    calulated_filter_state_6: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B6")
-    calulated_filter_state_7: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B7")
-    calulated_filter_state_8: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B8")
-    calulated_filter_state_9: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B9")
-    calulated_filter_state_10: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BA")
-    calulated_filter_state_11: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BB")
-    calulated_filter_state_12: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BC")
-    calulated_filter_state_13: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BD")
-    calulated_filter_state_14: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BE")
-    calulated_filter_state_15: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BF")
+    calulated_filter_state_1: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B0")
+    calulated_filter_state_2: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B1")
+    calulated_filter_state_3: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B2")
+    calulated_filter_state_4: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B3")
+    calulated_filter_state_5: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B4")
+    calulated_filter_state_6: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B5")
+    calulated_filter_state_7: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B6")
+    calulated_filter_state_8: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B7")
+    calulated_filter_state_9: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B8")
+    calulated_filter_state_10: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B9")
+    calulated_filter_state_11: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BA")
+    calulated_filter_state_12: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BB")
+    calulated_filter_state_13: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BC")
+    calulated_filter_state_14: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BD")
+    calulated_filter_state_15: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BE")
+    calulated_filter_state_16: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BF")
 
-    # Could also make another class for this - but again it's the only PV used
-    actual_filter_state_1: EpicsSignal = Component(EpicsSignal, ":FILTER1:INLIM")
-    actual_filter_state_2: EpicsSignal = Component(EpicsSignal, ":FILTER2:INLIM")
-    actual_filter_state_3: EpicsSignal = Component(EpicsSignal, ":FILTER3:INLIM")
-    actual_filter_state_4: EpicsSignal = Component(EpicsSignal, ":FILTER4:INLIM")
-    actual_filter_state_5: EpicsSignal = Component(EpicsSignal, ":FILTER5:INLIM")
-    actual_filter_state_6: EpicsSignal = Component(EpicsSignal, ":FILTER6:INLIM")
-    actual_filter_state_7: EpicsSignal = Component(EpicsSignal, ":FILTER7:INLIM")
-    actual_filter_state_8: EpicsSignal = Component(EpicsSignal, ":FILTER8:INLIM")
-    actual_filter_state_9: EpicsSignal = Component(EpicsSignal, ":FILTER9:INLIM")
-    actual_filter_state_10: EpicsSignal = Component(EpicsSignal, ":FILTER10:INLIM")
-    actual_filter_state_11: EpicsSignal = Component(EpicsSignal, ":FILTER11:INLIM")
-    actual_filter_state_12: EpicsSignal = Component(EpicsSignal, ":FILTER12:INLIM")
-    actual_filter_state_13: EpicsSignal = Component(EpicsSignal, ":FILTER13:INLIM")
-    actual_filter_state_14: EpicsSignal = Component(EpicsSignal, ":FILTER14:INLIM")
-    actual_filter_state_15: EpicsSignal = Component(EpicsSignal, ":FILTER15:INLIM")
-    actual_filter_state_16: EpicsSignal = Component(EpicsSignal, ":FILTER16:INLIM")
+    filter_1: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER1")
+    filter_2: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER2")
+    filter_3: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER3")
+    filter_4: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER4")
+    filter_5: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER5")
+    filter_6: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER6")
+    filter_7: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER7")
+    filter_8: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER8")
+    filter_9: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER9")
+    filter_10: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER10")
+    filter_11: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER11")
+    filter_12: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER12")
+    filter_13: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER13")
+    filter_14: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER14")
+    filter_15: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER15")
+    filter_16: AtteunatorFilter = Component(AtteunatorFilter, ":FILTER16")
 
     desired_transmission: EpicsSignal = Component(EpicsSignal, ":T2A:SETVAL1")
     use_current_energy: EpicsSignal = Component(
@@ -62,7 +61,6 @@ class Attenuator(Device):
 
     def get_calculated_filter_state_list(self) -> list[EpicsSignal]:
         return [
-            self.calulated_filter_state_0,
             self.calulated_filter_state_1,
             self.calulated_filter_state_2,
             self.calulated_filter_state_3,
@@ -78,45 +76,41 @@ class Attenuator(Device):
             self.calulated_filter_state_13,
             self.calulated_filter_state_14,
             self.calulated_filter_state_15,
+            self.calulated_filter_state_16,
         ]
 
     def get_actual_filter_state_list(self) -> list[EpicsSignal]:
         return [
-            self.actual_filter_state_1,
-            self.actual_filter_state_2,
-            self.actual_filter_state_3,
-            self.actual_filter_state_4,
-            self.actual_filter_state_5,
-            self.actual_filter_state_6,
-            self.actual_filter_state_7,
-            self.actual_filter_state_8,
-            self.actual_filter_state_9,
-            self.actual_filter_state_10,
-            self.actual_filter_state_11,
-            self.actual_filter_state_12,
-            self.actual_filter_state_13,
-            self.actual_filter_state_14,
-            self.actual_filter_state_15,
-            self.actual_filter_state_16,
+            self.filter_1.actual_filter_state,
+            self.filter_2.actual_filter_state,
+            self.filter_3.actual_filter_state,
+            self.filter_4.actual_filter_state,
+            self.filter_5.actual_filter_state,
+            self.filter_6.actual_filter_state,
+            self.filter_7.actual_filter_state,
+            self.filter_8.actual_filter_state,
+            self.filter_9.actual_filter_state,
+            self.filter_10.actual_filter_state,
+            self.filter_11.actual_filter_state,
+            self.filter_12.actual_filter_state,
+            self.filter_13.actual_filter_state,
+            self.filter_14.actual_filter_state,
+            self.filter_15.actual_filter_state,
+            self.filter_16.actual_filter_state,
         ]
 
     def set_transmission(self, transmission) -> SubscriptionStatus:
         """Get desired states and calculated states, return a status which is complete once they are equal"""
-        # put this in try block?
 
         LOGGER.info("Using current energy")
-        self.pv_use_current_energy.put(1)
+        self.use_current_energy.put(1)
         LOGGER.info(f"Setting desired transmission to {transmission}")
-        self.pv_desired_transmission.put(transmission)
+        self.desired_transmission.put(transmission)
         LOGGER.info("Sending change filter command")
-        self.pv_change.put(1)
+        self.change.put(1)
 
         # Get desired filter positions (16phase)
-
         desired_states = []
-
-        # Get the boolean desired state of each of the 16 filters
-        # TODO: put in a try catch block since the get might timeout
         for calculated_state in self.get_calculated_filter_state_list():
             value = int(calculated_state.get(timeout=10))
             desired_states.append(value == 1)
@@ -126,11 +120,7 @@ class Attenuator(Device):
         # Get the boolean actual state of each of the 16 filters
         for actual_state in self.get_actual_filter_state_list():
             value = actual_state.get(timeout=10)
-            actual_states.append(value == "in")
+            actual_states.append(value == "In")
 
+        # Transmission is set when all actual and desired states are equal
         return await_value(actual_states, desired_states, timeout=30)
-
-        # If all the actual states are equal to the desired state, we can move on
-
-        # and check if atteunator is ready
-        # Get actual positions (get filter positions() from 16 phase)
--- src/dodal/devices/attenuator/filter.py
+++ src/dodal/devices/attenuator/filter.py
@@ -1,22 +1,7 @@
-from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
+from ophyd import Component, Device, EpicsSignal
 
 from dodal.devices.attenuator.attenuator import Attenuator
 
 
 class AtteunatorFilter(Device):
-    pv_calculated_state_0: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B0")
-    pv_calculated_state_1: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B1")
-    pv_calculated_state_2: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B2")
-    pv_calculated_state_3: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B3")
-    pv_calculated_state_4: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B4")
-    pv_calculated_state_5: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B5")
-    pv_calculated_state_6: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B6")
-    pv_calculated_state_7: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B7")
-    pv_calculated_state_8: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B8")
-    pv_calculated_state_9: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.B9")
-    pv_calculated_state_10: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BA")
-    pv_calculated_state_11: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BB")
-    pv_calculated_state_12: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BC")
-    pv_calculated_state_13: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BD")
-    pv_calculated_state_14: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BE")
-    pv_calculated_state_15: EpicsSignal = Component(EpicsSignal, ":DEC_TO_BIN.BF")
+    actual_filter_state: EpicsSignal = Component(EpicsSignal, ":INLIM")
