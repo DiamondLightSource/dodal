819789af841befdf291474bf93a90dad01d17644
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-12 15:35:59 +0000

    deploy: 6e9077b7166348930f6e898e8c690f4b623bb3f9

--- 82_add_attenuator/_static/documentation_options.js
+++ 82_add_attenuator/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev7+g2b35cc9b',
+    VERSION: '0.0.6.dev50+g6e9077b7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
