583299f42552f9588b9d3e98bca55c5e59a5e4a7
noemifrisina <noemifrisina@users.noreply.github.com>
2023-06-13 14:39:12 +0000

    deploy: 247314fd4ba05ace3d50297bb9434557640e41e5

--- artemis_671_callbacks_in_grid_detection/_static/documentation_options.js
+++ artemis_671_callbacks_in_grid_detection/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev57+g1c3a1598',
+    VERSION: '0.0.6.dev58+g247314fd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
