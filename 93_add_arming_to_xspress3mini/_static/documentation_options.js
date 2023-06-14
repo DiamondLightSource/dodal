bff767264397a7f5cac9d36d1c709f5454b135e8
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-14 14:31:45 +0000

    deploy: fd4c5e745e41af4ce847588b08f57318342f0c08

--- 93_add_arming_to_xspress3mini/_static/documentation_options.js
+++ 93_add_arming_to_xspress3mini/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev56+g51e3843d',
+    VERSION: '0.0.6.dev57+gfd4c5e74',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
