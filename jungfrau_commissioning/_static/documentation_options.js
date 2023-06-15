3db80dbacaec77e1aa8ea8c3091b6c9c3f22e798
dperl-dls <dperl-dls@users.noreply.github.com>
2023-06-15 16:30:55 +0000

    deploy: 599e6e05e110e8cbe268a21ad45f314fbf3ef3da

--- jungfrau_commissioning/_static/documentation_options.js
+++ jungfrau_commissioning/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev8+g3c739a14',
+    VERSION: '0.0.6.dev9+g599e6e05',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
