ab2b90175e1e13296a4bc361ffd345c973bcd086
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-06-19 10:44:48 +0100

    consistent tests

--- tests/devices/unit_tests/test_xspress3mini.py
+++ tests/devices/unit_tests/test_xspress3mini.py
@@ -19,10 +19,11 @@ def test_arm_success_on_busy_state(fake_xspress3mini):
     assert status.done is False
     fake_xspress3mini.detector_state.sim_put(DetectorState.ACQUIRE.value)
     status.wait(timeout=1)
+    fake_xspress3mini.acquire_status.wait(timeout=1)
 
 
 def test_stage_in_busy_state(fake_xspress3mini):
     fake_xspress3mini.detector_state.sim_put(DetectorState.ACQUIRE.value)
     RE = RunEngine()
     RE(bps.stage(fake_xspress3mini))
-    fake_xspress3mini.acquire_status.wait(timeout=5)
+    fake_xspress3mini.acquire_status.wait(timeout=1)
