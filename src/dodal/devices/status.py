933f43aa62a7291cef860e003fa2be1679d921b7
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-06-21 16:48:14 +0100

    Change and add tests for await_value_in_list

--- src/dodal/devices/status.py
+++ src/dodal/devices/status.py
@@ -9,22 +9,19 @@ def await_value(
     subscribable: Any, expected_value: T, timeout: Union[None, int] = None
 ) -> SubscriptionStatus:
     def value_is(value, **_):
-        if type(expected_value) == list:
-            return value in expected_value
-        else:
-            return value == expected_value
+        return value == expected_value
 
     return SubscriptionStatus(subscribable, value_is, timeout=timeout)
 
 
 # Returns a status which is completed when the subscriptable contains a value within the expected_value list
 def await_value_in_list(
-    subscribable: Any, expected_value: Collection[Any], timeout: Union[None, int] = None
+    subscribable: Any, expected_value: list, timeout: Union[None, int] = None
 ) -> SubscriptionStatus:
     def value_is(value, **_):
-        if type(expected_value) == list:
-            return value in expected_value
-        else:
-            raise TypeError(f"{expected_value} is not a Collection type")
+        return value in expected_value
 
-    return SubscriptionStatus(subscribable, value_is, timeout=timeout)
+    if type(expected_value) != list:
+        raise TypeError(f"expected value {expected_value} is not a list")
+    else:
+        return SubscriptionStatus(subscribable, value_is, timeout=timeout)
