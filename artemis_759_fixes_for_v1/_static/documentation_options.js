a94990ab6acd8b76164eaefccf562d3cc328755e
DominicOram <DominicOram@users.noreply.github.com>
2023-06-27 17:04:55 +0000

    deploy: 61dc775b36c295f63be6fb4bfcb27dc98bca6196

--- artemis_759_fixes_for_v1/_static/documentation_options.js
+++ artemis_759_fixes_for_v1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev1+g8fbe3b72',
+    VERSION: '1.0.1.dev2+g61dc775b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
