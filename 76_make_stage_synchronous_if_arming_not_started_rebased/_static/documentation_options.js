b572d46a18dd0f7b1fd783c7099c2f140281bfcb
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-27 10:07:52 +0000

    deploy: 52bfa8c771819e4ec4e917915b96eab3e90fbc53

--- 76_make_stage_synchronous_if_arming_not_started_rebased/_static/documentation_options.js
+++ 76_make_stage_synchronous_if_arming_not_started_rebased/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev76+g3dbdef5b',
+    VERSION: '0.0.6.dev78+g52bfa8c7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
