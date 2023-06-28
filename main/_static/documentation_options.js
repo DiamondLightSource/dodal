60e9c9a96d3e9ed6231458a7671222d2f2729cf4
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-28 08:43:01 +0000

    deploy: 7e19b780079a62f851631b35d1817d8b9d6f64f1

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev114+g0de42e34',
+    VERSION: '1.0.1.dev7+g7e19b780',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
