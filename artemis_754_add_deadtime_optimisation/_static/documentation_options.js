3eae622147a1cc01fd1edf11df2051ad51775863
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-28 09:59:27 +0000

    deploy: f3d825138152af4c67ecbcbaf58be68aa22ddf58

--- artemis_754_add_deadtime_optimisation/_static/documentation_options.js
+++ artemis_754_add_deadtime_optimisation/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev18+g3436564f',
+    VERSION: '1.0.1.dev19+gf3d82513',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
