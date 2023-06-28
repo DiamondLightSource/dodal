e3932e95e68aa98c266c500c46c176b93da77cb1
olliesilvester <olliesilvester@users.noreply.github.com>
2023-06-28 08:32:39 +0000

    deploy: b4d83b96cb6a18ab225e2b1b5908e5a15a27e920

--- artemis_701_do_optimise_attenuation_scan_rebased/_static/documentation_options.js
+++ artemis_701_do_optimise_attenuation_scan_rebased/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev5+gee7e7b80',
+    VERSION: '1.0.1.dev6+gb4d83b96',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
