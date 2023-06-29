404f5ec97447fa41a4f1aa50c6254e459ab5c953
DominicOram <DominicOram@users.noreply.github.com>
2023-06-29 17:17:22 +0000

    deploy: 0c816d02076f281389a407478a18c251d1b820ea

--- 103_apply_flatfield_on_zoom_change/_static/documentation_options.js
+++ 103_apply_flatfield_on_zoom_change/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev35+g9bd11b1f',
+    VERSION: '1.0.1.dev36+g0c816d02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
