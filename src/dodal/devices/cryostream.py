747fb8f858d02b556f299f6bc92e67045f092bea
Dominic Oram <dominic.oram@diamond.ac.uk>
2023-07-04 14:54:40 +0100

    (#110) Fix black issues

--- src/dodal/devices/cryostream.py
+++ src/dodal/devices/cryostream.py
@@ -3,7 +3,6 @@ from ophyd import Device, EpicsSignal, EpicsSignalRO
 
 
 class Cryo(Device):
-
     course: EpicsSignal = Cpt(EpicsSignal, "-EA-CJET-01:COARSE:CTRL")
     fine: EpicsSignal = Cpt(EpicsSignal, "-EA-CJET-01:FINE:CTRL")
     temp: EpicsSignalRO = Cpt(EpicsSignalRO, "-EA-CSTRM-01:TEMP")
