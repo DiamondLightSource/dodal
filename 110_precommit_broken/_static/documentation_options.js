0e7495dcb5e124f703bc78ca4f8efa1da2145887
DominicOram <DominicOram@users.noreply.github.com>
2023-07-04 14:47:48 +0000

    deploy: 3d91c266c56752c5df4e0dcce4b77f6bbabbac3b

--- 110_precommit_broken/_static/documentation_options.js
+++ 110_precommit_broken/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev36+g747fb8f8',
+    VERSION: '1.0.1.dev37+g3d91c266',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
