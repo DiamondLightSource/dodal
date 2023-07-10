d288ac59f3052d53f2be1b7c141afff5f94c0aec
noemifrisina <noemifrisina@users.noreply.github.com>
2023-07-10 13:02:54 +0000

    deploy: b78af4a9927e3b525cbb15cabaae0271cbc06fa0

--- artemis-632-get-flux-from-pv/_static/documentation_options.js
+++ artemis-632-get-flux-from-pv/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.1.dev12+g257b2eb9',
+    VERSION: '1.1.1.dev13+gb78af4a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
