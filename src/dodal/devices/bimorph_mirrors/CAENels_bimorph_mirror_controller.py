ab9b77f34941ee81d511b2693d1db0aed07a523d
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-07-11 16:37:58 +0100

    Add CAENelsBimorphMirror8Channel.ASYN.CNCT Component

--- src/dodal/devices/bimorph_mirrors/CAENels_bimorph_mirror_controller.py
+++ src/dodal/devices/bimorph_mirrors/CAENels_bimorph_mirror_controller.py
@@ -2,5 +2,6 @@ from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 
 
 class CAENelsBimorphMirrorController(Device):
+    connected: EpicsSignalRO = Component(EpicsSignalRO, "ASYN.CNCT")
     busy: EpicsSignalRO = Component(EpicsSignalRO, "BUSY")
     groups_list: EpicsSignalRO = Component(EpicsSignalRO, "GROUPSLIST")
