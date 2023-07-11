b1222de9e915a461f298c071b6c7d147ecbdbe25
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-07-11 16:34:37 +0100

    Add CAENelsBimorphMirror8Channel.GROUPSLIST Component

--- src/dodal/devices/bimorph_mirrors/CAENels_bimorph_mirror_controller.py
+++ src/dodal/devices/bimorph_mirrors/CAENels_bimorph_mirror_controller.py
@@ -3,3 +3,4 @@ from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 
 class CAENelsBimorphMirrorController(Device):
     busy: EpicsSignalRO = Component(EpicsSignalRO, "BUSY")
+    groups_list: EpicsSignalRO = Component(EpicsSignalRO, "GROUPSLIST")
