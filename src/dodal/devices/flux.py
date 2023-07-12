f16b32afc799afc9263550fa66e02776181afd84
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-12 17:03:12 +0100

    Move device prefix to instantiation

--- src/dodal/devices/flux.py
+++ src/dodal/devices/flux.py
@@ -4,4 +4,4 @@ from ophyd import Component, Device, EpicsSignalRO
 class Flux(Device):
     """Simple device to get the flux reading"""
 
-    flux_reading: EpicsSignalRO = Component(EpicsSignalRO, "-MO-MAPT-01:Y:FLUX")
+    flux_reading: EpicsSignalRO = Component(EpicsSignalRO, "FLUX")
