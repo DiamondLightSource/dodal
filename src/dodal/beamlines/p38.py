e99d0eccbbd03872b9b9d833952335b579f3e001
Joseph Ware <joseph.ware@diamond.ac.uk>
2023-07-12 10:23:27 +0100

    Add p38 dodal module that is copy of p45 module

---
+++ src/dodal/beamlines/p38.py
@@ -0,0 +1,20 @@
+from dodal.devices.areadetector import AdAravisDetector
+from dodal.utils import BeamlinePrefix
+
+PREFIX: str = BeamlinePrefix("p38").beamline_prefix
+
+
+def sample(name: str = "sample_stage") -> TomoStageWithStretchAndSkew:
+    return TomoStageWithStretchAndSkew(name=name, prefix=f"{PREFIX}-MO-STAGE-01:")
+
+
+def choppers(name: str = "chopper") -> Choppers:
+    return Choppers(name=name, prefix=f"{PREFIX}-MO-CHOP-01:")
+
+
+def det(name: str = "det") -> AdAravisDetector:
+    return AdAravisDetector(name=name, prefix=f"{PREFIX}-EA-MAP-01:")
+
+
+def diff(name: str = "diff") -> AdAravisDetector:
+    return AdAravisDetector(name=name, prefix=f"{PREFIX}-EA-DIFF-01:")
