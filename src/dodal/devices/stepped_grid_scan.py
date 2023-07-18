e443e32198bacc0fd7389946785771e429f48a05
Tom Willemsen <tom.willemsen@diamond.ac.uk>
2023-07-18 15:38:10 +0100

    Fix merge

--- src/dodal/devices/stepped_grid_scan.py
+++ src/dodal/devices/stepped_grid_scan.py
@@ -2,6 +2,7 @@ import threading
 import time
 from dataclasses import dataclass
 
+import numpy as np
 from bluesky.plan_stubs import mv
 from dataclasses_json import DataClassJsonMixin
 from ophyd import (
@@ -17,7 +18,6 @@ from ophyd.status import DeviceStatus, StatusBase
 from dodal.devices.motors import XYZLimitBundle
 from dodal.devices.status import await_value
 from dodal.parameters.experiment_parameter_base import AbstractExperimentParameterBase
-from dodal.utils import Point3D
 
 
 @dataclass
@@ -102,7 +102,7 @@ class SteppedGridScanParams(DataClassJsonMixin, AbstractExperimentParameterBase)
     def is_3d_grid_scan(self):
         return self.z_steps > 0
 
-    def grid_position_to_motor_position(self, grid_position: Point3D) -> Point3D:
+    def grid_position_to_motor_position(self, grid_position: np.ndarray) -> np.ndarray:
         """Converts a grid position, given as steps in the x, y, z grid,
         to a real motor position.
 
@@ -113,11 +113,11 @@ class SteppedGridScanParams(DataClassJsonMixin, AbstractExperimentParameterBase)
             if not axis.is_within(position):
                 raise IndexError(f"{grid_position} is outside the bounds of the grid")
 
-        return Point3D(
-            self.x_axis.steps_to_motor_position(grid_position.x),
-            self.y_axis.steps_to_motor_position(grid_position.y),
-            self.z_axis.steps_to_motor_position(grid_position.z),
-        )
+        return np.array([
+            self.x_axis.steps_to_motor_position(grid_position[0]),
+            self.y_axis.steps_to_motor_position(grid_position[1]),
+            self.z_axis.steps_to_motor_position(grid_position[2]),
+        ])
 
 
 class GridScanCompleteStatus(DeviceStatus):
