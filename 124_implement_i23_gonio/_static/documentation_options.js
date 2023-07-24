cee9f28d0a51adb955f6e86e37576b4c4bec7af1
Tom-Willemsen <Tom-Willemsen@users.noreply.github.com>
2023-07-24 13:01:29 +0000

    deploy: 0986f62f15316c3261baffe892c29187aef40d26

--- 124_implement_i23_gonio/_static/documentation_options.js
+++ 124_implement_i23_gonio/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev13+g1c23b849',
+    VERSION: '1.2.1.dev14+g0986f62f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
