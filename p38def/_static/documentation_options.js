0e947b85faf8d4609fc3400d92c608bfed295d9f
keithralphs <keithralphs@users.noreply.github.com>
2023-07-26 10:46:59 +0000

    deploy: 481f5f4a80091a263e5461f8af4c88b370e32f81

--- p38def/_static/documentation_options.js
+++ p38def/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev2+gb2840c94',
+    VERSION: '1.2.1.dev3+g481f5f4a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
