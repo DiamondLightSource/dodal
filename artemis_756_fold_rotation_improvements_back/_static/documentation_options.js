8f4d8656f28d67b75cae1377c21badee73b03104
dperl-dls <dperl-dls@users.noreply.github.com>
2023-07-28 10:44:21 +0000

    deploy: 64ebb2adff5a0a40ee28ad40813ebb9dead25241

--- artemis_756_fold_rotation_improvements_back/_static/documentation_options.js
+++ artemis_756_fold_rotation_improvements_back/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6.dev64+ga726a7d6',
+    VERSION: '1.2.1.dev14+g64ebb2ad',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
