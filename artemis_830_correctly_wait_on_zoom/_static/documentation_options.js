fdc24dc1fa6781f9aed152c1303e04470906b42c
DominicOram <DominicOram@users.noreply.github.com>
2023-07-31 18:15:14 +0000

    deploy: 47859aa0c50b1ad9f86162c6e323a0d95e827444

--- artemis_830_correctly_wait_on_zoom/_static/documentation_options.js
+++ artemis_830_correctly_wait_on_zoom/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev3+ga3cdbbcf',
+    VERSION: '1.2.1.dev4+g47859aa0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
