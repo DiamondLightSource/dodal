3c663aeeac2a6da96a93ed38a3d7950ef4d65627
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-07 14:00:45 +0100

    Add basic signals to SlitMotor

--- src/dodal/devices/slits/slit_motor.py
+++ src/dodal/devices/slits/slit_motor.py
@@ -1,5 +1,24 @@
-from ophyd import Device
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
 from bluesky.protocols import Movable
 
+
 class SlitMotor(Device, Movable):
-    pass
+    """A class representing a slip motor.
+    
+    This could be gap, centre, x, y, etc...
+    
+    Unimplemented signals:
+        SPMG
+        Anything under "More"
+    """
+    value: EpicsSignal = Component(EpicsSignal, "VAL")
+    readback_value: EpicsSignalRO = Component(EpicsSignalRO, "RBV")
+    
+    tweak_forward: EpicsSignal = Component(EpicsSignal, "TWF")
+    tweak_reverse: EpicsSignal = Component(EpicsSignal, "TWR")
+    tweak_value: EpicsSignal = Component(EpicsSignal, "TWV")
+
+    done_moving: EpicsSignalRO = Component(EpicsSignalRO, "DMOV")
+    alarm_severity: EpicsSignalRO = Component(EpicsSignalRO, "SEVR")
+
+    stop: EpicsSignal = Component(EpicsSignal, "STOP")
