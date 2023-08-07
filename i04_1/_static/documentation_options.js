119f85945050bad54e6b428fd450fc50f3eb92b6
CoePaul <CoePaul@users.noreply.github.com>
2023-08-07 14:17:07 +0000

    deploy: 6856a8a2f55b9ea7055e19a69a4198413a3597e6

--- i04_1/_static/documentation_options.js
+++ i04_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev1+g666e3201',
+    VERSION: '1.2.1.dev2+g6856a8a2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
