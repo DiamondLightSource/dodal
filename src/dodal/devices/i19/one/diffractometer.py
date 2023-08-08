f0e6389b500052d14ccbd21c28f64dcc422d6c93
Paul Coe <paul.coe@diamond.ac.uk>
2023-08-08 14:45:07 +0100

    dodal#139 introduces separate PvUtils class
    * Breaks out static methods into a separate class
       to stand apart from the child class of Device

--- src/dodal/devices/i19/one/diffractometer.py
+++ src/dodal/devices/i19/one/diffractometer.py
@@ -2,26 +2,28 @@ from ophyd import Component as Cpt
 from ophyd import Device, EpicsMotor
 
 
-class Diffractometer(Device):
-    @staticmethod
-    def suffix_of(type: str, axis: str, index: int) -> str:
-        _common_template: str = "-%s-%02d:%s"
-        return _common_template % (type, axis, index)
+def _suffix_of(type: str, axis: str, index: int) -> str:
+    _common_template: str = "-%s-%02d:%s"
+    return _common_template % (type, axis, index)
 
+
+class PvUtils(object):
     @staticmethod
     def centring_motor(axis: str) -> EpicsMotor:
-        centring_suffix = Diffractometer.suffix_of("CENT", axis, 1)
+        centring_suffix = _suffix_of("CENT", axis, 1)
         return Cpt(EpicsMotor, suffix=centring_suffix)
 
     @staticmethod
     def goniometer(axis: str, index: int) -> EpicsMotor:
-        gonio_suffix = Diffractometer.suffix_of("GONIO", axis, index)
+        gonio_suffix = _suffix_of("GONIO", axis, index)
         return Cpt(EpicsMotor, suffix=gonio_suffix)
 
-    centre_x: EpicsMotor = centring_motor("X")
-    centre_y: EpicsMotor = centring_motor("Y")
-    centre_z: EpicsMotor = centring_motor("Z")
-    detector_distance: EpicsMotor = goniometer("DET", 3)
-    omega: EpicsMotor = goniometer("OMEGA", 1)
-    phi: EpicsMotor = goniometer("PHI", 2)
-    two_theta: EpicsMotor = goniometer("2THETA", 3)
+
+class Diffractometer(Device):
+    centre_x: EpicsMotor = PvUtils.centring_motor("X")
+    centre_y: EpicsMotor = PvUtils.centring_motor("Y")
+    centre_z: EpicsMotor = PvUtils.centring_motor("Z")
+    detector_distance: EpicsMotor = PvUtils.goniometer("DET", 3)
+    omega: EpicsMotor = PvUtils.goniometer("OMEGA", 1)
+    phi: EpicsMotor = PvUtils.goniometer("PHI", 2)
+    two_theta: EpicsMotor = PvUtils.goniometer("2THETA", 3)
