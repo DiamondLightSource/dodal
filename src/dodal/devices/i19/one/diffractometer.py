4ccef179f1b2fccd8657e6ac2b2f931caeb00da6
Paul Coe <paul.coe@diamond.ac.uk>
2023-08-08 14:54:53 +0100

    dodal#139 reorders suffix construction arguments
    * Owing to self-inflicted formatting issue
      ( caused by a swap in argument order)
      this commit renders all such arguments consistently ordered
      across the Diffractometer script

--- src/dodal/devices/i19/one/diffractometer.py
+++ src/dodal/devices/i19/one/diffractometer.py
@@ -2,20 +2,20 @@ from ophyd import Component as Cpt
 from ophyd import Device, EpicsMotor
 
 
-def _suffix_of(type: str, axis: str, index: int) -> str:
+def _suffix_of(type: str, index: int, axis: str) -> str:
     _common_template: str = "-%s-%02d:%s"
-    return _common_template % (type, axis, index)
+    return _common_template % (type, index, axis)
 
 
 class PvUtils(object):
     @staticmethod
     def centring_motor(axis: str) -> EpicsMotor:
-        centring_suffix = _suffix_of("CENT", axis, 1)
+        centring_suffix = _suffix_of("CENT", 1, axis)
         return Cpt(EpicsMotor, suffix=centring_suffix)
 
     @staticmethod
-    def goniometer(axis: str, index: int) -> EpicsMotor:
-        gonio_suffix = _suffix_of("GONIO", axis, index)
+    def goniometer(index: int, axis: str) -> EpicsMotor:
+        gonio_suffix = _suffix_of("GONIO", index, axis)
         return Cpt(EpicsMotor, suffix=gonio_suffix)
 
 
@@ -23,7 +23,7 @@ class Diffractometer(Device):
     centre_x: EpicsMotor = PvUtils.centring_motor("X")
     centre_y: EpicsMotor = PvUtils.centring_motor("Y")
     centre_z: EpicsMotor = PvUtils.centring_motor("Z")
-    detector_distance: EpicsMotor = PvUtils.goniometer("DET", 3)
-    omega: EpicsMotor = PvUtils.goniometer("OMEGA", 1)
-    phi: EpicsMotor = PvUtils.goniometer("PHI", 2)
-    two_theta: EpicsMotor = PvUtils.goniometer("2THETA", 3)
+    detector_distance: EpicsMotor = PvUtils.goniometer(3, "DET")
+    omega: EpicsMotor = PvUtils.goniometer(1, "OMEGA")
+    phi: EpicsMotor = PvUtils.goniometer(2, "PHI")
+    two_theta: EpicsMotor = PvUtils.goniometer(3, "2THETA")
