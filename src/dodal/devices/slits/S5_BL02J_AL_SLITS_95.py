707f59b9101e46cba9f54eb78460c5731b4637e9
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-09 11:30:34 +0100

    Create S5_BL02J_AL_SLITS_95 with x, y plus/minus and size/centre componenets

--- src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
+++ src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
@@ -5,11 +5,18 @@ from .slit_motor import SlitMotor
 
 
 class S5_BL02J_AL_SLITS_95(Device, Movable):
-    """Slit simulator set up as BL02J temporary equipment.
+    """Class to interact with slit simulator set up as BL02J temporary equipment.
     
-    Many thanks to Andrew Foster!"""
-    y_real: Device = Component(SlitMotor, "Y:PLUS")
+    Many thanks to Andrew Foster!
+    """
+    y_plus: Device = Component(SlitMotor, "Y:PLUS")
+    y_minus: Device = Component(SlitMotor, "Y:MINUS")
+    
+    x_plus: Device = Component(SlitMotor, "X:PLUS")
+    x_minus: Device = Component(SlitMotor, "X_MINUS")
 
-    def set(self, value):
-        return self.y_real.set(value)
+    y_size: Device = Component(SlitMotor, "Y:SIZE")
+    y_centre: Device = Component(SlitMotor, "Y:CENTRE")
 
+    x_size: Device = Component(SlitMotor, "X:SIZE")
+    x_centre: Device = Component(SlitMotor, "X:CENTRE")
