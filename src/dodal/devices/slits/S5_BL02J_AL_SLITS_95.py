cd61fa7991cfc025d039b1f2945c4492610c3132
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-09 13:43:13 +0100

    Make S5_BL02J_AL_SLITS_95 inherit GapAndCentreSlit2d

--- src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
+++ src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
@@ -2,9 +2,10 @@ from ophyd import Device, Component
 from bluesky.protocols import Movable
 
 from .slit_motor import SlitMotor
+from .gap_and_centre_slit_base_classes import GapAndCentreSlit2d
 
 
-class S5_BL02J_AL_SLITS_95(Device, Movable):
+class S5_BL02J_AL_SLITS_95(GapAndCentreSlit2d):
     """Class to interact with slit simulator set up as BL02J temporary equipment.
     
     Many thanks to Andrew Foster!
