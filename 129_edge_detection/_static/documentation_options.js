5e7665524a4e89fd9cbad72f7ea0ed75bca23449
Tom-Willemsen <Tom-Willemsen@users.noreply.github.com>
2023-08-09 16:21:08 +0000

    deploy: df955cad3fc7b2b496f32c6e1108eae5f2f6fe6c

--- 129_edge_detection/_static/documentation_options.js
+++ 129_edge_detection/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev2+gc66ac659',
+    VERSION: '1.2.1.dev7+gdf955cad',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
