71b3f5537c7d48f68d7b37e914744d177de26db5
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-10 09:01:47 +0100

    Fix typo, auto format

--- src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
+++ src/dodal/devices/slits/S5_BL02J_AL_SLITS_95.py
@@ -7,14 +7,15 @@ from .gap_and_centre_slit_base_classes import GapAndCentreSlit2d
 
 class S5_BL02J_AL_SLITS_95(GapAndCentreSlit2d):
     """Class to interact with slit simulator set up as BL02J temporary equipment.
-    
+
     Many thanks to Andrew Foster!
     """
+
     y_plus: Device = Component(SlitMotor, "Y:PLUS")
     y_minus: Device = Component(SlitMotor, "Y:MINUS")
-    
+
     x_plus: Device = Component(SlitMotor, "X:PLUS")
-    x_minus: Device = Component(SlitMotor, "X_MINUS")
+    x_minus: Device = Component(SlitMotor, "X:MINUS")
 
     y_size: Device = Component(SlitMotor, "Y:SIZE")
     y_centre: Device = Component(SlitMotor, "Y:CENTRE")
