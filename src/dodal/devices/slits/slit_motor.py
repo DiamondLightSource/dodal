d37228058a716e0cfede9391a9016c048d3018db
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-10 09:32:10 +0100

    Make SlitMotor.set acutally wait for await_value call

--- src/dodal/devices/slits/slit_motor.py
+++ src/dodal/devices/slits/slit_motor.py
@@ -7,23 +7,24 @@ from ..status import await_value
 
 
 class MoveStatus(IntEnum):
-    Moving = 0,
+    Moving = (0,)
     Stationary = 1
 
 
 class SlitMotor(Device, Movable):
     """A class representing a slit motor.
-    
+
     This could be gap, centre, x, y, etc...
-    
+
     Unimplemented signals:
         SPMG
         KILL
         Anything under "More"
     """
+
     value: EpicsSignal = Component(EpicsSignal, ".VAL")
     readback_value: EpicsSignalRO = Component(EpicsSignalRO, ".RBV")
-    
+
     tweak_forward: EpicsSignal = Component(EpicsSignal, ".TWF")
     tweak_reverse: EpicsSignal = Component(EpicsSignal, ".TWR")
     tweak_value: EpicsSignal = Component(EpicsSignal, ".TWV")
@@ -34,6 +35,6 @@ class SlitMotor(Device, Movable):
     stop_motor: EpicsSignal = Component(EpicsSignal, ".STOP")
 
     def set(self, target_value) -> SubscriptionStatus:
-        await_value(self.done_moving, MoveStatus.Stationary)
+        await_value(self.done_moving, MoveStatus.Stationary).wait()
 
         return self.value.set(target_value)
