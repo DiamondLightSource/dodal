083bb0ca80a1d30dc69f2a95e6b71bf87b5b75c7
Tom Willemsen <tom.willemsen@diamond.ac.uk>
2023-08-11 15:34:58 +0100

    refactoring

--- src/dodal/devices/oav/edge_detection/oav_with_edge_detection.py
+++ src/dodal/devices/oav/edge_detection/oav_with_edge_detection.py
@@ -1,48 +1,47 @@
-from dodal.devices.oav.edge_detection.edge_detect_utils import MxSampleDetect, ArrayProcessingFunctions, NONE_VALUE as INVALID_POSITION_VALUE
-from dodal.log import LOGGER
-from typing import Callable, Final, Tuple, TypeVar, Optional
-
-from ophyd.v2.core import AsyncStatus, StandardReadable, Reading
-from ophyd.v2.epics import epics_signal_r, SignalR, SignalRW
+import asyncio
+import time
+from typing import Callable, Final, Optional, Tuple, TypeVar
 
 import numpy as np
 from numpy.typing import NDArray
+from ophyd.v2.core import AsyncStatus, StandardReadable
+from ophyd.v2.epics import SignalR, SignalRW, epics_signal_r
 
-import asyncio
+from dodal.devices.oav.edge_detection.edge_detect_utils import (
+    NONE_VALUE as INVALID_POSITION_VALUE,
+)
+from dodal.devices.oav.edge_detection.edge_detect_utils import (
+    ArrayProcessingFunctions,
+    MxSampleDetect,
+)
+from dodal.log import LOGGER
 
-
-T = TypeVar('T')
-
-class _SoftSignal(SignalRW[T]):
-    def __init__(self, initial_value: T):
-        self.value: T = initial_value
-
-    async def _set(self, value: T):
-        self.value = value
-
-    def set(self, value: T, wait=True, timeout=None) -> AsyncStatus:
-        coro = self._set(value)
-        return AsyncStatus(coro)
-    
-    def read(self) -> T:
-        if self.value is None:
-            raise ValueError("Soft signal read before being set")
-        return self.value
+T = TypeVar("T")
 
 
 class EdgeDetection(StandardReadable):
-
-    INVALID_POSITION: Final[Tuple[int, int]] = (INVALID_POSITION_VALUE, INVALID_POSITION_VALUE)
+    INVALID_POSITION: Final[Tuple[int, int]] = (
+        INVALID_POSITION_VALUE,
+        INVALID_POSITION_VALUE,
+    )
 
     def __init__(self, prefix, name: str = ""):
-        self.array_data: SignalR[NDArray[np.uint8]] = epics_signal_r(NDArray[np.uint8], "pva://{}PVA:ARRAY".format(prefix))
+        self.array_data: SignalR[NDArray[np.uint8]] = epics_signal_r(
+            NDArray[np.uint8], "pva://{}PVA:ARRAY".format(prefix)
+        )
 
-        self.oav_width: SignalR[int] = epics_signal_r(int, "{}PVA:ArraySize1_RBV".format(prefix))
-        self.oav_height: SignalR[int] = epics_signal_r(int, "{}PVA:ArraySize2_RBV".format(prefix))
+        self.oav_width: SignalR[int] = epics_signal_r(
+            int, "{}PVA:ArraySize1_RBV".format(prefix)
+        )
+        self.oav_height: SignalR[int] = epics_signal_r(
+            int, "{}PVA:ArraySize2_RBV".format(prefix)
+        )
 
         self.timeout: float = 10.0
 
-        self.preprocess: Callable[[np.ndarray], np.ndarray] = ArrayProcessingFunctions.identity()
+        self.preprocess: Callable[
+            [np.ndarray], np.ndarray
+        ] = ArrayProcessingFunctions.identity()
         self.canny_upper: int = 100
         self.canny_lower: int = 50
         self.close_ksize: int = 5
@@ -56,14 +55,12 @@ class EdgeDetection(StandardReadable):
                 self.oav_width,
                 self.oav_height,
             ],
-            config=[
-            ],
+            config=[],
         )
 
         super().__init__(name=name)
 
     async def _get_tip_position(self) -> Tuple[Optional[int], Optional[int]]:
-
         sample_detection = MxSampleDetect(
             preprocess=self.preprocess,
             canny_lower=self.canny_lower,
@@ -89,9 +86,21 @@ class EdgeDetection(StandardReadable):
                 value = array_data.reshape(height, width)
             else:
                 # Something else?
-                raise ValueError("Unexpected data array size: expected {} (grayscale data) or {} (rgb data), got {}", num_pixels, num_pixels * 3, value_len)
-            
+                raise ValueError(
+                    "Unexpected data array size: expected {} (grayscale data) or {} (rgb data), got {}",
+                    num_pixels,
+                    num_pixels * 3,
+                    value_len,
+                )
+
+            start_time = time.time()
             location = sample_detection.processArray(value)
+            end_time = time.time()
+            LOGGER.warning(
+                "Sample location detection took {}ms".format(
+                    (end_time - start_time) * 1000.0
+                )
+            )
             tip_x = location.tip_x
             tip_y = location.tip_y
         except Exception as e:
@@ -104,10 +113,10 @@ class EdgeDetection(StandardReadable):
     async def read(self) -> Tuple[int | None, int | None]:
         return await asyncio.wait_for(self._get_tip_position(), timeout=self.timeout)
 
-    
+
 if __name__ == "__main__":
     # with DeviceCollector():
-    x = EdgeDetection(prefix="BL03I-DI-OAV-01:")
+    x = EdgeDetection(prefix="BL02J-DI-OAV-01:")
 
     async def acquire():
         await x.connect()
@@ -115,8 +124,11 @@ if __name__ == "__main__":
         tip = await x.read()
         return img, tip
 
-    img, tip = asyncio.get_event_loop().run_until_complete(asyncio.wait_for(acquire(), timeout=10))
+    img, tip = asyncio.get_event_loop().run_until_complete(
+        asyncio.wait_for(acquire(), timeout=10)
+    )
     print("Tip: {}".format(tip))
     import matplotlib.pyplot as plt
-    plt.imshow(img[""]["value"].reshape(768, 1024, 3))
+
+    plt.imshow(img[""]["value"].reshape(2176, 2112, 3))
     plt.show()
