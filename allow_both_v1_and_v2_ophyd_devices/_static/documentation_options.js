24619e72fedfdb2ab519e69e52d9a40092881a00
Tom-Willemsen <Tom-Willemsen@users.noreply.github.com>
2023-08-15 15:21:08 +0000

    deploy: d752192d8a929b5583bfc87e09c57fbd8446f41f

--- allow_both_v1_and_v2_ophyd_devices/_static/documentation_options.js
+++ allow_both_v1_and_v2_ophyd_devices/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.1.dev33+g498ce53d',
+    VERSION: '1.3.1.dev35+gd752192d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
