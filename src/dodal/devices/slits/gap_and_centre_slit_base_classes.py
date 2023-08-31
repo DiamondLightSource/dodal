070d5b05c954406193b6ff2400b8bb5a94740f73
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-08-31 16:35:30 +0100

    Change GapAndCentreSlit1d and -2d .set to accept one 2- or 4-tuple instead of 2 or 4 arguments (+self)

--- src/dodal/devices/slits/gap_and_centre_slit_base_classes.py
+++ src/dodal/devices/slits/gap_and_centre_slit_base_classes.py
@@ -1,31 +1,36 @@
 from abc import abstractmethod
 from bluesky.protocols import Movable
 from ophyd import Device
+from typing import Tuple
 
 
 class GapAndCentreSlit1d(Device, Movable):
     """Class defining interface between Ophyd and 1-dimensional gap and centre slits."""
+
     @abstractmethod
-    def set(self, x_centre_value, x_size_value):
+    def set(self, position_tuple: Tuple[float, float]):
         """Method to set position of slit.
-        
+
         Parameters:
-            x_centre_value: Central coordinate of gap
-            x_size_value: Width of gap
+            position_tuple: A 2-tuple containing:
+                x_centre_value: Central coordinate of gap
+                x_size_value: Width of gap
         """
         ...
 
 
 class GapAndCentreSlit2d(Device, Movable):
     """Class defining interface between Ophyd and 2-dimensional gap and centre slits."""
+
     @abstractmethod
-    def set(self, x_centre_value, x_size_value, y_centre_value, y_size_value):
+    def set(self, position_tuple: Tuple[float, float, float, float]):
         """Method to set position of slit.
-        
+
         Parameters:
-            x_centre_value: Central x-coordinate of gap
-            x_size_value: Width of gap in x-dimension
-            y_centre_value: Central y-coordinate of gap
-            y_size_value: Width of gap in y-dimension
+            position_tuple: A 4-tuple containing:
+                x_centre_value: Central x-coordinate of gap
+                x_size_value: Width of gap in x-dimension
+                y_centre_value: Central y-coordinate of gap
+                y_size_value: Width of gap in y-dimension
         """
         ...
