93ae571d1796b61c63b5b7be66ca64e967ba686e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-01 11:53:20 +0100

    Add a set method to switch light off on backlight

--- src/dodal/devices/backlight.py
+++ src/dodal/devices/backlight.py
@@ -10,3 +10,10 @@ class Backlight(Device):
     pos: EpicsSignal = Component(EpicsSignal, "-EA-BL-01:CTRL")
     # Toggle to switch it On or Off
     toggle: EpicsSignal = Component(EpicsSignal, "-EA-BLIT-01:TOGGLE")
+
+    def set(self, position: int):
+        self.pos.set(position)
+        if self.pos.get() == self.OUT:
+            self.toggle.set("Off")
+        else:
+            self.toggle.set("On")
