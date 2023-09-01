a0f8ac28c06709a29c4e95c4607629769e038495
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-01 17:52:36 +0100

    Add the positions PVs

--- src/dodal/devices/i24/dual_backlight.py
+++ src/dodal/devices/i24/dual_backlight.py
@@ -6,6 +6,22 @@ class BacklightPositioner(Device):
 
     position: EpicsSignal = Component(EpicsSignal, "MP:SELECT")
 
+    OUT: EpicsSignal = Component(EpicsSignal, "MP:SELECT.ZRST")
+    IN: EpicsSignal = Component(EpicsSignal, "MP:SELECT.ONST")
+    LoadCheck: EpicsSignal = Component(EpicsSignal, "MP:SELECT.TWST")
+    OAV2: EpicsSignal = Component(EpicsSignal, "MP:SELECT.THST")
+    Diode: EpicsSignal = Component(EpicsSignal, "MP:SELECT.FRST")
+
+    @property
+    def allowed_backlight_positions(self):
+        return [
+            self.OUT.get(),
+            self.IN.get(),
+            self.LoadCheck.get(),
+            self.OAV2.get(),
+            self.Diode.get(),
+        ]
+
 
 class DualBlacklight(Device):
     led1: EpicsSignal = Component(EpicsSignal, "-DI-LED-01:TOGGLE")
