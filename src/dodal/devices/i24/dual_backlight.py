f9ba2a3960093ce6da2ef68f2fa9ec7c402f5f63
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-04 11:22:41 +0100

    Add set method

--- src/dodal/devices/i24/dual_backlight.py
+++ src/dodal/devices/i24/dual_backlight.py
@@ -4,6 +4,7 @@ from ophyd import Component, Device, EpicsSignal, StatusBase
 class BacklightPositioner(Device):
     """Device to control the backlight position."""
 
+    # String description of the backlight position e.g. "In", "OAV2"
     position: EpicsSignal = Component(EpicsSignal, "MP:SELECT")
 
     OUT: EpicsSignal = Component(EpicsSignal, "MP:SELECT.ZRST")
@@ -23,11 +24,23 @@ class BacklightPositioner(Device):
         ]
 
 
-class DualBlacklight(Device):
+class DualBacklight(Device):
+    """
+    Device to trigger the dual backlight on I24.
+    This device is made up by two LEDs:
+        - LED1 is the "backlight", can be moved to 5 different positions.
+        - LED2 is a "frontlight", it does not move, just switches on and off.
+    """
+
     led1: EpicsSignal = Component(EpicsSignal, "-DI-LED-01:TOGGLE")
     pos1: BacklightPositioner = Component(BacklightPositioner, "-MO-BL-01:")
 
     led2: EpicsSignal = Component(EpicsSignal, "-DI-LED-02:TOGGLE")
 
-    def set(self, pos_to_reach: str) -> StatusBase:
-        pass
+    def set(self, position: str) -> StatusBase:
+        status = self.pos1.position.set(position)
+        if self.pos1.position.get() == self.pos1.OUT:
+            status &= self.led1.set("OFF")
+        else:
+            status &= self.led1.set("ON")
+        return status
