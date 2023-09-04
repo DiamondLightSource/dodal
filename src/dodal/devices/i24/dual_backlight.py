8bb5e1cd6c39f9c5b5452df197b2552652e6603b
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-04 13:49:59 +0100

    Add a couple of tests

--- src/dodal/devices/i24/dual_backlight.py
+++ src/dodal/devices/i24/dual_backlight.py
@@ -5,7 +5,7 @@ class BacklightPositioner(Device):
     """Device to control the backlight position."""
 
     # String description of the backlight position e.g. "In", "OAV2"
-    pos: EpicsSignal = Component(EpicsSignal, "MP:SELECT")
+    pos_level: EpicsSignal = Component(EpicsSignal, "MP:SELECT")
 
     zrst: EpicsSignal = Component(EpicsSignal, "MP:SELECT.ZRST")  # Out
     onst: EpicsSignal = Component(EpicsSignal, "MP:SELECT.ONST")  # In
@@ -30,10 +30,20 @@ class DualBacklight(Device):
     This device is made up by two LEDs:
         - LED1 is the "backlight", can be moved to 5 different positions.
         - LED2 is a "frontlight", it does not move, just switches on and off.
+
+    To set the position for LED1:
+        b = DualBacklight(name="backlight)
+        b.pos1.pos_level.set("OAV2")
+
+    To see get the available position values for LED1:
+        b.pos1.alowed_backlight_positions
+
+    Note that the two LED are independently switched on and off. When LED1 is
+    in "Out" position (switched off), LED2 might still be on.
     """
 
-    OUT = 0
-    IN = 1
+    OUT = "Out"
+    IN = "In"
 
     led1: EpicsSignal = Component(EpicsSignal, "-DI-LED-01:TOGGLE")
     pos1: BacklightPositioner = Component(BacklightPositioner, "-MO-BL-01:")
@@ -41,8 +51,11 @@ class DualBacklight(Device):
     led2: EpicsSignal = Component(EpicsSignal, "-DI-LED-02:TOGGLE")
 
     def set(self, position: str) -> StatusBase:
-        status = self.pos1.pos.set(position)
-        if self.pos1.pos.get() == self.pos1.zrst.get():
+        if position not in self.pos1.allowed_backlight_positions:
+            raise ValueError()
+
+        status = self.pos1.pos_level.set(position)
+        if self.pos1.pos_level.get() == self.pos1.zrst.get():
             status &= self.led1.set("OFF")
         else:
             status &= self.led1.set("ON")
--- tests/devices/unit_tests/i24/test_dual_backlight.py
+++ tests/devices/unit_tests/i24/test_dual_backlight.py
@@ -1,4 +1,6 @@
 import pytest
+from bluesky import plan_stubs as bps
+from bluesky.run_engine import RunEngine
 from ophyd.sim import make_fake_device
 
 from dodal.devices.i24.dual_backlight import DualBacklight
@@ -18,5 +20,28 @@ def fake_backlight() -> DualBacklight:
 
 
 def test_dual_backlight_can_be_written_and_read_from(fake_backlight: DualBacklight):
-    fake_backlight.led2.sim_put("ON")
-    assert fake_backlight.led2.get() == "ON"
+    fake_backlight.led2.sim_put("OFF")
+    assert fake_backlight.led2.get() == "OFF"
+
+
+def test_backlight_position(fake_backlight: DualBacklight):
+    fake_backlight.pos1.pos_level.sim_put(fake_backlight.IN)
+    assert fake_backlight.pos1.pos_level.get() == "In"
+
+
+def test_when_led1_out_it_switches_off(fake_backlight: DualBacklight):
+    RE = RunEngine()
+    RE(bps.abs_set(fake_backlight, fake_backlight.OUT))
+    assert fake_backlight.led1.get() == "OFF"
+
+
+def test_when_led1_not_out_it_switches_on(fake_backlight: DualBacklight):
+    RE = RunEngine()
+    RE(bps.abs_set(fake_backlight, "OAV2"))
+    assert fake_backlight.led1.get() == "ON"
+
+
+def test_allowed_positions(fake_backlight: DualBacklight):
+    p = fake_backlight.pos1.allowed_backlight_positions
+    assert type(p) is list
+    assert len(p) == 5
