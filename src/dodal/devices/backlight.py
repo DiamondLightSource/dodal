f640506ea49890235a95e16b7d2736c82c1405ca
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2023-09-04 09:53:50 +0100

    Turn off backlight when moving out (#158)
    * Add a device for the switch to start
    
    * Use only one device for backlight
    
    * Add a set method to switch light off on backlight
    
    * Add a test
    
    * Add a test for backlight moving in

--- src/dodal/devices/backlight.py
+++ src/dodal/devices/backlight.py
@@ -1,10 +1,20 @@
-from ophyd import Component, Device, EpicsSignal
+from ophyd import Component, Device, EpicsSignal, StatusBase
 
 
 class Backlight(Device):
-    """Simple device to trigger the pneumatic in/out"""
+    """Simple device to trigger the pneumatic in/out."""
 
     OUT = 0
     IN = 1
 
-    pos: EpicsSignal = Component(EpicsSignal, "CTRL")
+    pos: EpicsSignal = Component(EpicsSignal, "-EA-BL-01:CTRL")
+    # Toggle to switch it On or Off
+    toggle: EpicsSignal = Component(EpicsSignal, "-EA-BLIT-01:TOGGLE")
+
+    def set(self, position: int) -> StatusBase:
+        status = self.pos.set(position)
+        if self.pos.get() == self.OUT:
+            status &= self.toggle.set("Off")
+        else:
+            status &= self.toggle.set("On")
+        return status
