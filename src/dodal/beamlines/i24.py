a285893e4c793986e0b336ede39ec5e67bc48ac1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-09-04 14:15:36 +0100

    Add backlight device to i24

--- src/dodal/beamlines/i24.py
+++ src/dodal/beamlines/i24.py
@@ -4,6 +4,7 @@ from dodal.beamlines.beamline_utils import BL, device_instantiation
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.detector import DetectorParams
 from dodal.devices.eiger import EigerDetector
+from dodal.devices.i24.dual_backlight import DualBacklight
 from dodal.devices.i24.I24_detector_motion import DetectorMotion
 from dodal.devices.i24.i24_vgonio import VGonio
 from dodal.devices.oav.oav_detector import OAV
@@ -14,6 +15,21 @@ from dodal.utils import get_beamline_name, skip_device
 BL = get_beamline_name("s24")
 set_beamline(BL)
 set_utils_beamline(BL)
+
+
+def backlight(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> DualBacklight:
+    """Get the i24 backlight device, instantiate it if it hasn't already been.
+    If this is called when already instantiated in i24, it will return the existing object.
+    """
+    return device_instantiation(
+        device_factory=DualBacklight,
+        name="backlight",
+        prefix="",
+        wait=wait_for_connection,
+        fake=fake_with_ophyd_sim,
+    )
 
 
 @skip_device(lambda: BL == "s24")
