350390be5a46f4c0a5e53c0042272f84b11061b8
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2023-09-04 09:53:50 +0100

    Turn off backlight when moving out (#158)
    * Add a device for the switch to start
    
    * Use only one device for backlight
    
    * Add a set method to switch light off on backlight
    
    * Add a test
    
    * Add a test for backlight moving in

--- tests/devices/unit_tests/test_backlight.py
+++ tests/devices/unit_tests/test_backlight.py
@@ -1,11 +1,13 @@
 import pytest
+from bluesky import plan_stubs as bps
+from bluesky.run_engine import RunEngine
 from ophyd.sim import make_fake_device
 
 from dodal.devices.backlight import Backlight
 
 
 @pytest.fixture
-def fake_backlight():
+def fake_backlight() -> Backlight:
     FakeBacklight = make_fake_device(Backlight)
     fake_backlight: Backlight = FakeBacklight(name="backlight")
     return fake_backlight
@@ -14,3 +16,15 @@ def fake_backlight():
 def test_backlight_can_be_written_and_read_from(fake_backlight: Backlight):
     fake_backlight.pos.sim_put(fake_backlight.IN)
     assert fake_backlight.pos.get() == fake_backlight.IN
+
+
+def test_when_backlight_out_it_switches_off(fake_backlight: Backlight):
+    RE = RunEngine()
+    RE(bps.abs_set(fake_backlight, fake_backlight.OUT))
+    assert fake_backlight.toggle.get() == "Off"
+
+
+def test_when_backlight_in_it_switches_on(fake_backlight: Backlight):
+    RE = RunEngine()
+    RE(bps.abs_set(fake_backlight, fake_backlight.IN))
+    assert fake_backlight.toggle.get() == "On"
