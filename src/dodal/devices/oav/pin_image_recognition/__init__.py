6a22cca45c3c6908f07f8bfcba915933d55b85dd
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-10-03 12:30:45 +0100

    Lint and add ophyd-async to dependencies

--- src/dodal/devices/oav/pin_image_recognition/__init__.py
+++ src/dodal/devices/oav/pin_image_recognition/__init__.py
@@ -6,7 +6,7 @@ from typing import Optional, Tuple, Type, TypeVar
 import numpy as np
 from bluesky.protocols import Descriptor, Readable, Reading
 from numpy.typing import NDArray
-from ophyd_async.core import Device, SimSignalBackend, SignalR, SignalRW
+from ophyd_async.core import Device, SignalR, SignalRW, SimSignalBackend
 from ophyd_async.epics.signal import epics_signal_r
 
 from dodal.devices.oav.pin_image_recognition.utils import (
