192c5e4bd0477ed01d72f269a45a71e8c191d21c
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2023-10-10 13:21:44 +0000

    Add tests for beamline file

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -19,30 +19,30 @@ D11_PV = "DI-DCAM-03"
 D12_PV = "DI-DCAM-04"
 
 
-def d11(name: str = "D11") -> HDFStreamerDet:
+def d11(name: str = "D11", sim: bool = False) -> HDFStreamerDet:
     d11_dir = TmpDirectoryProvider()
     d11_dir._directory = Path(f"/dls/{BEAMLINE}/data/{VISIT}/{name}")
 
-    with DeviceCollector():
+    with DeviceCollector(sim=sim):
         d11_drv = ADDriver(f"{PREFIX}-{D11_PV}:DET:")
         d11_hdf = NDFileHDF(f"{PREFIX}-{D11_PV}:HDF5:")
         det = HDFStreamerDet(d11_drv, d11_hdf, d11_dir, name)
     return det
 
 
-def d12(name: str = "D12") -> HDFStreamerDet:
+def d12(name: str = "D12", sim: bool = False) -> HDFStreamerDet:
     d12_dir = TmpDirectoryProvider()
     d12_dir._directory = Path(f"/dls/{BEAMLINE}/data/{VISIT}/{name}")
 
-    with DeviceCollector():
+    with DeviceCollector(sim=sim):
         d12_drv = ADDriver(f"{PREFIX}-{D12_PV}:DET:")
         d12_hdf = NDFileHDF(f"{PREFIX}-{D12_PV}:HDF5:")
         det = HDFStreamerDet(d12_drv, d12_hdf, d12_dir, name)
     return det
 
 
-def panda(name: str = "PANDA") -> FlyingPanda:
-    with DeviceCollector():
+def panda(name: str = "PANDA", sim: bool = False) -> FlyingPanda:
+    with DeviceCollector(sim=sim):
         pbox = PandA("BL38P-PANDA")
         pbox.set_name(name)
     fp = FlyingPanda(pbox)
---
+++ tests/beamlines/unit_tests/test_p38.py
@@ -0,0 +1,47 @@
+import asyncio
+import sys
+from unittest.mock import patch
+
+import pytest
+from bluesky.run_engine import RunEngine, TransitionError
+
+EXPECTED_DEVICES = ["PANDA", "D12", "D11"]
+
+with patch.dict("os.environ", {"BEAMLINE": "p38"}, clear=True):
+    from dodal.beamlines import beamline_utils, p38
+    from dodal.utils import make_all_devices
+
+
+@pytest.fixture(scope="function")
+def RE(request):
+    loop = asyncio.new_event_loop()
+    loop.set_debug(True)
+    RE = RunEngine({}, call_returns_result=True, loop=loop)
+
+    def clean_event_loop():
+        if RE.state not in ("idle", "panicked"):
+            try:
+                RE.halt()
+            except TransitionError:
+                pass
+        loop.call_soon_threadsafe(loop.stop)
+        RE._th.join()
+        loop.close()
+
+    request.addfinalizer(clean_event_loop)
+    return RE
+
+
+def test_device_creation(RE):
+    devices = make_all_devices(p38, sim=True)
+    assert len(devices) > 0
+    for device_name in devices.keys():
+        assert device_name in EXPECTED_DEVICES
+    assert len(EXPECTED_DEVICES) == len(devices)
+
+
+def teardown_module():
+    beamline_utils.BL = "p38"
+    for module in list(sys.modules):
+        if module.endswith("beamline_utils"):
+            del sys.modules[module]
