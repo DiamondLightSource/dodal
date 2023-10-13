01f70c7ca34b221c932b0d910715f561e94a76c7
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2023-10-13 22:00:29 +0100

    Move tests and modify pilatus in line with ophyd-async changes

--- src/dodal/devices/areadetector/pilatus.py
+++ src/dodal/devices/areadetector/pilatus.py
@@ -1,28 +1,46 @@
-from ophyd_async.core import DirectoryProvider, StandardDetector
-from ophyd_async.epics.areadetector.writers.hdf_writer import HDFWriter
-from ophyd_async.epics.areadetector.writers.nd_file_hdf import NDFileHDF
-from ophyd_async.epics.areadetector.writers.nd_plugin import NDPluginStats
+from typing import Sequence, Tuple, cast
 
+from ophyd_async.core import DirectoryProvider, SignalR, StandardDetector
+from ophyd_async.epics.areadetector.writers import HDFWriter, NDFileHDF, NDPluginStats
 from ophyd_async.epics.areadetector.controllers import PilatusController
 from ophyd_async.epics.areadetector.drivers import ADDriverShapeProvider, PilatusDriver
 
 
 class HDFStatsPilatus(StandardDetector):
-    def __init__(self, prefix: str, directory_provider: DirectoryProvider):
-        drv = PilatusDriver(prefix)
-        hdf = NDFileHDF(prefix + "HDF:")
+    _controller: PilatusController
+    _writer: HDFWriter
 
+    def __init__(
+        self,
+        prefix: str,
+        directory_provider: DirectoryProvider,
+        name: str = "",
+        config_sigs: Sequence[Tuple[str, SignalR]] = (),
+    ):
+        self.drv = PilatusDriver(prefix + "DRV:")
+        self.hdf = NDFileHDF(prefix + "HDF:")
+        self.stats = NDPluginStats(prefix + "STATS:")
+
+        self._config_sigs = list(config_sigs)
         super().__init__(
-            PilatusController(drv),
+            PilatusController(self.drv),
             HDFWriter(
-                hdf,
+                self.hdf,
                 directory_provider,
                 lambda: self.name,
-                ADDriverShapeProvider(drv),
+                ADDriverShapeProvider(self.drv),
                 sum="NDStatsSum",
             ),
-            config_sigs=[drv.acquire_time, drv.acquire],
+            name=name,
         )
-        self.stats = NDPluginStats(prefix + "STATS:")
-        self.drv = drv
-        self.hdf = hdf
+
+    async def connect(self, sim: bool = False):
+        await super().connect(sim=sim)
+        driver = self._controller.driver
+        self._config_sigs = [
+            *self._config_sigs,
+            *[
+                (getattr(signal, "name"), cast(SignalR, signal))
+                for signal in [driver.acquire_time, driver.acquire]
+            ],
+        ]
