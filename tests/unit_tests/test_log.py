cd823b8f3ebdb32f970b201352f1d6af54c5caaf
David Perl <david.perl@diamond.ac.uk>
2023-10-31 09:15:34 +0000

    fix logging test

--- tests/unit_tests/test_log.py
+++ tests/unit_tests/test_log.py
@@ -112,16 +112,16 @@ def test_messages_logged_from_dodal_get_sent_to_graylog_and_file(
 ):
     for handler in log.LOGGER.handlers:
         handler.close()
-    mock_graylog_handler = MagicMock(spec=GELFTCPHandler)
-    mock_graylog_handler.return_value.level = logging.DEBUG
+    mock_graylog_handler_class = MagicMock(spec=GELFTCPHandler)
+    mock_graylog_handler_class.return_value.level = logging.DEBUG
     handlers = [None, None, None]
-    with patch("dodal.log.GELFTCPHandler", mock_graylog_handler):
+    with patch("dodal.log.GELFTCPHandler", mock_graylog_handler_class):
         handlers = set_up_logging_handlers(None, False)
     logger = log.LOGGER
     logger.info("test")
     mock_GELFTCPHandler = handlers[1]
-
-    mock_graylog_handler.assert_called_once_with("graylog2.diamond.ac.uk", 12218)
+    assert mock_GELFTCPHandler is not None
+    mock_graylog_handler_class.assert_called_once_with("graylog2.diamond.ac.uk", 12218)
     mock_GELFTCPHandler.handle.assert_called()
     mock_filehandler_emit.assert_called()
 
