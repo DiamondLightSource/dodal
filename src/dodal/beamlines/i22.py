a2abf03eb419d442f90a544a7f3ed61fca0b7946
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-10-31 14:02:06 +0000

    Name i22 devices

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -6,16 +6,18 @@ from dodal.parameters.gda_directory_provider import get_directory_provider
 
 
 def saxs() -> HDFStatsPilatus:
-    return HDFStatsPilatus("BL22I-EA-PILAT-01:", get_directory_provider())
+    return HDFStatsPilatus("BL22I-EA-PILAT-01:", get_directory_provider(), name="saxs")
 
 
 def waxs() -> HDFStatsPilatus:
-    return HDFStatsPilatus("BL22I-EA-PILAT-03:", get_directory_provider())
+    return HDFStatsPilatus("BL22I-EA-PILAT-03:", get_directory_provider(), name="waxs")
 
 
 def panda() -> PandA:
-    return PandA("BL22I-MO-PANDA-01:")
+    panda = PandA("BL22I-EA-PANDA-01")
+    panda.set_name("panda-01")
+    return panda
 
 
-def linkam() -> Linkam3:
-    return Linkam3("BL22I-EA-TEMPC-01:", name="linkam")
+#def linkam() -> Linkam3:
+#    return Linkam3("BL22I-EA-TEMPC-01:", name="linkam")
