6fcdbe55388930f16a35138934ff6a220cbc4e2c
subinsaji <subinsaji@users.noreply.github.com>
2023-11-02 16:37:10 +0000

    deploy: dabae64bd4d15b0239a58bd5c3d22985608bba9c

--- 895_hyperion_fgs_dwell_time/_static/documentation_options.js
+++ 895_hyperion_fgs_dwell_time/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.4.1.dev46+g8e5f842d',
+    VERSION: '1.6.1.dev27+gdabae64b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
