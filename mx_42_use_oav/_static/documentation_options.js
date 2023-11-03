bcd4e96cea001edd5c423079681985efa8b1770f
noemifrisina <noemifrisina@users.noreply.github.com>
2023-11-03 16:50:03 +0000

    deploy: b0fd3bca15bea28fb7e80ea49e8e3341593539cc

--- mx_42_use_oav/_static/documentation_options.js
+++ mx_42_use_oav/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.6.1.dev1+g1153b48c',
+    VERSION: '1.6.1.dev23+gb0fd3bca',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
