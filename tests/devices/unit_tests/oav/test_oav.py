f50849a953ea9a3004e673f0f872617e1cd28fb6
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-11-13 17:11:39 +0000

    A start - broken tests

--- tests/devices/unit_tests/oav/test_oav.py
+++ tests/devices/unit_tests/oav/test_oav.py
@@ -4,12 +4,16 @@ import pytest
 from ophyd.sim import instantiate_fake_device
 from ophyd.status import Status
 
-from dodal.devices.oav.oav_detector import OAV
+from dodal.devices.oav.oav_detector import OAV, OAVParams
+
+DISPLAY_CONFIGURATION = "tests/devices/unit_tests/test_display.configuration"
+ZOOM_LEVELS_XML = "tests/devices/unit_tests/test_jCameraManZoomLevels.xml"
 
 
 @pytest.fixture
 def oav() -> OAV:
-    oav: OAV = instantiate_fake_device(OAV)
+    oav_params = OAVParams(ZOOM_LEVELS_XML, DISPLAY_CONFIGURATION)
+    oav: OAV = instantiate_fake_device(OAV, params=oav_params)
     oav.proc.port_name.sim_put("proc")
     oav.cam.port_name.sim_put("CAM")
 
@@ -50,3 +54,14 @@ def test_when_zoom_level_changed_then_status_waits_for_all_plugins_to_be_updated
 
     assert mxsc_status in full_status
     assert mjpg_status in full_status
+
+
+# def test_get_micronsperpixel_from_oav(oav: OAV):
+#     oav.zoom_controller.level.sim_put("1.0x")
+
+#     assert oav.parameters.micronsPerXPixel == 2.87
+#     """^
+#   File "/scratch/uhz96441/workspaces/dodal/.venv/lib/python3.11/site-packages/ophyd/sim.py", line 1307, in instantiate_fake_device
+#     return dev_cls(**kwargs)
+#            ^^^^^^^^^^^^^^^^^
+# TypeError: OAV.__init__() got an unexpected keyword argument 'name'"""
