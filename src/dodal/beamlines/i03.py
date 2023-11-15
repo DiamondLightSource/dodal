3fc66cd4767948643c7449edd21fb32194a69a56
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-11-15 14:01:33 +0000

    add PandAFastGridScan to i03 devices

--- src/dodal/beamlines/i03.py
+++ src/dodal/beamlines/i03.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from ophyd_async.devices.panda import PandA
+from ophyd_async.panda import PandA
 
 from dodal.beamlines.beamline_utils import device_instantiation
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
@@ -16,6 +16,7 @@ from dodal.devices.flux import Flux
 from dodal.devices.focusing_mirror import FocusingMirror, VFMMirrorVoltages
 from dodal.devices.oav.oav_detector import OAV, OAVConfigParams
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
+from dodal.devices.panda_fast_grid_scan import PandAFastGridScan
 from dodal.devices.qbpm1 import QBPM1
 from dodal.devices.s4_slit_gaps import S4SlitGaps
 from dodal.devices.sample_shutter import SampleShutter
@@ -191,6 +192,21 @@ def fast_grid_scan(
     )
 
 
+def panda_fast_grid_scan(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> PandAFastGridScan:
+    """Get the i03 fast_grid_scan device, instantiate it if it hasn't already been.
+    If this is called when already instantiated in i03, it will return the existing object.
+    """
+    return device_instantiation(
+        device_factory=PandAFastGridScan,
+        name="fast_grid_scan",
+        prefix="-MO-SGON-01:PGS:",
+        wait=wait_for_connection,
+        fake=fake_with_ophyd_sim,
+    )
+
+
 @skip_device(lambda: BL == "s03")
 def oav(wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False) -> OAV:
     """Get the i03 OAV device, instantiate it if it hasn't already been.
@@ -345,9 +361,7 @@ def attenuator(
     )
 
 
-def panda(
-    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
-) -> PandA:
+def panda(wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False) -> PandA:
     """Get the i03 panda device, instantiate it if it hasn't already been.
     If this is called when already instantiated in i03, it will return the existing object.
     """
