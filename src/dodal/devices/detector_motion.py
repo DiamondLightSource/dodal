492d1f598b6d426e28d270b83ee277480ad7044c
Robert Tuck <robert.tuck@diamond.ac.uk>
2023-11-15 10:13:29 +0000

    (DiamondLightSource/hyperion#878) add ShutterState enum to detector_motion.py

--- src/dodal/devices/detector_motion.py
+++ src/dodal/devices/detector_motion.py
@@ -1,10 +1,15 @@
+from enum import IntEnum
+
 from ophyd import Component as Cpt
 from ophyd import Device, EpicsMotor, EpicsSignal, EpicsSignalRO
+
+class ShutterState(IntEnum):
+    CLOSED = 0
+    OPEN = 1
 
 
 class DetectorMotion(Device):
     """Physical motion and interlocks for detector travel"""
-
     upstream_x: EpicsMotor = Cpt(EpicsMotor, "-MO-DET-01:UPSTREAMX")
     downstream_x: EpicsMotor = Cpt(EpicsMotor, "-MO-DET-01:DOWNSTREAMX")
     x: EpicsMotor = Cpt(EpicsMotor, "-MO-DET-01:X")
