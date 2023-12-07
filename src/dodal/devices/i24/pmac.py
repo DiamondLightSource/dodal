b8e96be3dd45b8e4269ba11bb8ff132846ccd1dd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-12-07 11:13:06 +0000

    Add motor record to pmac

--- src/dodal/devices/i24/pmac.py
+++ src/dodal/devices/i24/pmac.py
@@ -1,6 +1,7 @@
 from ophyd import Component as Cpt
 from ophyd import (
     Device,
+    EpicsMotor,
     EpicsSignal,
 )
 
@@ -9,3 +10,7 @@ class PMAC(Device):
     """Device to control the chip stage on I24."""
 
     pmac_string: EpicsSignal = Cpt(EpicsSignal, "PMAC_STRING")
+
+    x: EpicsMotor = Cpt(EpicsMotor, "X")
+    y: EpicsMotor = Cpt(EpicsMotor, "Y")
+    z: EpicsMotor = Cpt(EpicsMotor, "Z")
