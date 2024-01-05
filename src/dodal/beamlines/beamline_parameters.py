18c841503d55ccb059cae7e3c3cee2bd64abcb72
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-01-05 11:34:26 +0000

    (#271) Remove dependencies on hyperion

--- src/dodal/beamlines/beamline_parameters.py
+++ src/dodal/beamlines/beamline_parameters.py
@@ -1,11 +1,15 @@
 from typing import Any, Tuple, cast
 
-from hyperion.log import LOGGER
-from hyperion.parameters.constants import BEAMLINE_PARAMETER_PATHS
-
+from dodal.log import LOGGER
 from dodal.utils import get_beamline_name
 
 BEAMLINE_PARAMETER_KEYWORDS = ["FB", "FULL", "deadtime"]
+
+BEAMLINE_PARAMETER_PATHS = {
+    "i03": "/dls_sw/i03/software/daq_configuration/domain/beamlineParameters",
+    "i04": "/dls_sw/i04/software/gda_versions/gda_9_29/workspace_git/gda-mx.git/configurations/i04-config/scripts/beamlineParameters",
+    "s03": "tests/test_data/test_beamline_parameters.txt",
+}
 
 
 class GDABeamlineParameters:
