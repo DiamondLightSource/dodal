714db0b817291890e00da58ab3c65a48841cb62a
olliesilvester <olliesilvester@users.noreply.github.com>
2024-01-10 13:06:51 +0000

    deploy: 17c56af02527600b2b449b5593b975c11fffffe1

--- 117_add_panda/_static/documentation_options.js
+++ 117_add_panda/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.10.1.dev66+gf1c1f82b',
+    VERSION: '1.10.1.dev67+g17c56af0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
