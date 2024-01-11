f9af4cf0c8e5f3b2848cfd479e4da4f2c21283e6
DominicOram <DominicOram@users.noreply.github.com>
2024-01-11 13:01:38 +0000

    deploy: 7d1c20c91efc7083bd30643870253183304d9b1a

---
+++ 277_zocalo_results_returning_slowly/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.11.2.dev17+g7d1c20c9',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
