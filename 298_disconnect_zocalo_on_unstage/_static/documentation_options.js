d801b0d6edb1ea81cc9a0a5727def2a22f4ecdc5
dperl-dls <dperl-dls@users.noreply.github.com>
2024-01-18 13:07:02 +0000

    deploy: 7081642e913117846794e16d43af4353345e4013

--- 298_disconnect_zocalo_on_unstage/_static/documentation_options.js
+++ 298_disconnect_zocalo_on_unstage/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.12.1.dev31+g32b18fd2',
+    VERSION: '1.12.1.dev32+g7081642e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
