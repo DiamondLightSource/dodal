0c0a5e4f475cf88e947be2177e72c1d16d2a392d
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2024-01-23 17:30:27 +0000

    Add motor records to pmac device (#259)
    * Add motor record to pmac
    
    * Add method to home stages to pmac
    
    * Rething home method
    
    * Use epicsmotors
    
    * Use put instead of set

--- src/dodal/devices/i24/pmac.py
+++ src/dodal/devices/i24/pmac.py
@@ -1,6 +1,7 @@
 from ophyd import Component as Cpt
 from ophyd import (
     Device,
+    EpicsMotor,
     EpicsSignal,
 )
 
@@ -9,3 +10,10 @@ class PMAC(Device):
     """Device to control the chip stage on I24."""
 
     pmac_string = Cpt(EpicsSignal, "PMAC_STRING")
+
+    x = Cpt(EpicsMotor, "X")
+    y = Cpt(EpicsMotor, "Y")
+    z = Cpt(EpicsMotor, "Z")
+
+    def home_stages(self):
+        self.pmac_string.put(r"\#1hmz\#2hmz\#3hmz", wait=True)
