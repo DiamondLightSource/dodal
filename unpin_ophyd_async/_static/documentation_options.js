08f64bdacbbd609ae4b51993e0f79211f2346e23
DominicOram <DominicOram@users.noreply.github.com>
2024-01-30 12:56:30 +0000

    deploy: ff9a68d3b82109768521643c25cd597a982bc7e1

---
+++ unpin_ophyd_async/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.13.3.dev1+gff9a68d3',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
