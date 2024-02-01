5d08f99592f3d65f14555960fbddeed98aad0527
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-02-01 17:31:06 +0000

    Fix incorrect PV name

--- src/dodal/devices/slits/i24_slits_04_virtual_motors.py
+++ src/dodal/devices/slits/i24_slits_04_virtual_motors.py
@@ -6,10 +6,10 @@ from dodal.devices.slits.slit_motor import SlitMotor
 
 
 class I24Slits04VirtualMotors(GapAndCentreSlit2d):
-    x_centre: Device = Component(SlitMotor, "X:CENTER")
+    x_centre: Device = Component(SlitMotor, "X:CENTRE")
     x_size: Device = Component(SlitMotor, "X:SIZE")
 
-    y_centre: Device = Component(SlitMotor, "Y:CENTER")
+    y_centre: Device = Component(SlitMotor, "Y:CENTRE")
     y_size: Device = Component(SlitMotor, "Y:SIZE")
 
     def set(self, position_tuple):
