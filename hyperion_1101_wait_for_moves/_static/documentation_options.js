ed12d0a897b31e4c6c4dd748e54bdea4e8678371
dperl-dls <dperl-dls@users.noreply.github.com>
2024-02-01 10:03:45 +0000

    deploy: 90d0e19427a6a22818deb3b2cff108ea6d1c1efa

---
+++ hyperion_1101_wait_for_moves/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.14.2.dev1+g90d0e194',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
