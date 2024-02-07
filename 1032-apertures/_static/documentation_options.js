f79e665b2cec2b518c25ae6d3bb18a4d36068444
stan-dot <stan-dot@users.noreply.github.com>
2024-02-07 14:36:21 +0000

    deploy: 06b58f44ff543a2171b21810e1f4e2ea4fd54bdf

--- 1032-apertures/_static/documentation_options.js
+++ 1032-apertures/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.13.1.dev46+ge245a8d3',
+    VERSION: '1.13.1.dev47+g06b58f44',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
