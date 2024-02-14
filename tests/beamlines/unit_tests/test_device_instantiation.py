b2c71b352ea3f6ec02db24a8b526c9630d40a19e
David Perl <david.perl@diamond.ac.uk>
2024-02-14 12:27:41 +0000

    #331 only run tests once for each BL and include skipped

--- tests/beamlines/unit_tests/test_device_instantiation.py
+++ tests/beamlines/unit_tests/test_device_instantiation.py
@@ -29,16 +29,17 @@ def test_device_creation(RE, beamline):
     Ensures that for every beamline all device factories are using valid args
     and creating types that conform to Bluesky protocols.
     """
-    for bl in [beamline, "s" + beamline[1:]]:
-        with patch.dict(os.environ, {"BEAMLINE": bl}, clear=True):
-            bl_mod = mock_bl(beamline)
-            devices = make_all_devices(bl_mod, fake_with_ophyd_sim=True)
-            for device_name, device in devices.items():
-                assert device_name in beamline_utils.ACTIVE_DEVICES
-                assert follows_bluesky_protocols(device)
-            assert len(beamline_utils.ACTIVE_DEVICES) == len(devices)
-            beamline_utils.clear_devices()
-            del bl_mod
+    with patch.dict(os.environ, {"BEAMLINE": beamline}, clear=True):
+        bl_mod = mock_bl(beamline)
+        devices = make_all_devices(
+            bl_mod, include_skipped=True, fake_with_ophyd_sim=True
+        )
+        for device_name, device in devices.items():
+            assert device_name in beamline_utils.ACTIVE_DEVICES
+            assert follows_bluesky_protocols(device)
+        assert len(beamline_utils.ACTIVE_DEVICES) == len(devices)
+        beamline_utils.clear_devices()
+        del bl_mod
 
 
 @pytest.mark.parametrize("beamline", ALL_BEAMLINES)
@@ -46,12 +47,15 @@ def test_devices_are_identical(RE, beamline):
     """
     Ensures that for every beamline all device functions prevent duplicate instantiation.
     """
-    for bl in [beamline, "s" + beamline[1:]]:
-        with patch.dict(os.environ, {"BEAMLINE": beamline}, clear=True):
-            bl_mod = mock_bl(beamline)
-            devices_a = make_all_devices(bl_mod, fake_with_ophyd_sim=True)
-            devices_b = make_all_devices(bl_mod, fake_with_ophyd_sim=True)
-            for device_name in devices_a.keys():
-                assert devices_a[device_name] is devices_b[device_name]
-            beamline_utils.clear_devices()
-            del bl_mod
+    with patch.dict(os.environ, {"BEAMLINE": beamline}, clear=True):
+        bl_mod = mock_bl(beamline)
+        devices_a = make_all_devices(
+            bl_mod, include_skipped=True, fake_with_ophyd_sim=True
+        )
+        devices_b = make_all_devices(
+            bl_mod, include_skipped=True, fake_with_ophyd_sim=True
+        )
+        for device_name in devices_a.keys():
+            assert devices_a[device_name] is devices_b[device_name]
+        beamline_utils.clear_devices()
+        del bl_mod
