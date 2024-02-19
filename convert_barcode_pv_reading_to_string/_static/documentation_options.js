7749dea1c762b6310f3906b66f5d6889aa52334b
olliesilvester <olliesilvester@users.noreply.github.com>
2024-02-19 11:21:49 +0000

    deploy: 05864cd50a8e6dcdd3a636bac4d31c8515797cf4

--- convert_barcode_pv_reading_to_string/_static/documentation_options.js
+++ convert_barcode_pv_reading_to_string/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.15.2.dev26+g002176c51',
+    VERSION: '1.15.2.dev35+g05864cd50',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
