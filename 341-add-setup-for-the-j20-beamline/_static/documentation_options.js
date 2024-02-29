2c3dec6aed126f1d690d40c9cef792f5c37da576
stan-dot <stan-dot@users.noreply.github.com>
2024-02-29 15:40:49 +0000

    deploy: 295370c4e2f0c38f5a9d4f44c15fd77350e51ed8

--- 341-add-setup-for-the-j20-beamline/_static/documentation_options.js
+++ 341-add-setup-for-the-j20-beamline/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.15.2.dev25+g5762712c3',
+    VERSION: '1.15.2.dev26+g295370c4e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
