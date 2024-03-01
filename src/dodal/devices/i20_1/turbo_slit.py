ac397e04c505b0e79ea52bff0c875ca4bb6d6b06
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-03-01 15:23:42 +0000

    add StandardMovable

--- src/dodal/devices/i20_1/turbo_slit.py
+++ src/dodal/devices/i20_1/turbo_slit.py
@@ -1,9 +1,24 @@
 
-from ophyd import Component, Device, EpicsSignal, StatusBase
-from ophyd_async.core import Device as OphydV2Device
+from enum import Enum
+
+from bluesky.protocols import Configurable, Movable, Stageable
+from ophyd_async.core import AsyncStatus, Device, Signal, StandardReadable
 
 
-class TurboSlit(OphydV2Device):
+class StandardMovable(Device, Movable, Configurable, Stageable):
+    pass
+
+
+class TestDetector(StandardReadable):
+    pass
+
+
+class ControlEnum(str, Enum):
+    value1 = "close"
+    value2 = "open"
+
+
+class TurboSlit(StandardMovable):
     """
     todo for now only the x motor
     add soft limits
@@ -11,14 +26,15 @@ class TurboSlit(OphydV2Device):
     set speed back to before movement
     """
 
-    motor_x = Component(
-        EpicsSignal,
-        "BL20J-OP-PCHRO-01:TS:XFINE",
+    motor_x = Signal(
+        "BL20J-OP-PCHRO-01:TS:XFINE", # type: ignore
+        0.01
     )
 
-    def set(self, position: str) -> StatusBase:
-        status = self.motor_x.set(position)
-        return status
+    def set(self, position: str) -> AsnycStatus:
+        pass
+        # status = self.motor_x.set(position)
+        # return status
 
 
 class AsyncTurboSlit(Device):
