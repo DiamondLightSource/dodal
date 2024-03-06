1b3b40861ccc70b576d2df25d5f8cbaa484b99a3
DominicOram <DominicOram@users.noreply.github.com>
2024-03-06 21:51:56 +0000

    deploy: 05e71e3fb9a95611d08f10f70690a76163ef4de5

--- hyperion_1228_tidy_up_aperture_reading/_static/documentation_options.js
+++ hyperion_1228_tidy_up_aperture_reading/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.18.1.dev62+g0ff3ec719',
+    VERSION: '1.18.1.dev63+g05e71e3fb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
