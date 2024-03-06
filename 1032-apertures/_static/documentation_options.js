b82ffc5502e801ae632cae01b4f6d87126f68855
DominicOram <DominicOram@users.noreply.github.com>
2024-03-06 15:44:28 +0000

    deploy: 101624ce4031c9a35751c03695eb62869dee2138

--- 1032-apertures/_static/documentation_options.js
+++ 1032-apertures/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.15.2.dev61+gb7bafab4',
+    VERSION: '1.18.1.dev52+g101624ce4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
