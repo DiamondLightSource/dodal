ca9d6df8f17f88ce0128af9cbdfd40d0cfc44a26
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-03-07 18:21:38 +0000

    (DiamondLightSource/hyperion#1235) Fix typing for 3.9

--- tests/devices/unit_tests/conftest.py
+++ tests/devices/unit_tests/conftest.py
@@ -1,4 +1,5 @@
 from functools import partial
+from typing import Union
 from unittest.mock import MagicMock, patch
 
 from ophyd.epics_motor import EpicsMotor
@@ -13,7 +14,7 @@ def mock_set(motor: EpicsMotor, val):
     return Status(done=True, success=True)
 
 
-def patch_motor(motor: EpicsMotor | ExtendedEpicsMotor, initial_position=0):
+def patch_motor(motor: Union[EpicsMotor, ExtendedEpicsMotor], initial_position=0):
     motor.user_setpoint.sim_put(initial_position)  # type: ignore
     motor.user_readback.sim_put(initial_position)  # type: ignore
     motor.motor_done_move.sim_put(1)  # type: ignore
