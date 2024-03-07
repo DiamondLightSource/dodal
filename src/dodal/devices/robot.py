aa8711865dbbdef3b164ff34debb1e3c0135fecc
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-03-07 18:54:23 +0000

    (DiamondLightSource/hyperion#1234) Add load to bart robot

--- src/dodal/devices/robot.py
+++ src/dodal/devices/robot.py
@@ -1,9 +1,14 @@
+import asyncio
 from collections import OrderedDict
+from dataclasses import dataclass
 from typing import Dict, Sequence
 
-from bluesky.protocols import Descriptor, Reading
-from ophyd_async.core import StandardReadable
-from ophyd_async.epics.signal import epics_signal_r
+from bluesky.protocols import Descriptor, Movable, Reading
+from ophyd_async.core import AsyncStatus, StandardReadable, observe_value
+from ophyd_async.epics.signal import epics_signal_r, epics_signal_x
+
+from dodal.devices.util.epics_util import epics_signal_rw_rbv
+from dodal.log import LOGGER
 
 
 class SingleIndexWaveformReadable(StandardReadable):
@@ -44,8 +49,16 @@ class SingleIndexWaveformReadable(StandardReadable):
         return desc
 
 
-class BartRobot(StandardReadable):
+@dataclass
+class SampleLocation:
+    puck: int
+    pin: int
+
+
+class BartRobot(StandardReadable, Movable):
     """The sample changing robot."""
+
+    LOAD_TIMEOUT = 60
 
     def __init__(
         self,
@@ -54,4 +67,27 @@ class BartRobot(StandardReadable):
     ) -> None:
         self.barcode = SingleIndexWaveformReadable(prefix + "BARCODE")
         self.gonio_pin_sensor = epics_signal_r(bool, prefix + "PIN_MOUNTED")
+        self.next_pin = epics_signal_rw_rbv(int, prefix + "NEXT_PIN")
+        self.next_puck = epics_signal_rw_rbv(int, prefix + "NEXT_PUCK")
+        self.load = epics_signal_x(prefix + "LOAD.PROC")
+        self.program_running = epics_signal_r(bool, prefix + "PROGRAM_RUNNING")
         super().__init__(name=name)
+
+    async def _load_pin_and_puck(self, sample_location: SampleLocation):
+        LOGGER.info(f"Loading pin {sample_location}")
+        async for value in observe_value(self.program_running):
+            if not value:
+                break
+            LOGGER.info("Waiting on robot program to finish")
+        await asyncio.gather(
+            self.next_puck.set(sample_location.puck),
+            self.next_pin.set(sample_location.pin),
+        )
+        await self.load.trigger()
+
+    def set(self, sample_location: SampleLocation) -> AsyncStatus:
+        return AsyncStatus(
+            asyncio.wait_for(
+                self._load_pin_and_puck(sample_location), timeout=self.LOAD_TIMEOUT
+            )
+        )
--- tests/devices/unit_tests/test_bart_robot.py
+++ tests/devices/unit_tests/test_bart_robot.py
@@ -1,11 +1,15 @@
+from asyncio import TimeoutError
+from unittest.mock import AsyncMock
+
 import pytest
 from ophyd_async.core import set_sim_value
 
-from dodal.devices.robot import BartRobot
+from dodal.devices.robot import BartRobot, SampleLocation
 
 
 async def _get_bart_robot() -> BartRobot:
     device = BartRobot("robot", "-MO-ROBOT-01:")
+    device.LOAD_TIMEOUT = 0.01  # type: ignore
     await device.connect(sim=True)
     return device
 
@@ -22,3 +26,24 @@ async def test_when_barcode_updates_then_new_barcode_read():
     expected_barcode = "expected"
     set_sim_value(device.barcode.bare_signal, [expected_barcode, "other_barcode"])
     assert (await device.barcode.read())["robot-barcode"]["value"] == expected_barcode
+
+
+@pytest.mark.asyncio
+async def test_given_program_running_when_load_pin_then_times_out():
+    device = await _get_bart_robot()
+    set_sim_value(device.program_running, True)
+    with pytest.raises(TimeoutError):
+        await device.set(SampleLocation(0, 0))
+
+
+@pytest.mark.asyncio
+async def test_given_program_not_running_when_load_pin_then_pin_loaded():
+    device = await _get_bart_robot()
+    set_sim_value(device.program_running, False)
+    device.load = AsyncMock(side_effect=device.load)
+    status = device.set(SampleLocation(15, 10))
+    await status
+    assert status.success
+    assert (await device.next_puck.get_value()) == 15
+    assert (await device.next_pin.get_value()) == 10
+    device.load.trigger.assert_called_once()  # type:ignore
