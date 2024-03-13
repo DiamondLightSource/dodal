607a257169e33591a5c7c1bfa24b45417d7cbfcb
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-03-13 13:17:05 +0000

    Update p38 to use ophyd-async devices

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -1,6 +1,10 @@
-from dodal.beamlines.beamline_utils import device_instantiation
+from ophyd_async.panda import PandA
+
+from dodal.beamlines.beamline_utils import device_instantiation, get_directory_provider
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.areadetector import AdAravisDetector
+from dodal.devices.areadetector.adaravis import SumHDFAravisDetector
+from dodal.devices.linkam3 import Linkam3
+from dodal.devices.tetramm import TetrammDetector
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import get_beamline_name
 
@@ -11,23 +15,52 @@ set_utils_beamline(BL)
 
 def d11(
     wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
-) -> AdAravisDetector:
+) -> SumHDFAravisDetector:
     return device_instantiation(
-        AdAravisDetector,
+        SumHDFAravisDetector,
         "D11",
         "-DI-DCAM-03:",
         wait_for_connection,
         fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
     )
 
 
 def d12(
     wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
-) -> AdAravisDetector:
+) -> SumHDFAravisDetector:
     return device_instantiation(
-        AdAravisDetector,
+        SumHDFAravisDetector,
         "D12",
         "-DI-DCAM-04:",
         wait_for_connection,
         fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+    )
+
+
+def linkam3(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> Linkam3:
+    return device_instantiation(
+        Linkam3, "linkam3", "-EA-LINKM-02:", wait_for_connection, fake_with_ophyd_sim
+    )
+
+
+def tetramm(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> TetrammDetector:
+    return device_instantiation(
+        TetrammDetector,
+        "tetramm",
+        "-EA-XBPM-01:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+    )
+
+
+def panda(wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False) -> PandA:
+    return device_instantiation(
+        PandA, "panda", "-EA-PANDA-01:", wait_for_connection, fake_with_ophyd_sim
     )
