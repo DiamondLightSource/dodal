5888437629a7e25045dcfefb9facc6413e043f1d
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-03-13 13:17:05 +0000

    Move tetramm test into standard package and fix test

--- tests/devices/unit_tests/test_tetramm.py
+++ tests/devices/unit_tests/test_tetramm.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from dodal.devices.tetramm import TetrammDriver
+from dodal.devices.tetramm import TetrammController
 
 TOLERANCE = 0.0001
 
@@ -8,8 +8,8 @@ TOLERANCE = 0.0001
 def test_max_frame_rate():
     drv = Mock()
     drv.minimum_frame_time = 0.1
-    assert TetrammDriver.max_frame_rate.__get__(drv) == 10
-    TetrammDriver.max_frame_rate.__set__(drv, 20)
+    assert TetrammController.max_frame_rate.__get__(drv) == 10
+    TetrammController.max_frame_rate.__set__(drv, 20)
     assert abs(drv.minimum_frame_time - 0.05) < TOLERANCE
 
 
@@ -20,15 +20,15 @@ def test_min_frame_time():
     drv.minimum_values_per_reading = 5
     drv.readings_per_frame = 1_000
 
-    assert abs(TetrammDriver.minimum_frame_time.__get__(drv) - 0.05) < TOLERANCE
+    assert abs(TetrammController.minimum_frame_time.__get__(drv) - 0.05) < TOLERANCE
 
-    TetrammDriver.minimum_frame_time.__set__(drv, 0.01)
+    TetrammController.minimum_frame_time.__set__(drv, 0.01)
     assert drv.readings_per_frame == 200
 
     # readings_per_frame should never be above maximum_readings_per_frame
-    TetrammDriver.minimum_frame_time.__set__(drv, 0.1)
+    TetrammController.minimum_frame_time.__set__(drv, 0.1)
     assert drv.readings_per_frame == 1000
 
     drv.maximum_readings_per_frame = 1200
-    TetrammDriver.minimum_frame_time.__set__(drv, 0.1)
+    TetrammController.minimum_frame_time.__set__(drv, 0.1)
     assert drv.readings_per_frame == 1200
