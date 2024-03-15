280038d391119348abbe7cd0c427bc861e2d598a
noemifrisina <noemifrisina@users.noreply.github.com>
2024-03-15 15:42:41 +0000

    deploy: f3d3eb86b786a56711659c34a037ae321a3d6ded

--- 356_zebra-to-ophyd-async/_static/documentation_options.js
+++ 356_zebra-to-ophyd-async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev14+g350ec7c3c',
+    VERSION: '1.20.1.dev15+gf3d3eb86b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
