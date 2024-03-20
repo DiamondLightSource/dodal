f81935610409d8b8fd89a04b7dfef0c7dc6f52d6
katesmith280 <katesmith280@users.noreply.github.com>
2024-03-20 13:58:36 +0000

    deploy: 67b7152145c31de4f82d700062acb531ddc3b58a

--- 385_aperture_readback_PVs/_static/documentation_options.js
+++ 385_aperture_readback_PVs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev12+g39357f2cf',
+    VERSION: '1.20.1.dev13+g67b715214',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
