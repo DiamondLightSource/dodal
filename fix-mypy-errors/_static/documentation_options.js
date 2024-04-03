ba0efe3fc79c1c6e1e4caa7821991495c3cdf8a5
callumforrester <callumforrester@users.noreply.github.com>
2024-04-03 08:35:10 +0000

    deploy: 4ad30f497399e6ddd05eae7a2a0038e43941c007

--- fix-mypy-errors/_static/documentation_options.js
+++ fix-mypy-errors/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev50+gb113bf03e',
+    VERSION: '1.20.1.dev51+g4ad30f497',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
