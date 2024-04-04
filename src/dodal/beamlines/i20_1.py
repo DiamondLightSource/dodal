22f8f1966c3cf31904dec3d445e47f42615792b4
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-04 11:16:05 +0100

    add docs to the beamline

--- src/dodal/beamlines/i20_1.py
+++ src/dodal/beamlines/i20_1.py
@@ -2,27 +2,19 @@ from dodal.beamlines.beamline_utils import device_instantiation
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.turbo_slit import TurboSlit
 from dodal.log import set_beamline as set_log_beamline
-from dodal.utils import get_beamline_name, get_hostname, skip_device
+from dodal.utils import get_beamline_name
 
 BL = get_beamline_name("i20_1")
 set_log_beamline(BL)
 set_utils_beamline(BL)
 
 
-def _is_i20_1_machine():
-    """
-    Devices using PVA can only connect from i20_1 machines, due to the absence of
-    PVA gateways at present.
-    """
-    hostname = get_hostname()
-    return hostname.startswith("i20_1")
-
-
-@skip_device(lambda: not _is_i20_1_machine())
 def turbo_slit(
     wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
 ) -> TurboSlit:
-    """Get the i20-1 motor"""
+    """
+    turboslit for selecting energy from the polychromator
+    """
 
     return device_instantiation(
         TurboSlit,
