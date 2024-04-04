92fd235c85b3fbde7a3ed95bf43d3d7ee6d0e042
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-04-04 12:12:50 +0100

    (#1283) Add microns-per-pixel to the oav snapshot to facilitate event generation for ispyb

--- src/dodal/devices/oav/grid_overlay.py
+++ src/dodal/devices/oav/grid_overlay.py
@@ -133,6 +133,9 @@ class SnapshotWithGrid(MJPG):
     last_path_outer = Component(Signal)
     last_path_full_overlay = Component(Signal)
 
+    microns_per_pixel_x = Component(Signal)
+    microns_per_pixel_y = Component(Signal)
+
     def post_processing(self, image: Image.Image):
         top_left_x = self.top_left_x.get()
         top_left_y = self.top_left_y.get()
