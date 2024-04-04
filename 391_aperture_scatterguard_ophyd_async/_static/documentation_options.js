81753aff2d280f18f98d7754eb7bea766bef5f74
katesmith280 <katesmith280@users.noreply.github.com>
2024-04-04 15:39:41 +0000

    deploy: 32be1b52b4cb512334d0616d571f84af8910588c

--- 391_aperture_scatterguard_ophyd_async/_static/documentation_options.js
+++ 391_aperture_scatterguard_ophyd_async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev27+g41436a3ab',
+    VERSION: '1.20.1.dev28+g32be1b52b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
