4eb4ebee547683164b69f4f2c899d72419b76fbd
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-04-08 14:50:09 +0000

    deploy: cc8886ba8b0f69ca880381595d96927ad0494fe0

--- adaravis/_static/documentation_options.js
+++ adaravis/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev75+gaf465a8a9',
+    VERSION: '1.20.1.dev76+gcc8886ba8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
