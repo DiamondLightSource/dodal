deea20b7e92e7981e98c061dcb303718d6f713d5
dperl-dls <dperl-dls@users.noreply.github.com>
2024-04-10 10:19:04 +0000

    deploy: 7325b3b0e6e6821730e0eff8b25d0f9997f0eea6

--- 391_aperture_scatterguard_ophyd_async/_static/documentation_options.js
+++ 391_aperture_scatterguard_ophyd_async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.20.1.dev83+g49fa71654',
+    VERSION: '1.20.1.dev84+g7325b3b0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
