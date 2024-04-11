0ae3a800d5949b3914f2dd1daa7fcb8a0c03bc52
callumforrester <callumforrester@users.noreply.github.com>
2024-04-11 12:07:43 +0000

    deploy: f98cb040d9c4fa6b645dad13cf2c1b7a033b8068

--- ophyd-async-slits/_static/documentation_options.js
+++ ophyd-async-slits/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev3+g7a5eb0d78',
+    VERSION: '1.21.1.dev5+gf98cb040d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
