c8a13c94f91451a0e1281681f89f502f233a8f85
DominicOram <DominicOram@users.noreply.github.com>
2024-04-11 18:37:12 +0000

    deploy: ff4622073d3c244a0730654dc1b4ba3b7cbf9e25

--- 435_create_webcam_device/_static/documentation_options.js
+++ 435_create_webcam_device/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev8+g2d3edf3b9',
+    VERSION: '1.21.1.dev8+gff4622073',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
