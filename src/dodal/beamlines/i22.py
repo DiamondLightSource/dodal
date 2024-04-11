0a5f334c38f1e23a030a4f2f71f48affb34cf64e
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-04-11 14:00:42 +0100

    Use AdAravis detectors from ophyd-async

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -1,3 +1,7 @@
+from ophyd_async.epics.areadetector import ADAravisDetector
+from ophyd_async.epics.areadetector.drivers import ADAravisDriver
+from ophyd_async.epics.areadetector.writers.nd_file_hdf import NDFileHDF
+
 from dodal.beamlines.beamline_utils import device_instantiation, get_directory_provider
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.tetramm import TetrammDetector
@@ -16,7 +20,7 @@ def i0(
     return device_instantiation(
         TetrammDetector,
         "i0",
-        "-EA-XBPM-02",
+        "-EA-XBPM-02:",
         wait_for_connection,
         fake_with_ophyd_sim,
         directory_provider=get_directory_provider(),
@@ -30,8 +34,50 @@ def it(
     return device_instantiation(
         TetrammDetector,
         "it",
-        "-EA-TTRM-02",
+        "-EA-TTRM-02:",
         wait_for_connection,
         fake_with_ophyd_sim,
         directory_provider=get_directory_provider(),
+    )
+
+
+def oav_driver(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> ADAravisDriver:
+    return device_instantiation(
+        ADAravisDriver,
+        "oav_driver",
+        "-DI-OAV-01:DRV:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
+def oav_writer(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> NDFileHDF:
+    return device_instantiation(
+        NDFileHDF,
+        "oav_writer",
+        "-DI-OAV-01:HDF:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
+def oav(
+    oav_driver: ADAravisDriver,
+    oav_writer: NDFileHDF,
+    wait_for_connection: bool = True,
+    fake_with_ophyd_sim: bool = False,
+) -> ADAravisDetector:
+    return device_instantiation(
+        ADAravisDetector,
+        "oav",
+        "-DI-OAV-01:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+        driver=oav_driver,
+        hdf=oav_writer
     )
--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -1,6 +1,9 @@
+from ophyd_async.epics.areadetector import ADAravisDetector
+from ophyd_async.epics.areadetector.drivers import ADAravisDriver
+from ophyd_async.epics.areadetector.writers import NDFileHDF
+
 from dodal.beamlines.beamline_utils import device_instantiation, get_directory_provider
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.areadetector import AdAravisDetector
 from dodal.devices.tetramm import TetrammDetector
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import get_beamline_name
@@ -10,29 +13,87 @@ set_log_beamline(BL)
 set_utils_beamline(BL)
 
 
+def d11_driver(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> ADAravisDriver:
+    return device_instantiation(
+        ADAravisDriver,
+        "d11_driver",
+        "-DI-DCAM-03:DRV:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
+def d11_writer(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> NDFileHDF:
+    return device_instantiation(
+        NDFileHDF,
+        "d11_writer",
+        "-DI-DCAM-03:HDF:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
 def d11(
+    d11_driver: ADAravisDriver,
+    d11_writer: NDFileHDF,
     wait_for_connection: bool = True,
     fake_with_ophyd_sim: bool = False,
-) -> AdAravisDetector:
+) -> ADAravisDetector:
     return device_instantiation(
-        AdAravisDetector,
+        ADAravisDetector,
         "D11",
         "-DI-DCAM-03:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+        driver=d11_driver,
+        hdf=d11_writer,
+    )
+
+
+def d12_driver(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> ADAravisDriver:
+    return device_instantiation(
+        ADAravisDriver,
+        "d12_driver",
+        "-DI-DCAM-04:DRV:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
+def d12_writer(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> NDFileHDF:
+    return device_instantiation(
+        NDFileHDF,
+        "d12_writer",
+        "-DI-DCAM-04:HDF:",
         wait_for_connection,
         fake_with_ophyd_sim,
     )
 
 
 def d12(
+    d12_driver: ADAravisDriver,
+    d12_writer: NDFileHDF,
     wait_for_connection: bool = True,
     fake_with_ophyd_sim: bool = False,
-) -> AdAravisDetector:
+) -> ADAravisDetector:
     return device_instantiation(
-        AdAravisDetector,
+        ADAravisDetector,
         "D12",
         "-DI-DCAM-04:",
         wait_for_connection,
         fake_with_ophyd_sim,
+        directory_provider=get_directory_provider(),
+        driver=d12_driver,
+        hdf=d12_writer,
     )
 
 
@@ -43,7 +104,7 @@ def i0(
     return device_instantiation(
         TetrammDetector,
         "i0",
-        "-EA-XBPM-01",
+        "-EA-XBPM-01:",
         wait_for_connection,
         fake_with_ophyd_sim,
         directory_provider=get_directory_provider(),
