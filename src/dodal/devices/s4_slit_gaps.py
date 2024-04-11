3f51cf8f3ca1d1a0b9f0fe8d3942cbfcd3e2eb41
Callum Forrester <callum.forrester@diamond.ac.uk>
2024-04-11 12:55:05 +0100

    Add docstrings to slit gaps

--- src/dodal/devices/s4_slit_gaps.py
+++ src/dodal/devices/s4_slit_gaps.py
@@ -4,7 +4,7 @@ from ophyd_async.epics.motion import Motor
 
 class S4SlitGaps(StandardReadable):
     """
-    Representation of S4 Slit Gaps
+    Representation of S4 Slit Gaps, a group of motors controlling the xray optics slits
     """
 
     def __init__(self, prefix: str, name: str = "") -> None:
@@ -26,12 +26,30 @@ class S4SlitGaps(StandardReadable):
 
 
 class S4SlitGapsGroup(StandardReadable):
+    """
+    A group of slits, since they normally come in groups following a standard PV naming
+    convention.
+    """
+
     def __init__(
         self,
         prefix: str,
         indices: range,
         name: str = "",
     ) -> None:
+        """
+        Constructor, formats prefix with indices for sub-devices. For example:
+        S4SlitGapsGroup("MY-SLITS-{0:02d}:", range(5)) will populate the group with 4
+        S4SlitGaps devices with prefixes MY-SLITS-00, MY-SLITS-01, MY-SLITS-02,
+        MY-SLITS-03. The index to access them will be the same as the PV number, so
+        MY-SLITS-02 is accessed via group.slits[2].
+
+        Args:
+            prefix: PV prefix with python formatting
+            indices: Indices of individual slits
+            name: Device name. Defaults to "".
+        """
+
         self.slits: DeviceVector[S4SlitGaps] = DeviceVector(
             {i: S4SlitGaps(prefix.format(i)) for i in indices}
         )
