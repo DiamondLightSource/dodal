bb34a7e16e8d6b7dc316b84918c8508a1f766f1c
callumforrester <callumforrester@users.noreply.github.com>
2024-04-12 09:33:54 +0000

    deploy: 800369cafe2e0f1fc64e93ae7bc0cec7ef8fbdd6

--- ophyd-async-slits/_static/documentation_options.js
+++ ophyd-async-slits/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev8+g242f97741',
+    VERSION: '1.21.1.dev9+g800369caf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
