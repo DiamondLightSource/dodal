30580ebc7d78eb99268fc5501b44d9e856645c23
dperl-dls <dperl-dls@users.noreply.github.com>
2024-04-12 14:25:22 +0000

    deploy: 1541f2242211a9ea98dc3c115f6c19c9ba4cd55c

--- 433_tidy_logs/_static/documentation_options.js
+++ 433_tidy_logs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev9+g030381f03',
+    VERSION: '1.21.1.dev10+g1541f2242',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
