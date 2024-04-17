33491abe89b1669b103879b84e7b9813954c20fd
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-04-17 08:23:14 +0000

    delete old file

--- src/dodal/devices/turbo_slit.py
+++ src/dodal/devices/turbo_slit.py
@@ -7,12 +7,12 @@ class TurboSlit(Device):
     This collection of motors coordinates time resolved XAS experiments.
     It selects a beam out of the polychromatic fan.
     There is a 0.1 degrees beam that can move along a 90-ish degree arc.
-    A turboslit device moves after the beam, so that the gap aligns with the beam. 
+    A turboslit device moves after the beam, so that the gap aligns with the beam.
     The turboslit gap can be made smaller to increase energy resolution.
     The xfine motor can move the slit in x direction at high frequencies for different scans.
     These slits can be scanned continously or in step mode.
     The relationship between the three motors is as follows:
-        - arc - position of the middle of the gap (coarse/ macro) extension 
+        - arc - position of the middle of the gap (coarse/ macro) extension
         - gap - width in mm, provides energy resolution
         - xfine selects the energy as part of the high frequency scan
     """
