a991d2fe7a96a8899d1a52b4893539a8dd32e265
Kate Smith <katemarylouisesmith@gmail.com>
2024-04-19 08:08:10 +0000

    remove unnecessary if statement (no more ExtendedMotor)

--- tests/devices/unit_tests/conftest.py
+++ tests/devices/unit_tests/conftest.py
@@ -21,9 +21,8 @@ def mock_move(motor: Motor, val, *args, **kwargs):
 def patch_motor(motor: Motor, initial_position=0):
     motor.user_setpoint._backend._set_value(initial_position)  # type: ignore
     motor.user_readback._backend._set_value(initial_position)  # type: ignore
-    if isinstance(motor, Motor):
-        motor.deadband._backend._set_value(0.001)  # type: ignore
-        motor.motor_done_move._backend._set_value(1)  # type: ignore
+    motor.deadband._backend._set_value(0.001)  # type: ignore
+    motor.motor_done_move._backend._set_value(1)  # type: ignore
     return patch.object(
         motor, "_move", AsyncMock(side_effect=partial(mock_move, motor))
     )
