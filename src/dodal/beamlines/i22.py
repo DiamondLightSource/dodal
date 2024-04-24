fda46fc2b92db20f8eefe67581d0950615f66245
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-04-24 17:41:25 +0100

    (#398) Add vfm and hfm to i22 and add i22 system test

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -5,6 +5,7 @@ from dodal.beamlines.beamline_utils import (
 )
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.visit import StaticVisitDirectoryProvider
+from dodal.devices.focusing_mirror import FocusingMirror
 from dodal.devices.tetramm import TetrammDetector
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import get_beamline_name
@@ -48,4 +49,30 @@ def it(
         wait_for_connection,
         fake_with_ophyd_sim,
         directory_provider=get_directory_provider(),
+    )
+
+
+def vfm(
+    wait_for_connection: bool = True,
+    fake_with_ophyd_sim: bool = False,
+) -> FocusingMirror:
+    return device_instantiation(
+        FocusingMirror,
+        "vfm",
+        "-OP-KBM-01:VFM:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
+    )
+
+
+def hfm(
+    wait_for_connection: bool = True,
+    fake_with_ophyd_sim: bool = False,
+) -> FocusingMirror:
+    return device_instantiation(
+        FocusingMirror,
+        "hfm",
+        "-OP-KBM-01:HFM:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
     )
