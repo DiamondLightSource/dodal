25afc4dd8ac127366e8a0865aac5b4a888c77f9c
callumforrester <callumforrester@users.noreply.github.com>
2024-04-25 09:42:33 +0000

    deploy: 40d8fded5907848a8df09b8283bfb364e6125665

--- 389-and-397-undulator-dcm-ophyd-async/_static/documentation_options.js
+++ 389-and-397-undulator-dcm-ophyd-async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev43+g22a0e1dfd',
+    VERSION: '1.21.1.dev44+g40d8fded5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
