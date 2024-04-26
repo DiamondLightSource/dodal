1ec4efce1a6ebe0004edecfdfb85ee9fc30a9171
DominicOram <DominicOram@users.noreply.github.com>
2024-04-26 11:46:50 +0000

    deploy: 47dc2731cda45a3ed05e9ddb317c5f3a869bfe55

--- 471_beam_centre/_static/documentation_options.js
+++ 471_beam_centre/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev55+gfad4c8cd1',
+    VERSION: '1.21.1.dev57+g47dc2731c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
