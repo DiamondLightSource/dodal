04b46e792a701d09f1388f21f463f144882011c3
joeshannon <joeshannon@users.noreply.github.com>
2024-04-26 15:02:21 +0000

    deploy: 0613e1ac5993885dad987c5d96db972941c2bade

--- i22-fswitch/_static/documentation_options.js
+++ i22-fswitch/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev53+g72f654658',
+    VERSION: '1.21.1.dev53+g0613e1ac5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
