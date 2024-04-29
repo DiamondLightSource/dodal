c1697256febcbf47e7cda1887dfc79e7615d2e3b
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-04-29 13:35:59 +0000

    deploy: 916c6c49326465380fb6c8a33263283c0ec80e42

--- pilatus/_static/documentation_options.js
+++ pilatus/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev63+g1125f9cb7',
+    VERSION: '1.21.1.dev70+g916c6c493',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
