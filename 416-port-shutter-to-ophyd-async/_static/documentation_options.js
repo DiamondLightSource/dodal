68f042281da4e3bec6279fd4b82f58ce5125197c
stan-dot <stan-dot@users.noreply.github.com>
2024-05-01 09:57:34 +0000

    deploy: 935b934d4328851cf997f47546dbed7b52ea3c68

--- 416-port-shutter-to-ophyd-async/_static/documentation_options.js
+++ 416-port-shutter-to-ophyd-async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.22.1.dev13+g81da739f4',
+    VERSION: '1.22.1.dev14+g935b934d4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
