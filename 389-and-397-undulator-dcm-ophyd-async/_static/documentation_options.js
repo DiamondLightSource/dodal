a4068a5840d4da14ab52c36f8fd832fa0243eaeb
DominicOram <DominicOram@users.noreply.github.com>
2024-05-03 15:31:55 +0000

    deploy: ea806c5045a084c8caf222f46acf7e8f24c1e569

--- 389-and-397-undulator-dcm-ophyd-async/_static/documentation_options.js
+++ 389-and-397-undulator-dcm-ophyd-async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.22.1.dev20+g23bded307',
+    VERSION: '1.22.1.dev21+gea806c504',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
