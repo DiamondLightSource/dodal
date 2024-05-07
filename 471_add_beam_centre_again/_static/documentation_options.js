113758ab9ddae3b2a445fb7d0e68318ec3222712
DominicOram <DominicOram@users.noreply.github.com>
2024-05-07 07:30:47 +0000

    deploy: 3de593bb820885ba0a737a8cadcef5e6e53ad6c6

--- 471_add_beam_centre_again/_static/documentation_options.js
+++ 471_add_beam_centre_again/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.23.1.dev15+g331dc0ce3',
+    VERSION: '1.23.1.dev16+g3de593bb8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
