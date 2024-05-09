23257057f314a05c6c43514f528216c9533101e9
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-05-09 16:49:52 +0100

    PandASubdirectoryProvider now has option to create specified directory on update

--- src/dodal/common/udc_directory_provider.py
+++ src/dodal/common/udc_directory_provider.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from ophyd_async.core import DirectoryInfo
@@ -7,11 +8,14 @@ from dodal.log import LOGGER
 
 
 class PandASubdirectoryProvider(UpdatingDirectoryProvider):
+    """Directory provider for the HDFPanda. Points to, and optionally creates, a panda subdirectory relative to the
+    directory path provided"""
+
     resource_dir = Path("panda")
 
     def __init__(self, directory: Path | None = None):
         if directory is None:
-            LOGGER.warn(
+            LOGGER.debug(
                 f"{self.__class__.__name__} instantiated with no root path, update() must be called before writing data!"
             )
         self._directory_info = (
@@ -20,7 +24,12 @@ class PandASubdirectoryProvider(UpdatingDirectoryProvider):
             else None
         )
 
-    def update(self, directory: Path):
+    def update(self, directory: Path, create_directory=False):
+        if create_directory:
+            panda_directory = f"{directory}/{self.resource_dir}"
+            if not os.path.isdir(panda_directory):
+                LOGGER.debug(f"Creating PandA PCAP subdirectory in {panda_directory}")
+                os.makedirs(panda_directory)
         self._directory_info = DirectoryInfo(
             root=directory, resource_dir=self.resource_dir
         )
