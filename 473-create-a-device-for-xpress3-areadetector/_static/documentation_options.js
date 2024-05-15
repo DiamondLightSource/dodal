779e541d88e553b43cd4e7609a948c1fcb7bab5f
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2024-05-15 12:30:37 +0000

    deploy: f6a829e9a27569da79c2525a969d7a7651092b19

--- 473-create-a-device-for-xpress3-areadetector/_static/documentation_options.js
+++ 473-create-a-device-for-xpress3-areadetector/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.23.1.dev133+g67fcb8d54',
+    VERSION: '1.23.1.dev134+gf6a829e9a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
