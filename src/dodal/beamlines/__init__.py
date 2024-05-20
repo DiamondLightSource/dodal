7b7550d37492e33c504f74aa54f411ecb31dc87d
Callum Forrester <callum.forrester@diamond.ac.uk>
2024-05-20 11:01:57 +0100

    Allow for multiple beamlines per dodal module

--- src/dodal/beamlines/__init__.py
+++ src/dodal/beamlines/__init__.py
@@ -1,6 +1,7 @@
 import importlib.util
+from functools import lru_cache
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Mapping
 
 # Where beamline names (per the ${BEAMLINE} environment variable don't always
 # match up, we have to map between them bidirectionally). The most common use case is
@@ -10,9 +11,6 @@ from typing import Iterable
 _BEAMLINE_NAME_OVERRIDES = {
     "i04-1": "i04_1",
     "i20-1": "i20_1",
-}
-_MODULE_NAME_OVERRIDES = {
-    beamline: module for module, beamline in _BEAMLINE_NAME_OVERRIDES.items()
 }
 
 
@@ -41,8 +39,27 @@ def all_beamline_names() -> Iterable[str]:
     Returns:
         Iterable[str]: Generator of beamline names that dodal supports
     """
+    inverse_mapping = _module_name_overrides()
     for module_name in all_beamline_modules():
-        yield _MODULE_NAME_OVERRIDES.get(module_name, module_name)
+        yield from inverse_mapping.get(module_name, {module_name})
+
+
+@lru_cache
+def _module_name_overrides() -> Mapping[str, set[str]]:
+    """
+    Get the inverse of _BEAMLINE_NAME_OVERRIDES so that modules can be mapped back to
+    beamlines. _BEAMLINE_NAME_OVERRIDES is expected to be a constant so the return
+    value is cached.
+
+    Returns:
+        Mapping[str, set[str]]: A dictionary mapping the name of a dodal module to the
+        set of beamlines it supports.
+    """
+
+    inverse_mapping: dict[str, set[str]] = {}
+    for beamline, module in _BEAMLINE_NAME_OVERRIDES.items():
+        inverse_mapping[module] = inverse_mapping.get(module, set()).union({beamline})
+    return inverse_mapping
 
 
 def module_name_for_beamline(beamline: str) -> str:
