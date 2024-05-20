723442ed8c03ecfb314ca2ca1e098b54795c9b93
dperl-dls <dperl-dls@users.noreply.github.com>
2024-05-20 16:10:30 +0000

    deploy: f0debc35e3d8020a5b552c9018a003fbe1922cb4

--- 551_fix_epicsmotor_mocking_for_tests/_static/documentation_options.js
+++ 551_fix_epicsmotor_mocking_for_tests/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.24.1.dev2+gd0fa4b048',
+    VERSION: '1.24.1.dev4+gf0debc35e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
