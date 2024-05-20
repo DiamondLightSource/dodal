5e827be570fc03dcdc59d8c5e6c5e750b85bf127
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-05-20 13:58:00 +0100

    Fix variable name

--- src/dodal/devices/slits/i24_slits_04_virtual_motors.py
+++ src/dodal/devices/slits/i24_slits_04_virtual_motors.py
@@ -9,7 +9,7 @@ class I24Slits04VirtualMotors(GapAndCenterSlit2d):
     x_center: Device = Component(SlitMotor, "X:CENTRE")
     x_size: Device = Component(SlitMotor, "X:SIZE")
 
-    x_center: Device = Component(SlitMotor, "Y:CENTRE")
+    y_center: Device = Component(SlitMotor, "Y:CENTRE")
     y_size: Device = Component(SlitMotor, "Y:SIZE")
 
     def set(self, position_tuple):
