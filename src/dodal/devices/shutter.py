e3ff34796f7d38e4e56aea99a9cbadbd72c03974
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-20 11:10:27 +0100

    fix import

--- src/dodal/devices/shutter.py
+++ src/dodal/devices/shutter.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from bluesky.protocols import Movable
-from ophyd_async.core import AsyncStatus, StandardReadable
+from ophyd_async.core import StandardReadable
 from ophyd_async.epics.signal import epics_signal_rw
 
 
