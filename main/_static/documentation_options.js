584417f32a95b4b1dbaf5aece6243fa41b4a0515
callumforrester <callumforrester@users.noreply.github.com>
2024-05-20 12:42:56 +0000

    deploy: 26342fff5195df4a61f791fd25bfcd1df66d37fa

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.24.1.dev1+gb8bed6671',
+    VERSION: '1.24.1.dev2+g26342fff5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
