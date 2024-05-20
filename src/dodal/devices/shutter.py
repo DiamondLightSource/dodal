e0d78a12a4a888f59de8c58919c915606b3d0e45
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-20 16:32:52 +0000

    more feedback included

--- src/dodal/devices/shutter.py
+++ src/dodal/devices/shutter.py
@@ -8,6 +8,7 @@ from ophyd_async.epics.signal import epics_signal_r, epics_signal_w
 class ShutterSetState(str, Enum):
     CLOSED = "close"
     OPEN = "open"
+    RESET = "reset"
 
 
 class ShutterState(str, Enum):
@@ -20,19 +21,22 @@ class ShutterState(str, Enum):
 
 class Shutter(StandardReadable, Movable):
     def __init__(self, prefix: str, name: str):
+        self.position_set = epics_signal_w(
+            write_pv=prefix + "CTRL2",
+            datatype=ShutterSetState,
+        )
         with self.add_children_as_readables():
             self.position_readback = epics_signal_r(
                 read_pv=prefix + "STA",
                 datatype=ShutterState,
             )
-            self.position_set = epics_signal_w(
-                write_pv=prefix + "CTRL2",
-                datatype=ShutterSetState,
-            )
         super().__init__(name=name)
 
     @AsyncStatus.wrap
     async def set(self, position: ShutterSetState):
+        if position == ShutterSetState.RESET:
+            return
+        assert position in ShutterState
         new_position = ShutterState(position)
         await self.position_set.set(position)
         return await wait_for_value(
