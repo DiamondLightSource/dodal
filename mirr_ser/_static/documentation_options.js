2dce7b7578f6da5ad5a075284d7240d03d07ba03
keithralphs <keithralphs@users.noreply.github.com>
2024-05-21 12:20:24 +0000

    deploy: 74ddf167e6a365bec0caae7628e05ffcfcd88fd1

--- mirr_ser/_static/documentation_options.js
+++ mirr_ser/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.24.1.dev2+gbba048e42',
+    VERSION: '1.25.1.dev1+g74ddf167e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
