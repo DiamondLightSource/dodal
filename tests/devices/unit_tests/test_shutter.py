4e861a38460246e5a0837e81e81ebde40000c28c
Stanisław Malinowski <56644812+stan-dot@users.noreply.github.com>
2024-05-21 11:01:54 +0100

    Update tests/devices/unit_tests/test_shutter.py
    Co-authored-by: DiamondJoseph <53935796+DiamondJoseph@users.noreply.github.com>

--- tests/devices/unit_tests/test_shutter.py
+++ tests/devices/unit_tests/test_shutter.py
@@ -22,11 +22,10 @@ async def sim_shutter():
     ],
 )
 async def test_set_opens_and_closes_shutter(state: ShutterState, sim_shutter: Shutter):
-    status = sim_shutter.set(state)
+    await sim_shutter.set(state)
     reading = await sim_shutter.read()
 
     shutter_position = reading.get("shutter-position", {})
     assert (
         shutter_position["value"] is state
     ), f"Unexpected value: {shutter_position['value']}"
-    await status
