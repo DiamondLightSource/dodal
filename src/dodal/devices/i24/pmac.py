ea97752bbfb3bb52d6f2b878917be7969f667684
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-05-21 10:54:33 +0100

    Tidy up PMAC motors and add a couple of tests

--- src/dodal/devices/i24/pmac.py
+++ src/dodal/devices/i24/pmac.py
@@ -1,50 +1,23 @@
-import asyncio
-from typing import Tuple
-
-from ophyd_async.core import AsyncStatus, StandardReadable
-from ophyd_async.core.signal import SignalRW, SignalX
-from ophyd_async.core.signal_backend import SignalBackend
-from ophyd_async.core.utils import DEFAULT_TIMEOUT
+from bluesky.protocols import Triggerable
+from ophyd_async.core import StandardReadable
+from ophyd_async.core.signal import SignalRW
 from ophyd_async.epics.motion import Motor
 from ophyd_async.epics.signal import epics_signal_rw
 
 
-class PMACStringHome(SignalX):
+class PMACStringHome(Triggerable):
     def __init__(
         self,
         pmac_str_sig: SignalRW,
         string_to_send: str,
-        backend: SignalBackend,
-        timeout: float | None = DEFAULT_TIMEOUT,
         name: str = "",
     ) -> None:
         self.signal = pmac_str_sig
         self.cmd_string = string_to_send
-        super().__init__(backend, timeout, name)
+        super().__init__(name)
 
     async def trigger(self):
         await self.signal.set(self.cmd_string)
-
-
-class StageMotors(StandardReadable):
-    def __init__(self, prefix: str, name: str = "") -> None:
-        self.x = Motor(prefix + "X")
-        self.y = Motor(prefix + "Y")
-        self.z = Motor(prefix + "Z")
-        super().__init__(name)
-
-    async def _move_stages(self, position: Tuple[float]):
-        if len(position) == 2:
-            await asyncio.gather(self.x._move(position[0]), self.y._move(position[1]))
-        if len(position) == 3:
-            await asyncio.gather(
-                self.x._move(position[0]),
-                self.y._move(position[1]),
-                self.z._move(position[2]),
-            )
-
-    def set(self, position) -> AsyncStatus:
-        return AsyncStatus(self._move_stages(position))
 
 
 class PMAC(StandardReadable):  # Should it also be a movable?
@@ -56,6 +29,8 @@ class PMAC(StandardReadable):  # Should it also be a movable?
         #     self.pmac_string, r"\#1hmz\#2hmz\#3hmz", backend=SoftSignalBackend(str)
         # )
 
-        self.stages = StageMotors(prefix)
+        self.x = Motor(prefix + "X")
+        self.y = Motor(prefix + "Y")
+        self.z = Motor(prefix + "Z")
 
         super().__init__(name)
