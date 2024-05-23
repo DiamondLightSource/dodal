e9d374be6190fdd3a37b229bfdebc7a804bc3510
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-23 16:40:06 +0000

    move to as zebra controlled shutter

---
+++ src/dodal/devices/zebra_controlled_shutter.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from bluesky.protocols import Movable
+from ophyd_async.core import AsyncStatus, StandardReadable, wait_for_value
+from ophyd_async.epics.signal import epics_signal_r, epics_signal_w
+
+
+class ZebraShutterState(str, Enum):
+    CLOSED = "Close"
+    OPEN = "Open"
+
+
+class ZebraShutter(StandardReadable, Movable):
+    def __init__(self, prefix: str, name: str):
+        self.position_set = epics_signal_w(
+            write_pv=prefix + "CTRL2",
+            datatype=ZebraShutterState,
+        )
+        with self.add_children_as_readables():
+            self.position_readback = epics_signal_r(
+                read_pv=prefix + "STA",
+                datatype=ZebraShutterState,
+            )
+        super().__init__(name=name)
+
+    @AsyncStatus.wrap
+    async def set(self, desired_position: ZebraShutterState):
+        await self.position_set.set(desired_position)
+        return await wait_for_value(
+            signal=self.position_readback, match=desired_position, timeout=50
+        )
--- tests/devices/unit_tests/test_shutter.py
+++ tests/devices/unit_tests/test_shutter.py
@@ -1,24 +1,28 @@
 import pytest
 from ophyd_async.core import DeviceCollector, callback_on_mock_put, set_mock_value
 
-from dodal.devices.shutter import Shutter, ShutterSetState, ShutterState
+from dodal.devices.zebra_controlled_shutter import (
+    ShutterSetState,
+    ZebraShutter,
+    ZebraShutterState,
+)
 
 
 @pytest.fixture
 async def sim_shutter():
     async with DeviceCollector(mock=True):
-        sim_shutter: Shutter = Shutter(
+        sim_shutter: ZebraShutter = ZebraShutter(
             prefix="sim_shutter",
             name="shutter",
         )
     return sim_shutter
 
 
-async def test_set_shutter_open(sim_shutter: Shutter):
+async def test_set_shutter_open(sim_shutter: ZebraShutter):
     desired_state: ShutterSetState = ShutterSetState.OPEN
 
     def set_correct_mock_value(*args, **kwargs):
-        set_mock_value(sim_shutter.position_readback, ShutterState.OPEN)
+        set_mock_value(sim_shutter.position_readback, ZebraShutterState.OPEN)
 
     callback_on_mock_put(sim_shutter.position_set, set_correct_mock_value)
     await sim_shutter.set(desired_state)
@@ -26,15 +30,15 @@ async def test_set_shutter_open(sim_shutter: Shutter):
 
     shutter_position = reading.get("shutter-position_readback", {})
     assert (
-        shutter_position["value"] is ShutterState.OPEN
+        shutter_position["value"] is ZebraShutterState.OPEN
     ), f"Unexpected value: {shutter_position['value']}"
 
 
-async def test_set_shutter_close(sim_shutter: Shutter):
+async def test_set_shutter_close(sim_shutter: ZebraShutter):
     desired_state: ShutterSetState = ShutterSetState.CLOSE
 
     def set_correct_mock_value(*args, **kwargs):
-        set_mock_value(sim_shutter.position_readback, ShutterState.CLOSED)
+        set_mock_value(sim_shutter.position_readback, ZebraShutterState.CLOSED)
 
     callback_on_mock_put(sim_shutter.position_set, set_correct_mock_value)
     await sim_shutter.set(desired_state)
@@ -42,5 +46,5 @@ async def test_set_shutter_close(sim_shutter: Shutter):
 
     shutter_position = reading.get("shutter-position_readback", {})
     assert (
-        shutter_position["value"] is ShutterState.CLOSED
+        shutter_position["value"] is ZebraShutterState.CLOSED
     ), f"Unexpected value: {shutter_position['value']}"
