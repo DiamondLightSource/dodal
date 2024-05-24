59bf92dbb22f9ecc9e8210da6aa52c2bba229f2e
joeshannon <joeshannon@users.noreply.github.com>
2024-05-24 19:04:07 +0000

    deploy: 1dd9a8e81706ae10c331b07e576bc3b3d6d190ce

--- 566-fswitch-config-fields/_static/documentation_options.js
+++ 566-fswitch-config-fields/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.25.1.dev7+g785ad48d8',
+    VERSION: '1.25.1.dev8+g1dd9a8e81',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
