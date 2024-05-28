c34e8e84911d3bffb193bed39b793b2b31116966
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-05-28 08:27:42 +0100

    update test shutter to use mock callback in fixtures@

--- tests/devices/unit_tests/test_shutter.py
+++ tests/devices/unit_tests/test_shutter.py
@@ -10,40 +10,26 @@ from dodal.devices.zebra_controlled_shutter import (
 @pytest.fixture
 async def sim_shutter():
     async with DeviceCollector(mock=True):
-        sim_shutter: ZebraShutter = ZebraShutter(
+        sim_shutter = ZebraShutter(
             prefix="sim_shutter",
             name="shutter",
         )
+
+        def propagate_status(value: ZebraShutterState):
+            set_mock_value(sim_shutter.position_readback, value)
+
+        callback_on_mock_put(sim_shutter.position_set, propagate_status)
     return sim_shutter
 
 
-async def test_set_shutter_open(sim_shutter: ZebraShutter):
-    desired_state = ZebraShutterState.OPEN
-
-    def set_correct_mock_value(*args, **kwargs):
-        set_mock_value(sim_shutter.position_readback, ZebraShutterState.OPEN)
-
-    callback_on_mock_put(sim_shutter.position_set, set_correct_mock_value)
-    await sim_shutter.set(desired_state)
+@pytest.mark.parametrize("new_state", [ZebraShutterState.OPEN, ZebraShutterState.CLOSE])
+async def test_set_shutter_open(
+    sim_shutter: ZebraShutter, new_state: ZebraShutterState
+):
+    await sim_shutter.set(new_state)
     reading = await sim_shutter.read()
 
     shutter_position = reading.get("shutter-position_readback", {})
     assert (
-        shutter_position["value"] is ZebraShutterState.OPEN
-    ), f"Unexpected value: {shutter_position['value']}"
-
-
-async def test_set_shutter_close(sim_shutter: ZebraShutter):
-    desired_state = ZebraShutterState.CLOSE
-
-    def set_correct_mock_value(*args, **kwargs):
-        set_mock_value(sim_shutter.position_readback, ZebraShutterState.CLOSE)
-
-    callback_on_mock_put(sim_shutter.position_set, set_correct_mock_value)
-    await sim_shutter.set(desired_state)
-    reading = await sim_shutter.read()
-
-    shutter_position = reading.get("shutter-position_readback", {})
-    assert (
-        shutter_position["value"] is ZebraShutterState.CLOSE
+        shutter_position["value"] is new_state
     ), f"Unexpected value: {shutter_position['value']}"
