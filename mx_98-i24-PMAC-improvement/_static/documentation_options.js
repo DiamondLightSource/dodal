126c677bce591a8cf39a5d447c376dc706f7a19b
noemifrisina <noemifrisina@users.noreply.github.com>
2024-05-28 14:21:09 +0000

    deploy: f88e1ed8e3ef618589a7ac0ab367894e31b889a4

--- mx_98-i24-PMAC-improvement/_static/documentation_options.js
+++ mx_98-i24-PMAC-improvement/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.25.1.dev21+g68c773503',
+    VERSION: '1.25.1.dev22+gf88e1ed8e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
