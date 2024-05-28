e6b911412924ecc333e756afaa07b68a031f4ab4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-05-28 13:44:56 +0100

    Simplify and avoid code repetition

--- src/dodal/devices/i24/pmac.py
+++ src/dodal/devices/i24/pmac.py
@@ -8,7 +8,7 @@ HOME_STR = r"\#1hmz\#2hmz\#3hmz"
 ZERO_STR = "!x0y0z0"
 
 
-class PMACStringHome(Triggerable):
+class PMACStringMove(Triggerable):
     def __init__(
         self,
         pmac_str_sig: SignalRW,
@@ -22,26 +22,16 @@ class PMACStringHome(Triggerable):
         await self.signal.set(self.cmd_string, wait=True)
 
 
-class PMACMoveToZero(Triggerable):
-    def __init__(self, pmac_str_sig: SignalRW, string_to_send: str) -> None:
-        self.signal = pmac_str_sig
-        self.cmd_string = string_to_send
-
-    @AsyncStatus.wrap
-    async def trigger(self):
-        await self.signal.set(self.cmd_string, wait=True)
-
-
 class PMAC(StandardReadable):
     """Device to control the chip stage on I24."""
 
     def __init__(self, prefix: str, name: str = "") -> None:
         self.pmac_string = epics_signal_rw(str, prefix + "PMAC_STRING")
-        self.home = PMACStringHome(
+        self.home = PMACStringMove(
             self.pmac_string,
             HOME_STR,
         )
-        self.to_xyz_zero = PMACMoveToZero(self.pmac_string, ZERO_STR)
+        self.to_xyz_zero = PMACStringMove(self.pmac_string, ZERO_STR)
 
         self.x = Motor(prefix + "X")
         self.y = Motor(prefix + "Y")
