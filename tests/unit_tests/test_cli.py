b02f6c12ce0b45b0d0bc106b60ba1b7351c52c4e
Callum Forrester <callum.forrester@diamond.ac.uk>
2024-05-29 13:06:54 +0100

    Fix CLI tests

--- tests/unit_tests/test_cli.py
+++ tests/unit_tests/test_cli.py
@@ -3,9 +3,11 @@ from unittest.mock import patch
 
 import pytest
 from click.testing import CliRunner, Result
+from ophyd_async.core import Device, NotConnected
 
 from dodal import __version__
 from dodal.cli import main
+from dodal.utils import AnyDevice
 
 # Test with an example beamline, device instantiation is already tested
 # in beamline unit tests
@@ -27,37 +29,81 @@ def test_cli_version(runner: CliRunner):
     assert result.stdout == f"{__version__}\n"
 
 
+ALL_SUCCESSFUL_DEVICES: tuple[dict[str, AnyDevice], dict[str, Exception]] = (
+    {f"device_{i}": Device() for i in range(6)},
+    {},
+)
+
+SOME_SUCCESSFUL_DEVICES: tuple[dict[str, AnyDevice], dict[str, Exception]] = (
+    {f"device_{i}": Device() for i in range(3)},
+    {f"device_{i}": TimeoutError() for i in range(3, 6)},
+)
+
+NO_SUCCESSFUL_DEVICES: tuple[dict[str, AnyDevice], dict[str, Exception]] = (
+    {},
+    {f"device_{i}": TimeoutError() for i in range(6)},
+)
+
+
 def test_cli_sets_beamline_environment_variable(runner: CliRunner):
     with patch.dict(os.environ, clear=True):
-        _mock_connect(EXAMPLE_BEAMLINE, runner=runner)
+        _mock_connect(
+            EXAMPLE_BEAMLINE,
+            runner=runner,
+            devices=ALL_SUCCESSFUL_DEVICES,
+        )
         assert os.environ["BEAMLINE"] == EXAMPLE_BEAMLINE
 
 
+#
 # We need to mock the environment because the CLI edits it and this could break other
 # tests
+#
+
+
 @patch.dict(os.environ, clear=True)
 def test_cli_connect(runner: CliRunner):
-    result = _mock_connect(EXAMPLE_BEAMLINE, runner=runner)
-    assert "3 devices connected" in result.stdout
-    assert "3 devices failed" in result.stdout
+    result = _mock_connect(
+        EXAMPLE_BEAMLINE,
+        runner=runner,
+        devices=ALL_SUCCESSFUL_DEVICES,
+    )
+    assert "6 devices connected" in result.stdout
 
 
-# We need to mock the environment because the CLI edits it and this could break other
-# tests
 @patch.dict(os.environ, clear=True)
 def test_cli_connect_in_sim_mode(runner: CliRunner):
-    result = _mock_connect("-s", EXAMPLE_BEAMLINE, runner=runner)
-    assert "3 devices connected (sim mode)" in result.stdout
-    assert "3 devices failed (sim mode)" in result.stdout
+    result = _mock_connect(
+        "-s",
+        EXAMPLE_BEAMLINE,
+        runner=runner,
+        devices=ALL_SUCCESSFUL_DEVICES,
+    )
+    assert "6 devices connected (sim mode)" in result.stdout
 
 
-def _mock_connect(*args, runner: CliRunner) -> Result:
+@patch.dict(os.environ, clear=True)
+@pytest.mark.parametrize("devices", [SOME_SUCCESSFUL_DEVICES, NO_SUCCESSFUL_DEVICES])
+def test_cli_connect_when_devices_error(
+    runner: CliRunner,
+    devices: tuple[dict[str, AnyDevice], dict[str, Exception]],
+):
+    with pytest.raises(NotConnected):
+        _mock_connect(
+            EXAMPLE_BEAMLINE,
+            runner=runner,
+            devices=SOME_SUCCESSFUL_DEVICES,
+        )
+
+
+def _mock_connect(
+    *args,
+    runner: CliRunner,
+    devices: tuple[dict[str, AnyDevice], dict[str, Exception]] = ({}, {}),
+) -> Result:
     with patch(
         "dodal.cli.make_all_devices",
-        return_value=(
-            {f"device_{i}": object() for i in range(3)},
-            {f"device_{i}": TimeoutError() for i in range(3, 6)},
-        ),
+        return_value=devices,
     ):
         result = runner.invoke(
             main,
