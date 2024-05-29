d008a145a129c398aa6b90a3dbb293ca9fbda58b
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-05-29 14:19:48 +0100

    Add linkam3 (#577)
    Adds a linkam device, adds the device to both i22 and p38, and adds some very basic unit tests.

--- src/dodal/beamlines/p38.py
+++ src/dodal/beamlines/p38.py
@@ -13,6 +13,7 @@ from dodal.common.beamlines.device_helpers import numbered_slits
 from dodal.common.visit import LocalDirectoryServiceClient, StaticVisitDirectoryProvider
 from dodal.devices.focusing_mirror import FocusingMirror
 from dodal.devices.i22.fswitch import FSwitch
+from dodal.devices.linkam3 import Linkam3
 from dodal.devices.slits import Slits
 from dodal.devices.tetramm import TetrammDetector
 from dodal.devices.undulator import Undulator
@@ -274,4 +275,17 @@ def panda3(
         wait_for_connection,
         fake_with_ophyd_sim,
         directory_provider=get_directory_provider(),
+    )
+
+
+@skip_device
+def linkam(
+    wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
+) -> Linkam3:
+    return device_instantiation(
+        Linkam3,
+        "linkam",
+        "-EA-LINKM-02:",
+        wait_for_connection,
+        fake_with_ophyd_sim,
     )
