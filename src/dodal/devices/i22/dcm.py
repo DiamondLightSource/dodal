bbc4922dd86c1fed18792f7a9e33f66fa28d4ec4
Callum Forrester <callum.forrester@diamond.ac.uk>
2024-06-04 15:06:14 +0100

    Add DCM tests

--- src/dodal/devices/i22/dcm.py
+++ src/dodal/devices/i22/dcm.py
@@ -1,8 +1,22 @@
+from dataclasses import dataclass
 from typing import Literal
 
 from ophyd_async.core import ConfigSignal, StandardReadable, soft_signal_r_and_setter
 from ophyd_async.epics.motion import Motor
 from ophyd_async.epics.signal import epics_signal_r
+
+
+@dataclass
+class CrystalMetadata:
+    """
+    Metadata used in the NeXus format,
+    see https://manual.nexusformat.org/classes/base_classes/NXcrystal.html
+    """
+
+    usage: Literal["Bragg", "Laue"] | None = None
+    type: str | None = None
+    reflection: tuple[int, int, int] | None = None
+    d_spacing: float | None = None
 
 
 class MonochromatingCrystal(StandardReadable):
@@ -14,50 +28,47 @@ class MonochromatingCrystal(StandardReadable):
     def __init__(
         self,
         prefix: str,
+        metadata: CrystalMetadata,
         temp_prefix: str = "",
         heater_temp_prefix: str = "",
-        usage: Literal["Bragg", "Laue"] | None = None,
-        type: str | None = None,
-        reflection: tuple[int, int, int] | None = None,
-        d_spacing: float | None = None,
         name: str = "",
     ) -> None:
         with self.add_children_as_readables():
             # Positionable Parameters
-            self.roll = Motor(prefix + "ROLL")
+            # self.roll = Motor(prefix + "ROLL")
             self.pitch = Motor(prefix + "PITCH")
 
-            # Temperatures
+            # # Temperatures
             self.temp = epics_signal_r(float, temp_prefix)
             self.heater_temp = epics_signal_r(float, heater_temp_prefix)
 
-            # Soft metadata
-            # If supplied include crystal details in output of read_configuration
-            with self.add_children_as_readables(ConfigSignal):
-                if usage is not None:
-                    self.crystal_usage, _ = soft_signal_r_and_setter(
-                        str, initial_value=usage
-                    )
-                else:
-                    self.crystal_usage = None
-                if type is not None:
-                    self.crystal_type, _ = soft_signal_r_and_setter(
-                        str, initial_value=type
-                    )
-                else:
-                    self.crystal_type = None
-                if reflection is not None:
-                    self.crystal_reflection, _ = soft_signal_r_and_setter(
-                        str, initial_value=reflection
-                    )
-                else:
-                    self.crystal_reflection = None
-                if d_spacing is not None:
-                    self.crystal_d_spacing, _ = soft_signal_r_and_setter(
-                        str, initial_value=d_spacing
-                    )
-                else:
-                    self.crystal_d_spacing = None
+        # Soft metadata
+        # If supplied include crystal details in output of read_configuration
+        with self.add_children_as_readables(ConfigSignal):
+            if metadata.usage is not None:
+                self.usage, _ = soft_signal_r_and_setter(
+                    str, initial_value=metadata.usage
+                )
+            else:
+                self.usage = None
+            if metadata.type is not None:
+                self.type, _ = soft_signal_r_and_setter(
+                    str, initial_value=metadata.type
+                )
+            else:
+                self.type = None
+            if metadata.reflection is not None:
+                self.reflection, _ = soft_signal_r_and_setter(
+                    str, initial_value=metadata.reflection
+                )
+            else:
+                self.reflection = None
+            if metadata.d_spacing is not None:
+                self.d_spacing, _ = soft_signal_r_and_setter(
+                    str, initial_value=metadata.d_spacing
+                )
+            else:
+                self.d_spacing = None
 
         super().__init__(name)
 
@@ -75,6 +86,8 @@ class DoubleCrystalMonochromator(StandardReadable):
     def __init__(
         self,
         prefix: str,
+        crystal_1_metadata: CrystalMetadata | None = None,
+        crystal_2_metadata: CrystalMetadata | None = None,
         name: str = "",
     ) -> None:
         with self.add_children_as_readables():
@@ -82,11 +95,13 @@ class DoubleCrystalMonochromator(StandardReadable):
             self.bragg = Motor(prefix + "BRAGG")
             self.crystal_1 = MonochromatingCrystal(
                 prefix + "XSTAL1:",
+                metadata=crystal_1_metadata or CrystalMetadata(),
                 temp_prefix=prefix + "PT100-1",
                 heater_temp_prefix=prefix + "PT100-2",
             )
             self.crystal_2 = MonochromatingCrystal(
                 prefix + "XSTAL2:",
+                metadata=crystal_2_metadata or CrystalMetadata(),
                 temp_prefix=prefix + "PT100-4",
                 heater_temp_prefix=prefix + "PT100-5",
             )
---
+++ tests/devices/i22/test_dcm.py
@@ -0,0 +1,264 @@
+from unittest.mock import ANY
+
+import pytest
+from bluesky.protocols import Reading
+from ophyd_async.core import DeviceCollector, assert_configuration, assert_reading
+
+from dodal.devices.i22.dcm import (
+    CrystalMetadata,
+    DoubleCrystalMonochromator,
+    MonochromatingCrystal,
+)
+
+
+@pytest.fixture
+async def dcm() -> DoubleCrystalMonochromator:
+    async with DeviceCollector(mock=True):
+        dcm = DoubleCrystalMonochromator(
+            "FOO",
+            crystal_1_metadata=CrystalMetadata(
+                usage="Bragg",
+                type="silicon",
+                reflection=(1, 1, 1),
+                d_spacing=3.13475,
+            ),
+            crystal_2_metadata=CrystalMetadata(
+                usage="Bragg",
+                type="silicon",
+                reflection=(1, 1, 1),
+                d_spacing=3.13475,
+            ),
+        )
+
+    return dcm
+
+
+@pytest.mark.parametrize(
+    "metadata,expected_absent",
+    [
+        (
+            CrystalMetadata(),
+            {
+                "crystal-usage",
+                "crystal-type",
+                "crystal-reflection",
+                "crystal-d_spacing",
+            },
+        ),
+        (
+            CrystalMetadata(usage="Bragg"),
+            {
+                "crystal-type",
+                "crystal-reflection",
+                "crystal-d_spacing",
+            },
+        ),
+        (
+            CrystalMetadata(usage="Bragg", reflection=[1, 1, 1]),
+            {
+                "crystal-type",
+                "crystal-d_spacing",
+            },
+        ),
+    ],
+)
+async def test_crystal_metadata_not_propagated_when_not_supplied(
+    metadata: CrystalMetadata,
+    expected_absent: set[str],
+):
+    async with DeviceCollector(mock=True):
+        crystal = MonochromatingCrystal("FOO", metadata=metadata)
+
+    configuration = await crystal.read_configuration()
+    assert expected_absent.isdisjoint(configuration)
+
+
+async def test_reading(dcm: DoubleCrystalMonochromator):
+    await assert_reading(
+        dcm,
+        {
+            "dcm-crystal_1-temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-heater_temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-pitch": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-energy": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-bragg": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-offset": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-perp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-backplate_temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-perp_temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-wavelength": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-heater_temp": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-pitch": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+        },
+    )
+
+
+async def test_configuration(dcm: DoubleCrystalMonochromator):
+    await assert_configuration(
+        dcm,
+        {
+            "dcm-wavelength-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-wavelength-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-pitch-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-pitch-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-usage": {
+                "value": "Bragg",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-reflection": {
+                "value": (1, 1, 1),
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-d_spacing": {
+                "value": 3.13475,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_1-type": {
+                "value": "silicon",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-offset-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-offset-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-perp-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-perp-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-pitch-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-pitch-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-usage": {
+                "value": "Bragg",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-reflection": {
+                "value": (1, 1, 1),
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-d_spacing": {
+                "value": 3.13475,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-crystal_2-type": {
+                "value": "silicon",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-energy-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-energy-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-bragg-velocity": {
+                "value": 0.0,
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+            "dcm-bragg-motor_egu": {
+                "value": "",
+                "timestamp": ANY,
+                "alarm_severity": ANY,
+            },
+        },
+    )
