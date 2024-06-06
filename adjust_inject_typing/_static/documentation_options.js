0639048a5bd4124c199671bc7a88b70e2bc9876a
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-06-06 12:04:06 +0000

    deploy: fb33a7abefbfd807ba3982d021c0f6a3976f3f39

--- adjust_inject_typing/_static/documentation_options.js
+++ adjust_inject_typing/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.26.1.dev5+gd03ebb252',
+    VERSION: '1.26.1.dev6+gfb33a7abe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
