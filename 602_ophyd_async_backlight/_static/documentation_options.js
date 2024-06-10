1a1d477524a5bd6cc740e0fc19ef7625f68e2538
DominicOram <DominicOram@users.noreply.github.com>
2024-06-10 07:31:58 +0000

    deploy: cbf45fa2334a78f7617536076f3674e5b9fe1e86

--- 602_ophyd_async_backlight/_static/documentation_options.js
+++ 602_ophyd_async_backlight/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.26.1.dev5+gdd3aae71d',
+    VERSION: '1.26.1.dev8+gcbf45fa23',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
