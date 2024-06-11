befbd53bb9ab008b06edaa05899823330d7e47dc
stan-dot <stan-dot@users.noreply.github.com>
2024-06-11 15:31:53 +0000

    deploy: 861e4096c7cb20d90ec7b7bb8afd29ab8375224f

---
+++ apply-DLS-device/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.27.1.dev2+g861e4096c',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
