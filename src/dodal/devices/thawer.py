2a9a891b211af052c4c9b4120bdba8304fe7c3f0
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-06-11 12:20:54 +0100

    Add timer to thawer

--- src/dodal/devices/thawer.py
+++ src/dodal/devices/thawer.py
@@ -1,7 +1,12 @@
+from asyncio import Task, create_task, sleep
 from enum import Enum
 
-from ophyd_async.core import StandardReadable
+from ophyd_async.core import AsyncStatus, StandardReadable, soft_signal_rw
 from ophyd_async.epics.signal import epics_signal_rw
+
+
+class ThawingException(Exception):
+    pass
 
 
 class ThawerStates(str, Enum):
@@ -12,4 +17,19 @@ class ThawerStates(str, Enum):
 class Thawer(StandardReadable):
     def __init__(self, prefix: str, name: str = "") -> None:
         self.control = epics_signal_rw(ThawerStates, prefix + ":CTRL")
+        self.thaw_time_s = soft_signal_rw(float, 10, name="thaw_time_s")
+        self.thawing_task: Task | None = None
         super().__init__(name)
+
+    @AsyncStatus.wrap
+    async def kickoff(self):
+        await self.control.set(ThawerStates.ON)
+        if self.thawing_task and not self.thawing_task.done:
+            raise ThawingException("Thawing task already in progress")
+        self.thawing_task = create_task(sleep(await self.thaw_time_s.get_value()))
+
+    @AsyncStatus.wrap
+    async def complete(self):
+        if not self.thawing_task:
+            raise ThawingException("Kickoff called before complete")
+        await self.thawing_task
