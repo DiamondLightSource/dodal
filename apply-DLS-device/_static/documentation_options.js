bed8aa06a890911ee3d4244f713b034b0250b9a9
stan-dot <stan-dot@users.noreply.github.com>
2024-06-12 13:49:24 +0000

    deploy: 80d7d4d19b877540cff4eba16ffb0aec6aa7bab9

--- apply-DLS-device/_static/documentation_options.js
+++ apply-DLS-device/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.27.1.dev4+g39c3361f9',
+    VERSION: '1.27.1.dev5+g80d7d4d19',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
