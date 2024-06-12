bc7b9a97d64c28be3214b2e0472500e6b6c47d23
stan-dot <stan-dot@users.noreply.github.com>
2024-06-12 15:48:50 +0000

    deploy: e90f489ce56889b6a3a4c461e936eb1052205db9

--- 578-add-a-flowchart-on-the-decisions-to-make-a-new-ophyd-device-for-dls/_static/documentation_options.js
+++ 578-add-a-flowchart-on-the-decisions-to-make-a-new-ophyd-device-for-dls/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.27.1.dev7+g281f77be2',
+    VERSION: '1.27.1.dev9+ge90f489ce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
