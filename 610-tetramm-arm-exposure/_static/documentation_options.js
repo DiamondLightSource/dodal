f28983738d79b61abe1453b5afd8154616c8f8d5
joeshannon <joeshannon@users.noreply.github.com>
2024-06-12 10:11:30 +0000

    deploy: d8f63c72d63f52aadb30f54dc369025aa7e36521

---
+++ 610-tetramm-arm-exposure/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.27.1.dev3+gd8f63c72d',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
