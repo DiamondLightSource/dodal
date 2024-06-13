5c625107dd865ed2deccde6c923d0007790e9763
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-06-13 17:01:24 +0100

    Add a test for shutter set

--- tests/devices/unit_tests/test_hutch_shutter.py
+++ tests/devices/unit_tests/test_hutch_shutter.py
@@ -1,11 +1,16 @@
+from typing import List
+from unittest.mock import call
+
+import bluesky.plan_stubs as bps
 import pytest
 from bluesky.run_engine import RunEngine
-from ophyd_async.core import set_mock_value
+from ophyd_async.core import callback_on_mock_put, get_mock_put, set_mock_value
 
 from dodal.devices.hutch_shutter import (
     HutchNotInterlockedError,
     HutchShutter,
     ShutterDemand,
+    ShutterState,
 )
 
 
@@ -15,10 +20,16 @@ async def fake_shutter() -> HutchShutter:
     shutter = HutchShutter("", name="fake_shutter")
     await shutter.connect(mock=True)
 
+    def set_status(value: ShutterDemand, *args, **kwargs):
+        value_sta = ShutterState.OPEN if value == "Open" else ShutterState.CLOSED
+        set_mock_value(shutter.status, value_sta)
+
+    callback_on_mock_put(shutter.control, set_status)
+
     return shutter
 
 
-def test_shutter_is_created(fake_shutter: HutchShutter):
+def test_shutter_can_be_created(fake_shutter: HutchShutter):
     assert isinstance(fake_shutter, HutchShutter)
 
 
@@ -30,3 +41,34 @@ async def test_shutter_raises_error_on_set_if_hutch_not_interlocked(
 
     with pytest.raises(HutchNotInterlockedError):
         await fake_shutter.set(ShutterDemand.CLOSE)
+
+
+@pytest.mark.parametrize(
+    "demand, expected_calls, expected_state",
+    [
+        (
+            ShutterDemand.OPEN,
+            [ShutterDemand.RESET, ShutterDemand.OPEN],
+            ShutterState.OPEN,
+        ),
+        (ShutterDemand.CLOSE, [ShutterDemand.CLOSE], ShutterState.CLOSED),
+    ],
+)
+async def test_shutter_operations(
+    demand: ShutterDemand,
+    expected_calls: List,
+    expected_state: ShutterState,
+    fake_shutter: HutchShutter,
+    RE,
+):
+    set_mock_value(fake_shutter.interlock.status, 0)
+
+    RE(bps.abs_set(fake_shutter, demand, wait=True))
+
+    assert await fake_shutter.status.get_value() == expected_state
+
+    call_list = []
+    for i in expected_calls:
+        call_list.append(call(i, wait=True, timeout=10.0))
+    mock_shutter_control = get_mock_put(fake_shutter.control)
+    mock_shutter_control.assert_has_calls(call_list)
