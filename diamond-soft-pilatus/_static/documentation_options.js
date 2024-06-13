70d2fe695397a45c833473735c4bd93549221d7b
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2024-06-13 16:10:43 +0000

    deploy: 80269bb16267d59a0f17529c117cf90ce9f05112

--- diamond-soft-pilatus/_static/documentation_options.js
+++ diamond-soft-pilatus/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.27.1.dev12+g197814b50',
+    VERSION: '1.27.1.dev13+g80269bb16',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
