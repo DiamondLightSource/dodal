4b955457b2376e2dfffac0ba2197869a60c8ef01
noemifrisina <noemifrisina@users.noreply.github.com>
2024-06-18 15:20:29 +0000

    deploy: 3dbaa01208914c8fd81758ffabf19d92ad61ff98

--- 611_hutch-shutter-device/_static/documentation_options.js
+++ 611_hutch-shutter-device/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.28.1.dev18+gb3d908b87',
+    VERSION: '1.28.1.dev21+g3dbaa0120',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
