8211c356c78f993d54c43c1798dfe32b7c81d284
DominicOram <DominicOram@users.noreply.github.com>
2024-06-18 16:33:18 +0000

    deploy: 30ce869a806a86b08ec8755fd7e5b2b5d3ed7218

--- 628_thumbnail_on_snapshots/_static/documentation_options.js
+++ 628_thumbnail_on_snapshots/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.28.1.dev7+g654205b58',
+    VERSION: '1.28.1.dev11+g30ce869a8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
