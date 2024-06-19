e4eaebf33daaf29fba5832244abf1e2794b5b617
rtuck99 <rtuck99@users.noreply.github.com>
2024-06-19 09:55:39 +0000

    deploy: 905e40a9d59aec57b1a871bc80fe6f5708ef9f1c

--- 229_convert_xbpm_feedback_to_ophyd_async/_static/documentation_options.js
+++ 229_convert_xbpm_feedback_to_ophyd_async/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.27.1.dev6+g63e6f7246',
+    VERSION: '1.28.1.dev16+g905e40a9d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
