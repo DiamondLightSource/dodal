fac99242b3d52058eae696eea3a9a1f14af80d5d
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2024-06-19 09:04:59 +0000

    deploy: 8e64834cf200f58c3148628147b5d46194b9d6d1

---
+++ 638-test_xspress3-timing-out/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.28.1.dev14+g8e64834cf',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
