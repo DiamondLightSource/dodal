935b68e0f6305cb30ef6f13e695d44be7f6bf32f
noemifrisina <noemifrisina@users.noreply.github.com>
2024-06-24 15:41:19 +0000

    deploy: 4710fa5918e4701b00b45f373c912e68955a61d3

--- mx_97-devices-for-i24-beamline-setup/_static/documentation_options.js
+++ mx_97-devices-for-i24-beamline-setup/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.28.1.dev41+g359719887',
+    VERSION: '1.28.1.dev42+g4710fa591',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
