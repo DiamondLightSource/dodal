f748a05d0a237ecc1067607d92260f573e42bdf8
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-07-02 13:14:58 +0100

    (#601) Remove vestigial limit checking features that were never used * Remove I23 gonio * Remove unused XYZLimitBundle and related classes * Remove validate_against_hardware as the original is_valid() method it is based on was never used

--- src/dodal/parameters/experiment_parameter_base.py
+++ src/dodal/parameters/experiment_parameter_base.py
@@ -1,26 +1,10 @@
 from abc import ABC, abstractmethod
-from collections.abc import Generator
-from typing import Any
 
-from bluesky import Msg
 from pydantic import BaseModel
 
 
 class AbstractExperimentParameterBase(BaseModel, ABC):
-    def validate_against_hardware(self, composite) -> Generator[Msg, Any, Any]:
-        """
-        Return a bluesky plan stub that can be called to validate the parameters against
-        beamline hardware (e.g. against motor limits).
-        Subclasses should override this if they need to perform such parameter validation.
-        In the event of the parameters being invalid, the plan should raise a ValueError
-
-        Args:
-            composite: A composite supplying devices required for validation.
-
-        Returns:
-            The plan stub generator
-        """
-        yield from iter([])
+    pass
 
 
 class AbstractExperimentWithBeamParams(AbstractExperimentParameterBase):
