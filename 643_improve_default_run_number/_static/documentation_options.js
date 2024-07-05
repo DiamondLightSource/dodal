f4ee8adf33721bea72dec275e742164099842212
dperl-dls <dperl-dls@users.noreply.github.com>
2024-07-05 09:52:54 +0000

    deploy: 5e3f7ef72ed1ae39a0a4983ef5a545c16ac5c8c0

--- 643_improve_default_run_number/_static/documentation_options.js
+++ 643_improve_default_run_number/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.29.2.dev10+g73b4adf38',
+    VERSION: '1.29.2.dev10+g5e3f7ef72',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
