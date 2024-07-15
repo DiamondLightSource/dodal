72d92f7094a82e11af7cfce136dd0beb19a2f3c0
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-15 15:28:52 +0100

    initial draft

--- src/dodal/devices/slits.py
+++ src/dodal/devices/slits.py
@@ -15,3 +15,17 @@ class Slits(StandardReadable):
             self.x_centre = Motor(prefix + "X:CENTRE")
             self.y_centre = Motor(prefix + "Y:CENTRE")
         super().__init__(name)
+
+
+class SpecialSlitsi22(StandardReadable):
+    """
+    TODO uncertain
+    """
+
+    def __init__(self, prefix: str, name: str = "") -> None:
+        with self.add_children_as_readables():
+            self.x_plus = Motor(prefix + "X:PLUS")
+            self.x_minus = Motor(prefix + "Y:MINUS")
+            self.y_plus = Motor(prefix + "X:PLUS")
+            self.y_minus = Motor(prefix + "Y:MINUS")
+        super().__init__(name)
