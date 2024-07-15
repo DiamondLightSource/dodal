15b437914ad970c9c9adce6ec826aaaa286eb48f
Robert Tuck <robert.tuck@diamond.ac.uk>
2024-07-15 14:52:18 +0100

    (DiamondLightSource/hyperion#1464) Change topup gating to prevent gating with long exposures

--- tests/plans/test_topup_plan.py
+++ tests/plans/test_topup_plan.py
@@ -34,7 +34,7 @@ def test_when_topup_before_end_of_collection_wait(
             ops_time=30.0,
         )
     )
-    fake_sleep.assert_called_once_with(60.0)
+    fake_sleep.assert_called_once_with(61.0)
 
 
 @patch("dodal.plans.check_topup.bps.rd")
@@ -92,3 +92,58 @@ def test_no_waiting_when_mode_does_not_allow_gating(
         )
     )
     fake_null.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "topup_start_countdown, topup_end_countdown, total_exposure_time, ops_time,"
+    "expected_wait, parameter_file",
+    # limit = 120, delay = 1
+    [
+        (100, 108, 121, 1, 0, "test_beamline_parameters.txt"),
+        (100, 108, 119, 1, 108 + 1, "test_beamline_parameters.txt"),
+        (110, 120, 120, 1, 0, "test_beamline_parameters.txt"),
+        (110.1, 120, 119.99, 1, 120 + 1, "test_beamline_parameters.txt"),
+        # limit = 35, delay = 1
+        (110.1, 120, 36, 1, 0, "topup_short_params.txt"),
+        (36, 42, 36, 1, 0, "topup_short_params.txt"),
+        (36, 42, 34, 4, 42 + 1, "topup_short_params.txt"),
+        (36, 42, 34, 2, 0, "topup_short_params.txt"),
+        (36, 42, 35, 3, 0, "topup_short_params.txt"),
+        (31.1, 39, 31, 1, 39 + 1, "topup_short_params.txt"),
+        (30.1, 38, 31, 1, 38 + 1, "topup_short_params.txt"),
+        # limit = 30, delay = 19
+        (30, 40, 29, 1, 0, "topup_long_delay.txt"),
+        (29, 39, 29, 1, 39 + 19, "topup_long_delay.txt"),
+        (29, 39, 28, 1, 0, "topup_long_delay.txt"),
+        (29, 39, 28, 2, 39 + 19, "topup_long_delay.txt"),
+        (29, 39, 27, 2, 0, "topup_long_delay.txt"),
+        (29, 39, 35, 1, 0, "topup_long_delay.txt"),
+    ],
+)
+@patch("dodal.plans.check_topup.bps.sleep")
+def test_topup_not_allowed_when_exceeds_threshold_percentage_of_topup_time(
+    mock_sleep,
+    RE: RunEngine,
+    synchrotron: Synchrotron,
+    topup_start_countdown: float,
+    topup_end_countdown: float,
+    total_exposure_time: float,
+    ops_time: float,
+    expected_wait: float,
+    parameter_file: str,
+):
+    set_mock_value(synchrotron.synchrotron_mode, SynchrotronMode.USER)
+    set_mock_value(synchrotron.top_up_start_countdown, topup_start_countdown)
+    set_mock_value(synchrotron.top_up_end_countdown, topup_end_countdown)
+
+    with patch(
+        "dodal.common.beamlines.beamline_parameters.BEAMLINE_PARAMETER_PATHS",
+        {"s03": f"tests/test_data/{parameter_file}"},
+    ):
+        RE(
+            check_topup_and_wait_if_necessary(
+                synchrotron, total_exposure_time, ops_time
+            )
+        )
+
+    mock_sleep.assert_called_with(expected_wait)
