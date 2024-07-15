e35937ec7ba6e425eb8e8add68e4207813977ba3
rtuck99 <rtuck99@users.noreply.github.com>
2024-07-15 15:34:28 +0000

    deploy: 50a4c60a24373afb1a1eeadc34d2eb52f575422b

---
+++ hyperion_1464_rotation_scans_timeout_at_long_exposures/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.29.4.dev3+g50a4c60a2',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
