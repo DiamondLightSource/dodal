28bcad164beae6c3b7e6f843ff99f8c3c6f0d762
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-26 13:19:48 +0000

    fix example beamline into a dataclass

--- src/dodal/beamlines/example_beamline.py
+++ src/dodal/beamlines/example_beamline.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from dataclasses import dataclass
 
 from bluesky.run_engine import call_in_bluesky_event_loop
 from ophyd_async.core import DEFAULT_TIMEOUT
@@ -20,6 +21,7 @@ class XYZDetector(OphydV2Device):
         raise NotImplementedError
 
 
+@dataclass
 class DeviceInitializationConfig:
     """
     eager ones all connect at the beginning in blueapi startup. if they fail to connect we get an error
@@ -33,20 +35,14 @@ class DeviceInitializationConfig:
     default_timeout_for_connect: float = DEFAULT_TIMEOUT
     default_use_mock_at_connection: bool = False
 
-    def __init__(self, **kwargs):
-        self.eager = kwargs.get("eager", False)
-        self.set_name = kwargs.get("set_name", True)
-        self.default_timeout_for_connect = kwargs.get("timeout", 0)
-        self.default_use_mock_at_connection = kwargs.get(
-            "use_mock_at_connection", False
-        )
-
 
 class DeviceInitializationController:
     device: AnyDevice | None = None
     config: DeviceInitializationConfig | None = None
 
-    def __init__(self, config: DeviceInitializationConfig, factory: Callable[[], AnyDevice]) -> None:
+    def __init__(
+        self, config: DeviceInitializationConfig, factory: Callable[[], AnyDevice]
+    ) -> None:
         self.factory = factory
         self.config = config
 
@@ -59,23 +55,28 @@ class DeviceInitializationController:
     def add_device_to_cache(self, device: AnyDevice) -> None:
         ACTIVE_DEVICES[device.name] = device
 
-    def __call__(self, connect=False, mock: bool | None = None, timeout: float | None = None) -> AnyDevice
+    def __call__(
+        self, connect=False, mock: bool | None = None, timeout: float | None = None
+    ) -> AnyDevice:
         device: AnyDevice = (
             # todo if we do not pass the name to the factory, we can not check if the device is in the cache.
             # there are many devices from the same factory
-            self.see_if_device_is_in_cache(factory.__name__) or factory()
+            self.see_if_device_is_in_cache(self.factory.__name__) or self.factory()
         )
-        
-        if self.config.set_name:
-            device.set_name(factory.__name__)
-            
+        if self.config and self.config.set_name:
+            device.set_name(self.factory.__name__)
         self.add_device_to_cache(device)
 
         if connect:
+            assert self.config
             call_in_bluesky_event_loop(
                 device.connect(
-                    timeout=self.config.default_timeout_for_connect if timeout is None else timeout,
-                    mock=self.config.default_use_mock_at_connection if mock is None else mock,
+                    timeout=self.config.default_timeout_for_connect
+                    if timeout is None
+                    else timeout,
+                    mock=self.config.default_use_mock_at_connection
+                    if mock is None
+                    else mock,
                 )
             )
 
@@ -83,12 +84,14 @@ class DeviceInitializationController:
 
 
 def instantiation_behaviour(
-    eager: bool = True
-    set_name: bool = True
-    default_timeout_for_connect: float = DEFAULT_TIMEOUT
-    default_use_mock_at_connection: bool = False
+    eager: bool = True,
+    set_name: bool = True,
+    default_timeout_for_connect: float = DEFAULT_TIMEOUT,
+    default_use_mock_at_connection: bool = False,
 ) -> Callable[[Callable[[], AnyDevice]], DeviceInitializationController]:
-    config = DeviceInitializationConfig(eager, set_name, default_timeout_for_connect, default_use_mock_at_connection)
+    config = DeviceInitializationConfig(
+        eager, set_name, default_timeout_for_connect, default_use_mock_at_connection
+    )
 
     def decorator(factory: Callable[[], AnyDevice]) -> DeviceInitializationController:
         controller = DeviceInitializationController(config, factory)
@@ -100,19 +103,23 @@ def instantiation_behaviour(
 beamline_prefix = "example:"
 
 
-@instantiation_behaviour(use_mock=True, timeout=10)
+@instantiation_behaviour(
+    default_use_mock_at_connection=True, default_timeout_for_connect=10
+)
 def detector_xyz():
     """Create an XYZ detector with specific settings."""
     return XYZDetector(name="det1", prefix=f"{beamline_prefix}xyz:")
 
 
-@instantiation_behaviour(eager=True, use_mock=True, timeout=10)
+@instantiation_behaviour(
+    eager=True, default_use_mock_at_connection=True, default_timeout_for_connect=10
+)
 def detector_xyz_variant():
     """Create an XYZ detector with specific settings."""
     return XYZDetector(name="det2-variant", prefix=f"{beamline_prefix}xyz:")
 
 
-cached_controllers: dict[str, DeviceInitializationController] = {
-    "det1": detector_xyz,
-    "det2": detector_xyz_variant,
+cached_devices: dict[str, AnyDevice] = {
+    "det1": detector_xyz(),
+    "det2": detector_xyz_variant(),
 }
