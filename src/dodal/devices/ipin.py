bdcd23efe9672615d6407bce5c087b1c13460f26
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-30 12:34:03 +0100

    fix naming

--- src/dodal/devices/ipin.py
+++ src/dodal/devices/ipin.py
@@ -2,10 +2,10 @@ from ophyd_async.core import HintedSignal, StandardReadable
 from ophyd_async.epics.signal import epics_signal_r
 
 
-class IPinAsync(StandardReadable):
+class IPin(StandardReadable):
     """Simple device to get the ipin reading"""
 
     def __init__(self, prefix: str, name: str = "") -> None:
-        pin_readback = epics_signal_r(float, prefix + "I")
-        self.add_readables([pin_readback], wrapper=HintedSignal)
+        with self.add_children_as_readables(wrapper=HintedSignal):
+            pin_readback = epics_signal_r(float, prefix + "I")
         super().__init__(name)
