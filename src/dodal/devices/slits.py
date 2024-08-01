50109d1d8f07f672692c97b6f205ccf0bd70b4a6
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-01 09:09:27 +0100

    adjust the s4 slits pv values

--- src/dodal/devices/slits.py
+++ src/dodal/devices/slits.py
@@ -19,13 +19,13 @@ class Slits(StandardReadable):
 
 class SpecialSlitsi22(StandardReadable):
     """
-    TODO uncertain
+    these are unlikely to be moved during a scan
     """
 
     def __init__(self, prefix: str, name: str = "") -> None:
         with self.add_children_as_readables():
-            self.x_plus = Motor(prefix + "X:PLUS")
-            self.x_minus = Motor(prefix + "X:MINUS")
-            self.y_plus = Motor(prefix + "Y:PLUS")
-            self.y_minus = Motor(prefix + "Y:MINUS")
+            self.x = Motor(prefix + "X")
+            self.y = Motor(prefix + "Y")
+            self.horizontal_dso = Motor(prefix + "HDSO")
+            self.vertical_dso = Motor(prefix + "VDSO")
         super().__init__(name)
