bd99bbc6ad0deee63da04b4c96aa8131f13527ad
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-02 10:48:08 +0000

    move devices from i18-bluesky

---
+++ src/dodal/devices/i18/table.py
@@ -0,0 +1,14 @@
+from bluesky.protocols import Movable
+from ophyd_async.core import (
+    StandardReadable,
+)
+from ophyd_async.epics.motion import Motor
+
+
+class Table(StandardReadable, Movable):
+    def __init__(self, motion_prefix: str, prefix: str = "", name: str = "") -> None:
+        with self.add_children_as_readables():
+            self.x = Motor(motion_prefix + "X")
+            self.y = Motor(motion_prefix + "Y")
+            self.z = Motor(motion_prefix + "Z")
+            self.theta = Motor(motion_prefix + "THETA")
