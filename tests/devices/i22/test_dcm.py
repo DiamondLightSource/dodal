7311a8efee11eb766ec571f3c556f8b7419071ef
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-02 10:22:04 +0000

    udpate fixtures

--- tests/devices/i22/test_dcm.py
+++ tests/devices/i22/test_dcm.py
@@ -49,32 +49,6 @@ def test_count_dcm(
     )
 
 
-<<<<<<< HEAD
-=======
-async def test_crystal_metadata_not_propagated_when_not_supplied():
-    async with DeviceCollector(mock=True):
-        dcm = DoubleCrystalMonochromator(
-            prefix="FOO-MO",
-            temperature_prefix="FOO-DI",
-            crystal_1_metadata=CrystalMetadata("Si111"),
-            crystal_2_metadata=CrystalMetadata("Si111"),
-        )
-
-    configuration = await dcm.read_configuration()
-    expected_absent_keys = {
-        "crystal-1-usage",
-        "crystal-1-type",
-        "crystal-1-reflection",
-        "crystal-1-d_spacing",
-        "crystal-2-usage",
-        "crystal-2-type",
-        "crystal-2-reflection",
-        "crystal-2-d_spacing",
-    }
-    assert expected_absent_keys.isdisjoint(configuration)
-
-
->>>>>>> 7c02f1411 (adapt the tests)
 @pytest.mark.parametrize(
     "energy,wavelength",
     [
