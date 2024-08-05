d77441349536dd5b0a49dd1cfa2ad4c0755544da
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-05 16:37:49 +0000

    move away from ophyd devices

---
+++ src/dodal/devices/i18/sim_detector.py
@@ -0,0 +1,18 @@
+from ophyd_async.epics.motion import Motor
+from ophyd_async.sim import PatternGenerator
+
+
+class SimDetector:
+    def __init__(self, name: str, motor: Motor, motor_field: str):
+        self.name = name
+        self.motor = motor
+        self.motor_field = motor_field
+        self.pattern_generator = PatternGenerator(
+            saturation_exposure_time=0.1, detector_height=100, detector_width=100
+        )
+
+    def read(self):
+        return {self.name: {"value": self.pattern_generator}}
+
+    def describe(self):
+        return {self.name: {"source": "synthetic", "dtype": "number"}}
