069ae752dbd5928c374e0278ff62ef440cebd8a7
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-08-07 14:29:04 +0000

    completed gap motor

--- src/dodal/devices/apple2_undulator.py
+++ src/dodal/devices/apple2_undulator.py
@@ -1,4 +1,4 @@
-from bluesky.protocols import Movable, Status
+from bluesky.protocols import Movable
 from ophyd_async.core import (
     AsyncStatus,
     ConfigSignal,
@@ -9,24 +9,43 @@ from ophyd_async.core import (
 from ophyd_async.epics.signal import epics_signal_r, epics_signal_rw
 
 
-class Undulator_Gap(StandardReadable, Movable):
+class UndulatorGap(StandardReadable, Movable):
+    """A collection of epics signals to control Apple 2 undulator gap motion.
+    Only PV used by beamline are added the full list is here:
+    /dls_sw/work/R3.14.12.7/support/insertionDevice/db/IDGapVelocityControl.template
+    /dls_sw/work/R3.14.12.7/support/insertionDevice/db/IDPhaseSoftMotor.template
+    """
+
     def __init__(self, prefix: str, name: str = ""):
+        # Gap demand set point and readback
         self.user_setpoint = epics_signal_rw(
             str, prefix + "BLGSET", prefix + "GAPSET.B"
         )
+
+        # Nothing move until this is set to 1 and it will return to 0 when done
         self.set_move = epics_signal_rw(int, prefix + "BLGSETP")
+
+        # These are gap velocity limit.
+        self.max_velocity = epics_signal_r(float, prefix + "BLGSETVEL.HOPR")
+        self.min_velocity = epics_signal_r(float, prefix + "BLGSETVEL.LOPR")
+        # These are gap limit.
+        self.high_limit = epics_signal_r(float, prefix + "BLGAPMTR.HLM")
+        self.low_limit = epics_signal_r(float, prefix + "BLGAPMTR.LLM")
+
+        # This is calculated acceleration from speed
+        self.acceleration_time = epics_signal_r(float, prefix + "IDGSETACC")
         with self.add_children_as_readables(ConfigSignal):
+            # Unit
             self.motor_egu = epics_signal_r(str, prefix + "BLGAPMTR.EGU")
+            # Gap velocity
             self.velocity = epics_signal_rw(float, prefix + "BLGSETVEL")
-            self.max_velocity = epics_signal_r(float, prefix + "BLGSETVEL.DRVH")
-            self.min_velocity = epics_signal_r(float, prefix + "BLGSETVEL.DRVL")
-            self.acceleration_time = epics_signal_r(float, prefix + "IDGSETACC")
 
         with self.add_children_as_readables(HintedSignal):
+            # Gap readback value
             self.user_readback = epics_signal_r(float, prefix + "CURRGAPD")
         super().__init__(name)
 
     @AsyncStatus.wrap
-    async def set(self, value) -> Status:
+    async def set(self, value) -> None:
         await self.user_setpoint.set(value=value)
-        return await set_and_wait_for_value(signal=self.set_move, value=0)
+        await set_and_wait_for_value(signal=self.set_move, value=0)
