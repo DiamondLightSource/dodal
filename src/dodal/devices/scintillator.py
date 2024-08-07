c03304e1f9bf940a4117656af2d43512502fc8c0
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-08-07 09:29:53 +0100

    use prefix

--- src/dodal/devices/scintillator.py
+++ src/dodal/devices/scintillator.py
@@ -5,6 +5,6 @@ from ophyd_async.epics.motion import Motor
 class Scintillator(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
         with self.add_children_as_readables():
-            self.y = Motor("-MO-SCIN-01:Y")
-            self.z = Motor("-MO-SCIN-01:Z")
+            self.y = Motor(prefix + "-MO-SCIN-01:Y")
+            self.z = Motor(prefix + "-MO-SCIN-01:Z")
         super().__init__(name)
