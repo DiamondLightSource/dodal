09e7cc0b8fecbde18970815a72357c7ad78c3abf
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-08-08 14:48:22 +0000

    added test

--- src/dodal/devices/apple2_undulator.py
+++ src/dodal/devices/apple2_undulator.py
@@ -1,12 +1,19 @@
+from enum import Enum
+
 from bluesky.protocols import Movable
 from ophyd_async.core import (
     AsyncStatus,
     ConfigSignal,
     HintedSignal,
     StandardReadable,
-    set_and_wait_for_value,
+    wait_for_value,
 )
 from ophyd_async.epics.signal import epics_signal_r, epics_signal_rw
+
+
+class UndulatorGatestatus(str, Enum):
+    open = "Open"
+    close = "Closed"
 
 
 class UndulatorGap(StandardReadable, Movable):
@@ -19,12 +26,12 @@ class UndulatorGap(StandardReadable, Movable):
     def __init__(self, prefix: str, name: str = ""):
         # Gap demand set point and readback
         self.user_setpoint = epics_signal_rw(
-            str, prefix + "BLGSET", prefix + "GAPSET.B"
+            str, prefix + "GAPSET.B", prefix + "BLGSET"
         )
-
         # Nothing move until this is set to 1 and it will return to 0 when done
         self.set_move = epics_signal_rw(int, prefix + "BLGSETP")
-
+        # Gate keeper open when move is requested, closed when move is completed
+        # self.gate = epics_signal_r(UndulatorGatestatus, prefix + "BLGATE")
         # These are gap velocity limit.
         self.max_velocity = epics_signal_r(float, prefix + "BLGSETVEL.HOPR")
         self.min_velocity = epics_signal_r(float, prefix + "BLGSETVEL.LOPR")
@@ -39,7 +46,8 @@ class UndulatorGap(StandardReadable, Movable):
             self.motor_egu = epics_signal_r(str, prefix + "BLGAPMTR.EGU")
             # Gap velocity
             self.velocity = epics_signal_rw(float, prefix + "BLGSETVEL")
-
+            # Gate keeper
+            self.gate = epics_signal_r(UndulatorGatestatus, prefix + "BLGATE")
         with self.add_children_as_readables(HintedSignal):
             # Gap readback value
             self.user_readback = epics_signal_r(float, prefix + "CURRGAPD")
@@ -47,5 +55,15 @@ class UndulatorGap(StandardReadable, Movable):
 
     @AsyncStatus.wrap
     async def set(self, value) -> None:
-        await self.user_setpoint.set(value=value)
-        await set_and_wait_for_value(signal=self.set_move, value=0)
+        if await self.gate.get_value() == UndulatorGatestatus.open:
+            raise RuntimeError(f"{self.name} is already in motion.")
+        await self.user_setpoint.set(value=str(value))
+        timeout = await self._cal_timeout()
+        await self.set_move.set(value=1)
+        await wait_for_value(self.gate, UndulatorGatestatus.close, timeout=timeout)
+
+    async def _cal_timeout(self) -> float:
+        vel = await self.velocity.get_value()
+        cur_pos = await self.user_readback.get_value()
+        target_pos = float(await self.user_setpoint.get_value())
+        return abs((target_pos - cur_pos) * 2.0 / vel)
