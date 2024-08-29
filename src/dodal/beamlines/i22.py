2a4c4327f70f8b9fc2c48801e9105582b5b251e6
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-08-29 14:40:46 +0100

    add return type annotation for testing coverage purposes

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -2,31 +2,27 @@ from pathlib import Path
 
 from ophyd_async.epics.adaravis import AravisDetector
 from ophyd_async.epics.adpilatus import PilatusDetector
+from ophyd_async.epics.areadetector import AravisDetector, PilatusDetector
 from ophyd_async.fastcs.panda import HDFPanda
+from ophyd_async.panda import HDFPanda
 
 from dodal.common.beamlines.beamline_utils import (
-    device_instantiation,
     get_path_provider,
     set_path_provider,
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.common.beamlines.device_helpers import numbered_slits
-from dodal.common.visit import RemoteDirectoryServiceClient, StaticVisitPathProvider
-from ophyd_async.panda import HDFPanda
-
-from dodal.common.beamlines.beamline_utils import (
-    get_directory_provider,
-    set_directory_provider,
-)
-from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.beamlines.device_factory import device_factory
 from dodal.common.beamlines.device_helpers import numbered_slits
-from dodal.common.visit import DirectoryServiceClient, StaticVisitDirectoryProvider
+from dodal.common.visit import (
+    RemoteDirectoryServiceClient,
+    StaticVisitPathProvider,
+)
 from dodal.devices.focusing_mirror import FocusingMirror
 from dodal.devices.i22.dcm import CrystalMetadata, DoubleCrystalMonochromator
 from dodal.devices.i22.fswitch import FSwitch
 from dodal.devices.i22.nxsas import NXSasMetadataHolder, NXSasOAV, NXSasPilatus
 from dodal.devices.linkam3 import Linkam3
+from dodal.devices.slits import Slits
 from dodal.devices.synchrotron import Synchrotron
 from dodal.devices.tetramm import TetrammDetector
 from dodal.devices.undulator import Undulator
@@ -52,7 +48,7 @@ set_path_provider(
 
 
 @device_factory()
-def saxs():
+def saxs() -> PilatusDetector:
     metadata_holder = NXSasMetadataHolder(
         x_pixel_size=(1.72e-1, "mm"),
         y_pixel_size=(1.72e-1, "mm"),
@@ -68,17 +64,17 @@ def saxs():
         drv_suffix="CAM:",
         hdf_suffix="HDF5:",
         metadata_holder=metadata_holder,
-        path_provider=get_path_provider()
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory()
-def synchrotron():
+def synchrotron() -> Synchrotron:
     return Synchrotron()
 
 
 @device_factory()
-def waxs():
+def waxs() -> PilatusDetector:
     metadata_holder = NXSasMetadataHolder(
         x_pixel_size=(1.72e-1, "mm"),
         y_pixel_size=(1.72e-1, "mm"),
@@ -99,7 +95,7 @@ def waxs():
 
 
 @device_factory()
-def i0():
+def i0() -> TetrammDetector:
     return TetrammDetector(
         prefix="-EA-XBPM-02:",
         type="Cividec Diamond XBPM",
@@ -108,7 +104,7 @@ def i0():
 
 
 @device_factory()
-def it():
+def it() -> TetrammDetector:
     return TetrammDetector(
         prefix="-EA-TTRM-02:",
         type="PIN Diode",
@@ -117,17 +113,17 @@ def it():
 
 
 @device_factory()
-def vfm():
+def vfm() -> FocusingMirror:
     return FocusingMirror(name="vfm", prefix="-OP-KBM-01:VFM:")
 
 
 @device_factory()
-def hfm():
+def hfm() -> FocusingMirror:
     return FocusingMirror(name="hfm", prefix="-OP-KBM-01:HFM:")
 
 
 @device_factory()
-def dcm():
+def dcm() -> DoubleCrystalMonochromator:
     prefix = BeamlinePrefix(BL).beamline_prefix
     silicon_crystal_metadata = CrystalMetadata(
         usage="Bragg",
@@ -145,7 +141,7 @@ def dcm():
 
 
 @device_factory()
-def undulator():
+def undulator() -> Undulator:
     return Undulator(
         prefix=f"{BeamlinePrefix(BL).insertion_prefix}-MO-SERVC-01:",
         poles=80,
@@ -156,37 +152,37 @@ def undulator():
 
 # slits section, s1 is on the frontend, s6 is closest to the sample
 @device_factory()
-def slits_1():
+def slits_1() -> Slits:
     return numbered_slits(1)
 
 
 @device_factory()
-def slits_2():
+def slits_2() -> Slits:
     return numbered_slits(2)
 
 
 @device_factory()
-def slits_3():
+def slits_3() -> Slits:
     return numbered_slits(3)
 
 
 @device_factory()
-def slits_4():
+def slits_4() -> Slits:
     return numbered_slits(4)
 
 
 @device_factory()
-def slits_5():
+def slits_5() -> Slits:
     return numbered_slits(5)
 
 
 @device_factory()
-def slits_6():
+def slits_6() -> Slits:
     return numbered_slits(6)
 
 
 @device_factory()
-def fswitch():
+def fswitch() -> FSwitch:
     return FSwitch(
         prefix="-MO-FSWT-01:",
         lens_geometry="paraboloid",
@@ -196,39 +192,39 @@ def fswitch():
 
 
 @device_factory()
-def panda1():
+def panda1() -> HDFPanda:
     return HDFPanda(
         prefix="-EA-PANDA-01:",
-        directory_provider=get_directory_provider(),
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory(skip=True)
-def panda2():
+def panda2() -> HDFPanda:
     return HDFPanda(
         prefix="-EA-PANDA-02:",
-        directory_provider=get_directory_provider(),
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory(skip=True)
-def panda3():
+def panda3() -> HDFPanda:
     return HDFPanda(
         prefix="-EA-PANDA-03:",
-        directory_provider=get_directory_provider(),
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory(skip=True)
-def panda4():
+def panda4() -> HDFPanda:
     return HDFPanda(
         prefix="-EA-PANDA-04:",
-        directory_provider=get_directory_provider(),
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory()
-def oav():
+def oav() -> AravisDetector:
     metadata_holder = NXSasMetadataHolder(
         x_pixel_size=(3.45e-3, "mm"),  # Double check this figure
         y_pixel_size=(3.45e-3, "mm"),
@@ -241,10 +237,10 @@ def oav():
         drv_suffix="DET:",
         hdf_suffix="HDF5:",
         metadata_holder=metadata_holder,
-        directory_provider=get_directory_provider(),
+        path_provider=get_path_provider(),
     )
 
 
 @device_factory()
-def linkam():
+def linkam() -> Linkam3:
     return Linkam3(prefix="-EA-TEMPC-05")
