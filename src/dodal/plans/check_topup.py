c3ad7e429575fad62efb8cf20a25b17bdc84f59c
rtuck99 <robert.tuck@diamond.ac.uk>
2024-08-29 11:04:39 +0100

    (DiamondLightSource/hyperion#1464) Change topup gating to prevent gating with long exposures (#674)
    * (DiamondLightSource/hyperion#1464) Change topup gating to prevent gating with long exposures
    
    * Additional commentary as per PR review
    
    * Fix whitespace

--- src/dodal/plans/check_topup.py
+++ src/dodal/plans/check_topup.py
@@ -1,11 +1,30 @@
+from typing import Any
+
 import bluesky.plan_stubs as bps
 
+from dodal.common.beamlines.beamline_parameters import (
+    get_beamline_parameters,
+)
 from dodal.devices.synchrotron import Synchrotron, SynchrotronMode
 from dodal.log import LOGGER
 
 ALLOWED_MODES = [SynchrotronMode.USER, SynchrotronMode.SPECIAL]
 DECAY_MODE_COUNTDOWN = -1  # Value of the start_countdown PV when in decay mode
 COUNTDOWN_DURING_TOPUP = 0
+
+DEFAULT_THRESHOLD_EXPOSURE_S = 120
+DEFAULT_TOPUP_GATE_DELAY_S = 1
+
+
+class TopupConfig:
+    # For planned exposures less than this value, wait for topup to finish instead of
+    # collecting throughout topup.
+    THRESHOLD_EXPOSURE_S = "dodal_topup_threshold_exposure_s"
+    # Additional configurable safety margin to wait after the end of topup, as the start
+    # and end countdowns do not have the same precision, and in addition we want to be sure
+    # that collection does not overlap with any transients that may occur after the
+    # nominal endpoint.
+    TOPUP_GATE_DELAY_S = "dodal_topup_end_delay_s"
 
 
 def _in_decay_mode(time_to_topup):
@@ -23,15 +42,38 @@ def _gating_permitted(machine_mode: SynchrotronMode):
     return False
 
 
-def _delay_to_avoid_topup(total_run_time, time_to_topup):
-    if total_run_time > time_to_topup:
-        LOGGER.info(
-            """
-            Total run time for this collection exceeds time to next top up.
-            Collection delayed until top up done.
-            """
+def _delay_to_avoid_topup(
+    total_run_time_s: float,
+    time_to_topup_s: float,
+    topup_configuration: dict,
+    total_exposure_time_s: float,
+) -> bool:
+    """Determine whether we should delay collection until after a topup. Generally
+    if a topup is due to occur during the collection we will delay collection until after the topup.
+    However for long-running collections, impact of the topup is potentially less and collection-duration may be
+    a significant fraction of the topup-interval, therefore we may wish to collect during a topup.
+
+    Args:
+        total_run_time_s: Anticipated time until end of the collection in seconds
+        time_to_topup_s: Time to the start of the topup as measured from the PV
+        topup_configuration: configuration dictionary
+        total_exposure_time_s: Total exposure time of the sample in s"""
+    if total_run_time_s > time_to_topup_s:
+        limit_s = topup_configuration.get(
+            TopupConfig.THRESHOLD_EXPOSURE_S, DEFAULT_THRESHOLD_EXPOSURE_S
         )
-        return True
+        gate = total_exposure_time_s < limit_s
+        if gate:
+            LOGGER.info(f"""
+                Exposure time of {total_exposure_time_s}s below the threshold of {limit_s}s.
+                Collection delayed until topup done.
+                """)
+        else:
+            LOGGER.info(f"""
+                Exposure time of {total_exposure_time_s}s meets the threshold of {limit_s}s.
+                Collection proceeding through topup.
+                """)
+        return gate
     LOGGER.info(
         """
         Total run time less than time to next topup. Proceeding with collection.
@@ -71,12 +113,25 @@ def check_topup_and_wait_if_necessary(
         return
     tot_run_time = total_exposure_time + ops_time
     end_topup = yield from bps.rd(synchrotron.top_up_end_countdown)
-    time_to_wait = (
-        end_topup if _delay_to_avoid_topup(tot_run_time, time_to_topup) else 0.0
+    topup_configuration = _load_topup_configuration_from_properties_file()
+    should_wait = _delay_to_avoid_topup(
+        tot_run_time,
+        time_to_topup,
+        topup_configuration,
+        total_exposure_time,
     )
+    topup_gate_delay = topup_configuration.get(
+        TopupConfig.TOPUP_GATE_DELAY_S, DEFAULT_TOPUP_GATE_DELAY_S
+    )
+    time_to_wait = end_topup + topup_gate_delay if should_wait else 0.0
 
     yield from bps.sleep(time_to_wait)
 
     check_start = yield from bps.rd(synchrotron.top_up_start_countdown)
     if check_start == COUNTDOWN_DURING_TOPUP:
         yield from wait_for_topup_complete(synchrotron)
+
+
+def _load_topup_configuration_from_properties_file() -> dict[str, Any]:
+    params = get_beamline_parameters()
+    return params.params
