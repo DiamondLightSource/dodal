4130b0f2206f66fc14da1f8bc746170db1108784
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-09-05 10:22:45 +0100

    Add ability to control shutter source (#770)
    * Add ability to control shutter source
    
    * Raise exception if shutter is trying to be manually controlled when in auto

--- tests/devices/unit_tests/test_shutter.py
+++ tests/devices/unit_tests/test_shutter.py
@@ -3,6 +3,7 @@ from ophyd_async.core import DeviceCollector, callback_on_mock_put, set_mock_val
 
 from dodal.devices.zebra_controlled_shutter import (
     ZebraShutter,
+    ZebraShutterControl,
     ZebraShutterState,
 )
 
@@ -18,7 +19,7 @@ async def sim_shutter():
     def propagate_status(value: ZebraShutterState, *args, **kwargs):
         set_mock_value(sim_shutter.position_readback, value)
 
-    callback_on_mock_put(sim_shutter.position_setpoint, propagate_status)
+    callback_on_mock_put(sim_shutter._manual_position_setpoint, propagate_status)
     return sim_shutter
 
 
@@ -33,3 +34,9 @@ async def test_set_shutter_open(
     assert (
         shutter_position["value"] is new_state
     ), f"Unexpected value: {shutter_position['value']}"
+
+
+async def test_given_shutter_in_auto_then_when_set_raises(sim_shutter: ZebraShutter):
+    set_mock_value(sim_shutter.control_mode, ZebraShutterControl.AUTO)
+    with pytest.raises(UserWarning):
+        await sim_shutter.set(ZebraShutterState.OPEN)
