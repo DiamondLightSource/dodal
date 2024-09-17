a18a373c3b22eb300bb9e6e64deb6a4b923ea68c
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-17 09:23:39 +0100

    motor import fix

--- src/dodal/devices/undulator.py
+++ src/dodal/devices/undulator.py
@@ -10,7 +10,6 @@ from ophyd_async.core import (
     StandardReadable,
     soft_signal_r_and_setter,
 )
-from ophyd_async.epics import Motor
 from ophyd_async.epics.motor import Motor
 from ophyd_async.epics.signal import epics_signal_r
 
