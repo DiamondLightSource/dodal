644ccadd1d07542d5dd86d2233e42569f1c6f1f8
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-20 16:11:50 +0000

    finally fixed tests

--- tests/beamlines/unit_tests/test_i22.py
+++ tests/beamlines/unit_tests/test_i22.py
@@ -1,11 +1,30 @@
 import pytest
+from ophyd_async.epics.adpilatus import PilatusDetector
 
 
-@pytest.mark.parametrize("module_and_devices_for_beamline", ["i-min"], indirect=True)
+@pytest.mark.parametrize("module_and_devices_for_beamline", ["i22"], indirect=True)
 def test_device_creation(RE, module_and_devices_for_beamline):
     _, devices = module_and_devices_for_beamline
     print(devices)
-    f = devices["fswitch"]
-    p = devices["panda1"]
-    assert f is not None
-    assert p is not None
+    expected_keys = [
+        "saxs",
+        "synchrotron",
+        "waxs",
+        # "i0", # todo it is missing the driver for now
+        # "it", # todo it is missing the driver for now
+        "dcm",
+        "undulator",
+        "vfm",
+        "hfm",
+        "fswitch",
+        "panda1",
+        "panda2",
+        "panda3",
+        "panda4",
+        "oav",
+        "linkam",
+    ]
+    for key in expected_keys:
+        assert key in devices
+        assert devices[key] is not None
+    assert isinstance(devices["saxs"], PilatusDetector)
