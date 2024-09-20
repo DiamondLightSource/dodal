048c7c7cf751e8b4a174534812bf731e6e6d28c9
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-20 15:58:11 +0000

    fix the test instance

--- tests/beamlines/unit_tests/test_i22.py
+++ tests/beamlines/unit_tests/test_i22.py
@@ -1,15 +1,11 @@
 import pytest
 
-from dodal.devices.synchrotron import Synchrotron
-
 
 @pytest.mark.parametrize("module_and_devices_for_beamline", ["i-min"], indirect=True)
 def test_device_creation(RE, module_and_devices_for_beamline):
     _, devices = module_and_devices_for_beamline
     print(devices)
-    # ns = devices["numbered_slits"]
-    s = devices["slits_6"]
-    s = devices["fswitch"]
+    f = devices["fswitch"]
     p = devices["panda1"]
-    assert s is not None
-    assert isinstance(s, Synchrotron)
+    assert f is not None
+    assert p is not None
