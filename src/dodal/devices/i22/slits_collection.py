18dbbeba35aa828775a9d2257747dcd7eb5fbdc1
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-23 14:46:38 +0100

    fix test to raise coverage

--- src/dodal/devices/i22/slits_collection.py
+++ src/dodal/devices/i22/slits_collection.py
@@ -4,12 +4,12 @@ from dodal.devices.slits import Slits
 
 
 class SlitsCollection(StandardReadable):
-    def __init__(self, prefix: str, length: int = 2, name: str = "") -> None:
+    def __init__(self, prefix: str, number: int = 2, name: str = "") -> None:
         with self.add_children_as_readables():
             self.slits: DeviceVector[Slits] = DeviceVector(
                 {
                     i: Slits(prefix=prefix + f"-AL-SLITS-{i:02}", name=f"slits_{i}")
-                    for i in range(1, length)
+                    for i in range(1, number + 1)
                 }
             )
         super().__init__(name)
--- tests/devices/i22/test_slits_collection.py
+++ tests/devices/i22/test_slits_collection.py
@@ -7,11 +7,12 @@ from dodal.devices.i22.slits_collection import SlitsCollection
 @pytest.fixture
 def slits_collection(static_path_provider: PathProvider, RE) -> SlitsCollection:
     with DeviceCollector(mock=True):
-        sc = SlitsCollection("six_slits", 6)
-    return sc
+        six_slits = SlitsCollection("six_slits", 6)
+    return six_slits
 
 
 async def test_config_data_present(slits_collection: SlitsCollection):
-    assert len([slits_collection.slits]) == 6
-    assert all(f"slits_{i}" in [slits_collection.slits] for i in range(1, 6))
+    assert len(slits_collection.slits.keys()) == 6
+    names_list: list[str] = [slit.name for _, slit in slits_collection.slits.items()]
+    assert all(f"six_slits-slits-{i}" in names_list for i in range(1, 5))
     assert slits_collection.name == "six_slits"
