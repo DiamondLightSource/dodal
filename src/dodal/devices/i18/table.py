061689e82f86cc8d2e1d18666d2be6b74a5a6841
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-09-30 13:17:33 +0100

    table error still

--- src/dodal/devices/i18/table.py
+++ src/dodal/devices/i18/table.py
@@ -14,12 +14,12 @@ class XYPosition:
 
 
 class Table(StandardReadable):
-    def __init__(self, motion_prefix: str, prefix: str = "", name: str = "") -> None:
+    def __init__(self, prefix: str = "", name: str = "") -> None:
         with self.add_children_as_readables():
-            self.x = Motor(motion_prefix + "X")
-            self.y = Motor(motion_prefix + "Y")
-            self.z = Motor(motion_prefix + "Z")
-            self.theta = Motor(motion_prefix + "THETA")
+            self.x = Motor(prefix + "X")
+            self.y = Motor(prefix + "Y")
+            self.z = Motor(prefix + "Z")
+            self.theta = Motor(prefix + "THETA")
 
     @AsyncStatus.wrap
     async def set_xy(self, value: XYPosition):
