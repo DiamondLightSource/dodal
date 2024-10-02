c7e8fac1c9e43f77463a4ad0cbc6c1a4c9fcac57
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-10-02 17:52:40 +0100

    Only open files once

--- src/dodal/devices/oav/oav_utils.py
+++ src/dodal/devices/oav/oav_utils.py
@@ -1,3 +1,4 @@
+import xml.etree.ElementTree as et
 from dataclasses import dataclass
 
 from dodal.devices.oav.oav_errors import OAVError_BeamPositionNotFound
@@ -17,9 +18,19 @@ class ZoomParams:
 # Probably needs something similar to OAVCOnfigParams but that just read the files,
 # fills a dictionary {"zoom_level_name": OAVParams} and returns it
 class OAVConfig:
-    def __init__(self, zoom_params_file: str, display_config: str):
-        self.zoom_params_file = zoom_params_file
-        self.display_config = display_config
+    def __init__(self, zoom_params_file: str, display_config_file: str):
+        self.zoom_params = self._get_zoom_params(zoom_params_file)
+        self.display_config = self._get_display_config(display_config_file)
+
+    def _get_display_config(self, display_config_file: str):
+        with open(display_config_file) as f:
+            file_lines = f.readlines()
+        return file_lines
+
+    def _get_zoom_params(self, zoom_params_file: str):
+        tree = et.parse(zoom_params_file)
+        root = tree.getroot()
+        return root.findall(".//zoomLevel")
 
     def _read_zoom_params(self):
         pass
@@ -27,13 +38,11 @@ class OAVConfig:
     def _read_display_config(self, zoom: str):
         crosshair_x_line = None
         crosshair_y_line = None
-        with open(self.display_config) as f:
-            file_lines = f.readlines()
-            for i in range(len(file_lines)):
-                if file_lines[i].startswith("zoomLevel = " + zoom):
-                    crosshair_x_line = file_lines[i + 1]
-                    crosshair_y_line = file_lines[i + 2]
-                    break
+        for i in range(len(self.display_config)):
+            if self.display_config[i].startswith("zoomLevel = " + zoom):
+                crosshair_x_line = self.display_config[i + 1]
+                crosshair_y_line = self.display_config[i + 2]
+                break
 
         if crosshair_x_line is None or crosshair_y_line is None:
             raise OAVError_BeamPositionNotFound(
@@ -42,7 +51,9 @@ class OAVConfig:
         return crosshair_x_line.split(" = ")[1], crosshair_y_line.split(" = ")[1]
 
     def get_parameters(self, allowed_zoom_levels: list[str]) -> dict[str, ZoomParams]:
-        pass
+        config = {zoom: ZoomParams for zoom in allowed_zoom_levels}
+        print(config)
+        # return config
 
 
 # NOTE: Beware of the zoom which could have an x or not, but in the files never does.
