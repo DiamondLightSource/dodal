7ddb202f2337e94412e4eb94b8614df22e7e3829
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-10-07 10:31:32 +0100

    Fix zoom prefix - hopefully last bit

--- src/dodal/devices/oav/oav_async.py
+++ src/dodal/devices/oav/oav_async.py
@@ -39,7 +39,7 @@ class ZoomController(StandardReadable):
 
     def __init__(self, prefix: str, name: str = "") -> None:
         super().__init__(name=name)
-        self.percentage = epics_signal_rw(float, "ZOOMPOSCMD")
+        self.percentage = epics_signal_rw(float, f"{prefix}ZOOMPOSCMD")
 
         # Level is the string description of the zoom level e.g. "1.0x" or "1.0"
         self.level = epics_signal_rw(str, f"{prefix}MP:SELECT")
