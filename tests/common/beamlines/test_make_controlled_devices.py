c0efa01370f516630337f1165fd44459a4b56940
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-10-08 16:38:04 +0100

    fix lint newlien

--- tests/common/beamlines/test_make_controlled_devices.py
+++ tests/common/beamlines/test_make_controlled_devices.py
@@ -15,4 +15,3 @@ def test_make_all_controlled_devices_import_module():
         mock_import.assert_called_once_with("test_module")
         assert isinstance(devices, dict)
         assert isinstance(exceptions, dict)
-
