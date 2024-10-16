dd0e7079a592a505ab2d0dfcd44e58e1f9389037
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-10-16 19:22:47 +0100

    Add SnapshotWithBeamCentre

---
+++ src/dodal/devices/oav/snapshots/snapshot.py
@@ -0,0 +1,57 @@
+from ophyd_async.core import soft_signal_rw
+from PIL import Image, ImageDraw
+
+from dodal.devices.oav.snapshots.MJPG import MJPG
+
+
+class SnapshotWithBeamCentre(MJPG):
+    """A child of MJPG which, when triggered, draws an outlined crosshair at the beam
+    centre in the image and saves the image to disk."""
+
+    CROSSHAIR_LENGTH_PX = 20
+    CROSSHAIR_OUTLINE_COLOUR = "Black"
+    CROSSHAIR_FILL_COLOUR = "White"
+
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.beam_centre_i = soft_signal_rw(int)
+        self.beam_centre_j = soft_signal_rw(int)
+        super().__init__(prefix, name)
+
+    async def post_processing(self, image: Image.Image):
+        beam_x = await self.beam_centre_i.get_value()
+        beam_y = await self.beam_centre_j.get_value()
+        SnapshotWithBeamCentre.draw_crosshair(image, beam_x, beam_y)
+
+        await self._save_image(image)
+
+    @classmethod
+    def draw_crosshair(cls, image: Image.Image, beam_x: int, beam_y: int):
+        draw = ImageDraw.Draw(image)
+        OUTLINE_WIDTH = 1
+        HALF_LEN = cls.CROSSHAIR_LENGTH_PX / 2
+        draw.rectangle(
+            [
+                beam_x - OUTLINE_WIDTH,
+                beam_y - HALF_LEN - OUTLINE_WIDTH,
+                beam_x + OUTLINE_WIDTH,
+                beam_y + HALF_LEN + OUTLINE_WIDTH,
+            ],
+            fill=cls.CROSSHAIR_OUTLINE_COLOUR,
+        )
+        draw.rectangle(
+            [
+                beam_x - HALF_LEN - OUTLINE_WIDTH,
+                beam_y - OUTLINE_WIDTH,
+                beam_x + HALF_LEN + OUTLINE_WIDTH,
+                beam_y + OUTLINE_WIDTH,
+            ],
+            fill=cls.CROSSHAIR_OUTLINE_COLOUR,
+        )
+        draw.line(
+            ((beam_x, beam_y - HALF_LEN), (beam_x, beam_y + HALF_LEN)),
+            fill=cls.CROSSHAIR_FILL_COLOUR,
+        )
+        draw.line(
+            ((beam_x - HALF_LEN, beam_y), (beam_x + HALF_LEN, beam_y)),
+            fill=cls.CROSSHAIR_FILL_COLOUR,
+        )
