103e177625ebb349d9b18bf2e36f9c18f66e0d59
Stanisław Malinowski <56644812+stan-dot@users.noreply.github.com>
2024-10-21 12:15:08 +0100

    Fix the expected test spacing values
    Previously this was hard coded to be a static float in milimeters for some reason, forgot to update this for this PR

--- tests/devices/i22/test_dcm.py
+++ tests/devices/i22/test_dcm.py
@@ -247,7 +247,7 @@ async def test_configuration(dcm: DoubleCrystalMonochromator):
                 "alarm_severity": ANY,
             },
             "dcm-crystal_2_d_spacing": {
-                "value": 3.13475,
+                "value": 0.31356,
                 "timestamp": ANY,
                 "alarm_severity": ANY,
             },
@@ -272,7 +272,7 @@ async def test_configuration(dcm: DoubleCrystalMonochromator):
                 "alarm_severity": ANY,
             },
             "dcm-crystal_1_d_spacing": {
-                "value": 3.13475,
+                "value": 0.31356,
                 "timestamp": ANY,
                 "alarm_severity": ANY,
             },
