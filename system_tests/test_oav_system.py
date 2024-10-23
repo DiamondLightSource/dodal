fe599eddeb3c94b9c28b52cc661a7b303759df0f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-10-23 13:09:55 +0100

    Hopefully fix system test

--- system_tests/test_oav_system.py
+++ system_tests/test_oav_system.py
@@ -1,6 +1,7 @@
 import bluesky.plan_stubs as bps
 import pytest
 from bluesky.run_engine import RunEngine
+from ophyd_async.core import DeviceCollector
 
 from dodal.devices.oav.oav_detector import OAV, OAVConfig, ZoomController
 
@@ -15,8 +16,16 @@ DISPLAY_CONFIGURATION = "tests/devices/unit_tests/test_display.configuration"
 ZOOM_LEVELS_XML = "tests/devices/unit_tests/test_jCameraManZoomLevels.xml"
 
 
-async def take_snapshot_with_grid(oav: OAV, snapshot_filename, snapshot_directory):
+@pytest.fixture
+async def oav() -> OAV:
+    oav_config = OAVConfig(ZOOM_LEVELS_XML, DISPLAY_CONFIGURATION)
+    async with DeviceCollector():
+        oav = OAV("", config=oav_config, name="oav")
     await oav.connect()
+    return oav
+
+
+def take_snapshot_with_grid(oav: OAV, snapshot_filename, snapshot_directory):
     yield from bps.abs_set(oav.grid_snapshot.top_left_x, TEST_GRID_TOP_LEFT_X)  # type: ignore # See: https://github.com/DiamondLightSource/dodal/issues/827
     yield from bps.abs_set(oav.grid_snapshot.top_left_y, TEST_GRID_TOP_LEFT_Y)  # type: ignore # See: https://github.com/DiamondLightSource/dodal/issues/827
     yield from bps.abs_set(oav.grid_snapshot.box_width, TEST_GRID_BOX_WIDTH)  # type: ignore # See: https://github.com/DiamondLightSource/dodal/issues/827
@@ -29,11 +38,10 @@ async def take_snapshot_with_grid(oav: OAV, snapshot_filename, snapshot_director
 
 # We need to find a better way of integrating this, see https://github.com/DiamondLightSource/mx-bluesky/issues/183
 @pytest.mark.skip(reason="Don't want to actually take snapshots during testing.")
-async def test_grid_overlay(RE: RunEngine):
+def test_grid_overlay(RE: RunEngine):
     beamline = "BL03I"
     oav_params = OAVConfig(ZOOM_LEVELS_XML, DISPLAY_CONFIGURATION)
     oav = OAV(name="oav", prefix=f"{beamline}", config=oav_params)
-    await oav.connect()
     snapshot_filename = "snapshot"
     snapshot_directory = "."
     RE(take_snapshot_with_grid(oav, snapshot_filename, snapshot_directory))
