6e611f2aa3a60ece9921460fee04b97735df93f5
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-10-24 14:28:47 +0100

    adapt tests

--- tests/beamlines/unit_tests/test_p38.py
+++ tests/beamlines/unit_tests/test_p38.py
@@ -15,17 +15,9 @@ def test_devices_diff_when_in_lab():
     assert saxs.__class__ == NXSasOAV, f"Expected NXSasOav, got {saxs.__class__}"
 
 
-# todo fix the following tests
-# FAILED tests/beamlines/unit_tests/test_i03.py::test_list - KeyError: "No beamline parameter path found, maybe 'BEAMLINE' environment variable is not set!"
-# FAILED tests/beamlines/unit_tests/test_p38.py::test_devices_diff_when_in_lab - AssertionError: Expected NXSasOav, got <class 'dodal.devices.i22.nxsas.NXSasPilatus'>
-# assert <class 'dodal.devices.i22.nxsas.NXSasPilatus'> == NXSasOAV
-#  +  where <class 'dodal.devices.i22.nxsas.NXSasPilatus'> = <dodal.devices.i22.nxsas.NXSasPilatus object at 0x7f016ca5b340>.__class__
-
-
-@pytest.mark.parametrize("module_and_devices_for_beamline", ["p38"], indirect=True)
+@pytest.mark.parametrize("module_and_devices_for_beamline", ["i22"], indirect=True)
 def test_device_creation(RE, module_and_devices_for_beamline):
     _, devices = module_and_devices_for_beamline
-    saxs: NXSasOAV = devices["saxs"]  # type: ignore
+    saxs = devices["saxs"]  # type: ignore
 
-    print(saxs)
     assert saxs.drv.trigger_mode.name == "BL22I-DI-DICAM-03:DET:TriggerMode"
