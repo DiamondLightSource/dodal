09826ffb0cafd465cd1fe480f22ab289183a0de6
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-11-06 10:05:53 +0000

    Add soft signals and calculation for getting beam size

--- src/dodal/devices/i24/focus_mode.py
+++ src/dodal/devices/i24/focus_mode.py
@@ -1,18 +1,63 @@
-from enum import Enum
-
-from ophyd_async.core import StandardReadable
+from ophyd_async.core import StandardReadable, StrictEnum
 from ophyd_async.epics.signal import epics_signal_rw
 
-# BL24I-OP-MFM-01:G1:TARGETAPPLY
+from dodal.common.signal_utils import create_hardware_backed_soft_signal
 
 
-class FocusMode(str, Enum):
+class HFocusMode(StrictEnum):
     focus10 = "HMFMfocus10"
-    # NOTE TODO This would be VMFMfocus10 for vertical
+    focus20d = "HMFMfocus20d"
+    focus30d = "HMFMfocus30d"
+    focus50d = "HMFMfocus50d"
+    focus1050d = "HMFMfocus1050d"
+    focus5010d = "HMFMfocus5010d"
+    focus3010d = "HMFMfocus3010d"
+
+
+class VFocusMode(StrictEnum):
+    focus10 = "VMFMfocus10"
+    focus20d = "VMFMfocus20d"
+    focus30d = "VMFMfocus30d"
+    focus50d = "VMFMfocus50d"
+    focus1050d = "VMFMfocus1050d"
+    focus5010d = "VMFMfocus5010d"
+    focus3010d = "VMFMfocus3010d"
+
+
+BEAM_SIZES = {
+    "focus10": [7, 7],
+    "focus20d": [20, 20],
+    "focus30d": [30, 30],
+    "focus50d": [50, 50],
+    "focus1050d": [10, 50],
+    "focus5010d": [50, 10],
+    "focus3010d": [30, 10],
+}
 
 
 class MirrorFocusMode(StandardReadable):
+    """A small device to read the focus mode and work out the beam size."""
+
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.horizontal = epics_signal_rw(FocusMode, prefix + "G1:TARGETAPPLY")
-        self.vertical = epics_signal_rw(FocusMode, prefix + "G0:TARGETAPPLY")
+        self.horizontal = epics_signal_rw(HFocusMode, prefix + "G1:TARGETAPPLY")
+        self.vertical = epics_signal_rw(VFocusMode, prefix + "G0:TARGETAPPLY")
+
+        with self.add_children_as_readables():
+            self.beam_size_x = create_hardware_backed_soft_signal(
+                int, self._get_beam_size_x
+            )
+            self.beam_size_y = create_hardware_backed_soft_signal(
+                int, self._get_beam_size_y
+            )
+
         super().__init__(name)
+
+    async def _get_beam_size_x(self) -> int:
+        h_mode = await self.horizontal.get_value()
+        beam_x = BEAM_SIZES[h_mode.removeprefix("HMFM")][0]
+        return beam_x
+
+    async def _get_beam_size_y(self) -> int:
+        v_mode = await self.vertical.get_value()
+        beam_y = BEAM_SIZES[v_mode.removeprefix("VMFM")][1]
+        return beam_y
