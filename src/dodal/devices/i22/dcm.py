67b43cdbb90d32e6ee30bbbc5e48296ca650c352
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-11-11 12:17:50 +0000

    fix import

--- src/dodal/devices/i22/dcm.py
+++ src/dodal/devices/i22/dcm.py
@@ -1,6 +1,4 @@
 import time
-from dataclasses import dataclass
-from typing import Literal
 
 import numpy as np
 from bluesky.protocols import Reading
@@ -14,22 +12,11 @@ from ophyd_async.core import (
 from ophyd_async.epics.core import epics_signal_r
 from ophyd_async.epics.motor import Motor
 
+from dodal.common.crystal_metadata import CrystalMetadata
+
 # Conversion constant for energy and wavelength, taken from the X-Ray data booklet
 # Converts between energy in KeV and wavelength in angstrom
 _CONVERSION_CONSTANT = 12.3984
-
-
-@dataclass(frozen=True, unsafe_hash=True)
-class CrystalMetadata:
-    """
-    Metadata used in the NeXus format,
-    see https://manual.nexusformat.org/classes/base_classes/NXcrystal.html
-    """
-
-    usage: Literal["Bragg", "Laue"] | None = None
-    type: str | None = None
-    reflection: tuple[int, int, int] | None = None
-    d_spacing: tuple[float, str] | None = None
 
 
 class DoubleCrystalMonochromator(StandardReadable):
