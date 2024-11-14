86d19c60948bc13da41ccc9c832126a940f3fdb5
stan-dot <stan-dot@users.noreply.github.com>
2024-11-14 16:41:34 +0000

    deploy: 82d6afc87cd3fdb5d3e3925ad8c6e45035e31f15

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.35.1.dev2+gad6cc0d84',
+    VERSION: '1.36.1.dev1+g82d6afc87',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
