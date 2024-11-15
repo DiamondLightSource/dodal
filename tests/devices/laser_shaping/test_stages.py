822bea244bbf0009be12129733f2f6369775b271
co2e14 <christian.orr@diamond.ac.uk>
2024-11-15 14:05:45 +0000

    change name to aithre_lasershaping Change stages to goniometer

--- tests/devices/laser_shaping/test_stages.py
+++ tests/devices/laser_shaping/test_stages.py
@@ -1,10 +1,10 @@
 from ophyd_async.core import get_mock
 
-from dodal.devices.laser_shaping.stages import GonioStages
+from dodal.devices.laser_shaping.goniometer import Goniometer
 
 
 async def test_stage_x_y():
-    stages = GonioStages(prefix="LA18L-MO-LSR-01:")
+    stages = Goniometer(prefix="LA18L-MO-LSR-01:")
     await stages.connect(mock=True)
     await stages.x.velocity.set(5)
     mock_velocity = get_mock(stages.x.velocity)
