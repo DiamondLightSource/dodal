e17736e06b101ae65f5d3ee1a963aaaea3a9b7e1
co2e14 <christian.orr@diamond.ac.uk>
2024-11-15 15:06:01 +0000

    function to check omega performance my moving it to pos and neg values at different speeds

--- src/dodal/devices/aithre_lasershaping/goniometer.py
+++ src/dodal/devices/aithre_lasershaping/goniometer.py
@@ -14,6 +14,23 @@ class Goniometer(StandardReadable):
         self.omega = Motor(prefix + "OMEGA")
         super().__init__(name)
 
+    async def checkOmegaPerformance(self):
+        for speed in [5, 10, 20, 40, 80, 90]:
+            for omegaval in [
+                300,
+                -300,
+                600,
+                -600,
+                1200,
+                -1200,
+                2400,
+                -2400,
+                3600,
+                3600,
+            ]:
+                await self.omega.velocity.set(speed)
+                await self.omega.set(omegaval)
+
 
 # to create this for lab 18-21
 #    GonioStages(prefix="LA18L-MO-LSR-01:")
