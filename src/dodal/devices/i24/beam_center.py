1777192e05c36338121f694a739a028a445fa20d
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2024-11-20 09:33:13 +0000

    Add small I24 devices (#894)
    * Start a mirror focus mode device
    
    * Rename
    
    * Add soft signals and calculation for getting beam size
    
    * Add details to focus mirror and detector beam center device for i24
    
    * Actually add the new file
    
    * Fix enums
    
    * Temporarily disable vgonio
    
    * Rename device
    
    * Add test
    
    * Add small device for pilatus filename template
    
    * And save the files
    
    * Add assertion check
    
    * Fix imports
    
    * Add units for beam size
    
    * Remove skip_devices for s24

---
+++ src/dodal/devices/i24/beam_center.py
@@ -0,0 +1,12 @@
+"""A small temporary device to get the beam center positions from \
+    eiger or pilatus detector on i24"""
+
+from ophyd_async.core import StandardReadable
+from ophyd_async.epics.core import epics_signal_rw
+
+
+class DetectorBeamCenter(StandardReadable):
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.beam_x = epics_signal_rw(float, prefix + "BeamX")
+        self.beam_y = epics_signal_rw(float, prefix + "BeamY")
+        super().__init__(name)
