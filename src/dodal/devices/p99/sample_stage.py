a477cf14d24fbc0146197ce770d31be5e8de50ba
Raymond Fan <raymond.fan@diamond.ac.uk>
2024-11-27 09:58:56 +0000

    Update src/dodal/devices/p99/sample_stage.py

--- src/dodal/devices/p99/sample_stage.py
+++ src/dodal/devices/p99/sample_stage.py
@@ -6,8 +6,8 @@ class SampleAngleStage(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
         with self.add_children_as_readables():
             self.theta = epics_signal_rw_rbv(float, prefix + "WRITETHETA", ":RBV")
-            self.roll = epics_signal_rw_rbv(float, prefix + "WRITEROLL", ":RBV"))
-            self.pitch = epics_signal_rw_rbv(float, prefix + "WRITEPITCH",":RBV"))
+            self.roll = epics_signal_rw_rbv(float, prefix + "WRITEROLL", ":RBV")
+            self.pitch = epics_signal_rw_rbv(float, prefix + "WRITEPITCH",":RBV")
         super().__init__(name=name)
 
 
