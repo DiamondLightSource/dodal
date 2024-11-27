7f072651385adaafda9d52771f52c7b21379a58f
Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>
2024-11-27 09:43:43 +0000

    Update src/dodal/devices/p99/sample_stage.py
    Co-authored-by: Raymond Fan <raymond.fan@diamond.ac.uk>

--- src/dodal/devices/p99/sample_stage.py
+++ src/dodal/devices/p99/sample_stage.py
@@ -5,9 +5,9 @@ from ophyd_async.epics.core import epics_signal_rw, epics_signal_rw_rbv
 class SampleAngleStage(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
         with self.add_children_as_readables():
-            self.theta = epics_signal_rw_rbv(float, prefix + "WRITETHETA")
-            self.roll = epics_signal_rw_rbv(float, prefix + "WRITEROLL")
-            self.pitch = epics_signal_rw_rbv(float, prefix + "WRITEPITCH")
+            self.theta = epics_signal_rw_rbv(float, prefix + "WRITETHETA", ":RBV")
+            self.roll = epics_signal_rw_rbv(float, prefix + "WRITEROLL", ":RBV"))
+            self.pitch = epics_signal_rw_rbv(float, prefix + "WRITEPITCH",":RBV"))
         super().__init__(name=name)
 
 
