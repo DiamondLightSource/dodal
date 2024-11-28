82ec40607596256173590e0d06c4a6a9bfec2cf0
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-28 11:33:18 +0000

    Replace BimorphMirror child decleration to use BimorphMirrorChannel

--- src/dodal/devices/bimorph_mirror.py
+++ src/dodal/devices/bimorph_mirror.py
@@ -1,7 +1,7 @@
 from typing import Annotated as A
 
 from bluesky.protocols import Movable
-from ophyd_async.core import SignalR, StandardReadable
+from ophyd_async.core import DeviceVector, SignalR, StandardReadable
 from ophyd_async.core import StandardReadableFormat as Format
 from ophyd_async.epics.core import PvSuffix, epics_signal_r
 
@@ -17,7 +17,9 @@ class BimorphMirror(StandardReadable, Movable):
         self.number_of_channels = number_of_channels
 
         with self.add_children_as_readables(Format.HINTED_SIGNAL):
-            self._vout_rbv_channels = [
-                epics_signal_r(float, prefix + "C" + str(i + 1) + ":VOUT_RBV")
-                for i in range(number_of_channels)
-            ]
+            self.channels = DeviceVector(
+                {
+                    i: BimorphMirrorChannel(f"{prefix}:C{i}", f"channel{i}")
+                    for i in range(1, number_of_channels + 1)
+                }
+            )
