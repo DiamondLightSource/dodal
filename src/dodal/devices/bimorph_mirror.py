70a75f5b6874f862f143b9a2f08946c3bba9d00f
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-28 11:15:44 +0000

    Create BimorphMirrorChannel with vout_rbv SignalR

--- src/dodal/devices/bimorph_mirror.py
+++ src/dodal/devices/bimorph_mirror.py
@@ -1,7 +1,13 @@
+from typing import Annotated as A
+
 from bluesky.protocols import Movable
-from ophyd_async.core import StandardReadable
+from ophyd_async.core import SignalR, StandardReadable
 from ophyd_async.core import StandardReadableFormat as Format
-from ophyd_async.epics.core import epics_signal_r
+from ophyd_async.epics.core import PvSuffix, epics_signal_r
+
+
+class BimorphMirrorChannel(StandardReadable):
+    vout_rbv: A[SignalR[float], PvSuffix("VOUT_RBV"), Format.HINTED_SIGNAL]
 
 
 class BimorphMirror(StandardReadable, Movable):
@@ -10,6 +16,6 @@ class BimorphMirror(StandardReadable, Movable):
 
         with self.add_children_as_readables(Format.HINTED_SIGNAL):
             self._vout_rbv_channels = [
-                epics_signal_r(float, prefix + "C" + str(i+1) + ":VOUT_RBV")
+                epics_signal_r(float, prefix + "C" + str(i + 1) + ":VOUT_RBV")
                 for i in range(number_of_channels)
             ]
