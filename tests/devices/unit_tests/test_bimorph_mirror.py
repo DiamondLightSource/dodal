3af9f884ea0e578b5ae1d0f0b3a5f8c0a8b799c9
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-29 14:41:12 +0000

    Add bimorph test_set_channels

--- tests/devices/unit_tests/test_bimorph_mirror.py
+++ tests/devices/unit_tests/test_bimorph_mirror.py
@@ -1,24 +1,23 @@
 import asyncio
+import pytest
 
 from bluesky.run_engine import RunEngine
 from ophyd_async.core import DeviceCollector
 
 from dodal.devices.bimorph_mirror import BimorphMirror
 
-RE = RunEngine()
-
-
-async def async_main():
+@pytest.fixture
+def mirror(RE: RunEngine) -> BimorphMirror:
     with DeviceCollector(mock=True):
         bm = BimorphMirror(prefix="BL02J-EA-IOC-97:G0:", number_of_channels=8)
-    out = await bm.read()
-    print(out)
-
-    await bm.set({1: 10.0})
-    print("\n")
-
-    out = await bm.read()
-    print(out)
+    
+    return bm
 
 
-asyncio.run(async_main())
+async def test_set_channels(mirror: BimorphMirror):
+    value = {2:50.0, 8:24.0}
+
+    await mirror.set(value)
+
+    assert (await mirror.channels[2].vout.get_value()) == 50.0
+    assert (await mirror.channels[8].vout.get_value()) == 24.0
