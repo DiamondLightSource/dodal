fbcad2801b6f84ed78bc920e92a64bd610def8eb
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-11-29 11:14:24 +0000

    Make sure to only stop the detector if it hasn't already started disarming

--- tests/devices/unit_tests/test_eiger.py
+++ tests/devices/unit_tests/test_eiger.py
@@ -740,3 +740,15 @@ def test_for_other_beamlines_i03_used_as_default(params: DetectorParams):
     )
     assert fake_eiger.beamline == "ixx"
     assert fake_eiger.timeouts == AVAILABLE_TIMEOUTS["i03"]
+
+
+def test_given_eiger_is_disarming_when_eiger_is_stopped_then_wait_for_disarming_to_finish(
+    fake_eiger: EigerDetector,
+):
+    fake_eiger.disarm_detector = MagicMock()
+    fake_eiger.disarming_status = (disarming_status := MagicMock())
+    disarming_status.done = False
+    fake_eiger.stop()
+
+    disarming_status.wait.assert_called_once()
+    fake_eiger.disarm_detector.assert_not_called()
