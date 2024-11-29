1126d69ccdd5b18de99c5b4dafe2ad583b5bd365
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-11-29 11:13:26 +0000

    Adjust adsim to use ophyd_async implementations

---
+++ src/dodal/beamlines/adsim.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+from ophyd_async.epics.adsimdetector import SimDetector
+
+from dodal.common.beamlines.beamline_utils import (
+    device_factory,
+    get_path_provider,
+    set_path_provider,
+)
+from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
+from dodal.common.beamlines.device_helpers import HDF5_PREFIX
+from dodal.common.visit import LocalDirectoryServiceClient, StaticVisitPathProvider
+from dodal.devices.adsim import SimStage
+from dodal.log import set_beamline as set_log_beamline
+from dodal.utils import BeamlinePrefix, get_beamline_name
+
+BL = get_beamline_name("bl01t")
+PREFIX = BeamlinePrefix(BL)
+set_log_beamline(BL)
+set_utils_beamline(BL)
+
+set_path_provider(
+    StaticVisitPathProvider(
+        BL,
+        Path("/dev/null"),
+        client=LocalDirectoryServiceClient(),
+    )
+)
+
+"""
+Usage Example
+-------------
+
+Start the simulated beamline by following the epics-containers tutorial at
+https://epics-containers.github.io/main/tutorials/launch_example.html
+
+How to use the devices in a plan:
+In an ipython terminal run:
+
+```python
+from bluesky.run_engine import RunEngine
+
+from dodal.beamlines.adsim import adsim, sim_motors
+from dodal.common.beamlines.beamline_utils import get_path_provider
+from dodal.plans import count
+
+RE = RunEngine()
+det = adsim(connect_immediately=True)
+RE(count([det], num=10))
+```
+
+"""
+
+
+@device_factory()
+def sim_stage() -> SimStage:
+    return SimStage(f"{PREFIX.beamline_prefix}-MO-SIM-01:")
+
+
+@device_factory()
+def adsim() -> SimDetector:
+    return SimDetector(
+        f"{PREFIX.beamline_prefix}-DI-CAM-01:",
+        path_provider=get_path_provider(),
+        drv_suffix="DET:",
+        hdf_suffix=HDF5_PREFIX,
+    )
--- src/dodal/devices/areadetector/__init__.py
+++ src/dodal/devices/areadetector/__init__.py
@@ -1,9 +1,7 @@
 from .adaravis import AdAravisDetector
-from .adsim import AdSimDetector
 from .adutils import Hdf5Writer, SynchronisedAdDriverBase
 
 __all__ = [
-    "AdSimDetector",
     "SynchronisedAdDriverBase",
     "Hdf5Writer",
     "AdAravisDetector",
