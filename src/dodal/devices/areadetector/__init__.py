e3bc815ea250f0d0dff539a2878a2fa881a04424
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2024-11-29 11:16:25 +0000

    Remove dead code

--- src/dodal/devices/areadetector/__init__.py
+++
@@ -1,8 +0,0 @@
-from .adaravis import AdAravisDetector
-from .adutils import Hdf5Writer, SynchronisedAdDriverBase
-
-__all__ = [
-    "SynchronisedAdDriverBase",
-    "Hdf5Writer",
-    "AdAravisDetector",
-]
