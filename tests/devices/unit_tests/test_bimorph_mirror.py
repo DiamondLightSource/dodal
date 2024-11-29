d0156c00b34c030a0cca4842703e64014418054c
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-11-29 15:15:31 +0000

    Rename test_set_channels, fix said test, and add test_set_channels_triggers_alltrgt_proc

--- tests/devices/unit_tests/test_bimorph_mirror.py
+++ tests/devices/unit_tests/test_bimorph_mirror.py
@@ -2,7 +2,7 @@ import asyncio
 
 import pytest
 from bluesky.run_engine import RunEngine
-from ophyd_async.core import DeviceCollector
+from ophyd_async.core import DeviceCollector, get_mock_put, set_mock_value
 
 from dodal.devices.bimorph_mirror import BimorphMirror
 
@@ -15,10 +15,27 @@ def mirror(RE: RunEngine) -> BimorphMirror:
     return bm
 
 
-async def test_set_channels(mirror: BimorphMirror):
-    value = {2:50.0, 8:24.0}
+async def test_set_channels_waits_for_readback(mirror: BimorphMirror):
+    value = {2: 50.0, 8: 24.0}
+
+    set_mock_value(mirror.channels[2].vout, 50.0)
+    set_mock_value(mirror.channels[8].vout, 24.0)
+    await mirror.set(value)
+
+    assert (await mirror.channels[2].vtrgt.get_value()) == 50.0
+    assert (await mirror.channels[8].vtrgt.get_value()) == 24.0
+
+
+async def test_set_channels_triggers_alltrgt_proc(mirror: BimorphMirror):
+    value = {2: 50.0, 8: 24.0}
+
+    mock_alltrgt_proc = get_mock_put(mirror.alltrgt_proc)
+
+    set_mock_value(mirror.channels[2].vout, 50.0)
+    set_mock_value(mirror.channels[8].vout, 24.0)
+
+    mock_alltrgt_proc.assert_not_called()
 
     await mirror.set(value)
 
-    assert (await mirror.channels[2].vout.get_value()) == 50.0
-    assert (await mirror.channels[8].vout.get_value()) == 24.0
+    mock_alltrgt_proc.assert_called_once()
