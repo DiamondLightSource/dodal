680234f1c8c96217a74879b4e334e8ca9492545e
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-01-10 16:19:45 +0000

    Convert s4_slit_gaps to device_factory

--- tests/beamlines/unit_tests/test_i03.py
+++ tests/beamlines/unit_tests/test_i03.py
@@ -3,7 +3,7 @@ from dodal.common.beamlines import beamline_utils
 
 
 def test_list():
-    i03.s4_slit_gaps(wait_for_connection=True, fake_with_ophyd_sim=True)
+    i03.s4_slit_gaps(connect_immediately=True, mock=True)
     assert beamline_utils.list_active_devices() == ["s4_slit_gaps"]
     beamline_utils.clear_devices()
     assert beamline_utils.list_active_devices() == []
