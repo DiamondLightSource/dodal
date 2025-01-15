157afd6c1b72810ee0a95e8706459b5d5de13279
Shihab S <162436767+shihab-dls@users.noreply.github.com>
2025-01-15 15:37:27 +0000

    Remove ophyd references from motion devices (#985)
    * Change p45 to phyd async
    
    * Use standard readable instead of device
    
    * Instantiate p45 devices correctly
    
    * Add prefix to device pvs
    
    * Convert i03 and i04 devices to ophyd_async
    
    ---------
    
    Co-authored-by: Shihab Suliman <rye74444@ws557.diamond.ac.uk>

--- src/dodal/devices/flux.py
+++ src/dodal/devices/flux.py
@@ -1,7 +1,14 @@
-from ophyd import Component, Device, EpicsSignalRO, Kind
+from ophyd_async.core import (
+    StandardReadable,
+    StandardReadableFormat,
+)
+from ophyd_async.epics.core import epics_signal_r
 
 
-class Flux(Device):
+class Flux(StandardReadable):
     """Simple device to get the flux reading"""
 
-    flux_reading = Component(EpicsSignalRO, "SAMP", kind=Kind.hinted)
+    def __init__(self, prefix: str, name="") -> None:
+        with self.add_children_as_readables(StandardReadableFormat.HINTED_SIGNAL):
+            self.flux_reading = epics_signal_r(float, prefix + "SAMP")
+            super().__init__(name=name)
