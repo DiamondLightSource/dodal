8b41e56db74f857525d1a17ba09b1597918d18e1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-01-17 18:37:49 +0000

    Actually save jungfrau

---
+++ src/dodal/devices/i24/jungfrau.py
@@ -0,0 +1,60 @@
+from enum import IntEnum
+
+from ophyd_async.core import StandardReadable, StrictEnum
+from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
+
+
+class TriggerMode(IntEnum):
+    SOFTWARE = 0
+    HARDWARE = 1
+
+
+class BurstMode(IntEnum):
+    ON = 1
+    OFF = 0
+
+
+class AcquireState(StrictEnum):
+    DONE = "Done"
+    ACQUIRE = "Acquiring"
+
+
+class WriteState(StrictEnum):
+    DONE = "Done"
+    ACQUIRE = "Writing"
+
+
+class GainMode(StrictEnum):
+    dynamic = "dynamic"
+    forceswitchg1 = "forceswitchg1"
+    forceswitchg2 = "forceswitchg2"
+
+
+# NOTE In the future, might want to switch this to standard detector?
+# TODO Check all types!
+class JungFrau1M(StandardReadable):
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self.trigger_mode = epics_signal_rw(TriggerMode, prefix + "Timing")
+        self.trigger_count = epics_signal_rw(int, prefix + "TriggerCount")
+        self.acquire_period_s = epics_signal_rw(float, prefix + "AcquirePeriod")
+        self.exposure_time_s = epics_signal_rw(float, prefix + "ExposureTime")
+
+        self.acquire_rbv = epics_signal_r(AcquireState, prefix + "Acquire_RBV")
+        self.state_rbv = epics_signal_r(int, prefix + "State_RBV")
+        self.writing_rbv = epics_signal_r(WriteState, prefix + "Writing_RBV")
+        self.acquire_start = epics_signal_rw(int, prefix + "Acquire")
+        self.clear_error = epics_signal_rw(float, prefix + "ClearError")
+        self.frames_written_rbv = epics_signal_r(int, prefix + "FramesWritten_RBV")
+        self.frame_count = epics_signal_rw(int, prefix + "FrameCount")
+        self.gain_mode = epics_signal_rw(GainMode, prefix + "GainMode")
+        self.error_rbv = epics_signal_r(str, prefix + "Error_RBV")
+        self.file_directory = epics_signal_rw(str, prefix + "FileDirectory")
+        self.file_name = epics_signal_rw(str, prefix + "FileName")
+
+        self.pedestal_mode = epics_signal_rw(int, prefix + "PedestalMode")
+        self.pedestal_frames = epics_signal_rw(int, prefix + "PedestalFrames")
+        self.pedestal_loops = epics_signal_rw(int, prefix + "PedestalLoops")
+
+        self.burst_mode = epics_signal_rw(BurstMode, prefix + "BurstMode")
+        self.power_state = epics_signal_rw(int, prefix + "PowerState")
+        super().__init__(name)
