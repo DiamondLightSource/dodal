7de07b9182e370d8d3ea43a6223ee66d72c99f89
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-01-24 10:56:08 +0000

    Fix enums

--- src/dodal/devices/i24/jungfrau.py
+++ src/dodal/devices/i24/jungfrau.py
@@ -14,14 +14,14 @@ class BurstMode(IntEnum):
     OFF = 0
 
 
-class AcquireState(StrictEnum):
-    DONE = "Done"
-    ACQUIRE = "Acquiring"
+class AcquireState(IntEnum):
+    DONE = 0
+    ACQUIRING = 1
 
 
-class WriteState(StrictEnum):
-    DONE = "Done"
-    WRITE = "Writing"
+class WriteState(IntEnum):
+    DONE = 0
+    WRITING = 1
 
 
 class GainMode(StrictEnum):
@@ -34,14 +34,14 @@ class GainMode(StrictEnum):
 # TODO Check all types!
 class JungFrau1M(StandardReadable):
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.trigger_mode = epics_signal_rw(TriggerMode, prefix + "Timing")
+        self.trigger_mode = epics_signal_rw(int, prefix + "Timing")
         self.trigger_count = epics_signal_rw(int, prefix + "TriggerCount")
         self.acquire_period_s = epics_signal_rw(float, prefix + "AcquirePeriod")
         self.exposure_time_s = epics_signal_rw(float, prefix + "ExposureTime")
 
-        self.acquire_rbv = epics_signal_r(AcquireState, prefix + "Acquire_RBV")
+        self.acquire_rbv = epics_signal_r(int, prefix + "Acquire_RBV")
         self.state_rbv = epics_signal_r(int, prefix + "State_RBV")
-        self.writing_rbv = epics_signal_r(WriteState, prefix + "Writing_RBV")
+        self.writing_rbv = epics_signal_r(int, prefix + "Writing_RBV")
         self.acquire_start = epics_signal_rw(int, prefix + "Acquire")
         self.clear_error = epics_signal_rw(float, prefix + "ClearError")
         self.frames_written_rbv = epics_signal_r(int, prefix + "FramesWritten_RBV")
@@ -55,6 +55,6 @@ class JungFrau1M(StandardReadable):
         self.pedestal_frames = epics_signal_rw(int, prefix + "PedestalFrames")
         self.pedestal_loops = epics_signal_rw(int, prefix + "PedestalLoops")
 
-        self.burst_mode = epics_signal_rw(BurstMode, prefix + "BurstMode")
+        self.burst_mode = epics_signal_rw(int, prefix + "BurstMode")
         self.power_state = epics_signal_rw(int, prefix + "PowerState")
         super().__init__(name)
