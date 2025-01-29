ae57c22eb170ef92ea9364541b63da30f6fbd52e
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-01-29 15:59:47 +0000

    Convert i24 to device_factory. Remove eiger pending mx-bluesky#62

--- tests/beamlines/unit_tests/test_i24.py
+++ tests/beamlines/unit_tests/test_i24.py
@@ -1,20 +1,6 @@
 import pytest
 
-from dodal.beamlines import i24
-from dodal.common.beamlines import beamline_utils
 from dodal.devices.i24.vgonio import VerticalGoniometer
-
-
-def test_list():
-    beamline_utils.clear_devices()
-    i24.zebra(wait_for_connection=False, fake_with_ophyd_sim=True)
-    i24.pmac(wait_for_connection=False, fake_with_ophyd_sim=True)
-    i24.vgonio(wait_for_connection=False, fake_with_ophyd_sim=True)
-    assert beamline_utils.list_active_devices() == [
-        "zebra",
-        "pmac",
-        "vgonio",
-    ]
 
 
 @pytest.mark.parametrize("module_and_devices_for_beamline", ["i24"], indirect=True)
