e0aac2302928f18c1eb7de87785f8b9fb18f7183
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-02-11 16:10:37 +0000

    Add grouping and num_frames_chunks set

--- src/dodal/devices/util/load_eiger_metadata_plan.py
+++ src/dodal/devices/util/load_eiger_metadata_plan.py
@@ -7,12 +7,10 @@ from dodal.devices.detector import DetectorParams
 def load_metadata(
     eiger: EigerDetector, energy: float, enable: bool, detector_params: DetectorParams
 ):
-    def _inner_plan():
-        yield from change_roi_mode(eiger, enable, detector_params)
-        yield from bps.abs_set(eiger.drv.photon_energy, energy)
-        yield from set_mx_settings_pvs(eiger, detector_params)
-
-    yield from _inner_plan()
+    yield from change_roi_mode(eiger, enable, detector_params)
+    yield from bps.abs_set(eiger.drv.photon_energy, energy)
+    yield from bps.abs_set(eiger.odin.num_frames_chunks, 1)
+    yield from set_mx_settings_pvs(eiger, detector_params, wait=True)
 
 
 def change_roi_mode(
@@ -31,14 +29,26 @@ def change_roi_mode(
     yield from bps.abs_set(eiger.odin.num_col_chunks, detector_dimensions.width)
 
 
-def set_mx_settings_pvs(eiger: EigerDetector, detector_params: DetectorParams):
+def set_mx_settings_pvs(
+    eiger: EigerDetector,
+    detector_params: DetectorParams,
+    wait: bool,
+    group="mx_settings",
+):
     beam_x_pixels, beam_y_pixels = detector_params.get_beam_position_pixels(
         detector_params.detector_distance
     )
 
-    yield from bps.abs_set(eiger.drv.beam_centre_x, beam_x_pixels)
-    yield from bps.abs_set(eiger.drv.beam_centre_y, beam_y_pixels)
-    yield from bps.abs_set(eiger.drv.det_distance, detector_params.detector_distance)
+    yield from bps.abs_set(eiger.drv.beam_centre_x, beam_x_pixels, group)
+    yield from bps.abs_set(eiger.drv.beam_centre_y, beam_y_pixels, group)
+    yield from bps.abs_set(
+        eiger.drv.det_distance, detector_params.detector_distance, group
+    )
 
-    yield from bps.abs_set(eiger.drv.omega_start, detector_params.omega_start)
-    yield from bps.abs_set(eiger.drv.omega_increment, detector_params.omega_increment)
+    yield from bps.abs_set(eiger.drv.omega_start, detector_params.omega_start, group)
+    yield from bps.abs_set(
+        eiger.drv.omega_increment, detector_params.omega_increment, group
+    )
+
+    if wait:
+        yield from bps.wait(group)
