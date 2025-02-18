3b67c39eb010370c58fe3a7b08cc5dbaf58b98f9
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-02-18 15:02:28 +0000

    WIP numtracker integration

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -5,6 +5,7 @@ from ophyd_async.epics.adpilatus import PilatusDetector
 from ophyd_async.fastcs.panda import HDFPanda
 
 from dodal.common.beamlines.beamline_utils import (
+    StartDocumentBasedPathProvider,
     device_factory,
     get_path_provider,
     set_path_provider,
@@ -34,19 +35,7 @@ BL = get_beamline_name("i22")
 PREFIX = BeamlinePrefix(BL)
 set_log_beamline(BL)
 set_utils_beamline(BL)
-
-# Currently we must hard-code the visit, determining the visit at runtime requires
-# infrastructure that is still WIP.
-# Communication with GDA is also WIP so for now we determine an arbitrary scan number
-# locally and write the commissioning directory. The scan number is not guaranteed to
-# be unique and the data is at risk - this configuration is for testing only.
-set_path_provider(
-    StaticVisitPathProvider(
-        BL,
-        Path("/dls/i22/data/2024/cm37271-2/bluesky"),
-        client=RemoteDirectoryServiceClient("http://i22-control:8088/api"),
-    )
-)
+set_path_provider(StartDocumentBasedPathProvider())
 
 
 @device_factory()
