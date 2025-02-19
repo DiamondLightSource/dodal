8c98a01b75505eb307937b4efc05b4d6e86425c6
DominicOram <DominicOram@users.noreply.github.com>
2025-02-19 09:34:08 +0000

    deploy: a2003819e2c4b895762410584c84a6a8d5ecae54

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.39.1.dev22+g611af93c4',
+    VERSION: '1.40.1.dev1+ga2003819e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
