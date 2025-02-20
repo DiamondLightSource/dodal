86b991314030b3164e9f0347f9c592ce459d31a6
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-20 15:59:43 +0000

    Include y_average, x_rms, y_rms as readables

--- src/dodal/devices/oml_autocollimator.py
+++ src/dodal/devices/oml_autocollimator.py
@@ -21,10 +21,10 @@ class Autocollimator(StandardReadable):
         with self.add_children_as_readables():
             self.x_average = epics_signal_r(float, f"{prefix}X:AVG")
 
-        self.y_average = epics_signal_r(float, f"{prefix}Y:AVG")
+            self.y_average = epics_signal_r(float, f"{prefix}Y:AVG")
 
-        self.x_rms = epics_signal_r(float, f"{prefix}X:RMS")
-        self.y_rms = epics_signal_r(float, f"{prefix}Y:RMS")
+            self.x_rms = epics_signal_r(float, f"{prefix}X:RMS")
+            self.y_rms = epics_signal_r(float, f"{prefix}Y:RMS")
 
         self.acquire = epics_signal_rw(AutocollimatorAcquire, f"{prefix}Acquire")
 
