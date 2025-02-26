0cae8c0143e3e873a096835d64fa73be923a289c
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-26 15:01:16 +0000

    Rename autocollimator attributes to mirror OAV

--- src/dodal/devices/oml_autocollimator.py
+++ src/dodal/devices/oml_autocollimator.py
@@ -11,20 +11,21 @@ class Autocollimator(StandardReadable):
     """Device representing Autocollimator in Optical Metrology Lab
 
     Attributes:
-        x_average: float readable average x value over two seconds
-        y_average: float readable average y value over two seconds
-        x_rms: float readable root mean sqaured x value over two seconds
-        y_rms: float readable root mean squared y value over two seconds
+        CentroidX_RBV: float readable average x value over two seconds
+        CentroidY_RBV: float readable average y value over two seconds
+        SigmaX_RBV: float readable root mean sqaured x value over two seconds
+        SigmaY_RBV: float readable root mean squared y value over two seconds
         acquire: AutocollimatorAcquire RW to start/start/monitor acquisition"""
 
     def __init__(self, prefix: str, name: str = ""):
         with self.add_children_as_readables():
-            self.x_average = epics_signal_r(float, f"{prefix}X:AVG")
+            # Attributes renamed to be similar to beamline OAVs:
+            self.CentroidX_RBV = epics_signal_r(float, f"{prefix}X:AVG")
 
-            self.y_average = epics_signal_r(float, f"{prefix}Y:AVG")
+            self.CentroidY_RBV = epics_signal_r(float, f"{prefix}Y:AVG")
 
-            self.x_rms = epics_signal_r(float, f"{prefix}X:RMS")
-            self.y_rms = epics_signal_r(float, f"{prefix}Y:RMS")
+            self.SigmaX_RBV = epics_signal_r(float, f"{prefix}X:RMS")
+            self.SigmaY_RBV = epics_signal_r(float, f"{prefix}Y:RMS")
 
         self.acquire = epics_signal_rw(AutocollimatorAcquire, f"{prefix}Acquire")
 
