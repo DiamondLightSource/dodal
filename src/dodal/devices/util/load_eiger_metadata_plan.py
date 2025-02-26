78ded5978ac3241c3cdfc829263551376ed0df12
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-02-26 15:36:40 +0000

    Add more arming steps

--- src/dodal/devices/util/load_eiger_metadata_plan.py
+++ src/dodal/devices/util/load_eiger_metadata_plan.py
@@ -2,10 +2,25 @@ import time
 
 import bluesky.plan_stubs as bps
 from bluesky.run_engine import RunEngine
-from mx_bluesky.common.utils.log import LOGGER
 from ophyd_async.epics.eiger import EigerDetector, EigerTriggerInfo
 
+from dodal.beamlines.i03 import fastcs_eiger
 from dodal.devices.detector import DetectorParams
+from dodal.log import LOGGER, do_default_logging_setup
+
+params = DetectorParams(
+    expected_energy_ev=12700,
+    exposure_time=0.004,
+    directory="/dls/i03/data/2025/cm40607-2/test_new_eiger",
+    prefix="",
+    detector_distance=255,
+    omega_start=0,
+    omega_increment=0.2,
+    num_images_per_trigger=1,
+    num_triggers=50,
+    use_roi_mode=True,
+    det_dist_to_beam_converter_path="/dls_sw/i03/software/daq_configuration/lookup/DetDistToBeamXYConverter.txt",
+)
 
 
 def load_metadata(
@@ -15,8 +30,11 @@ def load_metadata(
 ):
     start = time.time()
     assert detector_params.expected_energy_ev
-    yield from bps.stage(eiger)
-    LOGGER.info(f"Staging Eiger: {time.time() - start}s")
+    yield from bps.abs_set(eiger.odin.file_writer.capture, 0)  # type: ignore
+    yield from bps.abs_set(eiger.odin.meta.stop_writing, 1)  # type: ignore
+    LOGGER.info(f"Stopping Odin: {time.time() - start}s")
+    yield from set_cam_pvs(eiger, detector_params, wait=True)
+    LOGGER.info(f"Setting CAM PVs: {time.time() - start}s")
     yield from set_odin_pvs(eiger, detector_params, wait=True)
     LOGGER.info(f"Setting Odin PVs: {time.time() - start}s")
     yield from change_roi_mode(eiger, enable, detector_params, wait=True)
@@ -32,6 +50,27 @@ def load_metadata(
     )
 
     yield from bps.prepare(eiger, trigger_info)
+    LOGGER.info(f"Preparing Eiger: {time.time() - start}s")
+
+
+def set_cam_pvs(
+    eiger: EigerDetector,
+    detector_params: DetectorParams,
+    wait: bool,
+    group="cam_pvs",
+):
+    yield from bps.abs_set(
+        eiger.drv.acquire_time, detector_params.exposure_time, group=group
+    )
+    yield from bps.abs_set(
+        eiger.drv.acquire_period, detector_params.exposure_time, group=group
+    )
+    yield from bps.abs_set(eiger.drv.num_exposures, 1, group=group)
+    yield from bps.abs_set(eiger.drv.trigger_mode, "exts", group=group)
+    # Image mode not set...
+
+    if wait:
+        yield from bps.wait(group)
 
 
 def change_roi_mode(
@@ -121,3 +160,6 @@ def set_odin_pvs(
 
 if __name__ == "__main__":
     RE = RunEngine()
+    do_default_logging_setup()
+    eiger = fastcs_eiger(connect_immediately=True)
+    RE(load_metadata(eiger=eiger, enable=True, detector_params=params))
