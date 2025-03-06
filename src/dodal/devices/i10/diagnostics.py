fe4dd8d4b6f2647119bbe63505572299da906204
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-03-06 18:28:37 +0000

    change drop down to Position

--- src/dodal/devices/i10/diagnostics.py
+++ src/dodal/devices/i10/diagnostics.py
@@ -25,17 +25,17 @@ from dodal.devices.current_amplifiers import (
 )
 
 
-class D3DropDown(StrictEnum):
+class D3Position(StrictEnum):
     NOTHING = "Nothing"
     GRID = "Grid"
 
 
-class D5DropDown(StrictEnum):
+class D5Position(StrictEnum):
     CELL_IN = "Cell In"
     CELL_OUT = "Cell Out"
 
 
-class D5ADropDown(StrictEnum):
+class D5APosition(StrictEnum):
     OUT_OF_THE_BEAM = "Out of the beam"
     DIODE = "Diode"
     BLADE = "Blade"
@@ -45,13 +45,13 @@ class D5ADropDown(StrictEnum):
     GRID = "Grid"
 
 
-class D6DropDown(StrictEnum):
+class D6Position(StrictEnum):
     DIODE_OUT = "Diode Out"
     DIODE_IN = "Diode In"
     AU_MESH = "Au Mesh"
 
 
-class D7DropDown(StrictEnum):
+class D7Position(StrictEnum):
     OUT = "Out"
     SHUTTER = "Shutter"
 
@@ -75,7 +75,7 @@ class I10WebCamIODataType(StrictEnum):
     INT16 = "UInt16"
 
 
-class DropDownStage(StandardReadable, Movable):
+class Positioner(StandardReadable, Movable):
     """1D stage with a enum table to select positions."""
 
     def __init__(
@@ -83,13 +83,14 @@ class DropDownStage(StandardReadable, Movable):
         prefix: str,
         positioner_enum: type[StrictEnum],
         positioner_suffix: str = "",
-        dropdown_pv_suffix: str = ":MP:SELECT",
+        Positioner_pv_suffix: str = ":MP:SELECT",
         name: str = "",
     ) -> None:
         self._stage_motion = Motor(prefix=prefix + positioner_suffix)
         with self.add_children_as_readables(Format.CONFIG_SIGNAL):
-            self.stage_drop_down = epics_signal_rw(
-                positioner_enum, read_pv=prefix + positioner_suffix + dropdown_pv_suffix
+            self.stage_position = epics_signal_rw(
+                positioner_enum,
+                read_pv=prefix + positioner_suffix + Positioner_pv_suffix,
             )
         super().__init__(name=name)
         self.positioner_enum = positioner_enum
@@ -97,7 +98,7 @@ class DropDownStage(StandardReadable, Movable):
     @AsyncStatus.wrap
     async def set(self, value: StrictEnum) -> None:
         if value in self.positioner_enum:
-            await self.stage_drop_down.set(value=value)
+            await self.stage_position.set(value=value)
         else:
             raise ValueError(
                 f"{value} is not an allow position. Position must be: {self.positioner_enum}"
@@ -115,11 +116,11 @@ class I10PneumaticStage(StandardReadable):
         name: str = "",
     ) -> None:
         with self.add_children_as_readables(Format.HINTED_SIGNAL):
-            self.stage_drop_down_set = epics_signal_rw(
+            self.stage_position_set = epics_signal_rw(
                 InOutTable,
                 read_pv=prefix + "CON",
             )
-            self.stage_drop_down_readback = epics_signal_r(
+            self.stage_position_readback = epics_signal_r(
                 InOutReadBackTable,
                 read_pv=prefix + "STA",
             )
@@ -169,22 +170,22 @@ class ScreenCam(Device):
 
 
 class FullDiagnostic(Device):
-    """Compound device of a diagnostic with screen, webcam and dropdown stage."""
+    """Compound device of a diagnostic with screen, webcam and Positioner stage."""
 
     def __init__(
         self,
         prefix: str,
         positioner_enum: type[StrictEnum],
         positioner_suffix: str = "",
-        dropdown_pv_suffix: str = ":MP:SELECT",
+        Positioner_pv_suffix: str = ":MP:SELECT",
         cam_infix: str = "DCAM:",
         name: str = "",
     ) -> None:
-        self.positioner = DropDownStage(
+        self.positioner = Positioner(
             prefix=prefix,
             positioner_enum=positioner_enum,
             positioner_suffix=positioner_suffix,
-            dropdown_pv_suffix=dropdown_pv_suffix,
+            Positioner_pv_suffix=Positioner_pv_suffix,
         )
         self.screen = ScreenCam(
             prefix,
@@ -202,30 +203,30 @@ class I10Diagnostic(Device):
         self.d2 = ScreenCam(prefix=prefix + "PHDGN-02:")
         self.d3 = FullDiagnostic(
             prefix=prefix + "PHDGN-03:",
-            positioner_enum=D3DropDown,
+            positioner_enum=D3Position,
             positioner_suffix="DET:X",
         )
         self.d4 = ScreenCam(prefix=prefix + "PHDGN-04:")
-        self.d5 = DropDownStage(
+        self.d5 = Positioner(
             prefix=prefix + "IONC-01:",
-            positioner_enum=D5DropDown,
+            positioner_enum=D5Position,
             positioner_suffix="Y",
         )
 
-        self.d5A = DropDownStage(
+        self.d5A = Positioner(
             prefix=prefix + "PHDGN-06:",
-            positioner_enum=D5ADropDown,
+            positioner_enum=D5APosition,
             positioner_suffix="DET:X",
         )
 
         self.d6 = FullDiagnostic(
             prefix=prefix + "PHDGN-05:",
-            positioner_enum=D6DropDown,
+            positioner_enum=D6Position,
             positioner_suffix="DET:X",
         )
-        self.d7 = DropDownStage(
+        self.d7 = Positioner(
             prefix=prefix + "PHDGN-07:",
-            positioner_enum=D7DropDown,
+            positioner_enum=D7Position,
             positioner_suffix="Y",
         )
         super().__init__(name)
