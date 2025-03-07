d03646e81a8895dc42ee4ad558c1e359eb0e6935
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-03-07 10:47:45 +0000

    Remove redundant code

--- src/dodal/devices/oav/snapshots/snapshot_with_beam_centre.py
+++ src/dodal/devices/oav/snapshots/snapshot_with_beam_centre.py
@@ -1,43 +1,11 @@
 from ophyd_async.core import Reference, SignalR
-from PIL import Image, ImageDraw
+from PIL import Image
 
 from dodal.devices.areadetector.plugins.MJPG import MJPG
 
 CROSSHAIR_LENGTH_PX = 20
 CROSSHAIR_OUTLINE_COLOUR = "Black"
 CROSSHAIR_FILL_COLOUR = "White"
-
-
-def draw_crosshair(image: Image.Image, beam_x: int, beam_y: int):
-    draw = ImageDraw.Draw(image)
-    OUTLINE_WIDTH = 1
-    HALF_LEN = CROSSHAIR_LENGTH_PX / 2
-    draw.rectangle(
-        [
-            beam_x - OUTLINE_WIDTH,
-            beam_y - HALF_LEN - OUTLINE_WIDTH,
-            beam_x + OUTLINE_WIDTH,
-            beam_y + HALF_LEN + OUTLINE_WIDTH,
-        ],
-        fill=CROSSHAIR_OUTLINE_COLOUR,
-    )
-    draw.rectangle(
-        [
-            beam_x - HALF_LEN - OUTLINE_WIDTH,
-            beam_y - OUTLINE_WIDTH,
-            beam_x + HALF_LEN + OUTLINE_WIDTH,
-            beam_y + OUTLINE_WIDTH,
-        ],
-        fill=CROSSHAIR_OUTLINE_COLOUR,
-    )
-    draw.line(
-        ((beam_x, beam_y - HALF_LEN), (beam_x, beam_y + HALF_LEN)),
-        fill=CROSSHAIR_FILL_COLOUR,
-    )
-    draw.line(
-        ((beam_x - HALF_LEN, beam_y), (beam_x + HALF_LEN, beam_y)),
-        fill=CROSSHAIR_FILL_COLOUR,
-    )
 
 
 class SnapshotWithBeamCentre(MJPG):
