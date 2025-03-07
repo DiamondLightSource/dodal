7c586cf257385e6eba5a4842b39ea07625685d55
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-03-07 15:00:24 +0000

    Format visit tests

--- tests/common/test_visit.py
+++ tests/common/test_visit.py
@@ -40,88 +40,87 @@ async def test_when_get_current_collection_called_on_remote_directory_service_cl
     mock_request.assert_called_with("GET", f"{test_url}/numtracker")
 
 
-
 @pytest.fixture
 def start_doc_default_template() -> dict:
     return {
-            "uid":"27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
-            "time":1741264729.96875,
-            "versions":{
-                "ophyd":"1.10.0",
-                "bluesky":"1.13"
-            },
-            "data_session":"ab123",
-            "instrument":"p01",
-            "data_session_directory":"/p01/ab123",
-            "scan_id":22,
-            "plan_type":"generator",
-            "plan_name":"count",
-            "detectors":[
-                "det"
+        "uid": "27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
+        "time": 1741264729.96875,
+        "versions": {"ophyd": "1.10.0", "bluesky": "1.13"},
+        "data_session": "ab123",
+        "instrument": "p01",
+        "data_session_directory": "/p01/ab123",
+        "scan_id": 22,
+        "plan_type": "generator",
+        "plan_name": "count",
+        "detectors": ["det"],
+        "num_points": 1,
+        "num_intervals": 0,
+        "plan_args": {
+            "detectors": [
+                "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
             ],
-            "num_points":1,
-            "num_intervals":0,
-            "plan_args":{
-                "detectors":[
-                    "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
-                ],
-                "num":1,
-                "delay":0.0
-            },
-            "hints":{
-                "dimensions":[[["time"],"primary"]]
-            },
-            "shape":[1]
+            "num": 1,
+            "delay": 0.0,
+        },
+        "hints": {"dimensions": [[["time"], "primary"]]},
+        "shape": [1],
     }
 
-def test_start_document_based_path_provider_with_default_template_returns_correct_path_info(start_doc_default_template: RunStart):
+
+def test_start_document_based_path_provider_with_default_template_returns_correct_path_info(
+    start_doc_default_template: RunStart,
+):
     pp = StartDocumentBasedPathProvider()
-    pp.update_run(name = "start", start_doc = start_doc_default_template)
+    pp.update_run(name="start", start_doc=start_doc_default_template)
     path = pp("det")
 
-    assert path == PathInfo(directory_path=PosixPath('/p01/ab123'), filename='det-p01-22', create_dir_depth=0)
+    assert path == PathInfo(
+        directory_path=PosixPath("/p01/ab123"),
+        filename="det-p01-22",
+        create_dir_depth=0,
+    )
 
 
 @pytest.fixture
 def start_doc_custom_template() -> dict:
     return {
-            "uid":"27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
-            "time":1741264729.96875,
-            "versions":{
-                "ophyd":"1.10.0",
-                "bluesky":"1.13"
-            },
-            "data_session":"ab123",
-            "instrument":"p01",
-            "data_session_directory":"/p01/ab123",
-            "scan_id":22,
-            "template": "{device_name}-{instrument}-{scan_id}-custom",
-            "plan_type":"generator",
-            "plan_name":"count",
-            "detectors":[
-                "det"
+        "uid": "27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
+        "time": 1741264729.96875,
+        "versions": {"ophyd": "1.10.0", "bluesky": "1.13"},
+        "data_session": "ab123",
+        "instrument": "p01",
+        "data_session_directory": "/p01/ab123",
+        "scan_id": 22,
+        "template": "{device_name}-{instrument}-{scan_id}-custom",
+        "plan_type": "generator",
+        "plan_name": "count",
+        "detectors": ["det"],
+        "num_points": 1,
+        "num_intervals": 0,
+        "plan_args": {
+            "detectors": [
+                "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
             ],
-            "num_points":1,
-            "num_intervals":0,
-            "plan_args":{
-                "detectors":[
-                    "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
-                ],
-                "num":1,
-                "delay":0.0
-            },
-            "hints":{
-                "dimensions":[[["time"],"primary"]]
-            },
-            "shape":[1]
+            "num": 1,
+            "delay": 0.0,
+        },
+        "hints": {"dimensions": [[["time"], "primary"]]},
+        "shape": [1],
     }
 
-def test_start_document_based_path_provider_with_custom_template_returns_correct_path_info(start_doc_custom_template: RunStart):
+
+def test_start_document_based_path_provider_with_custom_template_returns_correct_path_info(
+    start_doc_custom_template: RunStart,
+):
     pp = StartDocumentBasedPathProvider()
-    pp.update_run(name = "start", start_doc = start_doc_custom_template)
+    pp.update_run(name="start", start_doc=start_doc_custom_template)
     path = pp("det")
 
-    assert path == PathInfo(directory_path=PosixPath('/p01/ab123'), filename='det-p01-22-custom', create_dir_depth=0)
+    assert path == PathInfo(
+        directory_path=PosixPath("/p01/ab123"),
+        filename="det-p01-22-custom",
+        create_dir_depth=0,
+    )
 
 
 @pytest.fixture
@@ -199,40 +198,36 @@ def test_start_document_based_path_provider_fails_with_missing_scan_id(
 @pytest.fixture
 def start_doc_default_data_session_directory() -> dict:
     return {
-            "uid":"27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
-            "time":1741264729.96875,
-            "versions":{
-                "ophyd":"1.10.0",
-                "bluesky":"1.13"
-            },
-            "data_session":"ab123",
-            "instrument":"p01",
-            "scan_id":22,
-            "plan_type":"generator",
-            "plan_name":"count",
-            "detectors":[
-                "det"
+        "uid": "27c48d2f-d8c6-4ac0-8146-fedf467ce11f",
+        "time": 1741264729.96875,
+        "versions": {"ophyd": "1.10.0", "bluesky": "1.13"},
+        "data_session": "ab123",
+        "instrument": "p01",
+        "scan_id": 22,
+        "plan_type": "generator",
+        "plan_name": "count",
+        "detectors": ["det"],
+        "num_points": 1,
+        "num_intervals": 0,
+        "plan_args": {
+            "detectors": [
+                "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
             ],
-            "num_points":1,
-            "num_intervals":0,
-            "plan_args":{
-                "detectors":[
-                    "<ophyd_async.epics.adaravis._aravis.AravisDetector object at 0x7f74c02b8710>"
-                ],
-                "num":1,
-                "delay":0.0
-            },
-            "hints":{
-                "dimensions":[[["time"],"primary"]]
-            },
-            "shape":[1]
+            "num": 1,
+            "delay": 0.0,
+        },
+        "hints": {"dimensions": [[["time"], "primary"]]},
+        "shape": [1],
     }
 
 
-def test_start_document_based_path_provider_sets_data_session_directory_default_to_tmp(start_doc_default_data_session_directory: RunStart):
+def test_start_document_based_path_provider_sets_data_session_directory_default_to_tmp(
+    start_doc_default_data_session_directory: RunStart,
+):
     pp = StartDocumentBasedPathProvider()
-    pp.update_run(name = "start", start_doc = start_doc_default_data_session_directory)
+    pp.update_run(name="start", start_doc=start_doc_default_data_session_directory)
     path = pp("det")
 
-    assert path == PathInfo(directory_path=PosixPath('/tmp'), filename='det-p01-22', create_dir_depth=0)
-
+    assert path == PathInfo(
+        directory_path=PosixPath("/tmp"), filename="det-p01-22", create_dir_depth=0
+    )
