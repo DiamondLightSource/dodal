8b7af2b1faba24a9f40b85069bb7ea73b6e07351
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-03-14 14:32:09 +0000

    Make signals for devices rw rather than just w

--- src/dodal/devices/electron_analyser/base_analyser.py
+++ src/dodal/devices/electron_analyser/base_analyser.py
@@ -1,9 +1,10 @@
-from bluesky.protocols import Movable
 from ophyd_async.core import StandardReadable
-from ophyd_async.epics.core import epics_signal_w
+from ophyd_async.epics.core import epics_signal_rw
+
+from dodal.devices.electron_analyser.base_region import TBaseRegion
 
 
-class BaseAnalyser(StandardReadable, Movable):
+class BaseAnalyser(StandardReadable):
     """
     Generic device to configure electron analyser with new region settings.
     Electron analysers should inherit from this class if they need further
@@ -23,32 +24,39 @@ class BaseAnalyser(StandardReadable, Movable):
         self.prefix = prefix
 
         with self.add_children_as_readables():
-            self.low_energy_signal = epics_signal_w(
+            self.low_energy_signal = epics_signal_rw(
                 float, self.prefix + BaseAnalyser.PV_LOW_ENERGY
             )
-            self.high_energy_signal = epics_signal_w(
+            self.high_energy_signal = epics_signal_rw(
                 float, self.prefix + BaseAnalyser.PV_HIGH_ENERGY
             )
 
-            self.slices_signal = epics_signal_w(
+            self.slices_signal = epics_signal_rw(
                 int, self.prefix + BaseAnalyser.PV_SLICES
             )
-            self.lens_mode_signal = epics_signal_w(
+            self.lens_mode_signal = epics_signal_rw(
                 str, self.prefix + BaseAnalyser.PV_LENS_MODE
             )
-            self.pass_energy_signal = epics_signal_w(
+            self.pass_energy_signal = epics_signal_rw(
                 int, self.prefix + BaseAnalyser.PV_PASS_ENERGY
             )
-            self.energy_step_signal = epics_signal_w(
+            self.energy_step_signal = epics_signal_rw(
                 float, self.prefix + BaseAnalyser.PV_ENERGY_STEP
             )
 
-            self.iterations_signal = epics_signal_w(
+            self.iterations_signal = epics_signal_rw(
                 int, self.prefix + BaseAnalyser.PV_ITERATIONS
             )
 
-            self.acquisition_mode_signal = epics_signal_w(
+            self.acquisition_mode_signal = epics_signal_rw(
                 str, self.prefix + BaseAnalyser.PV_ACQISITION_MODE
             )
 
         super().__init__(name)
+
+    def configure_with_region(
+        self, region: type[TBaseRegion], excitation_energy_eV: float
+    ) -> None:
+        # TODO: This needs to be done via plan or within the device for this class and subclasses.
+        # If via device, via Movable.set(...)?
+        pass
--- src/dodal/devices/electron_analyser/vgscienta/vgscienta_analyser.py
+++ src/dodal/devices/electron_analyser/vgscienta/vgscienta_analyser.py
@@ -1,4 +1,4 @@
-from ophyd_async.epics.core import epics_signal_w
+from ophyd_async.epics.core import epics_signal_rw
 
 from dodal.devices.electron_analyser.base_analyser import BaseAnalyser
 from dodal.devices.electron_analyser.vgscienta.vgscienta_region import (
@@ -22,29 +22,29 @@ class VGScientaAnalyser(BaseAnalyser):
         self.prefix = prefix
 
         with self.add_children_as_readables():
-            self.centre_energy_signal = epics_signal_w(
+            self.centre_energy_signal = epics_signal_rw(
                 float, self.prefix + VGScientaAnalyser.PV_CENTRE_ENERGY
             )
-            self.first_x_channel_signal = epics_signal_w(
+            self.first_x_channel_signal = epics_signal_rw(
                 int, self.prefix + VGScientaAnalyser.PV_FIRST_X_CHANNEL
             )
-            self.first_y_channel_signal = epics_signal_w(
+            self.first_y_channel_signal = epics_signal_rw(
                 int, self.prefix + VGScientaAnalyser.PV_FIRST_Y_CHANNEL
             )
-            self.last_x_channel_signal = epics_signal_w(
+            self.last_x_channel_signal = epics_signal_rw(
                 int, self.prefix + VGScientaAnalyser.PV_LAST_X_CHANNEL
             )
-            self.last_y_channel_signal = epics_signal_w(
+            self.last_y_channel_signal = epics_signal_rw(
                 int, self.prefix + VGScientaAnalyser.PV_LAST_Y_CHANNEL
             )
-            self.detector_mode_signal = epics_signal_w(
+            self.detector_mode_signal = epics_signal_rw(
                 DetectorMode, self.prefix + VGScientaAnalyser.PV_DETECTOR_MODE
             )
-            self.energy_step_signal = epics_signal_w(
+            self.energy_step_signal = epics_signal_rw(
                 float, self.prefix + VGScientaAnalyser.PV_ENERGY_STEP
             )
 
-            self.image_mode_signal = epics_signal_w(
+            self.image_mode_signal = epics_signal_rw(
                 str, self.prefix + VGScientaAnalyser.PV_IMAGE_MODE
             )
 
