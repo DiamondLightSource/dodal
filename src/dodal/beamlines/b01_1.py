2b36c81ff67873229f1e853d2b82d00c54557309
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-03-18 12:15:00 +0000

    add xyz positioner

--- src/dodal/beamlines/b01_1.py
+++ src/dodal/beamlines/b01_1.py
@@ -10,6 +10,7 @@ from dodal.common.beamlines.beamline_utils import (
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.beamlines.device_helpers import CAM_SUFFIX, HDF5_SUFFIX
+from dodal.devices.motors import XYZPositioner
 from dodal.common.visit import LocalDirectoryServiceClient, StaticVisitPathProvider
 from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
@@ -48,7 +49,7 @@ def panda() -> HDFPanda:
 
 
 @device_factory()
-def synchrotron() -> Synchrotron:
+def synchrotron_test() -> Synchrotron:
     return Synchrotron()
 
 
@@ -60,3 +61,12 @@ def manta() -> AravisDetector:
         drv_suffix=CAM_SUFFIX,
         fileio_suffix=HDF5_SUFFIX,
     )
+
+
+@device_factory()
+def stage_positioner() -> XYZPositioner:
+    return XYZPositioner(
+        f"{PREFIX.beamline_prefix}-MO-PPMAC-01:",
+        path_provider=get_path_provider(),
+    )
+
