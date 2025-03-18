60d8718616c94dc7f8520c9025ee87da07ae0665
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-03-18 14:34:23 +0000

    Update formating

--- tests/plan_stubs/test_data_session.py
+++ tests/plan_stubs/test_data_session.py
@@ -6,12 +6,16 @@ from dodal.plan_stubs.data_session import attach_data_session_metadata_wrapper
 
 
 def test_attach_data_session_metadata_wrapper(caplog, RE: RunEngine):
-
     def fake_plan() -> MsgGenerator[None]:
         yield from []
 
     path_provider = StartDocumentBasedPathProvider()
-    plan = attach_data_session_metadata_wrapper(plan = fake_plan(), provider = path_provider)
+    plan = attach_data_session_metadata_wrapper(
+        plan=fake_plan(), provider=path_provider
+    )
     RE(plan)
 
-    assert f"{path_provider} is not an UpdatingPathProvider, {attach_data_session_metadata_wrapper.__name__} will have no effect" in caplog.text
+    assert (
+        f"{path_provider} is not an UpdatingPathProvider, {attach_data_session_metadata_wrapper.__name__} will have no effect"
+        in caplog.text
+    )
