4055ba461b381b4d181d0547199f4a753288708d
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-03-19 11:28:43 +0000

    Revert "Roll back bluesky version (#1083)" (#1084)
    This reverts commit e3ec09dbbf25ed02872336858b99c9128ce6bea1.
    
    Co-authored-by: Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>

--- src/dodal/devices/thawer.py
+++ src/dodal/devices/thawer.py
@@ -1,6 +1,6 @@
 from asyncio import Task, create_task, sleep
 
-from bluesky.protocols import Stoppable
+from bluesky.protocols import Movable, Stoppable
 from ophyd_async.core import (
     AsyncStatus,
     Device,
@@ -21,18 +21,18 @@ class ThawerStates(StrictEnum):
     ON = "On"
 
 
-class ThawingTimer(Device, Stoppable):
+class ThawingTimer(Device, Stoppable, Movable[float]):
     def __init__(self, control_signal: SignalRW[ThawerStates]) -> None:
         self._control_signal_ref = Reference(control_signal)
         self._thawing_task: Task | None = None
         super().__init__("thaw_for_time_s")
 
     @AsyncStatus.wrap
-    async def set(self, time_to_thaw_for: float):
+    async def set(self, value: float):
         await self._control_signal_ref().set(ThawerStates.ON)
         if self._thawing_task and not self._thawing_task.done():
             raise ThawingException("Thawing task already in progress")
-        self._thawing_task = create_task(sleep(time_to_thaw_for))
+        self._thawing_task = create_task(sleep(value))
         try:
             await self._thawing_task
         finally:
