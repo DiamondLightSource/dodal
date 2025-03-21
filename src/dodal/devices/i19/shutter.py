28cde845c4f87b6e5dbe84aee8e7f1ce9166743f
Noemi <noemi.frisina@diamond.ac.uk>
2025-03-21 16:30:33 +0000

    Tidy up, add docstring and instantiate in beamlines

--- src/dodal/devices/i19/shutter.py
+++ src/dodal/devices/i19/shutter.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 import aiohttp
-from bluesky.protocols import Movable
+from bluesky.protocols import Movable, Reading
 from ophyd_async.core import AsyncStatus, StandardReadable
 from ophyd_async.epics.core import epics_signal_r
 
-from dodal.devices.hutch_shutter import HutchShutter, ShutterDemand
+from dodal.devices.hutch_shutter import HutchShutter, ShutterDemand, ShutterState
 from dodal.log import LOGGER
 
 OPTICS_BLUEAPI_URL = "https://i19-blueapi.diamond.ac.uk"
@@ -22,8 +22,22 @@ class HutchState(str, Enum):
 
 
 class AccessControlledShutter(StandardReadable, Movable):
+    """ I19-specific device to operate the hutch shutter.
+
+    This device will send a REST call to the blueapi instance controlling the optics \
+    hutch running on the I19 cluster, which will evaluate the current hutch in use vs \
+    the hutch sending the request and decide if the plan will be run or not.  
+    As the two hutches are located in series, checking the hutch in use is necessary to \
+    avoid accidentally operating the shutter from one hutch while the other has beamtime.
+    
+    The name of the hutch that wants to operate the shutter should be passed to the \
+    device upon instantiation.
+    
+    For details see the architecture described in 
+    https://github.com/DiamondLightSource/i19-bluesky/issues/30.
+    """
     def __init__(self, prefix: str, hutch: HutchState, name: str = "") -> None:
-        self.hutch_state = epics_signal_r(str, f"{prefix}:EHStatus.VALA")
+        self.shutter_status = epics_signal_r(ShutterState, f"{prefix}STA")
         self.hutch_request = hutch
         self.url = OPTICS_BLUEAPI_URL
         super().__init__(name)
