f51a4f682bbab564510dbad18e49b95eb0e09ec0
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-03-24 17:04:04 +0000

    Add docstrings to OAV devices

--- src/dodal/devices/oav/oav_detector.py
+++ src/dodal/devices/oav/oav_detector.py
@@ -122,6 +122,11 @@ class OAV(StandardReadable):
 
 
 class OAVBeamCentreFile(OAV):
+    """OAV device that reads its beam centre values from a file. The config parameter
+    must be a OAVConfigBeamCentre object, as this contains a filepath to where the beam
+    centre values are stored.
+    """
+
     def __init__(self, prefix: str, config: OAVConfigBeamCentre, name: str = ""):
         super().__init__(prefix, config, name)
 
@@ -135,6 +140,8 @@ class OAVBeamCentreFile(OAV):
 
 
 class OAVBeamCentrePV(OAV):
+    """OAV device that reads its beam centre values from PVs."""
+
     def __init__(
         self, prefix: str, config: OAVConfig, name: str = "", overlay_channel: int = 1
     ):
