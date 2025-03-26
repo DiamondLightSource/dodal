fdf3e8aa938ce9de52a8605545b796b96ce87660
James Gilbert <james.gilbert@diamond.ac.uk>
2025-03-26 09:02:23 +0000

    Change aithre-specific PVs back to read-writes

--- src/dodal/devices/aithre_lasershaping/laser_robot.py
+++ src/dodal/devices/aithre_lasershaping/laser_robot.py
@@ -1,8 +1,4 @@
-from ophyd_async.epics.core import (
-    epics_signal_r,
-    epics_signal_rw,
-    epics_signal_rw_rbv,
-)
+from ophyd_async.epics.core import epics_signal_rw
 
 from dodal.devices.robot import BartRobot
 
@@ -11,9 +7,7 @@ class LaserRobot(BartRobot):
     def __init__(self, name: str, prefix: str) -> None:
         super().__init__(name=name, prefix=prefix)
 
-        self.dewar_lid_heater = epics_signal_rw_rbv(str, prefix + "DW_1_CTRL")
+        self.dewar_lid_heater = epics_signal_rw(str, prefix + "DW_1_CTRL")
 
-        self.cryojet_retract = epics_signal_r(str, prefix + "OP_24_FORCE_OPTION")
-        self.beamline_safe = epics_signal_r(
-            str, prefix + "IP_16_FORCE_OPTION"
-        )  # 0 for on, 1 for no, 2 for off
+        self.cryojet_retract = epics_signal_rw(str, prefix + "OP_24_FORCE_OPTION")
+        self.beamline_safe = epics_signal_rw(str, prefix + "IP_16_FORCE_OPTION")
