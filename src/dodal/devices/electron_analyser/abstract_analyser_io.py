21d70001e93d7b3c7ea405cbaeeef10369040f7a
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-03-31 16:21:11 +0100

    Added generic data that can be read

--- src/dodal/devices/electron_analyser/abstract_analyser_io.py
+++ src/dodal/devices/electron_analyser/abstract_analyser_io.py
@@ -21,7 +21,6 @@ class AbstractAnalyserDriverIO(ABC, StandardReadable):
         self.adbase_cam = ADBaseIO(prefix, "adbase")
 
         with self.add_children_as_readables():
-            # Used for setting up region data acquisition
             self.low_energy = epics_signal_rw(float, prefix + "LOW_ENERGY")
             self.high_energy = epics_signal_rw(float, prefix + "HIGH_ENERGY")
             self.slices = epics_signal_rw(int, prefix + "SLICES")
