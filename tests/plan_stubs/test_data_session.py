810c22a5e50007b7f361d8bec8af536b027fecf2
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-04-10 10:12:42 +0000

    Update metadata wrapper test

--- tests/plan_stubs/test_data_session.py
+++ tests/plan_stubs/test_data_session.py
@@ -1,15 +1,22 @@
+from pathlib import Path
+
 from bluesky.run_engine import RunEngine
 from bluesky.utils import MsgGenerator
+from ophyd_async.core import PathInfo, PathProvider
 
-from dodal.common.visit import StartDocumentPathProvider
 from dodal.plan_stubs.data_session import attach_data_session_metadata_wrapper
+
+
+class FakeProvider(PathProvider):
+    def __call__(self, device_name: str | None = None) -> PathInfo:
+        return PathInfo(Path("/tmp"), "test")
 
 
 def test_attach_data_session_metadata_wrapper(caplog, RE: RunEngine):
     def fake_plan() -> MsgGenerator[None]:
         yield from []
 
-    path_provider = StartDocumentPathProvider()
+    path_provider = FakeProvider()
     plan = attach_data_session_metadata_wrapper(
         plan=fake_plan(), provider=path_provider
     )
