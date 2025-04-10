9cd372700e8102b579f8af825f019e184ffb2f0f
Abigail Emery <abigail.emery@diamond.ac.uk>
2025-04-10 15:36:43 +0000

    Use mock for path provider in test

--- tests/plan_stubs/test_data_session.py
+++ tests/plan_stubs/test_data_session.py
@@ -1,22 +1,16 @@
-from pathlib import Path
+from unittest.mock import Mock
 
 from bluesky.run_engine import RunEngine
 from bluesky.utils import MsgGenerator
-from ophyd_async.core import PathInfo, PathProvider
 
 from dodal.plan_stubs.data_session import attach_data_session_metadata_wrapper
-
-
-class FakeProvider(PathProvider):
-    def __call__(self, device_name: str | None = None) -> PathInfo:
-        return PathInfo(Path("/tmp"), "test")
 
 
 def test_attach_data_session_metadata_wrapper(caplog, RE: RunEngine):
     def fake_plan() -> MsgGenerator[None]:
         yield from []
 
-    path_provider = FakeProvider()
+    path_provider = Mock()
     plan = attach_data_session_metadata_wrapper(
         plan=fake_plan(), provider=path_provider
     )
