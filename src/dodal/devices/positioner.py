e8122a9d76808a0bdeb74a604972792d9d3eb000
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-04-15 15:51:38 +0100

    Tidy up positioner

--- src/dodal/devices/positioner.py
+++ src/dodal/devices/positioner.py
@@ -1,3 +1,5 @@
+from typing import TypeVar
+
 from bluesky.protocols import Movable
 from ophyd_async.core import (
     AsyncStatus,
@@ -10,32 +12,36 @@ from ophyd_async.epics.core import (
 )
 from ophyd_async.epics.motor import Motor
 
+T = TypeVar("T", bound=StrictEnum)
 
-class Positioner(StandardReadable, Movable):
+
+class Positioner(StandardReadable, Movable[T]):
     """1D stage with a enum table to select positions."""
 
     def __init__(
         self,
         prefix: str,
         positioner_enum: type[StrictEnum],
-        positioner_suffix: str = "",
-        Positioner_pv_suffix: str = ":MP:SELECT",
+        positioner_pv_suffix: str = ":MP:SELECT",
         name: str = "",
     ) -> None:
-        self._stage_motion = Motor(prefix=prefix + positioner_suffix)
+        self._stage_motion = Motor(prefix=prefix)
         with self.add_children_as_readables(Format.CONFIG_SIGNAL):
             self.stage_position = epics_signal_rw(
                 positioner_enum,
-                read_pv=prefix + positioner_suffix + Positioner_pv_suffix,
+                read_pv=prefix + positioner_pv_suffix,
             )
         super().__init__(name=name)
-        self.positioner_enum = positioner_enum
 
     @AsyncStatus.wrap
-    async def set(self, value: StrictEnum) -> None:
-        if value in self.positioner_enum:
-            await self.stage_position.set(value=value)
-        else:
-            raise TypeError(
-                f"{value} is not an allow position. Position must be: {self.positioner_enum}"
-            )
+    async def set(self, value: T) -> None:
+        await self.stage_position.set(value=value)
+
+
+def create_positioner(
+    datatype: type[T],
+    prefix,
+    positioner_pv_suffix: str = ":MP:SELECT",
+    name: str = "",
+) -> Positioner[T]:
+    return Positioner[datatype](prefix, datatype, positioner_pv_suffix, name)
