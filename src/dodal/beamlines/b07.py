0c2e15a26589d7e781d0ac020bbaab1c95c81d3e
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-04-15 14:56:40 +0000

    Added synchrotron to relavent beamlines. Removed name from device_factory functions

--- src/dodal/beamlines/b07.py
+++ src/dodal/beamlines/b07.py
@@ -5,6 +5,7 @@ from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beam
 from dodal.devices.electron_analyser.specs_analyser_io import (
     SpecsAnalyserDriverIO,
 )
+from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -12,6 +13,11 @@ BL = get_beamline_name("b07")
 PREFIX = BeamlinePrefix(BL, suffix="B")
 set_log_beamline(BL)
 set_utils_beamline(BL)
+
+
+@device_factory()
+def synchrotron() -> Synchrotron:
+    return Synchrotron()
 
 
 @device_factory()
