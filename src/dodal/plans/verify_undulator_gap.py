cb6202359268120d3ebcb5fcafd98fe4bdc673ab
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2025-04-16 17:41:38 +0100

    Change typing on check undulator gap devices

--- src/dodal/plans/verify_undulator_gap.py
+++ src/dodal/plans/verify_undulator_gap.py
@@ -2,14 +2,14 @@ from typing import Protocol, runtime_checkable
 
 from bluesky import plan_stubs as bps
 
-from dodal.devices.i03.dcm import DCM
+from dodal.devices.dcm import BaseDCM
 from dodal.devices.undulator import Undulator
 
 
 @runtime_checkable
 class CheckUndulatorDevices(Protocol):
     undulator: Undulator
-    dcm: DCM
+    dcm: BaseDCM
 
 
 def verify_undulator_gap(devices: CheckUndulatorDevices):
