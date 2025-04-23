e276a781211a53603c4683197a720245ab998472
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-04-23 16:38:15 +0100

    Fix tests

--- tests/devices/unit_tests/test_eiger.py
+++ tests/devices/unit_tests/test_eiger.py
@@ -269,10 +269,10 @@ def test_change_roi_mode_sets_correct_detector_size_constants(
 
     fake_eiger.change_roi_mode(roi_mode)
     mock_odin_height_set.assert_called_once_with(
-        expected_detector_dimensions.height, timeout=10
+        expected_detector_dimensions.height, timeout=ANY
     )
     mock_odin_width_set.assert_called_once_with(
-        expected_detector_dimensions.width, timeout=10
+        expected_detector_dimensions.width, timeout=ANY
     )
 
 
