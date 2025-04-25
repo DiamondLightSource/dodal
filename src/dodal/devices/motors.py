33782972f33c5f7d249782a5b8deed662bb05a4a
James Gilbert <james.gilbert@diamond.ac.uk>
2025-04-25 08:59:48 +0100

    Add I23-specific signals

--- src/dodal/devices/motors.py
+++ src/dodal/devices/motors.py
@@ -1,5 +1,15 @@
-from ophyd_async.core import StandardReadable
+import asyncio
+import math
+from enum import StrEnum
+
+from ophyd_async.core import StandardReadable, derived_signal_rw, soft_signal_rw
 from ophyd_async.epics.motor import Motor
+
+
+class Axis(StrEnum):
+    X = "X"
+    Y = "Y"
+    Z = "Z"
 
 
 class XYZPositioner(StandardReadable):
@@ -58,4 +68,44 @@ class SixAxisGonio(XYZPositioner):
             self.kappa = Motor(prefix + infix[3])
             self.phi = Motor(prefix + infix[4])
             self.omega = Motor(prefix + infix[5])
+
+            self.oav_i = derived_signal_rw(
+                self._get_i,
+                self._set_i,
+                x=self.x,
+                y=self.y,
+                z=self.z,
+                omega=self.omega,
+            )
+
+            self.oav_j = derived_signal_rw(
+                self._get_j,
+                self._set_j,
+                x=self.x,
+                y=self.y,
+                z=self.z,
+                omega=self.omega,
+            )
+
         super().__init__(name=name, prefix=prefix, infix=infix[0:3])
+
+    def _get_j(self, z: float, y: float, omega: float) -> float:
+        z_component = z * math.cos(math.radians(omega))
+        y_component = y * math.sin(math.radians(omega))
+        return z_component + y_component
+
+    async def _set_j(self, value: float) -> None:
+        omega = await self.omega.user_readback.get_value()
+        z_component = value * math.cos(math.radians(omega))
+        y_component = value * math.sin(math.radians(omega))
+        await asyncio.gather(
+            self.y.set(y_component),
+            self.z.set(z_component),
+            self.omega.set(omega),
+        )
+
+    def _get_i(self, x: float) -> float:
+        return x
+
+    async def _set_i(self, value: float) -> None:
+        await self.x.set(value)
