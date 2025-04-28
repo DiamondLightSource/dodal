1a3120ea0cd579fea6067a32531c44411689ad33
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-04-28 10:31:01 +0100

    respond to comments

--- src/dodal/devices/i18/d7positioner.py
+++ src/dodal/devices/i18/d7positioner.py
@@ -1,13 +1,12 @@
+import enum
 from ophyd_async.core import StandardReadable
-from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
+from ophyd_async.epics.core import epics_signal_r, epics_signal_rw, epics_signal_x
 
 
 class D7Positioner(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
-        self._prefix = prefix
         with self.add_children_as_readables():
-            self.setpoint = epics_signal_rw(float, prefix + ":SELECT")
-            self.readback = epics_signal_r(float, prefix + ":SELECT")
+            self.setpoint = epics_signal_rw(enum, prefix + ":SELECT")
             self.done = epics_signal_r(float, prefix + ":DMOV")
-            self.stop = epics_signal_rw(bool, prefix + ":STOP.PROC")
+            self.stop = epics_signal_x( prefix + ":STOP")
         super().__init__(name=name)
