1ffc48346772a299c61ce0ebd26c8963d5fde893
Iain Hall <iain.hall@diamond.ac.uk>
2025-04-28 12:58:01 +0100

    Add motors to beamline definition

--- src/dodal/beamlines/i18.py
+++ src/dodal/beamlines/i18.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from ophyd_async.fastcs.panda import HDFPanda
+# from ophyd_async.fastcs.panda import HDFPanda
 
 from dodal.common.beamlines.beamline_utils import (
     device_factory,
@@ -23,9 +23,11 @@ from dodal.devices.tetramm import TetrammDetector
 from dodal.devices.undulator import Undulator
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
+from ophyd_async.epics.motor import Motor
 
 BL = get_beamline_name("i18")
 PREFIX = BeamlinePrefix(BL)
+mono_base_pv = f"{PREFIX.beamline_prefix}-MO-DCM-01"
 set_log_beamline(BL)
 set_utils_beamline(BL)
 
@@ -38,7 +40,7 @@ set_utils_beamline(BL)
 set_path_provider(
     StaticVisitPathProvider(
         BL,
-        Path("/dls/i18/data/2024/cm37264-2/bluesky"),
+        Path("/dls/i18/data/2025/cm40636-2/bluesky"),
         client=LocalDirectoryServiceClient(),
     )
 )
@@ -65,23 +67,77 @@ def dcm() -> BaseDCM[RollCrystal, PitchAndRollCrystal]:
         xtal_2=PitchAndRollCrystal,
     )
 
+# ID gap [mm]
+@device_factory()
+def id_gap() -> Motor :
+    return Motor(f"{PREFIX.insertion_prefix}-MO-SERVC-01:BLGAPMTR")
+
+# Bragg angle [degrees]
+@device_factory()
+def dcm_bragg() -> Motor :
+    return Motor(f"{mono_base_pv}:BRAGG")
+
+# Perp [mm]
+@device_factory()
+def dcm_perp() -> Motor :
+    return Motor(f"{mono_base_pv}:PERP")
+
+# Energy [KeV]
+@device_factory()
+def dcm_energy() -> Motor :
+    return Motor(f"{mono_base_pv}:ENERGY")
+
+# Crystal1 roll [urad]
+@device_factory()
+def dcm_crystal1_roll() -> Motor :
+    return Motor(f"{mono_base_pv}:XTAL1:ROLL")
+
+# Crystal2 roll [urad]
+@device_factory()
+def dcm_crystal2_roll() -> Motor :
+    return Motor(f"{mono_base_pv}:XTAL2:ROLL")
+
+# Crystal type (Si111, Si311) - read only. Can use ophyd.pv_positioner.PVPositioner (see variant_positioner.py)
+@device_factory()
+def dcm_crystal() -> Motor :
+    return Motor(f"{mono_base_pv}:MP:X:SELECT")
 
 @device_factory()
-def slits_1() -> Slits:
+def s1_slits() -> Slits:
     return Slits(
         f"{PREFIX.beamline_prefix}-AL-SLITS-01:",
         x_centre="X:CENTER",
         y_centre="Y:CENTER",
     )
 
+@device_factory()
+def s2_slits() -> Slits:
+    return Slits(
+        f"{PREFIX.beamline_prefix}-AL-SLITS-02:",
+        x_centre="X:CENTER",
+        y_centre="Y:CENTER",
+    )
+
+"""
+GDA also has motors to control s3 individual blades positions. 
+"""
+@device_factory()
+def s3_slits() -> Slits:
+    return Slits(
+        f"{PREFIX.beamline_prefix}-AL-SLITS-03:",
+        x_centre="X:CENTER",
+        y_centre="Y:CENTER",
+    )
 
 # PandA IOC needs to be updated to support PVI
+"""
 @device_factory(skip=True)
 def panda1() -> HDFPanda:
     return HDFPanda(
         f"{PREFIX.beamline_prefix}-MO-PANDA-01:",
         path_provider=get_path_provider(),
     )
+"""
 
 
 @device_factory()
@@ -104,24 +160,39 @@ def it() -> TetrammDetector:
 @device_factory(skip=True)
 # VFM uses different IOC than HFM https://github.com/DiamondLightSource/dodal/issues/1009
 def vfm() -> KBMirror:
-    return KBMirror(f"{PREFIX.beamline_prefix}-OP-VFM-01:")
+    return KBMirror(f"{PREFIX.beamline_prefix}-OP-KBM-01:VFM:")
 
 
 @device_factory()
 def hfm() -> KBMirror:
-    return KBMirror(f"{PREFIX.beamline_prefix}-OP-HFM-01:")
+    return KBMirror(f"{PREFIX.beamline_prefix}-OP-KBM-01:HFM:")
 
 
 @device_factory()
 def d7diode() -> Diode:
     return Diode(f"{PREFIX.beamline_prefix}-DI-PHDGN-07:")
 
-
-@device_factory()
-def main_table() -> Table:
-    return Table(f"{PREFIX.beamline_prefix}-MO-TABLE-01:")
-
-
 @device_factory()
 def thor_labs_stage() -> ThorLabsStage:
     return ThorLabsStage(f"{PREFIX.beamline_prefix}-MO-TABLE-02:")
+
+# Table1 motors
+@device_factory()
+def table1() -> Table:
+    return Table(f"{PREFIX.beamline_prefix}-MO-TABLE-01:")
+
+@device_factory()
+def t1x() -> Motor:
+    return Motor(f"{PREFIX.beamline_prefix}-MO-TABLE-01:X")
+
+@device_factory()
+def t1y() -> Motor:
+    return Motor(f"{PREFIX.beamline_prefix}-MO-TABLE-01:Y")
+
+@device_factory()
+def t1z() -> Motor:
+    return Motor(f"{PREFIX.beamline_prefix}-MO-TABLE-01:Z")
+
+@device_factory()
+def t1theta() -> Motor:
+    return Motor(f"{PREFIX.beamline_prefix}-MO-TABLE-01:THETA")
