5f341803789db5f009116c40cfe1e5fee2de1f70
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-04-30 14:18:42 +0100

    Fix PR comments

--- src/dodal/beamlines/i04.py
+++ src/dodal/beamlines/i04.py
@@ -1,12 +1,9 @@
 import os
 
-from ophyd_async.fastcs.panda import HDFPanda
-
 from dodal.common.beamlines.beamline_parameters import get_beamline_parameters
 from dodal.common.beamlines.beamline_utils import (
     device_factory,
     device_instantiation,
-    get_path_provider,
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.aperturescatterguard import (
@@ -20,7 +17,7 @@ from dodal.devices.detector import DetectorParams
 from dodal.devices.detector.detector_motion import DetectorMotion
 from dodal.devices.diamond_filter import DiamondFilter, I04Filters
 from dodal.devices.eiger import EigerDetector
-from dodal.devices.fast_grid_scan import PandAFastGridScan, ZebraFastGridScan
+from dodal.devices.fast_grid_scan import ZebraFastGridScan
 from dodal.devices.flux import Flux
 from dodal.devices.i03.dcm import DCM
 from dodal.devices.i04.transfocator import Transfocator
@@ -123,7 +120,6 @@ def beamstop() -> Beamstop:
     return Beamstop(
         f"{PREFIX.beamline_prefix}-MO-BS-01:",
         beamline_parameters=get_beamline_parameters(),
-        name="beamstop",
     )
 
 
@@ -379,38 +375,11 @@ def diamond_filter() -> DiamondFilter[I04Filters]:
 
 
 @device_factory()
-def panda_fast_grid_scan() -> PandAFastGridScan:
-    """Get the i04 panda_fast_grid_scan device, instantiate it if it hasn't already been.
-    If this is called when already instantiated in i04, it will return the existing object.
-    This is used instead of the zebra_fast_grid_scan device when using the PandA.
-    """
-    return PandAFastGridScan(
-        prefix=f"{PREFIX.beamline_prefix}-MO-SGON-01:",
-        name="panda_fast_grid_scan",
-    )
-
-
-@device_factory()
-def panda() -> HDFPanda:
-    """Get the i04 panda device, instantiate it if it hasn't already been.
-    If this is called when already instantiated in i04, it will return the existing object.
-    """
-    return HDFPanda(
-        f"{PREFIX.beamline_prefix}-EA-PANDA-01:",
-        path_provider=get_path_provider(),
-        name="panda",
-    )
-
-
-@device_factory()
 def zocalo() -> ZocaloResults:
     """Get the i04 ZocaloResults device, instantiate it if it hasn't already been.
     If this is called when already instantiated in i04, it will return the existing object.
     """
-    return ZocaloResults(
-        name="zocalo",
-        prefix=PREFIX.beamline_prefix,
-    )
+    return ZocaloResults()
 
 
 @device_factory(skip=BL == "s03")
@@ -420,5 +389,4 @@ def pin_tip_detection() -> PinTipDetection:
     """
     return PinTipDetection(
         f"{PREFIX.beamline_prefix}-DI-OAV-01:",
-        "pin_tip_detection",
     )
