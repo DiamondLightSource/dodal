b93259f075615af9aa4260ff86bbf915c50669e7
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-07 09:45:29 +0100

    Refactor component calculations

--- src/dodal/devices/motors.py
+++ src/dodal/devices/motors.py
@@ -65,11 +65,11 @@ class SixAxisGonio(XYZPositioner):
     infix:
         EPICS PV, default is the ("X", "Y", "Z", "KAPPA", "PHI", "OMEGA").
     upward_axis_at_0:
-        Axis as a string, "X", "Y" or "Z", that points upwards at omega = 0 degrees.
-        Default "Y".
+        Axis as an enum (Axis.X, Axis.Y or Axis.Z), that points upwards when ω = 0°.
+        Default is Axis.Y
     upward_axis_at_minus_90:
-        Axis as a string, "X", "Y" or "Z", that points upwards at omega = -90 degrees.
-        Default "Z".
+        Axis as an enum (Axis.X, Axis.Y or Axis.Z), that points upwards when ω = -90°.
+        Default is Axis.Z
     Notes
     -----
     Example usage::
@@ -94,19 +94,19 @@ class SixAxisGonio(XYZPositioner):
             "PHI",
             "OMEGA",
         ),
-        upward_axis_at_0: Axis = "Y",
-        upward_axis_at_minus_90: Axis = "Z",
+        upward_axis_at_0: Axis = Axis.Y,
+        upward_axis_at_minus_90: Axis = Axis.Z,
     ):
         with self.add_children_as_readables():
             self.kappa = Motor(prefix + infix[3])
             self.phi = Motor(prefix + infix[4])
             self.omega = Motor(prefix + infix[5])
 
-            """Axes corresponding to the co-ordinate axis signals that move the stage
-            upwards when omega = 0 and omega = -90 respectively i.e. vertical and
-            horizontal movement in the rotating frame of reference of the stage"""
-
         super().__init__(name=name, prefix=prefix, infix=infix[0:3])
+
+        # Axes corresponding to the co-ordinate axis signals that move the stage
+        # upwards when omega = 0 and omega = -90 respectively i.e. vertical and
+        # horizontal movement in the rotating frame of reference of the stage
         self._vertical_stage_axis = self._get_axis_signal(upward_axis_at_0)
         self._horizontal_stage_axis = self._get_axis_signal(upward_axis_at_minus_90)
 
@@ -123,16 +123,18 @@ class SixAxisGonio(XYZPositioner):
     def _get_j(
         self, vertical_stage_axis: float, horizontal_stage_axis: float, omega: float
     ) -> float:
-        vertical_axis_component = vertical_stage_axis * math.cos(math.radians(omega))
-        horizontal_axis_component = horizontal_stage_axis * math.sin(
-            math.radians(omega)
+        vertical_axis_component = calculate_vertical_j_component(
+            vertical_stage_axis, omega
+        )
+        horizontal_axis_component = calculate_horizontal_j_component(
+            horizontal_stage_axis, omega
         )
         return vertical_axis_component + horizontal_axis_component
 
     async def _set_j(self, value: float) -> None:
         omega = await self.omega.user_readback.get_value()
-        vertical_component = value * math.cos(math.radians(omega))
-        horizontal_component = value * math.sin(math.radians(omega))
+        vertical_component = calculate_vertical_j_component(value, omega)
+        horizontal_component = calculate_horizontal_j_component(value, omega)
         await asyncio.gather(
             self._vertical_stage_axis.set(vertical_component),
             self._horizontal_stage_axis.set(horizontal_component),
@@ -153,3 +155,11 @@ class SixAxisGonio(XYZPositioner):
                 return self.y
             case Axis.Z:
                 return self.z
+
+
+def calculate_vertical_j_component(length: float, angle: float):
+    return length * math.cos(math.radians(angle))
+
+
+def calculate_horizontal_j_component(length: float, angle: float):
+    return length * math.sin(math.radians(angle))
