b01a846522b6f718329d3559f6b7652b668a4262
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2025-05-08 13:14:01 +0000

    Remove locate method as better implementation required

--- src/dodal/devices/i16/mecarobot.py
+++ src/dodal/devices/i16/mecarobot.py
@@ -1,12 +1,10 @@
 import asyncio
 
-from bluesky.protocols import Locatable, Location
 from ophyd_async.core import (
     DerivedSignalFactory,
     DeviceVector,
     StandardReadable,
     Transform,
-    soft_signal_rw,
     wait_for_value,
 )
 from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
@@ -71,7 +69,7 @@ class RobotTransform(Transform):
         return derived_readings
 
 
-class Meca500(StandardReadable, Locatable[CartesianSpace]):
+class Meca500(StandardReadable):
     """Meca500 device that derives x, y, z, alpha, beta, and gamma, from joints."""
 
     def __init__(self, prefix="", name="") -> None:
@@ -102,34 +100,19 @@ class Meca500(StandardReadable, Locatable[CartesianSpace]):
             joint_6=self.joints[5],
         )
 
-        self.x = self._factory.derived_signal_rw(float, "x")
-        self.y = self._factory.derived_signal_rw(float, "y")
-        self.z = self._factory.derived_signal_rw(float, "z")
-        self.alpha = self._factory.derived_signal_rw(float, "alpha")
-        self.beta = self._factory.derived_signal_rw(float, "beta")
-        self.gamma = self._factory.derived_signal_rw(float, "gamma")
-
-        self.x_sp = soft_signal_rw(float, name="x")
-        self.y_sp = soft_signal_rw(float, name="y")
-        self.z_sp = soft_signal_rw(float, name="z")
-        self.alpha_sp = soft_signal_rw(float, name="alpha")
-        self.beta_sp = soft_signal_rw(float, name="beta")
-        self.gamma_sp = soft_signal_rw(float, name="gamma")
+        with self.add_children_as_readables():
+            self.x = self._factory.derived_signal_rw(float, "x")
+            self.y = self._factory.derived_signal_rw(float, "y")
+            self.z = self._factory.derived_signal_rw(float, "z")
+            self.alpha = self._factory.derived_signal_rw(float, "alpha")
+            self.beta = self._factory.derived_signal_rw(float, "beta")
+            self.gamma = self._factory.derived_signal_rw(float, "gamma")
 
         super().__init__(name=name)
 
     async def set(self, target_cartesian: CartesianSpace) -> None:  # type: ignore  # noqa: D102
         """Set cartesian position of manipulator."""
         transform = await self._factory.transform()
-
-        await asyncio.gather(
-            self.x_sp.set(target_cartesian["x"]),
-            self.y_sp.set(target_cartesian["y"]),
-            self.z_sp.set(target_cartesian["z"]),
-            self.alpha_sp.set(target_cartesian["alpha"]),
-            self.beta_sp.set(target_cartesian["beta"]),
-            self.gamma_sp.set(target_cartesian["gamma"]),
-        )
 
         values = await asyncio.gather(
             *(self.joints[i].get_value() for i in range(len(self.joints)))
@@ -150,25 +133,3 @@ class Meca500(StandardReadable, Locatable[CartesianSpace]):
         )
         await self.move_joints_array.set(True)
         await wait_for_value(self.eom, 0.0, timeout=5)
-
-    async def locate(self) -> Location[CartesianSpace]:
-        """Return last commanded and current cartesian position of tooltip."""
-        virtual_setpoints = CartesianSpace(
-            x=await self.x_sp.get_value(),
-            y=await self.y_sp.get_value(),
-            z=await self.z_sp.get_value(),
-            alpha=await self.alpha_sp.get_value(),
-            beta=await self.beta_sp.get_value(),
-            gamma=await self.gamma_sp.get_value(),
-        )
-
-        virtual_readbacks = CartesianSpace(
-            x=await self.x.get_value(),
-            y=await self.y.get_value(),
-            z=await self.z.get_value(),
-            alpha=await self.alpha.get_value(),
-            beta=await self.beta.get_value(),
-            gamma=await self.gamma.get_value(),
-        )
-
-        return Location(setpoint=virtual_setpoints, readback=virtual_readbacks)
