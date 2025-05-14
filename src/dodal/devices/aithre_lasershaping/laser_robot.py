66678f4a365a9c33553b97571c164216ec1299ae
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-14 09:21:28 +0100

    Split r/w of beamline_safe to have different types

--- src/dodal/devices/aithre_lasershaping/laser_robot.py
+++ src/dodal/devices/aithre_lasershaping/laser_robot.py
@@ -1,6 +1,14 @@
-from ophyd_async.epics.core import epics_signal_rw
+from enum import Enum
+
+from ophyd_async.epics.core import epics_signal_r, epics_signal_rw, epics_signal_w
 
 from dodal.devices.robot import BartRobot
+
+
+class BeamlineSafe(Enum):
+    ON = "On"
+    NO = "No"
+    OFF = "Off"
 
 
 class LaserRobot(BartRobot):
@@ -10,4 +18,7 @@ class LaserRobot(BartRobot):
         self.dewar_lid_heater = epics_signal_rw(str, prefix + "DW_1_CTRL")
 
         self.cryojet_retract = epics_signal_rw(str, prefix + "OP_24_FORCE_OPTION")
-        self.beamline_safe = epics_signal_rw(str, prefix + "IP_16_FORCE_OPTION")
+        self.set_beamline_safe = epics_signal_w(
+            BeamlineSafe, prefix + "IP_16_FORCE_OPTION"
+        )
+        self.read_beamline_safe = epics_signal_r(bool, prefix + "ROBOT_IP_16_BITS.B0")
