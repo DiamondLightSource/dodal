ca0f33c6e6a665a5c7877586780ee697f638ae21
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-19 14:40:08 +0100

    Move super init to try and give PVs a proper name

--- src/dodal/devices/aithre_lasershaping/laser_robot.py
+++ src/dodal/devices/aithre_lasershaping/laser_robot.py
@@ -12,8 +12,6 @@ class BeamlineSafe(StrictEnum):
 
 class LaserRobot(BartRobot):
     def __init__(self, name: str, prefix: str) -> None:
-        super().__init__(name=name, prefix=prefix)
-
         self.dewar_lid_heater = epics_signal_rw(str, prefix + "DW_1_CTRL")
 
         self.cryojet_retract = epics_signal_rw(str, prefix + "OP_24_FORCE_OPTION")
@@ -21,3 +19,4 @@ class LaserRobot(BartRobot):
             BeamlineSafe, prefix + "IP_16_FORCE_OPTION"
         )
         self.read_beamline_safe = epics_signal_r(int, prefix + "ROBOT_IP_16_BITS.B0")
+        super().__init__(name=name, prefix=prefix)
