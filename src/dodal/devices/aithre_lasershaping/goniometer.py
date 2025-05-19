e9afdf22b9ca33022ded00cacd0e2f122a55d03c
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-19 16:41:17 +0100

    Expand doc string to explain signals

--- src/dodal/devices/aithre_lasershaping/goniometer.py
+++ src/dodal/devices/aithre_lasershaping/goniometer.py
@@ -6,7 +6,16 @@ from ophyd_async.epics.motor import Motor
 
 
 class Goniometer(StandardReadable):
-    """Goniometer and the stages it sits on"""
+    """The Aithre lab goniometer and the XYZ stage it sits on.
+
+    `x`, `y` and `z` control the axes of the positioner at the base, while `sampy` and
+    `sampz` control the positioner of the sample. `omega` is the rotation about the
+    x-axis (along the length of the sample holder).
+
+    The `vertical_position` signal refers to the height of the sample from the point of
+    view of the OAV and setting this value moves the sample vertically in the OAV plane
+    regardless of the current rotation.
+    """
 
     def __init__(self, prefix: str, name: str = "") -> None:
         self.x = Motor(prefix + "X")
