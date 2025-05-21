773768490b8c9ff79276f517743537e1296fca57
James Gilbert <james.gilbert@diamond.ac.uk>
2025-05-21 16:39:49 +0100

    Instantiate oav in aithre

--- src/dodal/beamlines/aithre.py
+++ src/dodal/beamlines/aithre.py
@@ -1,5 +1,10 @@
 from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.devices.aithre_lasershaping.goniometer import Goniometer
+from dodal.devices.oav.oav_detector import NullZoomController, OAVBeamCentreFile
+from dodal.devices.oav.oav_parameters import OAVConfigBeamCentre
+
+ZOOM_PARAMS_FILE = "/dls_sw/i23/software/aithre/aithre_oav.xml"
+DISPLAY_CONFIG = "/dls_sw/i23/software/aithre/aithre_display.configuration"
 
 PREFIX = "LA18L"
 
@@ -7,3 +12,13 @@ PREFIX = "LA18L"
 @device_factory()
 def goniometer() -> Goniometer:
     return Goniometer(f"{PREFIX}-MO-LSR-01:", "goniometer")
+
+
+@device_factory()
+def oav(params: OAVConfigBeamCentre | None = None) -> OAVBeamCentreFile:
+    return OAVBeamCentreFile(
+        prefix=f"{PREFIX}-DI-OAV-01:",
+        config=params or OAVConfigBeamCentre(ZOOM_PARAMS_FILE, DISPLAY_CONFIG),
+        name="oav",
+        zoom_controller=NullZoomController(),
+    )
