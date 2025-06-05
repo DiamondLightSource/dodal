738cd1a08e9d32ea346c6836c0e97c83ec5dd303
Douglas Winter <douglas.winter@diamond.ac.uk>
2025-06-05 10:58:12 +0000

    Add MotorGroup for conceptually related motors

--- src/dodal/beamlines/k11.py
+++ src/dodal/beamlines/k11.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 
-from ophyd_async.epics.motor import Motor
-
 from dodal.common.beamlines.beamline_utils import (
     device_factory,
     set_path_provider,
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.visit import RemoteDirectoryServiceClient, StaticVisitPathProvider
+from dodal.devices.motors import MotorGroup
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -26,10 +25,12 @@ set_path_provider(
 
 
 @device_factory()
-def kb_cs_x() -> Motor:
-    return Motor(f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:X")
-
-
-@device_factory()
-def kb_cs_y() -> Motor:
-    return Motor(f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:Y")
+def kb() -> MotorGroup:
+    """KB mirrors in pixel space"""
+    return MotorGroup(
+        name="kb",
+        motor_name_to_pv={
+            "x": f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:X",
+            "y": f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:Y",
+        },
+    )
