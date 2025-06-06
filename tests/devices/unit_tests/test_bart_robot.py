3dde0ed53f22a0b0b088b72ed42ed8d04220819e
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-06-06 17:09:04 +0100

    Make the bart_robot timeout test slightly less hair-trigger

--- tests/devices/unit_tests/test_bart_robot.py
+++ tests/devices/unit_tests/test_bart_robot.py
@@ -15,8 +15,8 @@ from dodal.devices.robot import BartRobot, PinMounted, RobotLoadFailed, SampleLo
 
 async def _get_bart_robot() -> BartRobot:
     device = BartRobot("robot", "-MO-ROBOT-01:")
-    device.LOAD_TIMEOUT = 0.01  # type: ignore
-    device.NOT_BUSY_TIMEOUT = 0.01  # type: ignore
+    device.LOAD_TIMEOUT = 0.05  # type: ignore
+    device.NOT_BUSY_TIMEOUT = 0.05  # type: ignore
     await device.connect(mock=True)
     return device
 
@@ -137,7 +137,7 @@ async def test_set_waits_for_both_timeouts(mock_wait_for: AsyncMock):
     device = await _get_bart_robot()
     device._load_pin_and_puck = MagicMock()
     await device.set(SampleLocation(1, 2))
-    mock_wait_for.assert_awaited_once_with(ANY, timeout=0.02)
+    mock_wait_for.assert_awaited_once_with(ANY, timeout=0.1)
 
 
 async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and_still_throw_if_error_not_cleared():
