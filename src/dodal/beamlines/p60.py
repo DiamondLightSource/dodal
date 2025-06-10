ddd5870805811919efe2fdd660b5c871e7565a4c
eir17846 <victor.rogalev@diamond.ac.uk>
2025-06-10 10:34:04 +0000

    add MgKa and Alka source devices in p60

--- src/dodal/beamlines/p60.py
+++ src/dodal/beamlines/p60.py
@@ -3,6 +3,7 @@ from dodal.common.beamlines.beamline_utils import (
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.electron_analyser.vgscienta import VGScientaAnalyserDriverIO
+from dodal.devices.p60.lab_xray_source import LabXraySource, LabXraySourceReadable
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -15,3 +16,13 @@ set_utils_beamline(BL)
 @device_factory()
 def analyser_driver() -> VGScientaAnalyserDriverIO:
     return VGScientaAnalyserDriverIO(prefix=f"{PREFIX.beamline_prefix}-EA-DET-01:CAM:")
+
+
+@device_factory()
+def AlKa_source() -> LabXraySourceReadable:
+    return LabXraySourceReadable(LabXraySource.AlKa, "AlKa_source")
+
+
+@device_factory()
+def MgKa_source() -> LabXraySourceReadable:
+    return LabXraySourceReadable(LabXraySource.MgKa, "MgKa_source")
--- src/dodal/devices/p60/lab_xray_source.py
+++ src/dodal/devices/p60/lab_xray_source.py
@@ -11,16 +11,13 @@ from ophyd_async.core import StandardReadableFormat as Format
 
 class LabXraySource(Enum):
     AlKa = 1486.6
-    MgKa = 1280.0
+    MgKa = 1253.6
 
 
-class LabXraySourceMotor(StandardReadable):
+class LabXraySourceReadable(StandardReadable):
     """Simple device to get the laboratory x-ray tube energy reading"""
 
     def __init__(self, xraysource: Enum, name: str) -> None:
         with self.add_children_as_readables(Format.HINTED_SIGNAL):
             self.user_readback = soft_signal_rw(float, initial_value=xraysource.value)
         super().__init__(name=name)
-
-
-mySource = LabXraySourceMotor(LabXraySource.AlKa, "mySource")
