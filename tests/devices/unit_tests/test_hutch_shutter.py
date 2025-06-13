31d6a6ca1f01c38f0eec0bdb56a1222dca38dfcd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-06-13 15:22:03 +0100

    Fix test

--- tests/devices/unit_tests/test_hutch_shutter.py
+++ tests/devices/unit_tests/test_hutch_shutter.py
@@ -35,14 +35,14 @@ def test_shutter_can_be_created(fake_shutter: HutchShutter):
     assert isinstance(fake_shutter, HutchShutter)
 
 
-async def test_shutter_raises_error_on_set_if_hutch_not_interlocked(
+async def test_shutter_raises_error_on_set_if_hutch_not_interlocked_on_open(
     fake_shutter: HutchShutter,
 ):
     set_mock_value(fake_shutter.interlock.status, 1)
     assert await fake_shutter.interlock.shutter_safe_to_operate() is False
 
     with pytest.raises(ShutterNotSafeToOperateError):
-        await fake_shutter.set(ShutterDemand.CLOSE)
+        await fake_shutter.set(ShutterDemand.OPEN)
 
 
 @pytest.mark.parametrize(
