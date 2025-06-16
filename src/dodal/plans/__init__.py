57fd808b0d4a4638ab7b34dd62f848ad890857e3
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-06-16 15:48:51 +0000

    Remove bimorph_optimisation from plan __init__ __all__

--- src/dodal/plans/__init__.py
+++ src/dodal/plans/__init__.py
@@ -1,5 +1,4 @@
-from .bimorph import bimorph_optimisation
 from .scanspec import spec_scan
 from .wrapped import count
 
-__all__ = ["count", "spec_scan", "bimorph_optimisation"]
+__all__ = ["count", "spec_scan"]
