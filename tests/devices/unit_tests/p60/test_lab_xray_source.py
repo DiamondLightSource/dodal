894e4f322deaa8f82e294f62fc2fa6b2d2bdfcd7
eir17846 <victor.rogalev@diamond.ac.uk>
2025-06-17 15:10:36 +0000

    add test - move energy fails

--- tests/devices/unit_tests/p60/test_lab_xray_source.py
+++ tests/devices/unit_tests/p60/test_lab_xray_source.py
@@ -99,3 +99,20 @@ def test_move_fail(
         AssertionError, match=re.escape("does not implement all Movable methods")
     ):
         RE(mv(mg_kalpha_source, new_energy))
+
+
+def test_move_energy_fail(
+    al_kalpha_source: LabXraySourceReadable,
+    mg_kalpha_source: LabXraySourceReadable,
+    RE: RunEngine,
+):
+    new_energy = 100.0
+    with pytest.raises(
+        AssertionError, match=re.escape("does not implement all Movable methods")
+    ):
+        RE(mv(al_kalpha_source.energy_ev, new_energy))
+
+    with pytest.raises(
+        AssertionError, match=re.escape("does not implement all Movable methods")
+    ):
+        RE(mv(mg_kalpha_source.energy_ev, new_energy))
