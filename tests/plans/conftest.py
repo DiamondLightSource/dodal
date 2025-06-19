5eebb094167154783f5cb7bad5911531a06b9fa5
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-06-19 14:13:44 +0100

    Revert previous move of NullPatternGenerator into ophyd-async

--- tests/plans/conftest.py
+++ tests/plans/conftest.py
@@ -1,11 +1,11 @@
+import asyncio
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 from bluesky.run_engine import RunEngine
 from ophyd_async.core import PathProvider, StandardDetector, init_devices
-from ophyd_async.sim import SimBlobDetector, SimMotor
-from ophyd_async.sim._pattern_generator import NullPatternGenerator
+from ophyd_async.sim import PatternGenerator, SimBlobDetector, SimMotor
 from tests.constants import UNDULATOR_ID_GAP_LOOKUP_TABLE_PATH
 
 from dodal.devices.common_dcm import BaseDCM
@@ -36,7 +36,32 @@ def det(
     tmp_path: Path,
     path_provider,
 ) -> StandardDetector:
-    pattern_generator = NullPatternGenerator()
+    class DevNullPatternGenerator(PatternGenerator):
+        def __init__(self, sleep=asyncio.sleep):
+            super().__init__(sleep)
+            self.n_images = 0
+
+        def open_file(self, path: Path, width: int, height: int):
+            pass
+
+        async def write_images_to_file(
+            self, exposure: float, period: float, number_of_frames: int
+        ):
+            self.n_images += number_of_frames
+
+        def generate_point(self, channel: int = 1, high_energy: bool = False) -> float:
+            return 0.0
+
+        async def wait_for_next_index(self, timeout: float):
+            pass
+
+        def get_last_index(self) -> int:
+            return self.n_images
+
+        def close_file(self):
+            pass
+
+    pattern_generator = DevNullPatternGenerator()
     with init_devices(mock=True):
         det = SimBlobDetector(path_provider, pattern_generator)
     return det
