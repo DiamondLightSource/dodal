d95a7335ff38775ff62b0bb8432f55ab51e96155
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-06-20 11:39:19 +0100

    added base table

---
+++ src/dodal/devices/base_table.py
@@ -0,0 +1,35 @@
+from ophyd_async.core import StandardReadable
+from ophyd_async.epics.motor import Motor
+
+
+class BaseTable(StandardReadable):
+    """Base class for a sample table with two motors representing X and Y directions."""
+
+    def __init__(
+        self,
+        prefix: str,
+        x: str = "X",
+        y: str = "Y",
+        name: str = "",
+    ) -> None:
+        with self.add_children_as_readables():
+            self.x = Motor(prefix + x)
+            self.y = Motor(prefix + y)
+        super().__init__(name=name)
+
+
+class PitchedBaseTable(BaseTable):
+    """Base class for a sample table with two motors representing gaps
+    in X and Y directions, with additional motor for pitch."""
+
+    def __init__(
+        self,
+        prefix: str,
+        x: str = "X",
+        y: str = "Y",
+        pitch: str = "PITCH",
+        name: str = "",
+    ) -> None:
+        with self.add_children_as_readables():
+            self.pitch = Motor(prefix + pitch)
+        super().__init__(prefix=prefix, x=x, y=y, name=name)
