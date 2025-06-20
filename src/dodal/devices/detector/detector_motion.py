3de9d89a44a639f3d7773c56da89e89208258c63
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-06-20 20:39:09 +0100

    Refactor 3+ axis devices to use common root device

--- src/dodal/devices/detector/detector_motion.py
+++ src/dodal/devices/detector/detector_motion.py
@@ -1,6 +1,8 @@
-from ophyd_async.core import Device, StrictEnum
+from ophyd_async.core import StrictEnum
 from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
 from ophyd_async.epics.motor import Motor
+
+from dodal.devices.motors import XYZStage
 
 
 class ShutterState(StrictEnum):
@@ -8,35 +10,35 @@ class ShutterState(StrictEnum):
     OPEN = "Open"
 
 
-class DetectorMotion(Device):
-    def __init__(self, prefix: str, name: str = ""):
-        device_prefix = "-MO-DET-01:"
-        pmac_prefix = "-MO-PMAC-02:"
+class DetectorMotion(XYZStage):
+    _device_prefix = "-MO-DET-01:"
+    _pmac_prefix = "-MO-PMAC-02:"
 
-        self.upstream_x = Motor(f"{prefix}{device_prefix}UPSTREAMX")
-        self.downstream_x = Motor(f"{prefix}{device_prefix}DOWNSTREAMX")
-        self.x = Motor(f"{prefix}{device_prefix}X")
-        self.y = Motor(f"{prefix}{device_prefix}Y")
-        self.z = Motor(f"{prefix}{device_prefix}Z")
-        self.yaw = Motor(f"{prefix}{device_prefix}YAW")
+    def __init__(self, prefix: str, name: str = ""):
+        device_prefix = f"{prefix}{self._device_prefix}"
+        pmac_prefix = f"{prefix}{self._pmac_prefix}"
+
+        self.upstream_x = Motor(f"{device_prefix}UPSTREAMX")
+        self.downstream_x = Motor(f"{device_prefix}DOWNSTREAMX")
+        self.yaw = Motor(f"{device_prefix}YAW")
 
         self.shutter = epics_signal_rw(
-            ShutterState, f"{prefix}{device_prefix}SET_SHUTTER_STATE"
+            ShutterState, f"{device_prefix}SET_SHUTTER_STATE"
         )
         self.shutter_closed_lim = epics_signal_r(
-            float, f"{prefix}{device_prefix}CLOSE_LIMIT"
+            float, f"{device_prefix}CLOSE_LIMIT"
         )  # on limit = 1, off = 0
         self.shutter_open_lim = epics_signal_r(
-            float, f"{prefix}{device_prefix}OPEN_LIMIT"
+            float, f"{device_prefix}OPEN_LIMIT"
         )  # on limit = 1, off = 0
         self.z_disabled = epics_signal_r(
-            float, f"{prefix}{device_prefix}Z:DISABLED"
+            float, f"{device_prefix}Z:DISABLED"
         )  # robot interlock, 0=ok to move, 1=blocked
         self.crate_power = epics_signal_r(
-            float, f"{prefix}{pmac_prefix}CRATE2_HEALTHY"
+            float, f"{pmac_prefix}CRATE2_HEALTHY"
         )  # returns 0 if no power
         self.in_robot_load_safe_position = epics_signal_r(
-            int, f"{prefix}{pmac_prefix}GPIO_INP_BITS.B2"
+            int, f"{pmac_prefix}GPIO_INP_BITS.B2"
         )  # returns 1 if safe
 
-        super().__init__(name)
+        super().__init__(device_prefix, name)
--- src/dodal/devices/i10/mirrors.py
+++ src/dodal/devices/i10/mirrors.py
@@ -1,18 +1,16 @@
-from ophyd_async.core import StandardReadable
 from ophyd_async.epics.core import epics_signal_rw
 from ophyd_async.epics.motor import Motor
 
+from dodal.devices.motors import XYZStage
 
-class PiezoMirror(StandardReadable):
+
+class PiezoMirror(XYZStage):
     def __init__(
         self,
         prefix: str,
         name: str = "",
     ):
         with self.add_children_as_readables():
-            self.x = Motor(prefix + "X")
-            self.y = Motor(prefix + "Y")
-            self.z = Motor(prefix + "Z")
             self.yaw = Motor(prefix + "YAW")
             self.pitch = Motor(prefix + "PITCH")
             self.roll = Motor(prefix + "ROLL")
@@ -21,4 +19,4 @@ class PiezoMirror(StandardReadable):
                 read_pv=prefix + "FPITCH:RBV:AI",
                 write_pv=prefix + "FPITCH:DMD:AO",
             )
-        super().__init__(name=name)
+        super().__init__(prefix, name)
