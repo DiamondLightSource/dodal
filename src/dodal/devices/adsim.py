9f8592df4630023bfa5b46cef137bea3bec04059
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-06-20 20:37:04 +0100

    Change adsim stage motors
    Change admin stage motor names to x and theta rather than x and y, so planned tested on adsim can then be used on the training rigs.

--- src/dodal/devices/adsim.py
+++ src/dodal/devices/adsim.py
@@ -9,5 +9,5 @@ class SimStage(StandardReadable):
     def __init__(self, prefix: str, name: str = "sim"):
         with self.add_children_as_readables():
             self.x = Motor(prefix + "M1")
-            self.y = Motor(prefix + "M2")
+            self.theta = Motor(prefix + "M2")
         super().__init__(name=name)
