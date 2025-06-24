cfda42aa17a25843f0921ede3cfd980e3e99aecd
Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>
2025-06-24 16:55:05 +0100

    chore: Unify Motor group implementations (#1277)
    ---------
    
    Co-authored-by: Callum Forrester <callum.forrester@diamond.ac.uk>

--- src/dodal/devices/i10/rasor/rasor_motors.py
+++ src/dodal/devices/i10/rasor/rasor_motors.py
@@ -2,20 +2,6 @@ from ophyd_async.core import StandardReadable
 from ophyd_async.epics.motor import Motor
 
 
-class PinHole(StandardReadable):
-    "Two motors stage for rasor pinhole"
-
-    def __init__(
-        self,
-        prefix: str,
-        name: str = "",
-    ):
-        with self.add_children_as_readables():
-            self.x = Motor(prefix + "X")
-            self.y = Motor(prefix + "Y")
-        super().__init__(name=name)
-
-
 class Diffractometer(StandardReadable):
     def __init__(
         self,
