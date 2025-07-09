5debc41bc30fd82b6fe2f804ab2c7a59d7aa41eb
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-07-09 14:31:24 +0100

    Revert load_json_file_to_class func changes as not needed

--- src/dodal/common/data_util.py
+++ src/dodal/common/data_util.py
@@ -1,25 +1,22 @@
 import os
-from typing import Any, TypeVar
+from typing import TypeVar
 
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel
 
 TBaseModel = TypeVar("TBaseModel", bound=BaseModel)
 
 
 def load_json_file_to_class(
-    t: Any,
+    t: type[TBaseModel],
     file: str,
-) -> Any:
+) -> TBaseModel:
     if not os.path.isfile(file):
         raise FileNotFoundError(f"Cannot find file {file}")
 
     with open(file) as f:
         json_obj = f.read()
-    adapter = TypeAdapter(t)
-    result = adapter.validate_json(json_obj)
-
-    # Safely cast result back to the expected generic type
-    return result  # type: ignore[return-value]
+    cls = t.model_validate_json(json_obj)
+    return cls
 
 
 def save_class_to_json_file(model: BaseModel, file: str) -> None:
