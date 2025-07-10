6e35caa5695760fe0918a8f3d32613f62d155967
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-07-10 14:39:01 +0100

    Remove pass energy type as not used yet

--- src/dodal/devices/electron_analyser/abstract/types.py
+++ src/dodal/devices/electron_analyser/abstract/types.py
@@ -8,4 +8,3 @@ TAcquisitionMode = TypeVar("TAcquisitionMode", bound=StrictEnum)
 # when connected.
 TLensMode = TypeVar("TLensMode", bound=SupersetEnum | StrictEnum)
 TPsuMode = TypeVar("TPsuMode", bound=SupersetEnum | StrictEnum)
-TPassEnergy = TypeVar("TPassEnergy", bound=StrictEnum | int)
