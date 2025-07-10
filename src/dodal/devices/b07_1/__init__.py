df5da67a342875048df75a506cc628af2acbe243
eir17846 <victor.rogalev@diamond.ac.uk>
2025-07-10 15:58:49 +0000

    ruff

--- src/dodal/devices/b07_1/__init__.py
+++ src/dodal/devices/b07_1/__init__.py
@@ -4,4 +4,9 @@ from dodal.devices.b07_1.ccmc import (
 )
 from dodal.devices.b07_1.enums import Grating, LensMode
 
-__all__ = ["Grating", "LensMode", "ChannelCutMonochromator", "ChannelCutMonochromatorPositions"]
+__all__ = [
+    "Grating",
+    "LensMode",
+    "ChannelCutMonochromator",
+    "ChannelCutMonochromatorPositions",
+]
