673543fdeb027dc88097c62cfc99d52bf80f30d9
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-07-11 08:09:56 +0000

    i11 robot signals added

--- src/dodal/devices/i11/i11_robot.py
+++ src/dodal/devices/i11/i11_robot.py
@@ -1,17 +1,34 @@
+import time
+from typing import Annotated as A
+
 from bluesky.protocols import Locatable, Location
-from ophyd_async.core import (
-    AsyncStatus,
-    StandardReadable,
-)
-from ophyd_async.epics.core import (
-    epics_signal_x,
-)
+from ophyd_async.core import AsyncStatus, SignalRW, StandardReadable
+from ophyd_async.epics.core import PvSuffix, epics_signal_x
+
+from dodal.log import Logger
+
+# class RobotJobs(StrictEnum):
+
+# class RobotSampleState(StrictEnum):
+#     CAROSEL = "on carousel"
 
 
 class I11Robot(StandardReadable, Locatable):
     def __init__(self, prefix: str, name=""):
         self.start = epics_signal_x(prefix + "START.VAL")
         self.hold = epics_signal_x(prefix + "HOLD.VAL")
+        self.job = epics_signal_x(prefix + "JOB.VAL")
+        self.svon = epics_signal_x(prefix + "SVON.VAL")
+        self.err = epics_signal_x(prefix + "ERR.VAL")
+
+        # self.robot_plc_check = "BL11I-MO-STEP-19:PLCSTATUS_OK"
+        # self.robot_feedrate_check = "BL11I-MO-STEP-19:FEEDRATE"
+        # self.robot_feedrate_limit = "BL11I-MO-STEP-19:FEEDRATE_LIMIT"
+
+        self.robot_sample_state: A[SignalRW[int], PvSuffix.rbv("D010")]
+        self.next_sample_position: A[SignalRW[int], PvSuffix.rbv("D011")]
+        self.current_sample_position: A[SignalRW[int], PvSuffix.rbv("D012")]
+        self.door_latch_state = A[SignalRW[int], PvSuffix.rbv("NEEDRECOVER")]
 
         super().__init__(name=name)
 
@@ -23,3 +40,7 @@ class I11Robot(StandardReadable, Locatable):
     async def locate(self) -> Location:
         # setpoint readback
         return Location(setpoint=1, readback=1)
+
+    def start():
+        start_time = time.time()
+        Logger.debug(f"Robot Started: {start_time}")
