410274c6dbb63a9d92516c1383cfecba86db5fc0
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-07-16 15:56:28 +0000

    added mythen connect test

--- tests/devices/i11/test_mythen.py
+++ tests/devices/i11/test_mythen.py
@@ -1,11 +1,26 @@
-import bluesky.plan_stubs as bps
-from bluesky.run_engine import RunEngine
+import pytest
 
+from dodal.common.beamlines.beamline_utils import (
+    get_path_provider,
+)
 from dodal.devices.i11.mythen import Mythen3
 
+# def test_mythen3_prepare(RE: RunEngine, mythen3: Mythen3):
+#     def _inner_prepare(mythen3: Mythen3):
+#         m = mythen3()
+#         yield from bps.prepare(m)
 
-def test_mythen3_prepare(RE: RunEngine, mythen3: Mythen3):
-    def _inner_prepare(mythen3: Mythen3):
-        yield from bps.prepare(mythen3)
+#     RE(_inner_prepare(mythen3))
 
-    RE(_inner_prepare(mythen3))
+
+@pytest.fixture
+async def connect_mythen() -> Mythen3:
+    mythen = Mythen3(
+        prefix="FOO-EA-DET-07:",
+        path_provider=get_path_provider(),
+        drv_suffix="DET",
+        fileio_suffix="HDF:",
+    )
+    await mythen.connect(mock=True)
+
+    return mythen
