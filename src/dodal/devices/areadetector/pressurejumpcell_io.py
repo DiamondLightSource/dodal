30b17789a3e763d2a761271f67902be1dff36c19
Will Barnett <william.barnett@diamond.ac.uk>
2025-07-18 15:02:37 +0100

    Revised pressure cell area detector based on ophyd-async v0.11

--- src/dodal/devices/areadetector/pressurejumpcell_io.py
+++ src/dodal/devices/areadetector/pressurejumpcell_io.py
@@ -1,29 +1,25 @@
-from enum import Enum
+from typing import Annotated as A
 
-from ophyd_async.core import SubsetEnum
+from ophyd_async.core import SignalRW, StrictEnum
 from ophyd_async.epics import adcore
-from ophyd_async.epics.signal import epics_signal_rw_rbv
+from ophyd_async.epics.core import PvSuffix
 
 
-class PressureJumpCellTriggerMode(str, Enum):
+class PressureJumpCellTriggerMode(StrictEnum):
     INTERNAL = "Internal"
     EXTERNAL = "Exernal"
 
-class PressureJumpCellAdcTriggerMode(str, Enum):
+
+class PressureJumpCellAdcTriggerMode(StrictEnum):
     SINGLE = "Single"
     MULTIPLE = "Multiple"
     CONTINUOUS = "Continuous"
 
+
 class PressureJumpCellDriverIO(adcore.ADBaseIO):
-    def __init__(self, prefix: str, name: str = "") -> None:
-        self.trigger_mode = epics_signal_rw_rbv(
-            PressureJumpCellTriggerMode, prefix + "TriggerMode"
-        )
-        super().__init__(prefix, name=name)
+    trigger_mode: A[SignalRW[PressureJumpCellTriggerMode], PvSuffix.rbv("TriggerMode")]
+
 
 class PressureJumpCellAdcIO(adcore.ADBaseIO):
-    def __init__(self, prefix: str, name: str = "") -> None:
-        self.adc_trigger_mode = epics_signal_rw_rbv(
-            PressureJumpCellAdcTriggerMode, prefix + "TriggerMode"
-        )
-        super().__init__(prefix, name=name)
+    trigger_mode: A[SignalRW[PressureJumpCellAdcTriggerMode], PvSuffix.rbv("TriggerMode")]
+
