84aaa8d3f2ed3828b460ab7e6049d51639cd9235
Dominic Oram <dominic.oram@diamond.ac.uk>
2025-07-21 11:05:49 +0100

    Add ability to check cryostream and move fluorescence detector

--- src/dodal/devices/fluorescence_detector_motion.py
+++ src/dodal/devices/fluorescence_detector_motion.py
@@ -1,8 +1,8 @@
 from ophyd_async.core import StandardReadable, StrictEnum
-from ophyd_async.epics.core import epics_signal_r
+from ophyd_async.epics.core import epics_signal_rw
 
 
-class FluorescenceDetectorControlState(StrictEnum):
+class InOut(StrictEnum):
     OUT = "Out"
     IN = "In"
 
@@ -10,7 +10,5 @@ class FluorescenceDetectorControlState(StrictEnum):
 class FluorescenceDetector(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
         with self.add_children_as_readables():
-            self.pos = epics_signal_r(
-                FluorescenceDetectorControlState, prefix + "-EA-FLU-01:CTRL"
-            )
+            self.pos = epics_signal_rw(InOut, prefix + "-EA-FLU-01:CTRL")
         super().__init__(name)
