7c9e0cf989df660d4caacf2adaf8779abfc54bd2
eir17846 <victor.rogalev@diamond.ac.uk>
2025-07-22 09:00:32 +0000

    udpate enum names

--- src/dodal/devices/i05/enums.py
+++ src/dodal/devices/i05/enums.py
@@ -2,7 +2,7 @@ from ophyd_async.core import StrictEnum
 
 
 class Grating(StrictEnum):
-    RH_400 = "400 lines/mm"
+    PT_400 = "400 lines/mm"
     C_1600 = "C 1600 lines/mm"
     RH_1600 = "Rh 1600 lines/mm"
-    B_800 = "B 800 lines/mm"
+    PT_800 = "B 800 lines/mm"
