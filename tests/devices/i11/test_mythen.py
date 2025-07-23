e0d9880d12ccdfc4b4267e10cc4268363fd59318
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-07-23 15:05:04 +0000

    added robot test

--- tests/devices/i11/test_mythen.py
+++ tests/devices/i11/test_mythen.py
@@ -1,5 +1,6 @@
 import pytest
 
+# from ophyd_async.testing import set_mock_value
 from dodal.common.beamlines.beamline_utils import (
     get_path_provider,
 )
@@ -14,13 +15,15 @@ from dodal.devices.i11.mythen import Mythen3
 
 
 @pytest.fixture
-async def connect_mythen() -> Mythen3:
+async def test_mythen() -> Mythen3:
     mythen = Mythen3(
-        prefix="FOO-EA-DET-07:",
+        prefix="BL11I-EA-DET-07:",
         path_provider=get_path_provider(),
         drv_suffix="DET",
         fileio_suffix="HDF:",
     )
     await mythen.connect(mock=True)
+
+    # set_mock_value(mythen.x_mm.user_readback, x)
 
     return mythen
