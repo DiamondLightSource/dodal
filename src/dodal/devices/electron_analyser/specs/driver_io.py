c55bfcd7ce578268ee64dfd2abb8db0eb49c3bb5
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-07-23 11:57:16 +0100

    Undone changes to specs io

--- src/dodal/devices/electron_analyser/specs/driver_io.py
+++ src/dodal/devices/electron_analyser/specs/driver_io.py
@@ -18,7 +18,6 @@ from dodal.devices.electron_analyser.abstract.base_driver_io import (
 from dodal.devices.electron_analyser.abstract.types import TLensMode, TPsuMode
 from dodal.devices.electron_analyser.specs.enums import AcquisitionMode
 from dodal.devices.electron_analyser.specs.region import SpecsRegion
-from dodal.devices.electron_analyser.util import to_kinetic_energy
 
 
 class SpecsAnalyserDriverIO(
@@ -72,15 +71,10 @@ class SpecsAnalyserDriverIO(
             self.psu_mode.set(region.psu_mode),
         )
         if region.acquisition_mode == AcquisitionMode.FIXED_TRANSMISSION:
-            await self.energy_step.set(region.energy_step)
+            await self.centre_energy.set(region.centre_energy)
 
         if region.acquisition_mode == AcquisitionMode.FIXED_ENERGY:
-            source = self._get_energy_source(region.excitation_energy_source)
-            excitation_energy = await source.get_value()
-            centre_energy = to_kinetic_energy(
-                region.centre_energy, region.energy_mode, excitation_energy
-            )
-            await self.centre_energy.set(centre_energy)
+            await self.energy_step.set(region.energy_step)
 
     def _create_angle_axis_signal(self, prefix: str) -> SignalR[Array1D[np.float64]]:
         angle_axis = derived_signal_r(
