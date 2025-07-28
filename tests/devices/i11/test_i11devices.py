bf03b14022fbb1f2cee7d791c9c4b181c9b0b73a
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-07-28 14:04:10 +0000

    added more robot tests

--- tests/devices/i11/test_i11devices.py
+++ tests/devices/i11/test_i11devices.py
@@ -3,7 +3,7 @@ from ophyd_async.core import init_devices
 from ophyd_async.testing import set_mock_value
 
 from dodal.devices.i11.cyberstar_blower import CyberstarBlower
-from dodal.devices.i11.i11_robot import NX100Robot, RobotJobs
+from dodal.devices.i11.i11_robot import NX100Robot, RobotJobs, RobotSampleState
 from dodal.devices.i11.spinner import Spinner
 
 
@@ -21,6 +21,58 @@ async def test_robot_moves_to_position(i11_robot: NX100Robot) -> None:
     await i11_robot.set(10)
     location = await i11_robot.locate()
     assert location["readback"] == 10
+
+
+async def test_robot_recover(i11_robot: NX100Robot) -> None:
+    set_mock_value(i11_robot.job, RobotJobs.GRIPC)  # Set to ONGRIP state
+    set_mock_value(i11_robot.err, True)  # Set to ERROR state
+
+    await i11_robot.recover()
+
+    assert await i11_robot.err.get_value() == 0
+    assert await i11_robot.job.get_value() == RobotJobs.RECOVER
+
+
+async def test_robot_stage(i11_robot: NX100Robot) -> None:
+    set_mock_value(i11_robot.servo_on, False)  # Set to OFF state
+    set_mock_value(i11_robot.hold, True)  # Set to hold state
+
+    await i11_robot.stage()
+
+    assert await i11_robot.servo_on.get_value() == 1
+    assert await i11_robot.hold.get_value() == 0
+
+
+async def test_robot_pause_resume(i11_robot: NX100Robot) -> None:
+    set_mock_value(i11_robot.hold, False)  # Set to not hold state
+    await i11_robot.pause()
+    assert await i11_robot.hold.get_value() == 1
+
+    set_mock_value(i11_robot.hold, True)  # Set to hold state
+    await i11_robot.resume()
+    assert await i11_robot.hold.get_value() == 0
+
+
+async def test_when_sample_state_clear_sample_tablein(i11_robot: NX100Robot) -> None:
+    set_mock_value(
+        i11_robot.robot_sample_state, RobotSampleState.ONGRIP
+    )  # Set to not ongrip state
+
+    await i11_robot.clear_sample(table_in=True)
+    assert await i11_robot.job.get_value() == RobotJobs.TABLEIN
+
+
+async def test_when_robot_must_stop_and_success_false(
+    i11_robot: NX100Robot,
+) -> None:
+    set_mock_value(
+        i11_robot.robot_sample_state, RobotSampleState.ONGRIP
+    )  # set to ongrip
+    set_mock_value(i11_robot.hold, False)  # Set to not hold state
+    await i11_robot.stop(success=False)
+
+    assert await i11_robot.hold.get_value() == 0
+    assert await i11_robot.job.get_value() == RobotJobs.TABLEIN
 
 
 @pytest.fixture
