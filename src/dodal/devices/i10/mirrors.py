49c2eb5cad55ecfb919e20e18ca62d9ee298141c
eir17846 <victor.rogalev@diamond.ac.uk>
2025-07-30 21:28:00 +0000

    create base mirror

--- src/dodal/devices/i10/mirrors.py
+++ src/dodal/devices/i10/mirrors.py
@@ -1,19 +1,15 @@
 from ophyd_async.epics.core import epics_signal_rw
-from ophyd_async.epics.motor import Motor
 
-from dodal.devices.motors import XYZStage
+from dodal.devices.common_mirror import BaseMirror
 
 
-class PiezoMirror(XYZStage):
+class PiezoMirror(BaseMirror):
     def __init__(
         self,
         prefix: str,
         name: str = "",
     ):
         with self.add_children_as_readables():
-            self.yaw = Motor(prefix + "YAW")
-            self.pitch = Motor(prefix + "PITCH")
-            self.roll = Motor(prefix + "ROLL")
             self.fine_pitch = epics_signal_rw(
                 float,
                 read_pv=prefix + "FPITCH:RBV:AI",
