6b57679887358fbf0ee99f4546e1949bd1905c62
eir17846 <victor.rogalev@diamond.ac.uk>
2025-07-31 15:44:58 +0000

    remove XY coll mirror - will be a separate issue

--- src/dodal/devices/common_mirror.py
+++ src/dodal/devices/common_mirror.py
@@ -1,20 +1,7 @@
 from ophyd_async.epics.core import epics_signal_rw
 from ophyd_async.epics.motor import Motor
 
-from dodal.devices.motors import XYStage, XYZStage
-
-
-class XYCollMirror(XYStage):
-    def __init__(
-        self,
-        prefix: str,
-        name: str = "",
-    ):
-        with self.add_children_as_readables():
-            self.yaw = Motor(prefix + "YAW")
-            self.pitch = Motor(prefix + "PITCH")
-            self.roll = Motor(prefix + "ROLL")
-        super().__init__(prefix, name)
+from dodal.devices.motors import XYZStage
 
 
 class XYZCollMirror(XYZStage):
