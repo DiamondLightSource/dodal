eba5f2322e0ff8fe5a546ffa9def6faf21b6f94b
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-07-31 15:40:01 +0100

    Add back topup tests

--- tests/plan_stubs/test_topup_plan.py
+++ tests/plan_stubs/test_topup_plan.py
@@ -101,10 +101,10 @@ def test_no_waiting_when_mode_does_not_allow_gating(
     "expected_wait, parameter_file",
     # limit = 120, delay = 1
     [
-        # (100, 108, 121, 1, 0, "test_beamline_parameters.txt"),
-        # (100, 108, 119, 1, 108 + 1, "test_beamline_parameters.txt"),
-        # (110, 120, 120, 1, 0, "test_beamline_parameters.txt"),
-        # (110.1, 120, 119.99, 1, 120 + 1, "test_beamline_parameters.txt"),
+        (100, 108, 121, 1, 0, "test_beamline_parameters.txt"),
+        (100, 108, 119, 1, 108 + 1, "test_beamline_parameters.txt"),
+        (110, 120, 120, 1, 0, "test_beamline_parameters.txt"),
+        (110.1, 120, 119.99, 1, 120 + 1, "test_beamline_parameters.txt"),
         # limit = 35, delay = 1
         (110.1, 120, 36, 1, 0, "topup_short_params.txt"),
         (36, 42, 36, 1, 0, "topup_short_params.txt"),
