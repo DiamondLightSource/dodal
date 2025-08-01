b055f3b241377591e1d78dc362738e62dc6336ac
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-08-01 17:16:44 +0100

    Removed spaces between variables

--- tests/devices/unit_tests/test_data/paths.py
+++ tests/devices/unit_tests/test_data/paths.py
@@ -1,11 +1,8 @@
 from os.path import join
 
 TEST_DATA_PATH = "tests/devices/unit_tests/test_data"
-
 TEST_BEAMLINE_UNDULATOR_TO_GAP_LUT = join(
     TEST_DATA_PATH, "test_beamline_undulator_to_gap_lookup_table.txt"
 )
-
 TEST_LUT_TXT = join(TEST_DATA_PATH, "test_lookup_table.txt")
-
 TEST_LUT_2_TXT = join(TEST_DATA_PATH, "test_lookup_table_2.txt")
