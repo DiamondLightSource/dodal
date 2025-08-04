8c0351a5fd25131a94ccbda325505fdbd8031a72
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-08-04 10:44:21 +0100

    Fixed topup tests

--- tests/plan_stubs/test_data/__init__.py
+++ tests/plan_stubs/test_data/__init__.py
@@ -4,3 +4,8 @@ from pathlib import Path
 TEST_DATA_PATH = Path(__file__).parent
 TEST_TOPUP_SHORT_PARAMS_TXT = join(TEST_DATA_PATH, "topup_short_params.txt")
 TEST_TOPUP_LONG_DELAY_TXT = join(TEST_DATA_PATH, "topup_long_delay.txt")
+
+__all__ = [
+    "TEST_TOPUP_SHORT_PARAMS_TXT",
+    "TEST_TOPUP_LONG_DELAY_TXT",
+]
