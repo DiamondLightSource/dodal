45a25672cf121297639aedbcdc07ad5140d8d1ec
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-08-06 09:52:15 +0100

    Corrected text_oav_zoom_level file name

--- tests/test_data/__init__.py
+++ tests/test_data/__init__.py
@@ -6,7 +6,7 @@ BAD_BEAMLINE_PARAMETERS = join(TEST_DATA_PATH, "bad_beamlineParameters")
 I04_BEAMLINE_PARAMETERS = join(TEST_DATA_PATH, "i04_beamlineParameters")
 TEST_BEAMLINE_PARAMETERS_TXT = join(TEST_DATA_PATH, "test_beamline_parameters.txt")
 TEST_DISPLAY_CONFIG = join(TEST_DATA_PATH, "test_display.configuration")
-TEST_OAV_ZOOM_LEVELS_XML = join(TEST_DATA_PATH, "test_jCameraManZoomLevels.xml")
+TEST_OAV_ZOOM_LEVELS_XML = join(TEST_DATA_PATH, "test_oav_zoom_levels.xml")
 
 __all__ = [
     "BAD_BEAMLINE_PARAMETERS",
