d34b2381e96935577622bf3c0573bc297f88d438
Paul Hathaway <7045365+phathaway@users.noreply.github.com>
2025-08-07 17:33:40 +0100

    I24: Update pmac pvs (#1344)
    Fixes Issue #1319
    
    Update PMAC PVs for I24 including
    * Fix pmac prefix
    * Fix pmac homing string
    * Fix coordinate system index
    * Update tests for string constants
    * Update prefix for stages

--- src/dodal/beamlines/i24.py
+++ src/dodal/beamlines/i24.py
@@ -136,9 +136,9 @@ def pmac() -> PMAC:
     """Get the i24 PMAC device, instantiate it if it hasn't already been.
     If this is called when already instantiated in i24, it will return the existing object.
     """
-    # prefix not BL but ME14E
     return PMAC(
-        "ME14E-MO-CHIP-01:",
+        PREFIX.beamline_prefix,
+        "pmac",
     )
 
 
