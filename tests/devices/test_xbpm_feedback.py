e58312c85788917d23665112d5e7fbb22f4c132f
Robert Tuck <robert.tuck@diamond.ac.uk>
2025-08-14 14:39:59 +0100

    Add read_commissioning_mode plan helper, unit tests

--- tests/devices/test_xbpm_feedback.py
+++ tests/devices/test_xbpm_feedback.py
@@ -7,6 +7,7 @@ from bluesky.run_engine import RunEngine
 from ophyd_async.core import init_devices
 from ophyd_async.testing import set_mock_value
 
+from dodal.devices.baton import Baton
 from dodal.devices.xbpm_feedback import XBPMFeedback
 
 
@@ -84,3 +85,18 @@ def test_logging_while_waiting_for_XBPM(
         record.getMessage() == "Waiting for XBPM" for record in caplog.records
     )
     assert log_messages == expected_log_messages
+
+
+@patch("dodal.devices.xbpm_feedback.observe_value")
+@patch("dodal.devices.xbpm_feedback.periodic_reminder")
+def test_xbpm_feedback_does_not_wait_if_commissioning_mode_enabled(
+    mock_periodic_reminder: AsyncMock,
+    mock_observe_value: AsyncMock,
+    fake_xbpm_feedback: XBPMFeedback,
+    baton_in_commissioning_mode: Baton,
+    RE: RunEngine,
+):
+    set_mock_value(fake_xbpm_feedback.pos_stable, False)
+    RE(bps.trigger(fake_xbpm_feedback, wait=True))
+    mock_periodic_reminder.assert_not_called()
+    mock_observe_value.assert_not_called()
