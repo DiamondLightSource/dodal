5b1a7e299970597759af219bcba5c676e341e733
oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
2025-08-14 09:22:19 +0100

    Create SelectedSource enum and remove used region parameter logic (#1448)
    * Create SelectedSource enum and remove used region parameter logic
    
    * Removed test to check for invalid source as no longer needed with enums
    
    * Update energy_sources fixture

--- src/dodal/beamlines/b07.py
+++ src/dodal/beamlines/b07.py
@@ -3,6 +3,7 @@ from dodal.common.beamlines.beamline_utils import (
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.b07 import Grating, LensMode, PsuMode
+from dodal.devices.electron_analyser import SelectedSource
 from dodal.devices.electron_analyser.specs import SpecsAnalyserDriverIO
 from dodal.devices.pgm import PGM
 from dodal.devices.synchrotron import Synchrotron
@@ -33,5 +34,5 @@ def analyser_driver() -> SpecsAnalyserDriverIO[LensMode, PsuMode]:
         prefix=f"{PREFIX.beamline_prefix}-EA-DET-01:CAM:",
         lens_mode_type=LensMode,
         psu_mode_type=PsuMode,
-        energy_sources={"source1": pgm().energy.user_readback},
+        energy_sources={SelectedSource.SOURCE1: pgm().energy.user_readback},
     )
