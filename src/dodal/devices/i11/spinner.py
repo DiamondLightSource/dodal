1dd16293791dbdc3f4053e6382336fe9621a3d22
Richard Dixey <185198552+RJCD-Diamond@users.noreply.github.com>
2025-08-18 11:24:31 +0100

    Add i11 beamline module, Mythen3 detector and basic devices (#1377)
    * added i11.py, i11 specific devices
    
    * renamed stages
    
    * added diff stages
    
    * made cryostream into standardreadable
    
    * cryostreams 1+2 working
    
    * mythen3 baseclass added
    
    * mythen3 connecting
    
    * removed mac, renamed detector to mythen
    
    * changed cstrm to ocs
    
    * added deadtime to mythen3
    
    * added sample spinner
    
    * added eurotherm + cyberstar blower
    
    * add with children as readbles to classes
    
    * changed eurotherm signals to rbv
    
    * change blower names to csb1/2
    
    * fixed error on mythen3 docstring
    
    * added basic methods to cyberstar blower
    
    * fixed signal types in CryoStream
    
    * added series to cryostream
    
    * changed methods to bluesky protocol
    
    * added initial robot class
    
    * i11 robot connecting
    
    * changed sample_env name to spinner
    
    * fixed spinner in bl module, change start signal to x
    
    * spin signal to rw
    
    * i11 robot signals added
    
    * super added to mythen, modified robot
    
    * added mythen3 test
    
    * added mythen connect test
    
    * added docstrings for blower and eurotherm
    
    * i11 robot added load_sample and methods
    
    * changed jaws to grip
    
    * stop and start methods added
    
    * added robot test
    
    * added robot test
    
    * modify cyberstarblower and add test, updated mythen deadtime
    
    * import motors from ophyd_async
    
    * improve Eurotherm
    
    * impproved robot and spinner inheretance
    
    * fixed naming
    
    * fixes to tests
    
    * json
    
    * updates to tests
    
    * added blower tests
    
    * added more robot tests
    
    * parameterised tests
    
    * fixed type checking
    
    * add full coverage for i11robot
    
    * fixed type
    
    * added test to mythen3
    
    * restored settings
    
    * tidied up mythen and added more tests
    
    * improved spinner, added set speed and test
    
    * edits to improve typechecking and other small improvements
    
    * added robot at position logging test
    
    * removed _get_i11_robot
    
    * changed i11robot to NX100
    
    * reorganise imports
    
    * lowercase file
    
    * seperated spinner pause resume tests
    
    * removed eurotherm infixes
    
    * minor update to further paramterise nx100 test
    
    * Update test_i11devices.py
    
    * added missing type hints to eurotherm, blower and mythen3
    
    * made eurothermal rbv suffix constant
    
    * made robot states more descriptive
    
    * fixed carousel spelling on robot
    
    * add readables to cryo
    
    * made blowers and eurotherms compositional
    
    * added docs strings to i11 robot
    
    * updated cryostream imports
    
    * using ophyd_async enums on/off enabled/disabled
    
    * type checking fixed for cyberstar blower, and Eurotherms

---
+++ src/dodal/devices/i11/spinner.py
@@ -0,0 +1,35 @@
+from bluesky.protocols import Pausable
+from ophyd_async.core import (
+    StandardReadable,
+    StrictEnum,
+    set_and_wait_for_value,
+)
+from ophyd_async.epics.core import epics_signal_rw
+
+
+class SpinEnable(StrictEnum):
+    DISABLE = "Disabled"
+    ENABLE = "Enabled"
+
+
+class Spinner(StandardReadable, Pausable):
+    """This is a simple sample spinner, that has enable and speed (%)"""
+
+    def __init__(
+        self,
+        prefix: str,
+        name: str = "",
+        disable_suffix: str = "SPIN:DISABLE",
+        speed_suffix: str = "SPIN:SPEED",
+    ):
+        with self.add_children_as_readables():
+            self.enable = epics_signal_rw(SpinEnable, prefix + disable_suffix)
+            self.speed = epics_signal_rw(float, prefix + speed_suffix)
+
+        super().__init__(name=name)
+
+    async def pause(self):
+        await set_and_wait_for_value(self.enable, SpinEnable.DISABLE)
+
+    async def resume(self):
+        await set_and_wait_for_value(self.enable, SpinEnable.ENABLE)
