04b9608d30a8fe649cbcca519cdac5b5b5f3fad1
oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
2025-09-03 12:54:53 +0100

    Create test detector.py for electron analyser tests (#1472)
    * Add default image mode setting for analysers and clean up imports
    
    * Set default image mode to SINGLE in ElectronAnalyserController and clean up test assertions
    
    * set image mode before arm - may be we should improve doc to say the order explicitly here.
    
    * move image_mode set to stage method
    
    * Refactor staging methods to set image mode directly
    
    * Remove image mode setting from trigger method in AbstractElectronAnalyserDetector
    
    * Enhance staging methods to ensure detector readiness and set default image mode to SINGLE
    
    * Fixed tests
    
    * Fixed flakey test, added test for unstage
    
    * Removed debugging flakey test function
    
    * Removed unneeded tests
    
    * Move Stageable to ElectronAnalyserDetector
    
    * Reorded doc string of stage so it matches order of functions.
    
    * Added test_detector.py
    
    * Created devices/controllers.py and updated devices to use
    
    * Fixed tests
    
    * Updated doc string
    
    * Updated doc string
    
    * Merge on top of ConstanstDeadTimeController branch
    
    * fixed tests
    
    * Updated merlin controller to use ConstantDeadTimeController
    
    * Add parametised test
    
    * made deadtime veriable
    
    * Reverted changes to detector
    
    * Reverted other changes to detector
    
    * Update tests/devices/unit_tests/electron_analyser/abstract/test_base_detector.py
    
    Co-authored-by: rtuck99 <robert.tuck@diamond.ac.uk>
    
    * Removed check before replace in test_detector.py
    
    ---------
    
    Co-authored-by: Fajin Yuan <fajin.yuan@diamond.ac.uk>
    Co-authored-by: rtuck99 <robert.tuck@diamond.ac.uk>

--- tests/devices/unit_tests/electron_analyser/abstract/test_base_detector.py
+++ tests/devices/unit_tests/electron_analyser/abstract/test_base_detector.py
@@ -2,182 +2,92 @@ from unittest.mock import AsyncMock
 
 import pytest
 from bluesky import plan_stubs as bps
-from bluesky.protocols import Triggerable
 from bluesky.run_engine import RunEngine
 from ophyd_async.core import SignalR, init_devices
-from ophyd_async.epics.adcore import ADBaseController
+from ophyd_async.testing import (
+    assert_configuration,
+    assert_reading,
+    set_mock_value,
+)
 
 import dodal.devices.b07 as b07
 import dodal.devices.i09 as i09
-from dodal.devices.electron_analyser import (
-    GenericElectronAnalyserDetector,
+from dodal.devices.electron_analyser.abstract import (
+    AbstractAnalyserDriverIO,
+    AbstractElectronAnalyserDetector,
 )
 from dodal.devices.electron_analyser.specs import SpecsDetector
 from dodal.devices.electron_analyser.vgscienta import VGScientaDetector
 from dodal.testing.electron_analyser import create_detector
-from tests.devices.unit_tests.electron_analyser.helper_util import get_test_sequence
 
 
 @pytest.fixture(
     params=[
-        SpecsDetector[b07.LensMode, b07.PsuMode],
         VGScientaDetector[i09.LensMode, i09.PsuMode, i09.PassEnergy],
+        SpecsDetector[b07.LensMode, b07.PsuMode],
     ]
 )
 async def sim_detector(
     request: pytest.FixtureRequest,
     energy_sources: dict[str, SignalR[float]],
-) -> GenericElectronAnalyserDetector:
+    RE: RunEngine,
+) -> AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO]:
     async with init_devices(mock=True):
         sim_detector = await create_detector(
             request.param,
             prefix="TEST:",
             energy_sources=energy_sources,
         )
+    # Needed for specs so we don't get division by zero error.
+    set_mock_value(sim_detector.driver.slices, 1)
     return sim_detector
 
 
-@pytest.fixture
-def sequence_file_path(
-    sim_detector: GenericElectronAnalyserDetector,
-) -> str:
-    return get_test_sequence(type(sim_detector))
-
-
-def test_analyser_detector_loads_sequence_correctly(
-    sim_detector: GenericElectronAnalyserDetector,
-    sequence_file_path: str,
-) -> None:
-    seq = sim_detector.load_sequence(sequence_file_path)
-    assert seq is not None
-
-
-async def test_analyser_detector_stage(
-    sim_detector: GenericElectronAnalyserDetector,
-) -> None:
-    sim_detector.controller.disarm = AsyncMock()
-    sim_detector.driver.stage = AsyncMock()
-
-    await sim_detector.stage()
-
-    sim_detector.controller.disarm.assert_awaited_once()
-    sim_detector.driver.stage.assert_awaited_once()
-
-
-async def test_analyser_detector_unstage(
-    sim_detector: GenericElectronAnalyserDetector,
-) -> None:
-    sim_detector.controller.disarm = AsyncMock()
-    sim_detector.driver.unstage = AsyncMock()
-
-    await sim_detector.unstage()
-
-    sim_detector.controller.disarm.assert_awaited_once()
-    sim_detector.driver.unstage.assert_awaited_once()
-
-
-def test_analyser_detector_creates_region_detectors(
-    sim_detector: GenericElectronAnalyserDetector,
-    sequence_file_path: str,
-) -> None:
-    seq = sim_detector.load_sequence(sequence_file_path)
-    region_detectors = sim_detector.create_region_detector_list(sequence_file_path)
-
-    assert len(region_detectors) == len(seq.get_enabled_regions())
-    for det in region_detectors:
-        assert det.region.enabled is True
-        assert det.name == sim_detector.name + "_" + det.region.name
-
-
-def test_analyser_detector_has_driver_as_child_and_region_detector_does_not(
-    sim_detector: GenericElectronAnalyserDetector,
-    sequence_file_path: str,
-) -> None:
-    # Remove parent name from driver name so it can be checked it exists in
-    # _child_devices dict
-    driver_name = sim_detector.driver.name
-    if sim_detector.name + "-" in driver_name:
-        driver_name = driver_name.replace(sim_detector.name + "-", "")
-
-    assert sim_detector.driver.parent == sim_detector
-    assert sim_detector._child_devices.get(driver_name) is not None
-
-    region_detectors = sim_detector.create_region_detector_list(sequence_file_path)
-
-    for det in region_detectors:
-        assert det._child_devices.get(driver_name) is None
-        assert det.driver.parent == sim_detector
-
-
-def assert_detector_trigger_uses_controller_correctly(
-    detector: Triggerable,
-    controller: ADBaseController,
-    RE: RunEngine,
-) -> None:
-    controller.arm = AsyncMock()
-    controller.wait_for_idle = AsyncMock()
-
-    RE(bps.trigger(detector), wait=True)
-
-    controller.arm.assert_awaited_once()
-    controller.wait_for_idle.assert_awaited_once()
-
-
 def test_analyser_detector_trigger(
-    sim_detector: GenericElectronAnalyserDetector,
+    sim_detector: AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO],
     RE: RunEngine,
 ) -> None:
-    assert_detector_trigger_uses_controller_correctly(
-        sim_detector, sim_detector.controller, RE
-    )
+    sim_detector.controller.arm = AsyncMock()
+    sim_detector.controller.wait_for_idle = AsyncMock()
+
+    RE(bps.trigger(sim_detector), wait=True)
+
+    sim_detector.controller.arm.assert_awaited_once()
+    sim_detector.controller.wait_for_idle.assert_awaited_once()
 
 
-async def test_analyser_region_detector_trigger_sets_driver_with_region(
-    sim_detector: GenericElectronAnalyserDetector,
-    sequence_file_path: str,
-    RE: RunEngine,
+async def test_analyser_detector_read(
+    sim_detector: AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO],
 ) -> None:
-    region_detectors = sim_detector.create_region_detector_list(
-        sequence_file_path, enabled_only=False
-    )
-
-    for reg_det in region_detectors:
-        reg_det.driver.set = AsyncMock()
-
-        assert_detector_trigger_uses_controller_correctly(
-            reg_det, reg_det.controller, RE
-        )
-        reg_det.driver.set.assert_awaited_once_with(reg_det.region)
+    driver = sim_detector.driver
+    driver_read = await driver.read()
+    await assert_reading(sim_detector, driver_read)
 
 
-async def test_analyser_detector_config_read(
-    sim_detector: GenericElectronAnalyserDetector,
+async def test_analyser_describe(
+    sim_detector: AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO],
 ) -> None:
-    # avoid division by zero in angle axis calculation
-    await sim_detector.driver.slices.set(1)
-    configuration_read = await sim_detector.read_configuration()
-    expected_config_keys = [
-        f"{sim_detector.name}-_driver-centre_energy",
-        f"{sim_detector.name}-_driver-lens_mode",
-        f"{sim_detector.name}-_driver-excitation_energy_source",
-        f"{sim_detector.name}-_driver-acquisition_mode",
-        f"{sim_detector.name}-_driver-psu_mode",
-        f"{sim_detector.name}-_driver-energy_mode",
-        f"{sim_detector.name}-_driver-region_name",
-        f"{sim_detector.name}-_driver-energy_step",
-        f"{sim_detector.name}-_driver-slices",
-        f"{sim_detector.name}-_driver-pass_energy",
-        f"{sim_detector.name}-_driver-high_energy",
-        f"{sim_detector.name}-_driver-iterations",
-        f"{sim_detector.name}-_driver-low_energy",
-        f"{sim_detector.name}-_driver-acquire_time",
-        f"{sim_detector.name}-_driver-total_steps",
-        f"{sim_detector.name}-_driver-total_time",
-        f"{sim_detector.name}-_driver-energy_axis",
-        f"{sim_detector.name}-_driver-binding_energy_axis",
-        f"{sim_detector.name}-_driver-angle_axis",
+    energy_array = await sim_detector.driver.energy_axis.get_value()
+    angle_array = await sim_detector.driver.angle_axis.get_value()
+    data = await sim_detector.describe()
+    assert data[f"{sim_detector.name}-_driver-image"]["shape"] == [
+        len(angle_array),
+        len(energy_array),
     ]
-    assert all(item in configuration_read for item in expected_config_keys)
 
 
-# ToDo - Add test that data being read is correct from plan
+async def test_analyser_detector_configuration(
+    sim_detector: AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO],
+) -> None:
+    driver = sim_detector.driver
+    driver_config = await driver.read_configuration()
+    await assert_configuration(sim_detector, driver_config)
+
+
+async def test_analyser_detector_describe_configuration(
+    sim_detector: AbstractElectronAnalyserDetector[AbstractAnalyserDriverIO],
+) -> None:
+    driver = sim_detector.driver
+    driver_describe_config = await driver.describe_configuration()
+
+    assert await sim_detector.describe_configuration() == driver_describe_config
