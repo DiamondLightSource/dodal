ff0b6320c0ab27efaa4026e3ad9ec23d934b6213
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-04 17:09:28 +0100

    Build a table for the mapt positions using device vectors

---
+++ src/dodal/devices/i19/mapt_configuration.py
@@ -0,0 +1,21 @@
+from ophyd_async.core import DeviceVector, SignalR, StandardReadable
+from ophyd_async.epics.signal import epics_signal_r
+
+
+class MAPTConfiguration(StandardReadable):
+    """Readable device that can be used to build the read-only MAPT configuration table for aperture motors in the available positions.
+    Can be used for both eh1 and eh2 on I19.
+
+    """
+
+    def __init__(
+        self, prefix: str, motor_name: str, aperture_list: list[int], name: str = ""
+    ) -> None:
+        with self.add_children_as_readables():
+            self.in_positions: DeviceVector[SignalR[float]] = DeviceVector(
+                {
+                    pos: epics_signal_r(float, f"{prefix}{pos}UM:{motor_name}")
+                    for pos in aperture_list
+                }
+            )
+        super().__init__(name)
--- src/dodal/devices/i19/pin_col_stages.py
+++ src/dodal/devices/i19/pin_col_stages.py
@@ -1,7 +1,8 @@
 from bluesky.protocols import Movable
-from ophyd_async.core import StandardReadable
-from ophyd_async.epics.signal import epics_signal_rw
+from ophyd_async.core import StandardReadable, StrictEnum
+from ophyd_async.epics.signal import epics_signal_r, epics_signal_rw
 
+from dodal.devices.i19.mapt_configuration import MAPTConfiguration
 from dodal.devices.motors import XYStage
 
 _PIN = "-MO-PIN-01:"
@@ -9,14 +10,50 @@ _COL = "-MO-COL-01:"
 _CONFIG = "-OP-PCOL-01:"
 
 
+APERTURE_SIZES = [20, 40, 100, 3000]
+
+
+class PinColConfig(StrictEnum):
+    PCOL20 = "20um"
+    PCOL40 = "40um"
+    PCOL100 = "100um"
+    PCOL3000 = "3000um"
+    PINOUT = "OUT - PINX"
+    COLOUT = "OUT - COLX"
+
+
+class PinColOut(StrictEnum):
+    NOTOUT = "NOT OUT"
+    OUT = "OUT"
+
+
 class PinholeStage(XYStage):
+    """Pinhole stages xy motors."""
+
     def __init__(self, prefix: str, name: str = ""):
         super().__init__(prefix, name)
 
 
 class CollimatorStage(XYStage):
+    """Collimator stage xy motors."""
+
     def __init__(self, prefix: str, name: str = ""):
         super().__init__(prefix, name)
+
+
+class PinColConfiguration(StandardReadable):
+    def __init__(
+        self, prefix: str, apertures: list[int] = APERTURE_SIZES, name: str = ""
+    ) -> None:
+        with self.add_children_as_readables():
+            self.selection = epics_signal_rw(PinColConfig, f"{prefix}CONFIG")
+            self.pin_x = MAPTConfiguration(prefix, "PINX", apertures)
+            self.pin_Y = MAPTConfiguration(prefix, "PINY", apertures)
+            self.col_x = MAPTConfiguration(prefix, "COLX", apertures)
+            self.col_y = MAPTConfiguration(prefix, "COLY", apertures)
+            self.pin_x_out = epics_signal_r(float, f"{prefix}OUT:PINX")
+            self.col_x_out = epics_signal_r(float, f"{prefix}OUT:COLX")
+        super().__init__(name)
 
 
 class PinColControl(StandardReadable, Movable):
@@ -31,5 +68,7 @@ class PinColControl(StandardReadable, Movable):
         with self.add_children_as_readables():
             self.pinhole = PinholeStage(f"{prefix}{pin_infix}")
             self.collimator = CollimatorStage(f"{prefix}{col_infix}")
-            self.config = epics_signal_rw(int, f"{prefix}{config_infix}CONFIG")
+            self.config = PinColConfiguration(f"{prefix}{config_infix}CONFIG")
+            self.config = epics_signal_rw(PinColConfig, f"{prefix}{config_infix}CONFIG")
+            self.is_out = epics_signal_r(PinColOut, f"{prefix}{config_infix}IS_OUT")
         super().__init__(name=name)
