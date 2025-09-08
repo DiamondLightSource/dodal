6a2c4aa39e4aeca836f4b30636114d540b48ed08
Sze Ching <teo.ching@diamond.ac.uk>
2025-09-08 12:15:29 +0100

    Add motor limit checking from ophyd_async

--- src/dodal/devices/smargon.py
+++ src/dodal/devices/smargon.py
@@ -177,6 +177,7 @@ class Smargon(XYZStage, Movable):
             for motor_name, new_setpoint in value.items():
                 if new_setpoint is not None and isinstance(new_setpoint, int | float):
                     axis: Motor = getattr(self, motor_name)
+                    await axis.check_motor_limit(axis.user_setpoint, new_setpoint)
                     put_completion = await set_and_wait_for_value(
                         axis.user_setpoint,
                         new_setpoint,
