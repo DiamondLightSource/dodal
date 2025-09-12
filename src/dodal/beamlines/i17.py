5a4124ea994f8e583434dafb8e8a05ec20aa6cdd
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2025-09-12 13:30:30 +0000

    remove suffix I

--- src/dodal/beamlines/i17.py
+++ src/dodal/beamlines/i17.py
@@ -13,7 +13,7 @@ from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
 BL = get_beamline_name("i17")
-PREFIX = BeamlinePrefix(BL, suffix="I")
+PREFIX = BeamlinePrefix(BL)
 set_log_beamline(BL)
 set_utils_beamline(BL)
 
