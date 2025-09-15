2ce34be95bbfe4b91df49a11f48c444ca00d5bb2
Shreelakshmi Iyengar <xxu39114@pc0117.cs.diamond.ac.uk>
2025-09-15 16:05:25 +0100

    fix: change InOut to InOutUpper since values of InOut defined in ophyd_async are not all capital

--- src/dodal/devices/i19/backlight.py
+++ src/dodal/devices/i19/backlight.py
@@ -1,15 +1,22 @@
 from bluesky.protocols import Movable
-from ophyd_async.core import AsyncStatus, InOut, StandardReadable
+from ophyd_async.core import AsyncStatus, StandardReadable, StrictEnum
 from ophyd_async.epics.core import epics_signal_rw
 
 
-class BacklightPosition(StandardReadable, Movable[InOut]):
+class InOutUpper(StrictEnum):
+    IN = "IN"
+    OUT = "OUT"
+
+
+class BacklightPosition(StandardReadable, Movable[InOutUpper]):
     """Device moves backlight to the IN or OUT position since controls side manages switching the light on/off"""
 
     def __init__(self, prefix: str, name: str = "") -> None:
-        self.position = epics_signal_rw(InOut, prefix + "-EA-IOC-12:AD1:choiceButton")
+        self.position = epics_signal_rw(
+            InOutUpper, prefix + "-EA-IOC-12:AD1:choiceButton"
+        )
         super().__init__(name)
 
     @AsyncStatus.wrap
-    async def set(self, value: InOut):
+    async def set(self, value: InOutUpper):
         await self.position.set(value, wait=True)
