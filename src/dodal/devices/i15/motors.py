76a7aba998a4f26c436db47197f004fee2de0bac
Mark Booth <1576793+Mark-Booth@users.noreply.github.com>
2025-09-15 16:56:41 +0100

    Move XYZPitchYawStage from devices.i15.motors to devices.motors

--- src/dodal/devices/i15/motors.py
+++ src/dodal/devices/i15/motors.py
@@ -1,23 +1,6 @@
 from ophyd_async.epics.motor import Motor
 
-from dodal.devices.motors import _X, _Y, _Z, Stage, XYZStage
-
-
-class XYZPitchYawStage(XYZStage):
-    def __init__(
-        self,
-        prefix: str,
-        name: str = "",
-        x_infix: str = _X,
-        y_infix: str = _Y,
-        z_infix: str = _Z,
-        pitch_infix="PITCH",
-        yaw_infix="YAW",
-    ):
-        with self.add_children_as_readables():
-            self.pitch = Motor(prefix + pitch_infix)
-            self.yaw = Motor(prefix + yaw_infix)
-        super().__init__(prefix, name, x_infix, y_infix, z_infix)
+from dodal.devices.motors import Stage
 
 
 class UpstreamDownstreamPair(Stage):
