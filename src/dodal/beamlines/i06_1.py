02b853f5828bdb0e018e9b1103c782af7d7784ae
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2025-09-18 14:45:51 +0100

    add PGM support for i06 #116 (#1526)
    * add PGM support for i06 #116
    
    * add PGM support using i06_shared for i06 and i06-1
    
    * add PGM support for i06 #116
    
    * add PGM support using i06_shared for i06 and i06-1
    
    * configure _BEAMLINE_SHARED to include i06_shared for i06 and i06-1
    
    * fix spell mistake
    
    * Update src/dodal/beamlines/i06_shared.py
    
    remove name argument as it is the same as default value
    
    Co-authored-by: oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>

---
+++ src/dodal/beamlines/i06_1.py
@@ -0,0 +1,15 @@
+from dodal.common.beamlines.beamline_utils import device_factory
+from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
+from dodal.devices.synchrotron import Synchrotron
+from dodal.log import set_beamline as set_log_beamline
+from dodal.utils import BeamlinePrefix, get_beamline_name
+
+BL = get_beamline_name("i06-1")
+PREFIX = BeamlinePrefix(BL, suffix="J")
+set_log_beamline(BL)
+set_utils_beamline(BL)
+
+
+@device_factory()
+def synchrotron() -> Synchrotron:
+    return Synchrotron()
