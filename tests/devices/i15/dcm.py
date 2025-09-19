b7f755c030d2e89f484212871393691a129eec4f
Mark Booth <1576793+Mark-Booth@users.noreply.github.com>
2025-09-19 14:49:15 +0100

    Try to fix tests/devices/i15/dcm.py

--- tests/devices/i15/dcm.py
+++
@@ -1,7 +0,0 @@
-from dodal.devices.common_dcm import StationaryCrystal
-from dodal.devices.i15.dcm import DCM
-
-
-def test_make_crystals():
-    dcm = DCM("prefix:", StationaryCrystal, StationaryCrystal)
-    assert dcm.xtal_1.roll_in_mrad.user_setpoint.source == "ca://prefix:ROLL.VAL"
