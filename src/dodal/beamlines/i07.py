4fd79abe4543a3e0e2e1b128eb3938ee7990ac52
grc37356 <david.garman@diamond.ac.uk>
2025-09-19 10:56:12 +0000

    Add undulator to i07

--- src/dodal/beamlines/i07.py
+++ src/dodal/beamlines/i07.py
@@ -1,6 +1,7 @@
 from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.i07.dcm import DCM
+from dodal.devices.undulator import Undulator
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -19,3 +20,14 @@ def dcm() -> DCM:
         "dcm",
     )
     return dcm
+
+
+@device_factory()
+def undulator() -> Undulator:
+    """Get the i07 undulator device, instantiate it if it hasn't already been.
+    If this is called when already instantiated it will return the existing object.
+    """
+    return Undulator(
+        prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:",
+        id_gap_lookup_table_path="/dls_sw/i07/software/gda/config/lookupTables/IIDCalibrationTable.txt",
+    )
