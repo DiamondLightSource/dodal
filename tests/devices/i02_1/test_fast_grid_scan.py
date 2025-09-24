0862c2f5b909eaa5953b9078d0797e559828c474
olliesilvester <122091460+olliesilvester@users.noreply.github.com>
2025-09-24 17:47:46 +0100

    Refactor common gridscan (#1479)
    * Refactor common gridscan

--- tests/devices/i02_1/test_fast_grid_scan.py
+++ tests/devices/i02_1/test_fast_grid_scan.py
@@ -1,23 +1,23 @@
+import pytest
 from ophyd_async.core import init_devices
+from ophyd_async.testing import set_mock_value
 
-from dodal.devices.fast_grid_scan import ZebraFastGridScan
-from dodal.devices.i02_1.fast_grid_scan import TwoDFastGridScan
-
-
-def test_fast_grid_scan_has_same_attributes_as_parent(RE):
-    # We never call super().__init__ on the FGS device, since it needs to override
-    # a lot of the attributes as soft signals. Useful to check that the device
-    # looks the same
-    with init_devices(mock=True):
-        vmxm_fgs = TwoDFastGridScan("vmxm")
-        parent_fgs = ZebraFastGridScan("parent")
-    vmxm_fgs_attrs = set(vars(vmxm_fgs).keys())
-    parent_fgs_attrs = set(vars(parent_fgs).keys())
-
-    assert vmxm_fgs_attrs == parent_fgs_attrs
+from dodal.devices.i02_1.fast_grid_scan import (
+    ZebraFastGridScanTwoD,
+)
 
 
-def test_calc_expected_images(RE):
-    with init_devices(mock=True):
-        vmxm_fgs = TwoDFastGridScan("vmxm")
-    assert vmxm_fgs._calculate_expected_images(5, 10, 100) == 50
+@pytest.fixture
+async def fast_grid_scan():
+    async with init_devices(mock=True):
+        fast_grid_scan = ZebraFastGridScanTwoD(prefix="", motion_controller_prefix="")
+    return fast_grid_scan
+
+
+async def test_i02_1_gridscan_has_2d_behaviour(fast_grid_scan: ZebraFastGridScanTwoD):
+    three_d_movables = ["z_step_size_mm", "z2_start_mm", "y2_start_mm", "z_steps"]
+    for movable in three_d_movables:
+        assert movable not in fast_grid_scan.movable_params.keys()
+    set_mock_value(fast_grid_scan.x_steps, 5)
+    set_mock_value(fast_grid_scan.y_steps, 4)
+    assert await fast_grid_scan.expected_images.get_value() == 20
