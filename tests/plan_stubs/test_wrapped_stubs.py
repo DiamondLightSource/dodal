d4919e8942a217f2ab516afd26c8e10be2f7650f
Joseph Ware <53935796+DiamondJoseph@users.noreply.github.com>
2025-10-03 15:44:56 +0100

    chore: Update dependencies (#1599)

--- tests/plan_stubs/test_wrapped_stubs.py
+++ tests/plan_stubs/test_wrapped_stubs.py
@@ -16,6 +16,8 @@ from dodal.plan_stubs.wrapped import (
     wait,
 )
 
+_EMPTY = ()
+
 
 @pytest.fixture
 def x_axis() -> SimMotor:
@@ -125,23 +127,23 @@ def test_sleep():
 def test_wait():
     # Waits for all groups
     assert list(wait()) == [
-        Msg("wait", group=None, timeout=None, error_on_timeout=True)
+        Msg("wait", group=None, timeout=None, error_on_timeout=True, watch=_EMPTY)
     ]
 
 
 def test_wait_group():
     assert list(wait("foo")) == [
-        Msg("wait", group="foo", timeout=None, error_on_timeout=True)
+        Msg("wait", group="foo", timeout=None, error_on_timeout=True, watch=_EMPTY)
     ]
 
 
 def test_wait_timeout():
     assert list(wait(timeout=5.0)) == [
-        Msg("wait", group=None, timeout=5.0, error_on_timeout=True)
+        Msg("wait", group=None, timeout=5.0, error_on_timeout=True, watch=_EMPTY)
     ]
 
 
 def test_wait_group_and_timeout():
     assert list(wait("foo", 5.0)) == [
-        Msg("wait", group="foo", timeout=5.0, error_on_timeout=True)
+        Msg("wait", group="foo", timeout=5.0, error_on_timeout=True, watch=_EMPTY)
     ]
