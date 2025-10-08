5533774ab25d21a676515098239b457961adc879
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-10-08 15:50:57 +0000

    Remove EnumDevice, renamed shutter.py to fast_shutters.py

--- tests/devices/test_fast_shutters.py
+++ tests/devices/test_fast_shutters.py
@@ -1,9 +1,9 @@
 import pytest
-from bluesky import FailedStatus, RunEngine
+from bluesky import RunEngine
 from bluesky import plan_stubs as bps
 from ophyd_async.core import StrictEnum, init_devices
 
-from dodal.devices.shutters import GenericShutter
+from dodal.devices.fast_shutter import GenericShutter
 
 
 class ShutterStates(StrictEnum):
@@ -26,24 +26,19 @@ def shutter(RE: RunEngine) -> GenericShutter[ShutterStates]:
 async def test_shutter_set_open_close_without_knowing_enum_values(
     shutter: GenericShutter, RE: RunEngine
 ) -> None:
-    RE(bps.mv(shutter, shutter.OPEN))
+    RE(bps.mv(shutter, shutter.open_state))
     assert await shutter.state.get_value() == ShutterStates.OPEN
-    RE(bps.mv(shutter, shutter.CLOSE))
+    RE(bps.mv(shutter, shutter.close_state))
     assert await shutter.state.get_value() == ShutterStates.CLOSE
 
 
-def test_shutter_invalid_set(shutter: GenericShutter, RE: RunEngine) -> None:
-    with pytest.raises(FailedStatus):
-        RE(bps.mv(shutter, ShutterStates.FAULT))
-
-
 async def test_shutter_is_open(shutter: GenericShutter, RE: RunEngine) -> None:
-    RE(bps.mv(shutter, shutter.OPEN))
+    RE(bps.mv(shutter, shutter.open_state))
     assert await shutter.is_open()
     assert not await shutter.is_closed()
 
 
 async def test_shutter_is_closed(shutter: GenericShutter, RE: RunEngine) -> None:
-    RE(bps.mv(shutter, shutter.CLOSE))
+    RE(bps.mv(shutter, shutter.close_state))
     assert not await shutter.is_open()
     assert await shutter.is_closed()
