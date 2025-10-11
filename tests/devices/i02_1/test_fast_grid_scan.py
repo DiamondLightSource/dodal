fbf26bee43818c59b032162af856e34aa8186c08
rtuck99 <robert.tuck@diamond.ac.uk>
2025-10-11 17:04:45 +0100

    Move waiting logic for scan invalid into FGS device (#1540)
    * Initial changes, check X/Y/Z_SCAN_VALID, change setup plan to just call prepare
    
    * Beef up gridscan device tests
    
    * Make type-checking happy
    
    * Speedup gridscan test slightly
    
    * Fixup fast_grid_scan with changes from refactoring
    
    * Change grid scan prepare to raise GridScanInvalidException instead of TimeoutError
    
    * Make pyright happy
    
    * Remove redundant comment
    
    * Additional doc comment for set_fast_grid_scan_params
    
    ---------
    
    Co-authored-by: Dominic Oram <dominic.oram@diamond.ac.uk>

--- tests/devices/i02_1/test_fast_grid_scan.py
+++ tests/devices/i02_1/test_fast_grid_scan.py
@@ -17,7 +17,7 @@ async def fast_grid_scan():
 async def test_i02_1_gridscan_has_2d_behaviour(fast_grid_scan: ZebraFastGridScanTwoD):
     three_d_movables = ["z_step_size_mm", "z2_start_mm", "y2_start_mm", "z_steps"]
     for movable in three_d_movables:
-        assert movable not in fast_grid_scan.movable_params.keys()
+        assert movable not in fast_grid_scan._movable_params.keys()
     set_mock_value(fast_grid_scan.x_steps, 5)
     set_mock_value(fast_grid_scan.y_steps, 4)
     assert await fast_grid_scan.expected_images.get_value() == 20
