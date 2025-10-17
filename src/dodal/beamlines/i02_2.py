a69cc64b5264ecc5b17cd5e89eb91721c50c5514
Ollie Silvester <oliver.silvester@diamond.ac.uk>
2025-10-17 14:48:30 +0000

    Remove attenuator from i02-2 as non standard PV names

--- src/dodal/beamlines/i02_2.py
+++ src/dodal/beamlines/i02_2.py
@@ -4,12 +4,6 @@ from dodal.common.beamlines.beamline_utils import (
     device_factory,
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.attenuator.attenuator import EnumFilterAttenuator
-from dodal.devices.attenuator.filter_selections import (
-    I02_2FilterOneSelections,
-    I02_2FilterThreeSelections,
-    I02_2FilterTwoSelections,
-)
 from dodal.devices.motors import XYStage
 from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
@@ -36,19 +30,3 @@ def sample_motors() -> XYStage:
     If this is called when already instantiated in i02-2, it will return the existing object.
     """
     return XYStage(f"{PREFIX.beamline_prefix}-MO-GONIO-01:SAMPLE:")
-
-
-@device_factory()
-def attenuator() -> EnumFilterAttenuator:
-    """Get the i02-2 attenuator device, instantiate it if it hasn't already been.
-    If this is called when already instantiated in i02-2, it will return the existing object.
-    """
-
-    return EnumFilterAttenuator(
-        f"{PREFIX.beamline_prefix}-OP-ATTN-01:",
-        (
-            I02_2FilterOneSelections,
-            I02_2FilterTwoSelections,
-            I02_2FilterThreeSelections,
-        ),
-    )
