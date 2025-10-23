db8a8888560df414c483416ac0d04e943af5a879
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-10-23 08:32:21 +0000

    Create common fixtures which can be reused by other repos

--- conftest.py
+++ conftest.py
@@ -1,18 +1,12 @@
-import asyncio
 import logging
 import sys
-import time
-from collections.abc import Mapping
 from os import environ
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 import pytest
-from bluesky.run_engine import RunEngine
-from ophyd.status import Status
 from ophyd_async.core import (
     PathInfo,
-    PathProvider,
 )
 from tests.devices.i10.test_data import LOOKUP_TABLE_PATH
 from tests.devices.test_daq_configuration import MOCK_DAQ_CONFIG_PATH
@@ -25,7 +19,6 @@ from dodal.common.beamlines import beamline_utils
 from dodal.common.visit import (
     DirectoryServiceClient,
     LocalDirectoryServiceClient,
-    StaticVisitPathProvider,
 )
 from dodal.log import LOGGER, GELFTCPHandler, set_up_all_logging_handlers
 
@@ -94,48 +87,5 @@ def dummy_visit_client() -> DirectoryServiceClient:
     return LocalDirectoryServiceClient()
 
 
-@pytest.fixture
-async def static_path_provider(
-    tmp_path: Path, dummy_visit_client: DirectoryServiceClient
-) -> PathProvider:
-    svpp = StaticVisitPathProvider(
-        beamline="ixx", root=tmp_path, client=dummy_visit_client
-    )
-    await svpp.update()
-    return svpp
-
-
-@pytest.fixture
-def run_engine_documents(run_engine: RunEngine) -> Mapping[str, list[dict]]:
-    docs: dict[str, list[dict]] = {}
-
-    def append_and_print(name, doc):
-        if name not in docs:
-            docs[name] = []
-        docs[name] += [doc]
-
-    run_engine.subscribe(append_and_print)
-    return docs
-
-
-def failed_status(failure: Exception) -> Status:
-    status = Status()
-    status.set_exception(failure)
-    return status
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def _ensure_running_bluesky_event_loop():
-    run_engine = RunEngine()
-    # make sure the event loop is thoroughly up and running before we try to create
-    # any ophyd_async devices which might need it
-    timeout = time.monotonic() + 1
-    while not run_engine.loop.is_running():
-        await asyncio.sleep(0)
-        if time.monotonic() > timeout:
-            raise TimeoutError("This really shouldn't happen but just in case...")
-
-
-@pytest.fixture()
-async def run_engine():
-    yield RunEngine()
+# Add common fixtures to be used in testing
+pytest_plugins = ["dodal.testing.common_fixtures"]
