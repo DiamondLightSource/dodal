ee479bfbf7f117924d3b7bf7577c8061cf3732ae
Huw <huw.shorthouse@diamond.ac.uk>
2025-10-28 18:56:28 +0000

    Merlin now follows new guidance (apart from missing IO class for triggering): https://blueskyproject.io/ophyd-async/main/how-to/implement-ad-detector.html

--- src/dodal/devices/i13_1/merlin.py
+++ src/dodal/devices/i13_1/merlin.py
@@ -1,32 +1,39 @@
-from ophyd_async.core import PathProvider, StandardDetector
+from collections.abc import Sequence
+
+from ophyd_async.core import PathProvider, SignalR
 from ophyd_async.epics import adcore
 
 from dodal.common.beamlines.device_helpers import CAM_SUFFIX, HDF5_SUFFIX
-from dodal.devices.i13_1.merlin_controller import MerlinController
+
+from .merlin_controller import MerlinController
 
 
-class Merlin(StandardDetector):
-    _controller: MerlinController
-    _writer: adcore.ADHDFWriter
-
+class Merlin(adcore.AreaDetector[MerlinController]):
     def __init__(
         self,
         prefix: str,
         path_provider: PathProvider,
         drv_suffix=CAM_SUFFIX,
+        writer_cls: type[adcore.ADWriter] = adcore.ADHDFWriter,
         fileio_suffix=HDF5_SUFFIX,
         name: str = "",
+        config_sigs: Sequence[SignalR] = (),
+        plugins: dict[str, adcore.NDPluginBaseIO] | None = None,
     ):
-        self.drv = adcore.ADBaseIO(prefix + drv_suffix)
-        self.hdf = adcore.NDFileHDFIO(prefix + fileio_suffix)
+        driver = adcore.ADBaseIO(prefix + drv_suffix)
+        controller = MerlinController(driver)
+        writer = writer_cls.with_io(
+            prefix,
+            path_provider,
+            dataset_source=driver,
+            fileio_suffix=fileio_suffix,
+            plugins=plugins,
+        )
 
         super().__init__(
-            MerlinController(self.drv),
-            adcore.ADHDFWriter(
-                fileio=self.hdf,
-                path_provider=path_provider,
-                dataset_describer=adcore.ADBaseDatasetDescriber(self.drv),
-            ),
-            config_sigs=(self.drv.acquire_period, self.drv.acquire_time),
+            controller=controller,
+            writer=writer,
+            plugins=plugins,
             name=name,
+            config_sigs=config_sigs,
         )
--- src/dodal/devices/i13_1/merlin_controller.py
+++ src/dodal/devices/i13_1/merlin_controller.py
@@ -1,54 +1,52 @@
 import asyncio
-import logging
 
 from ophyd_async.core import (
-    DEFAULT_TIMEOUT,
-    AsyncStatus,
+    # DEFAULT_TIMEOUT,
     TriggerInfo,
 )
 from ophyd_async.epics.adcore import (
-    DEFAULT_GOOD_STATES,
+    ADBaseController,
     ADBaseIO,
     ADImageMode,
-    ADState,
 )
-from ophyd_async.epics.core import stop_busy_record
-
-from dodal.devices.controllers import ConstantDeadTimeController
 
 
-class MerlinController(ConstantDeadTimeController):
-    def __init__(
-        self,
-        driver: ADBaseIO,
-        good_states: frozenset[ADState] = DEFAULT_GOOD_STATES,
-    ) -> None:
-        self.driver = driver
-        self.good_states = good_states
-        self.frame_timeout: float = 0
-        self._arm_status: AsyncStatus | None = None
-        for drv_child in self.driver.children():
-            logging.debug(drv_child)
+class MerlinController(ADBaseController[ADBaseIO]):
+    # def __init__(
+    #     self,
+    #     driver: ADBaseIO,
+    #     good_states: frozenset[ADState] = DEFAULT_GOOD_STATES,
+    # ) -> None:
+    #     self.driver = driver
+    #     self.good_states = good_states
+    #     self.frame_timeout: float = 0
+    #     self._arm_status: AsyncStatus | None = None
+    #     for drv_child in self.driver.children():
+    #         logging.debug(drv_child)
 
-        super().__init__(driver, 0.002)
+    #     super().__init__(driver, 0.002)
 
     def get_deadtime(self, exposure: float | None) -> float:
         return 0.001
 
     async def prepare(self, trigger_info: TriggerInfo):
-        self.frame_timeout = (
-            DEFAULT_TIMEOUT + await self.driver.acquire_time.get_value()
-        )
+        if (exposure := trigger_info.livetime) is not None:
+            await self.driver.acquire_time.set(exposure)
+
+        # self.frame_timeout = (
+        #     DEFAULT_TIMEOUT + await self.driver.acquire_time.get_value()
+        # )
+
         await asyncio.gather(
             self.driver.num_images.set(trigger_info.total_number_of_exposures),
             self.driver.image_mode.set(ADImageMode.MULTIPLE),
         )
 
-    async def wait_for_idle(self):
-        if self._arm_status:
-            await self._arm_status
+    # async def wait_for_idle(self):
+    #     if self._arm_status:
+    #         await self._arm_status
 
-    async def disarm(self):
-        # We can't use caput callback as we already used it in arm() and we can't have
-        # 2 or they will deadlock
-        await stop_busy_record(self.driver.acquire, False, timeout=1)
+    # async def disarm(self):
+    #     # We can't use caput callback as we already used it in arm() and we can't have
+    #     # 2 or they will deadlock
+    #     await stop_busy_record(self.driver.acquire, False, timeout=1)
