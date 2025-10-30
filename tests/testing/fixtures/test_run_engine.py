9f98b8823ea2833b28a298291407c8e7b2186911
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-10-30 15:28:55 +0000

    Fixed test, corrected error message to file handler

--- tests/testing/fixtures/test_run_engine.py
+++ tests/testing/fixtures/test_run_engine.py
@@ -15,15 +15,19 @@ def test_run_engine_fixture_has_no_file_handler_leak(
     pid = os.getpid()
     n_open_files = len(os.listdir(f"/proc/{pid}/fd"))
     if iteration == 0:
-        _baseline_n_open_files = n_open_files + 1
+        _baseline_n_open_files = n_open_files
     else:
+        if _baseline_n_open_files is None:
+            raise Exception(
+                "Unable to determine number of file handlers as it is None."
+            )
         try:
-            delta = _baseline_n_open_files == n_open_files
+            delta = n_open_files - _baseline_n_open_files
             # Allow a small threshold from other processes e.g from pytest itself.
             assert delta < threshold
         except AssertionError as exc:
             raise AssertionError(
-                "Detected memeory leak, the number of open files has increased from "
+                "Detected file handle leak, the number of open files has increased from "
                 f"{_baseline_n_open_files} to {n_open_files} when calling the "
                 "run_engine fixture",
             ) from exc
