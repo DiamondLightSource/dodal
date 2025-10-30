428cbfe4d64a235c8b88dd077d65541a726a5359
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-10-30 11:44:36 +0000

    Added small threshold to trigger test fail, increased number of iterations

--- tests/testing/fixtures/test_run_engine.py
+++ tests/testing/fixtures/test_run_engine.py
@@ -3,10 +3,11 @@ import os
 import pytest
 from bluesky import RunEngine
 
-_baseline_n_open_files = None  # module-level storage for first run
+_baseline_n_open_files = None
+threshold = 5
 
 
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(11))
 def test_run_engine_fixture_has_no_memory_leak(
     run_engine: RunEngine, iteration: int
 ) -> None:
@@ -14,10 +15,12 @@ def test_run_engine_fixture_has_no_memory_leak(
     pid = os.getpid()
     n_open_files = len(os.listdir(f"/proc/{pid}/fd"))
     if iteration == 0:
-        _baseline_n_open_files = n_open_files
+        _baseline_n_open_files = n_open_files + 1
     else:
         try:
-            assert _baseline_n_open_files == n_open_files
+            delta = _baseline_n_open_files == n_open_files
+            # Allow a small threshold from other processes e.g from pytest itself.
+            assert delta < threshold
         except AssertionError as exc:
             raise AssertionError(
                 "Detected memeory leak, the number of open files has increased from "
