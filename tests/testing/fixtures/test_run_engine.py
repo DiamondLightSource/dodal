abd6d741fc4fe02d4331936ebefe64eee2612157
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-10-30 10:25:57 +0000

    Adjusted variable names

--- tests/testing/fixtures/test_run_engine.py
+++ tests/testing/fixtures/test_run_engine.py
@@ -3,21 +3,24 @@ import os
 import pytest
 from bluesky import RunEngine
 
-_baseline = None  # module-level storage for first run
+_baseline_n_open_files = None  # module-level storage for first run
 
 
 @pytest.mark.parametrize("iteration", range(5))
-def test_run_engine_has_no_memory_leak(run_engine: RunEngine, iteration: int) -> None:
-    global _baseline
+def test_run_engine_fixture_has_no_memory_leak(
+    run_engine: RunEngine, iteration: int
+) -> None:
+    global _baseline_n_open_files
     pid = os.getpid()
-    open_files = len(os.listdir(f"/proc/{pid}/fd"))
+    n_open_files = len(os.listdir(f"/proc/{pid}/fd"))
     if iteration == 0:
-        _baseline = open_files
+        _baseline_open_files = n_open_files
     else:
         try:
-            assert _baseline == open_files
+            assert _baseline_n_open_files == n_open_files
         except AssertionError as exc:
             raise AssertionError(
                 "Detected memeory leak, the number of open files has increased from "
-                f"{_baseline} to {open_files} when calling the run_engine fixture",
+                f"{_baseline_n_open_files} to {n_open_files} when calling the "
+                "run_engine fixture",
             ) from exc
