77ff2269f9eb7a7826db7bdec63bd9a2f731eb35
Sze Ching <teo.ching@diamond.ac.uk>
2025-11-03 13:33:57 +0000

    Fix test

--- tests/devices/oav/test_oav.py
+++ tests/devices/oav/test_oav.py
@@ -184,4 +184,5 @@ async def test_oav_with_null_zoom_controller_set_zoom_level_other_than_1(
     null_controller: NullZoomController,
 ):
     with pytest.raises(Exception, match="Attempting to set zoom level"):
-        null_controller.set("2.0x")
+        status = null_controller.set("2.0x")
+        await status
