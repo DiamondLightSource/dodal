c2fe3a5a6d342c9f5cd5fe3d31f588bfeb867941
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-11-05 09:49:48 +0000

    remove static path provider from i22.py

--- src/dodal/beamlines/i22.py
+++ src/dodal/beamlines/i22.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from ophyd_async.epics.adaravis import AravisDetector
 from ophyd_async.epics.adcore import NDPluginBaseIO, NDPluginStatsIO
 from ophyd_async.epics.adpilatus import PilatusDetector
@@ -8,7 +6,6 @@ from ophyd_async.fastcs.panda import HDFPanda
 from dodal.common.beamlines.beamline_utils import (
     device_factory,
     get_path_provider,
-    set_path_provider,
 )
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.beamlines.device_helpers import CAM_SUFFIX, DET_SUFFIX, HDF5_SUFFIX
@@ -16,7 +13,6 @@ from dodal.common.crystal_metadata import (
     MaterialsEnum,
     make_crystal_metadata_from_material,
 )
-from dodal.common.visit import RemoteDirectoryServiceClient, StaticVisitPathProvider
 from dodal.devices.bimorph_mirror import BimorphMirror
 from dodal.devices.focusing_mirror import FocusingMirror
 from dodal.devices.i22.dcm import DCM
@@ -36,19 +32,6 @@ BL = get_beamline_name("i22")
 PREFIX = BeamlinePrefix(BL)
 set_log_beamline(BL)
 set_utils_beamline(BL)
-
-# Currently we must hard-code the visit, determining the visit at runtime requires
-# infrastructure that is still WIP.
-# Communication with GDA is also WIP so for now we determine an arbitrary scan number
-# locally and write the commissioning directory. The scan number is not guaranteed to
-# be unique and the data is at risk - this configuration is for testing only.
-set_path_provider(
-    StaticVisitPathProvider(
-        BL,
-        Path("/dls/i22/data/2025/cm40643-4/"),
-        client=RemoteDirectoryServiceClient("http://i22-control:8088/api"),
-    )
-)
 
 
 @device_factory()
