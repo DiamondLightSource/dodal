c3fb578ae47d414661bd3f576a4a4862ef450269
Victor Rogalev <Villtord@users.noreply.github.com>
2025-11-11 16:42:34 +0000

    Create hard-x-ray undulator for i09 branches (#1531)
    * create hard-x-ray undulator and add to i09
    
    * move comments to a parametere
    
    * add "comments" parameter to lookuptable in hard undulator
    
    * extract update lookup table cache method
    
    * few comments added
    
    * typo fix
    
    * add reading test
    
    * add more tests
    
    * more tests
    
    * tests
    
    * remove lut path
    
    * more tests!!!
    
    * update docs
    
    * remove order from set method - it is not how currently beamline use it
    
    * fix cached lookup value
    
    * update tests
    
    * set method docs update
    
    * make gap calculation function callable parameter
    
    * review: order extracted to a separate class
    
    * replace column numbers with constants
    
    * amend docs
    
    * change names of parameters in calculation gap function
    
    * amend test string
    
    * add valueerror message with valid orders
    
    * restructure tests, add docs
    
    * amend comment
    
    * Sorted out theory of gap calculation
    
    * Refactor undulator class, make hardUndulator only set gap in mm
    
    * fix tests, amend docstrings
    
    * update tests and docs
    
    * move _set_gap to a base undulator class
    
    * Remove order from hard undulator
    
    * add test move to same gap
    
    * Remove hard undulator class and use base undulator instead
    
    * fix test
    
    * amend test after rebase
    
    * Apply suggestion from @oliwenmandiamond
    
    Co-authored-by: oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
    
    * Apply suggestion from @oliwenmandiamond
    
    Co-authored-by: oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
    
    * run pre-commit
    
    * fix failing test
    
    * rework - remove unrelated, create abstract
    
    * fix tests
    
    * amend docstring
    
    * make ruff happy
    
    * fix typo and remove identical constructor
    
    * add empty line
    
    ---------
    
    Co-authored-by: eir17846 <victor.rogalev@diamond.ac.uk>
    Co-authored-by: oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>

--- src/dodal/beamlines/i09_1.py
+++ src/dodal/beamlines/i09_1.py
@@ -11,7 +11,7 @@ from dodal.devices.electron_analyser import EnergySource
 from dodal.devices.electron_analyser.specs import SpecsDetector
 from dodal.devices.i09_1 import LensMode, PsuMode
 from dodal.devices.synchrotron import Synchrotron
-from dodal.devices.undulator import UndulatorOrder
+from dodal.devices.undulator import UndulatorInMm, UndulatorOrder
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -40,7 +40,7 @@ def energy_source() -> EnergySource:
 
 # Connect will work again after this work completed
 # https://jira.diamond.ac.uk/browse/I09-651
-@device_factory()
+@device_factory(skip=True)
 def analyser() -> SpecsDetector[LensMode, PsuMode]:
     return SpecsDetector[LensMode, PsuMode](
         prefix=f"{PREFIX.beamline_prefix}-EA-DET-02:CAM:",
@@ -48,6 +48,11 @@ def analyser() -> SpecsDetector[LensMode, PsuMode]:
         psu_mode_type=PsuMode,
         energy_source=energy_source(),
     )
+
+
+@device_factory()
+def undulator() -> UndulatorInMm:
+    return UndulatorInMm(prefix=f"{PREFIX.beamline_prefix}-MO-UND-01:")
 
 
 @device_factory()
