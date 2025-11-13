8048fc5b2f30086f5ea764b0e4d56b248fb43f26
Raymond Fan <raymond.fan@diamond.ac.uk>
2025-11-13 13:28:56 +0000

    1643 create apple2 id for i06 and i05 (#1650)
    * change fault to status and use enable instead
    
    * add i05 id
    
    * make apple2 and controller with lockedPhaseAxis
    
    * add i06 and i05 apple2 with locked phase axes
    
    * correct reference
    
    * add some comments
    
    * fixes extract_phase_val
    
    * add test for lockedController
    
    * add test for apple2 set method
    
    * first docstring
    
    * fixes docstring
    
    * clean up phase motor data class
    
    * change to EnabledDisabledUpper
    
    * update to use gap and phase
    
    * fixes fat finger

--- tests/devices/i10/test_i10_apple2.py
+++ tests/devices/i10/test_i10_apple2.py
@@ -19,6 +19,7 @@ from ophyd_async.testing import (
 
 from dodal.devices.apple2_undulator import (
     BeamEnergy,
+    EnabledDisabledUpper,
     InsertionDeviceEnergy,
     InsertionDevicePolarisation,
     Pol,
@@ -61,7 +62,7 @@ async def mock_id_gap(prefix: str = "BLXX-EA-DET-007:") -> UndulatorGap:
     set_mock_value(mock_id_gap.velocity, 1)
     set_mock_value(mock_id_gap.user_readback, 20)
     set_mock_value(mock_id_gap.user_setpoint, "20")
-    set_mock_value(mock_id_gap.fault, 0)
+    set_mock_value(mock_id_gap.status, EnabledDisabledUpper.ENABLED)
     return mock_id_gap
 
 
@@ -89,7 +90,7 @@ async def mock_phase_axes(prefix: str = "BLXX-EA-DET-007:") -> UndulatorPhaseAxe
     set_mock_value(mock_phase_axes.top_inner.user_setpoint_readback, 0)
     set_mock_value(mock_phase_axes.btm_outer.user_setpoint_readback, 0)
     set_mock_value(mock_phase_axes.btm_inner.user_setpoint_readback, 0)
-    set_mock_value(mock_phase_axes.fault, 0)
+    set_mock_value(mock_phase_axes.status, EnabledDisabledUpper.ENABLED)
     return mock_phase_axes
 
 
@@ -112,7 +113,7 @@ async def mock_jaw_phase(prefix: str = "BLXX-EA-DET-007:") -> UndulatorJawPhase:
     set_mock_value(mock_jaw_phase.gate, UndulatorGateStatus.CLOSE)
     set_mock_value(mock_jaw_phase.jaw_phase.velocity, 2)
     set_mock_value(mock_jaw_phase.jaw_phase.user_readback, 0)
-    set_mock_value(mock_jaw_phase.fault, 0)
+    set_mock_value(mock_jaw_phase.status, EnabledDisabledUpper.ENABLED)
     return mock_jaw_phase
 
 
@@ -280,11 +281,18 @@ async def test_fail_i10_apple2_controller_set_undefined_pol(
 async def test_fail_i10_apple2_controller_set_id_not_ready(
     mock_id_controller: I10Apple2Controller,
 ):
-    set_mock_value(mock_id_controller.apple2().gap().fault, 1)
+    set_mock_value(
+        mock_id_controller.apple2().gap().status, EnabledDisabledUpper.DISABLED
+    )
     with pytest.raises(RuntimeError) as e:
         await mock_id_controller.energy.set(600)
-    assert str(e.value) == mock_id_controller.apple2().gap().name + " is in fault state"
-    set_mock_value(mock_id_controller.apple2().gap().fault, 0)
+    assert (
+        str(e.value)
+        == mock_id_controller.apple2().gap().name + " is DISABLED and cannot move."
+    )
+    set_mock_value(
+        mock_id_controller.apple2().gap().status, EnabledDisabledUpper.ENABLED
+    )
     set_mock_value(mock_id_controller.apple2().gap().gate, UndulatorGateStatus.OPEN)
     with pytest.raises(RuntimeError) as e:
         await mock_id_controller.energy.set(600)
