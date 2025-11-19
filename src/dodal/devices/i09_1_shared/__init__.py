43a05f47c0e166c2f0959bb3f3dd21950df3d124
eir17846 <victor.rogalev@diamond.ac.uk>
2025-11-19 17:36:52 +0000

    fix typo

--- src/dodal/devices/i09_1_shared/__init__.py
+++ src/dodal/devices/i09_1_shared/__init__.py
@@ -8,8 +8,7 @@ from .hard_undulator_functions import (
 __all__ = [
     "calculate_gap_i09_hu",
     "get_hu_lut_as_dict",
-    "calculate_energy_i09_hu"
+    "calculate_energy_i09_hu",
     "HardInsertionDeviceEnergy",
     "HardEnergy",
 ]
-
