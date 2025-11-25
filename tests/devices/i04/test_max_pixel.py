7fd3c258a010bc18ae8636d9158ff93e3157fbd7
Srishty <srish.saj@gmail.com>
2025-11-25 16:10:32 +0000

    attemt own tests

--- tests/devices/i04/test_max_pixel.py
+++ tests/devices/i04/test_max_pixel.py
@@ -1,10 +1,30 @@
-from unittest.mock import patch
+from collections.abc import AsyncGenerator
+from unittest.mock import AsyncMock, MagicMock, Mock, patch
 
 import cv2
 import numpy as np
 import pytest
+from ophyd_async.core import init_devices
 
 from dodal.devices.i04.max_pixel import MaxPixel
+
+# @pytest.fixture
+# async def max_pixel() -> AsyncGenerator[tuple[MaxPixel, MagicMock], None]:
+#     async with init_devices(mock=True):
+#         # mock_ap_sg = MagicMock()
+#         # mock_ap_sg.return_value.selected_aperture.set = AsyncMock()
+#         # mock_ap_sg.return_value.selected_aperture.get_value = AsyncMock()
+#         max_pixel = MaxPixel(
+#             prefix="",
+#             name="test_scin",
+#         )
+#         max_pixel.
+# with ExitStack() as motor_patch_stack:
+#     for motor in [scintillator.y_mm, scintillator.z_mm]:
+#         motor_patch_stack.enter_context(patch_motor(motor))
+#     await scintillator.y_mm.set(5)
+#     await scintillator.z_mm.set(5)
+#     yield scintillator, mock_ap_sg
 
 
 @pytest.fixture
@@ -16,14 +36,18 @@ def mock_array():
     )
 
 
-def test_preprocessed_data_grayscale_and_blur(mock_array):
+async def test_preprocessed_data_grayscale_and_blur(mock_array):
     device = MaxPixel("TEST:")
     # Monkeypatch array_data to be our mock array
-    device.array_data = cv2.cvtColor(  # type: ignore
-        np.stack([mock_array] * 3, axis=-1),  # make it 3-channel BGR
-        cv2.COLOR_BGR2GRAY,
-    )
-    blurred = device.preprocessed_data()
+    mock_array_signal = Mock()
+    mock_array_signal.get_value = mock_array
+
+    # device.array_data.get_value = cv2.cvtColor(  # type: ignore
+    #     np.stack([mock_array] * 3, axis=-1),  # make it 3-channel BGR
+    #     cv2.COLOR_BGR2GRAY,
+    # )
+
+    blurred = await device.preprocessed_data()
     assert isinstance(blurred, np.ndarray)
     assert blurred.shape == mock_array.shape
 
