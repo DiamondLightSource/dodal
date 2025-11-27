0568905c3938766aa52d06d535ba8a0be647e933
Srishty <srish.saj@gmail.com>
2025-11-27 11:18:25 +0000

    make changes from Dom's comments

--- src/dodal/devices/i04/max_pixel.py
+++ src/dodal/devices/i04/max_pixel.py
@@ -12,16 +12,20 @@ KERNAL_SIZE = (7, 7)
 
 
 class MaxPixel(StandardReadable, Triggerable):
-    """Gets the max pixel from the image"""
+    """Gets the max pixel (brightest pixel) from the image after some image processing."""
 
     def __init__(self, prefix: str, name: str = "") -> None:
         self.array_data = epics_signal_r(np.ndarray, f"pva://{prefix}PVA:ARRAY")
         self.max_pixel_val, self._max_val_setter = soft_signal_r_and_setter(float)
         super().__init__(name)
 
-    async def preprocessed_data(self):
+    async def _convert_to_gray_and_blur(self):
         """
-        Preprocess the image array data (convert to grayscale and apply a guassian blur)
+        Preprocess the image array data (convert to grayscale and apply a gaussian blur)
+        Image is converted to grayscale (using a weighted mean as green contributes more to brightness)
+        as we aren't interested in data relating to colour. A blur is then applied to mitigate
+        errors due to rogue hot pixels and provide smoother results for binarization of the image,
+        which in turn gives a better ellipse fit.
         """
         data = await self.array_data.get_value()
         gray_arr = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
--- tests/devices/i04/test_max_pixel.py
+++ tests/devices/i04/test_max_pixel.py
@@ -1,7 +1,6 @@
 from collections.abc import AsyncGenerator
-from unittest.mock import ANY, AsyncMock, MagicMock, Mock, patch
+from unittest.mock import ANY, AsyncMock, MagicMock, patch
 
-# import cv2
 import cv2
 import numpy as np
 import pytest
@@ -19,26 +18,28 @@ async def max_pixel() -> AsyncGenerator[MaxPixel]:
 
 
 @pytest.mark.parametrize(
-    "arr_m, expected",
+    "preprocessed_data, expected",
     [([1, 2, 3], 3), ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], 0), ([-9, -8, 0], 0)],
 )
-@patch("dodal.devices.i04.max_pixel.MaxPixel.preprocessed_data")
+@patch("dodal.devices.i04.max_pixel.MaxPixel._convert_to_gray_and_blur")
 async def test_returns_max(
-    mocked_preprocessed_data: AsyncMock, arr_m, expected, max_pixel: MaxPixel
+    mocked_preprocessed_data: AsyncMock,
+    preprocessed_data,
+    expected,
+    max_pixel: MaxPixel,
 ):
-    # set_mock_value(max_pixel.array_data, arr) -> should test that the preprocessed bit works
-    mocked_preprocessed_data.return_value = arr_m
+    mocked_preprocessed_data.return_value = preprocessed_data
     await max_pixel.trigger()
     assert await max_pixel.max_pixel_val.get_value() == expected
 
 
 @patch("dodal.devices.i04.max_pixel.cv2.cvtColor")
 @patch("dodal.devices.i04.max_pixel.cv2.GaussianBlur")
-async def test_preprocessed_data_grayscale(
+async def test_preprocessed_data_grayscale_is_called(
     mocked_cv2_blur: MagicMock, mocked_cv2_grey: MagicMock, max_pixel: MaxPixel
 ):
     data = np.array([1])
     set_mock_value(max_pixel.array_data, data)
-    await max_pixel.preprocessed_data()
+    await max_pixel._convert_to_gray_and_blur()
     mocked_cv2_grey.assert_called_once_with(data, cv2.COLOR_BGR2GRAY)
     mocked_cv2_blur.assert_called_once_with(ANY, KERNAL_SIZE, 0)
