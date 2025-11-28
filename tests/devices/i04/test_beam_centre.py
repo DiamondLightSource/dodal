ceb2a4cf6bf7eba8e43d40c6c3b7b479b1089365
Srishty <srish.saj@gmail.com>
2025-11-28 15:51:46 +0000

    add more tests

--- tests/devices/i04/test_beam_centre.py
+++ tests/devices/i04/test_beam_centre.py
@@ -1,10 +1,13 @@
 from collections.abc import AsyncGenerator
-from unittest.mock import ANY, AsyncMock, MagicMock, patch
+from unittest.mock import ANY, AsyncMock, MagicMock, call, patch
 
+import git
+import numpy as np
 import pytest
+from cv2 import threshold
 from ophyd_async.core import init_devices, set_mock_value
 
-from dodal.devices.i04.beam_centre import CentreEllipseMethod
+from dodal.devices.i04.beam_centre import CentreEllipseMethod, binary_img
 
 
 @pytest.fixture
@@ -14,13 +17,37 @@ async def beam_centre_ellipse() -> AsyncGenerator[CentreEllipseMethod]:
     yield max_pixel
 
 
-@patch("dodal.devices.i04.max_pixel.cv2.GaussianBlur")
-def test_binary_img():
+test_arr = np.array(
+    [
+        [[123, 100, 100], [0, 0, 0], [0, 0, 0]],
+        [[123, 100, 100], [0, 0, 0], [0, 0, 0]],
+        [[123, 100, 100], [0, 0, 0], [0, 0, 0]],
+        [[123, 100, 100], [0, 0, 0], [0, 0, 0]],
+    ],
+    dtype=np.uint8,
+)
+
+expected_result = np.array(
+    [[255, 0, 0], [255, 0, 0], [255, 0, 0], [255, 0, 0]],
+    dtype=np.uint8,
+)
+
+
+@patch("dodal.devices.i04.beam_centre.cv2.threshold")
+async def test_binary_img(mock_threshold_func: MagicMock):
+    await binary_img(test_arr)  # maybe you need to use real image data for this to work
+    assert mock_threshold_func.call_count == 2
+    # assert mock_threshold_func.call_args_list[0] == call()
+    # assert result == expected_result
+
     # Patch each cv2 function that is used. For each one, do assert_called_once_with(...). Also set return value for
     # cv2.threshold and assert binary_img() == this return value
 
 
-def test_fit_ellipse_good_params():
+@patch("dodal.devices.i04.beam_centre.cv2.fit_ellipse")
+def test_fit_ellipse_good_params(fit_ellipse_mock: MagicMock):
+    fit_ellipse_mock.return_value()
+
     # patch cv2 functiosn and assert_called_once_with, assert return value is correct by setting return value of
     # the cv2.fitellipse magicmock
     pass
