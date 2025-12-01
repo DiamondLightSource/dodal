006eb785f2c2a14806475cf659481bf1114f3056
Richard Dixey <Richard.Dixey@diamond.ac.uk>
2025-12-01 11:30:28 +0000

    add hybrid fascs/adodin eiger. moved from ophyd_async branch

---
+++ src/dodal/devices/async_adeiger/adeiger.py
@@ -0,0 +1,53 @@
+from dodal.devices.eiger.adodin_io import Odin, OdinWriter
+from ophyd_async.core import (
+    AsyncStatus,
+    PathProvider,
+    StandardDetector,
+    TriggerInfo,
+)
+from ophyd_async.epics.adcore import NDPluginBaseIO
+from ophyd_async.fastcs.eiger._eiger_controller import EigerController
+from ophyd_async.fastcs.eiger._eiger_io import EigerDriverIO
+
+
+class EigerDetector(StandardDetector):
+    """Ophyd-async implementation of an Eiger Detector."""
+
+    _controller: EigerController
+    _writer: OdinWriter
+
+    def __init__(
+        self,
+        prefix: str,
+        path_provider: PathProvider,
+        drv_suffix="-EA-EIGER-01:",
+        hdf_suffix="-EA-EIGER-01:OD:",
+        odin_nodes: int = 4,
+        plugins: dict[str, NDPluginBaseIO] | None = None,
+        odin_writer_number: int = 1,
+        name="",
+    ):
+        # NOTE: filename_suffix is  _000001 if BlocksPerFile is 0 (off) or
+        # until you collect more frames than the BlockSize,
+        # at which point it rolls over to _000002, etc. Upto the number of nodes.
+        # see _odin_io: _get_odin_filename_suffix
+        # TODO: https://github.com/bluesky/ophyd-async/issues/1137
+
+        self.drv = EigerDriverIO(prefix + drv_suffix)
+        self.odin = Odin(prefix + hdf_suffix, nodes=odin_nodes)
+
+        super().__init__(
+            EigerController(self.drv),
+            OdinWriter(
+                path_provider,
+                self.odin,
+                self.drv.detector.bit_depth_image,
+                plugins=plugins,
+                odin_writer_number=odin_writer_number,  # see TODO
+            ),
+            name=name,
+        )
+
+    @AsyncStatus.wrap
+    async def prepare(self, value: TriggerInfo) -> None:
+        await super().prepare(value)
