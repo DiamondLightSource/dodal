bac01a581310e1f02915786ba349f94de6d6587a
grc37356 <david.garman@diamond.ac.uk>
2025-12-11 11:43:28 +0000

    Add undulator for I16

--- src/dodal/beamlines/i16.py
+++ src/dodal/beamlines/i16.py
@@ -1,3 +1,4 @@
+from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.devices.undulator import UndulatorInMm
 from dodal.log import set_beamline as set_log_beamline
@@ -9,11 +10,7 @@ set_utils_beamline(BL)
 PREFIX = BeamlinePrefix(BL)
 
 
-# I think this requires a standard undulator (the i16 one seems to just get and set the
-# gap) and then a separate class to manage the energy calculation.  This will probably
-# be required for the ccm when we get that going.
-
-
+@device_factory()
 def id() -> UndulatorInMm:
     return UndulatorInMm(
         name="id", prefix=f"{BeamlinePrefix(BL).insertion_prefix}-MO-SERVC-01:"
