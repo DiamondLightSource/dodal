3ed47aecec68352c7488563b753b07c06713be36
grc37356 <david.garman@diamond.ac.uk>
2025-12-11 16:04:40 +0000

    Harmonic added, comments addressed

--- src/dodal/beamlines/i16.py
+++ src/dodal/beamlines/i16.py
@@ -1,6 +1,6 @@
-from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.undulator import UndulatorInMm
+from dodal.device_manager import DeviceManager
+from dodal.devices.undulator import UndulatorInMm, UndulatorOrder
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -10,8 +10,14 @@ set_utils_beamline(BL)
 PREFIX = BeamlinePrefix(BL)
 
 
-@device_factory()
+devices = DeviceManager()
+
+
+@devices.factory()
 def id() -> UndulatorInMm:
-    return UndulatorInMm(
-        name="id", prefix=f"{BeamlinePrefix(BL).insertion_prefix}-MO-SERVC-01:"
-    )
+    return UndulatorInMm(name="id", prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:")
+
+
+@devices.factory()
+def harmonic() -> UndulatorOrder:
+    return UndulatorOrder("harmonic")
