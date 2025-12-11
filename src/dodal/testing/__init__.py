46a38b8267795cc34ec434f21005c61dfddef290
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-12-11 15:28:38 +0000

    Use mock config server in tests and delete test config files

--- src/dodal/testing/__init__.py
+++ src/dodal/testing/__init__.py
@@ -1,3 +1,4 @@
 from .mock_config_server.mock_config_server import MockConfigServer
+from .mock_config_server.mock_oav_config import MockOavConfig
 
-__all__ = ["MockConfigServer"]
+__all__ = ["MockConfigServer", "MockOavConfig"]
