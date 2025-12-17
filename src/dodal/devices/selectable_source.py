f2e552c036efbb8a3cc2b3738c5f5f478ec9a77a
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2025-12-17 17:08:26 +0000

    Added doc string to SourceSelector and get_obj_from_selected_source

--- src/dodal/devices/selectable_source.py
+++ src/dodal/devices/selectable_source.py
@@ -13,6 +13,8 @@ T = TypeVar("T")
 
 
 def get_obj_from_selected_source(selected_source: SelectedSource, s1: T, s2: T) -> T:
+    """Util function that maps enum values for SelectedSource to two objects. It then
+    returns one of the objects that corrosponds to the selected_source value."""
     match selected_source:
         case SelectedSource.SOURCE1:
             return s1
@@ -21,6 +23,9 @@ def get_obj_from_selected_source(selected_source: SelectedSource, s1: T, s2: T) 
 
 
 class SourceSelector(StandardReadable, Movable[SelectedSource]):
+    """Device that holds a selected_source signal enum of SelectedSource. Useful for
+    beamlines with multiple sources to coordinate which energy source or shutter to use."""
+
     def __init__(self, name: str = ""):
         with self.add_children_as_readables():
             self.selected_source = soft_signal_rw(
