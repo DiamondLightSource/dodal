76ffa38fccc4ce5db71453268e655446d298c473
Will Barnett <william.barnett@diamond.ac.uk>
2025-12-19 08:09:55 +0000

    Pressure cell AD added some controller tests

--- tests/devices/test_pressure_jump_cell_detector.py
+++ tests/devices/test_pressure_jump_cell_detector.py
@@ -1,9 +1,19 @@
+import asyncio
+
 import pytest
-from ophyd_async.core import PathProvider, init_devices, set_mock_value
+from ophyd_async.core import (
+    DetectorTrigger,
+    PathProvider,
+    TriggerInfo,
+    callback_on_mock_put,
+    init_devices,
+    set_mock_value,
+)
 
 from dodal.devices.areadetector import PressureJumpCellDetector
 from dodal.devices.areadetector.pressurejumpcell_io import (
     AdcTriggerMode,
+    AdcTriggerState,
     PressureJumpCellTriggerMode,
 )
 
@@ -14,6 +24,38 @@ async def cell_ad(static_path_provider: PathProvider) -> PressureJumpCellDetecto
         pjump_ad = PressureJumpCellDetector("DEMO-PJUMPAD-01:", static_path_provider)
 
     return pjump_ad
+
+
+@pytest.fixture
+async def cell_ad_with_mocked_arm(
+    cell_ad: PressureJumpCellDetector,
+) -> PressureJumpCellDetector:
+    async def set_acquire(value, *_, **__):
+        async def value_set():
+            await asyncio.sleep(0)
+            set_mock_value(cell_ad.driver.acquire, value)
+
+        asyncio.create_task(value_set())
+
+    async def set_state(value, *_, **__):
+        async def value_set_state_armed():
+            await asyncio.sleep(0)
+            set_mock_value(cell_ad.trig.capture, value)
+            if value:
+                await asyncio.sleep(0)
+                set_mock_value(
+                    cell_ad.trig.state, AdcTriggerState.ARMED
+                )  # prepare capture
+                set_mock_value(cell_ad.driver.acquire, True)
+                await asyncio.sleep(0)
+                set_mock_value(cell_ad.trig.state, AdcTriggerState.IDLE)  # end capture
+
+        asyncio.create_task(value_set_state_armed())
+
+    callback_on_mock_put(cell_ad.driver.acquire, set_acquire)
+    callback_on_mock_put(cell_ad.trig.capture, set_state)
+
+    return cell_ad
 
 
 async def test_pjumpcellad_driver_includes_trigger_mode(
@@ -47,3 +89,57 @@ async def test_pjumpcellad_trigger_includes_triggermode(
     await cell_ad.trig.trigger_mode.set(AdcTriggerMode.SINGLE)
 
     assert await cell_ad.trig.trigger_mode.get_value() == AdcTriggerMode.SINGLE
+
+
+async def test_pjumpcellad_controller(
+    cell_ad_with_mocked_arm: PressureJumpCellDetector,
+):
+    cell_ad_ctrl = cell_ad_with_mocked_arm._controller
+
+    await cell_ad_ctrl.prepare(TriggerInfo(trigger=DetectorTrigger.INTERNAL))
+
+    await cell_ad_ctrl.arm()
+
+    await cell_ad_ctrl.wait_for_idle()
+
+
+async def test_pjumpcellad_disarm_sets_capture(
+    cell_ad_with_mocked_arm: PressureJumpCellDetector,
+):
+    cell_ad_ctrl = cell_ad_with_mocked_arm._controller
+
+    await cell_ad_ctrl.prepare(TriggerInfo(trigger=DetectorTrigger.INTERNAL))
+
+    await cell_ad_ctrl.arm()
+
+    await cell_ad_ctrl.disarm()
+
+    assert not await cell_ad_ctrl.trig.capture.get_value()
+
+
+async def test_pjumpcellad_disarm_sets_driver_acquire(
+    cell_ad_with_mocked_arm: PressureJumpCellDetector,
+):
+    cell_ad_ctrl = cell_ad_with_mocked_arm._controller
+
+    await cell_ad_ctrl.prepare(TriggerInfo(trigger=DetectorTrigger.INTERNAL))
+
+    await cell_ad_ctrl.arm()
+
+    await cell_ad_ctrl.disarm()
+
+    assert not await cell_ad_ctrl.driver.acquire.get_value()
+
+
+async def test_pjumpcellad_setting_trigger_info_livetime_sets_acquire_time(
+    cell_ad_with_mocked_arm: PressureJumpCellDetector,
+):
+    cell_ad_ctrl = cell_ad_with_mocked_arm._controller
+
+    livetime_value = 123
+
+    await cell_ad_ctrl.prepare(
+        TriggerInfo(trigger=DetectorTrigger.INTERNAL, livetime=livetime_value)
+    )
+
+    assert await cell_ad_ctrl.driver.acquire_time.get_value() == livetime_value
