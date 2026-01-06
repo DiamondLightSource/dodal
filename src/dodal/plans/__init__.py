747ff681828b844a4bfcab1a3cee69397ba1641b
Emily Arnold <emily.arnold@diamond.ac.uk>
2026-01-06 09:50:03 +0000

    Change wrapped plans from inferred grids to explicit grids

--- src/dodal/plans/__init__.py
+++ src/dodal/plans/__init__.py
@@ -1,21 +1,33 @@
 from .spec_path import spec_scan
 from .wrapped import (
     count,
+    list_grid_rscan,
+    list_grid_scan,
     list_rscan,
     list_scan,
+    num_grid_rscan,
+    num_grid_scan,
     num_rscan,
     num_scan,
+    step_grid_rscan,
+    step_grid_scan,
     step_rscan,
     step_scan,
 )
 
 __all__ = [
     "count",
+    "list_grid_rscan",
+    "list_grid_scan",
     "list_rscan",
     "list_scan",
+    "num_grid_rscan",
+    "num_grid_scan",
     "num_rscan",
     "num_scan",
     "spec_scan",
+    "step_grid_rscan",
+    "step_grid_scan",
     "step_rscan",
     "step_scan",
 ]
