5481910a006a15687eb39e10f056c363848af210
Peter Holloway <peter.holloway@diamond.ac.uk>
2026-01-07 15:00:49 +0000

    Convert adsim module to use device manager

--- src/dodal/beamlines/adsim.py
+++ src/dodal/beamlines/adsim.py
@@ -1,11 +1,8 @@
 from ophyd_async.epics.adsimdetector import SimDetector
 
-from dodal.common.beamlines.beamline_utils import (
-    device_factory,
-    get_path_provider,
-)
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.beamlines.device_helpers import DET_SUFFIX, HDF5_SUFFIX
+from dodal.device_manager import DeviceManager
 from dodal.devices.motors import XThetaStage
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix
@@ -14,6 +11,8 @@ BL = "adsim"
 PREFIX = BeamlinePrefix("t01")
 set_log_beamline(BL)
 set_utils_beamline(BL)
+
+devices = DeviceManager()
 
 
 """
@@ -38,44 +37,47 @@ How to use the devices in a plan:
 In an ipython terminal run:
 
 ```python
-from pathlib import Path
-
 from bluesky.run_engine import RunEngine
-from ophyd_async.core import StaticPathProvider, UUIDFilenameProvider
 
-from dodal.beamlines.adsim import det, stage
-from dodal.common.beamlines.beamline_utils import set_path_provider
+from dodal.beamlines.adsim import devices
 from dodal.plans import count
 
 
-set_path_provider(
-    StaticPathProvider(
-        UUIDFilenameProvider(),
-        "/tmp", # The directory for `det` to write to- may be mounted as a volume
-    )
-)
-
 run_engine = RunEngine()
-d = det(connect_immediately=True)
-s = stage(connect_immediately=True)
+
+built = devices.build_and_connect().or_raise()
+d = built["det"]
+s = built["stage"]
+
 run_engine(count([d], num=10))
 ```
-
 """
 
 
-@device_factory()
+@devices.fixture
+def path_provider():
+    from pathlib import Path
+
+    from ophyd_async.core import StaticPathProvider, UUIDFilenameProvider
+
+    return StaticPathProvider(
+        UUIDFilenameProvider(),
+        Path("/tmp"),  # The directory for `det` to write to- may be mounted as a volume
+    )
+
+
+@devices.factory()
 def stage() -> XThetaStage:
     return XThetaStage(
         f"{PREFIX.beamline_prefix}-MO-SIMC-01:", x_infix="M1", theta_infix="M2"
     )
 
 
-@device_factory()
-def det() -> SimDetector:
+@devices.factory()
+def det(path_provider) -> SimDetector:
     return SimDetector(
         f"{PREFIX.beamline_prefix}-DI-CAM-01:",
-        path_provider=get_path_provider(),
+        path_provider=path_provider,
         drv_suffix=DET_SUFFIX,
         fileio_suffix=HDF5_SUFFIX,
     )
