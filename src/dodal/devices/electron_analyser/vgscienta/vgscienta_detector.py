e0d5a176c2c2b462a8cd826a13b843abf542cd44
oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
2026-01-09 15:27:22 +0000

    Add DualFastShutter (#1795)
    * Add DualFastShutter
    
    * Add doc strings to shutters
    
    * Add tests for dual_fast_shutter
    
    * Added tests for read
    
    * Use asycio.gather in test
    
    * Improve I09 beamline device layout
    
    * Added doc string to SourceSelector and get_obj_from_selected_source
    
    * Added test_selectable_source
    
    * Added additional tests for selectable_source
    
    * minor test update
    
    * Added read only signals for shutter_device_name
    
    * Fixed tests
    
    * Fix test flakeyness
    
    * Fix controller arg
    
    * Fixed flakey test
    
    * Update doc string
    
    * Remove pyproject.toml changes as now added to ophyd-async main
    
    * Imrpove doc strings
    
    * Setup new test names
    
    * Revert remaining changes to pyproject.toml
    
    * Added dual_fast_shutter_fixture
    
    * Added code coverage
    
    * Restored pyproject.toml

--- src/dodal/devices/electron_analyser/vgscienta/vgscienta_detector.py
+++ src/dodal/devices/electron_analyser/vgscienta/vgscienta_detector.py
@@ -5,10 +5,7 @@ from dodal.devices.electron_analyser.base.base_controller import (
 )
 from dodal.devices.electron_analyser.base.base_detector import ElectronAnalyserDetector
 from dodal.devices.electron_analyser.base.base_region import TLensMode, TPsuMode
-from dodal.devices.electron_analyser.base.energy_sources import (
-    DualEnergySource,
-    EnergySource,
-)
+from dodal.devices.electron_analyser.base.energy_sources import AbstractEnergySource
 from dodal.devices.electron_analyser.vgscienta.vgscienta_driver_io import (
     VGScientaAnalyserDriverIO,
 )
@@ -17,6 +14,8 @@ from dodal.devices.electron_analyser.vgscienta.vgscienta_region import (
     VGScientaRegion,
     VGScientaSequence,
 )
+from dodal.devices.fast_shutter import FastShutter
+from dodal.devices.selectable_source import SourceSelector
 
 
 class VGScientaDetector(
@@ -33,7 +32,9 @@ class VGScientaDetector(
         lens_mode_type: type[TLensMode],
         psu_mode_type: type[TPsuMode],
         pass_energy_type: type[TPassEnergyEnum],
-        energy_source: DualEnergySource | EnergySource,
+        energy_source: AbstractEnergySource,
+        shutter: FastShutter | None = None,
+        source_selector: SourceSelector | None = None,
         name: str = "",
     ):
         # Save to class so takes part with connect()
@@ -44,7 +45,7 @@ class VGScientaDetector(
         controller = ElectronAnalyserController[
             VGScientaAnalyserDriverIO[TLensMode, TPsuMode, TPassEnergyEnum],
             VGScientaRegion[TLensMode, TPassEnergyEnum],
-        ](self.driver, energy_source, 0)
+        ](self.driver, energy_source, shutter, source_selector)
 
         sequence_class = VGScientaSequence[
             lens_mode_type, psu_mode_type, pass_energy_type
