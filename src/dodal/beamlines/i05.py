03148707777f5dad09347d7ec9faeb22b692c25a
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-01-21 13:27:51 +0000

    Move ID to shared, remove PGM dependency

--- src/dodal/beamlines/i05.py
+++ src/dodal/beamlines/i05.py
@@ -1,49 +1,17 @@
-from dodal.beamlines.i05_shared import devices as i05_devices
+from dodal.beamlines.i05_shared import devices as i05_shared_devices
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.device_manager import DeviceManager
-from dodal.devices.insertion_device import (
-    Apple2,
-    UndulatorGap,
-    UndulatorLockedPhaseAxes,
-)
-from dodal.devices.pgm import PlaneGratingMonochromator
-from dodal.devices.temperture_controller import (
-    Lakeshore336,
-)
+from dodal.devices.temperture_controller import Lakeshore336
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
 devices = DeviceManager()
-devices.include(i05_devices)
+devices.include(i05_shared_devices)
 
 BL = get_beamline_name("i05")
 PREFIX = BeamlinePrefix(BL)
 set_log_beamline(BL)
 set_utils_beamline(BL)
-
-
-@devices.factory()
-def id_gap() -> UndulatorGap:
-    return UndulatorGap(prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:")
-
-
-@devices.factory()
-def id_phase() -> UndulatorLockedPhaseAxes:
-    return UndulatorLockedPhaseAxes(
-        prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:",
-        top_outer="PL",
-        btm_inner="PU",
-    )
-
-
-@devices.factory()
-def id(
-    id_gap: UndulatorGap,
-    id_phase: UndulatorLockedPhaseAxes,
-    pgm: PlaneGratingMonochromator,
-) -> Apple2:
-    """i05 insertion device."""
-    return Apple2(id_gap=id_gap, id_phase=id_phase)
 
 
 @devices.factory()
--- src/dodal/beamlines/i05_shared.py
+++ src/dodal/beamlines/i05_shared.py
@@ -1,10 +1,20 @@
+from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.device_manager import DeviceManager
 from dodal.devices.i05.enums import Grating
+from dodal.devices.insertion_device import (
+    Apple2,
+    UndulatorGap,
+    UndulatorLockedPhaseAxes,
+)
 from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.devices.synchrotron import Synchrotron
-from dodal.utils import BeamlinePrefix
+from dodal.log import set_beamline as set_log_beamline
+from dodal.utils import BeamlinePrefix, get_beamline_name
 
-PREFIX = BeamlinePrefix("i05", "I")
+BL = get_beamline_name("i05-shared")
+PREFIX = BeamlinePrefix("i05-shared", "I")
+set_log_beamline(BL)
+set_utils_beamline(BL)
 
 devices = DeviceManager()
 
@@ -20,3 +30,26 @@ def pgm() -> PlaneGratingMonochromator:
         prefix=f"{PREFIX.beamline_prefix}-OP-PGM-01:",
         grating=Grating,
     )
+
+
+@devices.factory()
+def id_gap() -> UndulatorGap:
+    return UndulatorGap(prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:")
+
+
+@devices.factory()
+def id_phase() -> UndulatorLockedPhaseAxes:
+    return UndulatorLockedPhaseAxes(
+        prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:",
+        top_outer="PL",
+        btm_inner="PU",
+    )
+
+
+@devices.factory()
+def id(
+    id_gap: UndulatorGap,
+    id_phase: UndulatorLockedPhaseAxes,
+) -> Apple2[UndulatorLockedPhaseAxes]:
+    """i05 insertion device."""
+    return Apple2[UndulatorLockedPhaseAxes](id_gap=id_gap, id_phase=id_phase)
