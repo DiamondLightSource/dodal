f13703a271b3f694f38fc3ab4d9aa20a574aba7d
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-01-22 13:14:34 +0000

    change i10

--- src/dodal/beamlines/i10.py
+++ src/dodal/beamlines/i10.py
@@ -6,8 +6,8 @@ note:
     idd == id1,    idu == id2.
 """
 
-from dodal.common.beamlines.beamline_utils import device_factory
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
+from dodal.device_manager import DeviceManager
 from dodal.devices.current_amplifiers import CurrentAmpDet
 from dodal.devices.i10 import (
     I10Diagnostic,
@@ -35,11 +35,11 @@ BL = get_beamline_name("i10")
 set_log_beamline(BL)
 set_utils_beamline(BL)
 PREFIX = BeamlinePrefix(BL)
-
+devices = DeviceManager()
 """Mirrors"""
 
 
-@device_factory()
+@devices.factory()
 def focusing_mirror() -> PiezoMirror:
     return PiezoMirror(prefix=f"{PREFIX.beamline_prefix}-OP-FOCS-01:")
 
@@ -47,12 +47,12 @@ def focusing_mirror() -> PiezoMirror:
 """Optic slits"""
 
 
-@device_factory()
+@devices.factory()
 def slits() -> I10Slits:
     return I10Slits(prefix=f"{PREFIX.beamline_prefix}-AL-SLITS-")
 
 
-@device_factory()
+@devices.factory()
 def slits_current() -> I10SlitsDrainCurrent:
     return I10SlitsDrainCurrent(prefix=f"{PREFIX.beamline_prefix}-")
 
@@ -60,14 +60,14 @@ def slits_current() -> I10SlitsDrainCurrent:
 """Diagnostics"""
 
 
-@device_factory()
+@devices.factory()
 def diagnostics() -> I10Diagnostic:
     return I10Diagnostic(
         prefix=f"{PREFIX.beamline_prefix}-DI-",
     )
 
 
-@device_factory()
+@devices.factory()
 def d5a_det() -> I10Diagnostic5ADet:
     return I10Diagnostic5ADet(prefix=f"{PREFIX.beamline_prefix}-DI-")
 
@@ -75,100 +75,112 @@ def d5a_det() -> I10Diagnostic5ADet:
 """Rasor devices"""
 
 
-@device_factory()
+@devices.factory()
 def pin_hole() -> XYStage:
     return XYStage(prefix="ME01D-EA-PINH-01:")
 
 
-@device_factory()
+@devices.factory()
 def det_slits() -> DetSlits:
     return DetSlits(prefix="ME01D-MO-APTR-0")
 
 
-@device_factory()
+@devices.factory()
 def diffractometer() -> Diffractometer:
     return Diffractometer(prefix="ME01D-MO-DIFF-01:")
 
 
-@device_factory()
+@devices.factory()
 def pa_stage() -> PaStage:
     return PaStage(prefix="ME01D-MO-POLAN-01:")
 
 
-@device_factory()
+@devices.factory()
 def sample_stage() -> XYZStage:
     return XYZStage(prefix="ME01D-MO-CRYO-01:")
 
 
-@device_factory()
+@devices.factory()
 def rasor_temperature_controller() -> Lakeshore340:
     return Lakeshore340(
         prefix="ME01D-EA-TCTRL-01:",
     )
 
 
-@device_factory()
+@devices.factory()
 def rasor_femto() -> RasorFemto:
     return RasorFemto(
         prefix="ME01D-EA-IAMP",
     )
 
 
-@device_factory()
+@devices.factory()
 def rasor_det_scalers() -> RasorScalerCard1:
     return RasorScalerCard1(prefix="ME01D-EA-SCLR-01:SCALER1")
 
 
-@device_factory()
+@devices.factory()
 def rasor_sr570() -> RasorSR570:
     return RasorSR570(
         prefix="ME01D-EA-IAMP",
     )
 
 
-@device_factory()
-def rasor_sr570_pa_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_sr570_pa_scaler_det(
+    rasor_sr570: RasorSR570, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_sr570().ca1,
-        counter=rasor_det_scalers().det,
+        current_amp=rasor_sr570.ca1,
+        counter=rasor_det_scalers.det,
     )
 
 
-@device_factory()
-def rasor_femto_pa_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_femto_pa_scaler_det(
+    rasor_femto: RasorFemto, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_femto().ca1,
-        counter=rasor_det_scalers().det,
+        current_amp=rasor_femto.ca1,
+        counter=rasor_det_scalers.det,
     )
 
 
-@device_factory()
-def rasor_sr570_fluo_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_sr570_fluo_scaler_det(
+    rasor_sr570: RasorSR570, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_sr570().ca2,
-        counter=rasor_det_scalers().fluo,
+        current_amp=rasor_sr570.ca2,
+        counter=rasor_det_scalers.fluo,
     )
 
 
-@device_factory()
-def rasor_femto_fluo_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_femto_fluo_scaler_det(
+    rasor_femto: RasorFemto, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_femto().ca2,
-        counter=rasor_det_scalers().fluo,
+        current_amp=rasor_femto.ca2,
+        counter=rasor_det_scalers.fluo,
     )
 
 
-@device_factory()
-def rasor_sr570_drain_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_sr570_drain_scaler_det(
+    rasor_sr570: RasorSR570, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_sr570().ca3,
-        counter=rasor_det_scalers().drain,
+        current_amp=rasor_sr570.ca3,
+        counter=rasor_det_scalers.drain,
     )
 
 
-@device_factory()
-def rasor_femto_drain_scaler_det() -> CurrentAmpDet:
+@devices.factory()
+def rasor_femto_drain_scaler_det(
+    rasor_femto: RasorFemto, rasor_det_scalers: RasorScalerCard1
+) -> CurrentAmpDet:
     return CurrentAmpDet(
-        current_amp=rasor_femto().ca3,
-        counter=rasor_det_scalers().drain,
+        current_amp=rasor_femto.ca3,
+        counter=rasor_det_scalers.drain,
     )
