8113c8da11138156272b2a3b899860ce37ceae4a
eir17846 <victor.rogalev@diamond.ac.uk>
2026-01-23 15:57:01 +0000

    remove dummy class and use existing apple2 controller

--- src/dodal/beamlines/k07.py
+++ src/dodal/beamlines/k07.py
@@ -3,35 +3,33 @@ from pathlib import Path
 from daq_config_server.client import ConfigServer
 from ophyd_async.core import StrictEnum
 
-from dodal.common.beamlines.beamline_utils import (
-    device_factory,
-)
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.apple2_undulator import (
+from dodal.device_manager import DeviceManager
+from dodal.devices.insertion_device import (
     Apple2,
+    Apple2EnforceLHMoveController,
+    ConfigServerEnergyMotorLookup,
     InsertionDeviceEnergy,
     InsertionDevicePolarisation,
+    LookupTableColumnConfig,
     UndulatorGap,
     UndulatorPhaseAxes,
 )
-from dodal.devices.k07 import K07Apple2Controller
-from dodal.devices.k07.insertion_device import (
-    K07_GAP_POLY_DEG_COLUMNS,
-    K07_PHASE_POLY_DEG_COLUMNS,
-)
 from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.devices.synchrotron import Synchrotron
-from dodal.devices.util.lookup_tables_apple2 import (
-    ConfigServerEnergyMotorLookup,
-    LookupTableConfig,
-)
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
+devices = DeviceManager()
+
+
 K07_CONF_CLIENT = ConfigServer(url="https://daq-config.diamond.ac.uk")
+
 LOOK_UPTABLE_DIR = "/dls_sw/k07/software/gda/workspace_git/gda-diamond.git/configurations/k07/lookupTables/"
 GAP_LOOKUP_FILE_NAME = "JIDEnergy2GapCalibrations.csv"
 PHASE_LOOKUP_FILE_NAME = "JIDEnergy2PhaseCalibrations.csv"
+K07_GRATING_COLUMNS = "Grating"
+K07_PHASE_POLY_DEG_COLUMNS = ["0th-order"]
 
 BL = get_beamline_name("k07")
 PREFIX = BeamlinePrefix(BL)
@@ -39,7 +37,7 @@ set_log_beamline(BL)
 set_utils_beamline(BL)
 
 
-@device_factory()
+@devices.factory()
 def synchrotron() -> Synchrotron:
     return Synchrotron()
 
@@ -49,18 +47,25 @@ class Grating(StrictEnum):
     NO_GRATING = "No Grating"
 
 
-# Insertion device objects
+# Grating does not exist yet - this class is a placeholder for when it does
+@devices.factory(skip=True)
+def pgm() -> PlaneGratingMonochromator:
+    return PlaneGratingMonochromator(
+        prefix=f"{PREFIX.beamline_prefix}-OP-PGM-01:",
+        grating=Grating,
+    )
 
 
-# Insertion device gap and phase do not exist yet - these classes are placeholders for when they do
-@device_factory(skip=True)
+# Insertion device does not exist yet - these classes are placeholders at the moment.
+@devices.factory(skip=True)
 def id_gap() -> UndulatorGap:
     return UndulatorGap(
         prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:",
     )
 
 
-@device_factory(skip=True)
+# Insertion device does not exist yet - these classes are placeholders at the moment.
+@devices.factory(skip=True)
 def id_phase() -> UndulatorPhaseAxes:
     return UndulatorPhaseAxes(
         prefix=f"{PREFIX.insertion_prefix}-MO-SERVC-01:",
@@ -71,48 +76,53 @@ def id_phase() -> UndulatorPhaseAxes:
     )
 
 
-# Insertion device raw does not exist yet - this class is a placeholder for when it does
-@device_factory(skip=True)
-def id() -> Apple2:
-    return Apple2(
-        id_gap=id_gap(),
-        id_phase=id_phase(),
+# Insertion device does not exist yet - these classes are placeholders at the moment.
+@devices.factory(skip=True)
+def id(
+    id_gap: UndulatorGap, id_phase: UndulatorPhaseAxes
+) -> Apple2[UndulatorPhaseAxes]:
+    """K07 insertion device."""
+    return Apple2[UndulatorPhaseAxes](
+        id_gap=id_gap,
+        id_phase=id_phase,
     )
 
 
-# Insertion device controller does not exist yet - this class is a placeholder for when it does
-@device_factory(skip=True)
-def id_controller() -> K07Apple2Controller:
-    return K07Apple2Controller(
-        apple2=id(),
+# Insertion device does not exist yet - temporary use Apple2EnforceLHMoveController
+@devices.factory(skip=True)
+def id_controller(
+    id: Apple2[UndulatorPhaseAxes],
+) -> Apple2EnforceLHMoveController[UndulatorPhaseAxes]:
+    """i21 insertion device controller."""
+    return Apple2EnforceLHMoveController[UndulatorPhaseAxes](
+        apple2=id,
         gap_energy_motor_lut=ConfigServerEnergyMotorLookup(
-            lut_config=LookupTableConfig(poly_deg=K07_GAP_POLY_DEG_COLUMNS),
+            lut_config=LookupTableColumnConfig(grating=K07_GRATING_COLUMNS),
             config_client=K07_CONF_CLIENT,
             path=Path(LOOK_UPTABLE_DIR, GAP_LOOKUP_FILE_NAME),
         ),
         phase_energy_motor_lut=ConfigServerEnergyMotorLookup(
-            lut_config=LookupTableConfig(poly_deg=K07_PHASE_POLY_DEG_COLUMNS),
+            lut_config=LookupTableColumnConfig(
+                grating=K07_GRATING_COLUMNS, poly_deg=K07_PHASE_POLY_DEG_COLUMNS
+            ),
             config_client=K07_CONF_CLIENT,
-            path=Path(LOOK_UPTABLE_DIR, PHASE_LOOKUP_FILE_NAME),
+            path=Path(LOOK_UPTABLE_DIR, GAP_LOOKUP_FILE_NAME),
         ),
+        units="eV",
     )
 
 
-# Insertion device energy does not exist yet - this class is a placeholder for when it does
-@device_factory(skip=True)
-def id_energy() -> InsertionDeviceEnergy:
-    return InsertionDeviceEnergy(id_controller=id_controller())
+# Insertion device does not exist yet - these classes are placeholders at the moment.
+@devices.factory(skip=True)
+def id_energy(
+    id_controller: Apple2EnforceLHMoveController[UndulatorPhaseAxes],
+) -> InsertionDeviceEnergy:
+    return InsertionDeviceEnergy(id_controller)
 
 
-@device_factory(skip=True)
-def id_polarisation() -> InsertionDevicePolarisation:
-    return InsertionDevicePolarisation(id_controller=id_controller())
-
-
-# Grating does not exist yet - this class is a placeholder for when it does
-@device_factory(skip=True)
-def pgm() -> PlaneGratingMonochromator:
-    return PlaneGratingMonochromator(
-        prefix=f"{PREFIX.beamline_prefix}-OP-PGM-01:",
-        grating=Grating,
-    )
+# Insertion device does not exist yet - these classes are placeholders at the moment.
+@devices.factory(skip=True)
+def id_polarisation(
+    id_controller: Apple2EnforceLHMoveController[UndulatorPhaseAxes],
+) -> InsertionDevicePolarisation:
+    return InsertionDevicePolarisation(id_controller)
--- src/dodal/devices/k07/__init__.py
+++
@@ -1,3 +0,0 @@
-from .insertion_device import K07Apple2Controller
-
-__all__ = ["K07Apple2Controller"]
