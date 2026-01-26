2dbd6ae5f7a2c2115e91bb02fddfb7a7b0e21568
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-01-26 12:18:59 +0000

    fix enum

--- src/dodal/beamlines/i06_shared.py
+++ src/dodal/beamlines/i06_shared.py
@@ -1,10 +1,12 @@
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.device_manager import DeviceManager
+from dodal.devices.i06_shared import I06Grating
 from dodal.devices.insertion_device import (
     Apple2,
     UndulatorGap,
     UndulatorLockedPhaseAxes,
 )
+from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
@@ -59,3 +61,13 @@ def idu_phase() -> UndulatorLockedPhaseAxes:
 def idu(idu_gap: UndulatorGap, idu_phase: UndulatorLockedPhaseAxes) -> Apple2:
     """i06 upstream insertion device."""
     return Apple2(id_gap=idu_gap, id_phase=idu_phase)
+
+
+@devices.factory()
+def pgm() -> PlaneGratingMonochromator:
+    "I10 Plane Grating Monochromator, it can change energy via pgm.energy.set(<energy>)"
+    return PlaneGratingMonochromator(
+        prefix=f"{PREFIX.beamline_prefix}-OP-PGM-01:",
+        grating=I06Grating,
+        grating_pv="NLINES2",
+    )
