86fa9e60b4aa2b0a44c5611509b6f1da8377e4d5
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-01-29 13:38:53 +0000

    add test for em stage

---
+++ tests/devices/i10_1/electromagnet/test_stages.py
@@ -0,0 +1,16 @@
+from ophyd_async.core import init_devices
+from ophyd_async.testing import assert_reading, partial_reading
+
+from dodal.devices.i10_1 import ElectromagnetStage
+
+
+async def test_i10j_scalercard_read():
+    with init_devices(mock=True):
+        mock_em_stage = ElectromagnetStage(prefix="BLXX-EA-EMAG-01:")
+    await assert_reading(
+        mock_em_stage,
+        {
+            "mock_em_stage-y": partial_reading(0.0),
+            "mock_em_stage-pitch": partial_reading(0.0),
+        },
+    )
