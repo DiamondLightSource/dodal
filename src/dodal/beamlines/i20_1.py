9999fd8e7c93e5615f678e1cde0ab3f6d2f79020
Subu Ghimire <254926418+subughimire420@users.noreply.github.com>
2026-01-30 10:21:00 +0000

    K->P

--- src/dodal/beamlines/i20_1.py
+++ src/dodal/beamlines/i20_1.py
@@ -17,7 +17,7 @@ from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
 BL = get_beamline_name("i20-1")
-PREFIX = BeamlinePrefix(BL, suffix="K")
+PREFIX = BeamlinePrefix(BL, suffix="P")
 set_log_beamline(BL)
 set_utils_beamline(BL)
 
