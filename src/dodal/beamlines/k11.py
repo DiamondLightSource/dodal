923fd5aa0d8806239c8e4b07a4f6049bef159970
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-02-06 11:47:59 +0000

    Update k11 to new device factory

--- src/dodal/beamlines/k11.py
+++ src/dodal/beamlines/k11.py
@@ -1,13 +1,12 @@
+from functools import cache
 from pathlib import Path
 
+from ophyd_async.core import PathProvider
 from ophyd_async.epics.motor import Motor
 
-from dodal.common.beamlines.beamline_utils import (
-    device_factory,
-    set_path_provider,
-)
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.common.visit import RemoteDirectoryServiceClient, StaticVisitPathProvider
+from dodal.device_manager import DeviceManager
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -16,20 +15,24 @@ PREFIX = BeamlinePrefix(beamline)
 set_log_beamline(beamline)
 set_utils_beamline(beamline)
 
-set_path_provider(
-    StaticVisitPathProvider(
+devices = DeviceManager()
+
+
+@devices.fixture
+@cache
+def path_provider() -> PathProvider:
+    return StaticVisitPathProvider(
         beamline,
         Path("/dls/k11/data/2025/cm40627-3"),
         client=RemoteDirectoryServiceClient("https://k11-control:8088/api"),
     )
-)
 
 
-@device_factory()
+@devices.factory()
 def kb_x() -> Motor:
     return Motor(f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:X")
 
 
-@device_factory()
+@devices.factory()
 def kb_y() -> Motor:
     return Motor(f"{PREFIX.beamline_prefix}-OP-KBM-01:CS:Y")
