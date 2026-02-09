a7cb62aae1d987e68aedf9b2e36e8f509546172f
Villtord <Villtord@users.noreply.github.com>
2026-02-09 12:19:06 +0000

    deploy: fa22fd0ab55aa63445da1d6462ec02c82cd64611

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.0.1.dev11+gebba33bcc',
+    VERSION: '2.0.1.dev12+gfa22fd0ab',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
