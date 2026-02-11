aa40631e0ac8508761dd09759f4654f2853a4f86
oliwenmandiamond <136330507+oliwenmandiamond@users.noreply.github.com>
2026-02-11 14:14:17 +0000

    Add B07 and B07-1 sample stage (#1913)
    * Add B07 sample stage
    
    * Add doc strings
    
    * Add b07_motors_test
    
    * Remove mock=True
    
    * Reduce number of parametrize
    
    * Corrected test name
    
    * Applied feedback

--- src/dodal/beamlines/b07.py
+++ src/dodal/beamlines/b07.py
@@ -1,9 +1,15 @@
 from dodal.beamlines.b07_shared import devices as b07_shared_devices
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.device_manager import DeviceManager
-from dodal.devices.beamlines.b07 import Grating, LensMode, PsuMode
+from dodal.devices.beamlines.b07 import (
+    B07SampleManipulator52B,
+    Grating,
+    LensMode,
+    PsuMode,
+)
 from dodal.devices.electron_analyser.base import EnergySource
 from dodal.devices.electron_analyser.specs import SpecsDetector
+from dodal.devices.motors import XYZPolarStage
 from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
@@ -39,4 +45,17 @@ def analyser(energy_source: EnergySource) -> SpecsDetector[LensMode, PsuMode]:
         lens_mode_type=LensMode,
         psu_mode_type=PsuMode,
         energy_source=energy_source,
+    )
+
+
+@devices.factory()
+def sm52b() -> B07SampleManipulator52B:
+    return B07SampleManipulator52B(prefix=f"{B_PREFIX.beamline_prefix}-EA-SM-52:")
+
+
+@devices.factory()
+def sm21b() -> XYZPolarStage:
+    """Sample manipulator. NOTE: The polar attribute is equivalent to GDA roty."""
+    return XYZPolarStage(
+        prefix=f"{B_PREFIX.beamline_prefix}-EA-SM-21:", polar_infix="ROTY"
     )
