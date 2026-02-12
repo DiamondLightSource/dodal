0e2b58b71f6553bce647313b74b3146c8a830af2
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-12 12:45:03 +0000

    rewrite attribute docs. add more PVs from GDA beamlines code

--- src/dodal/devices/synchrotron.py
+++ src/dodal/devices/synchrotron.py
@@ -18,10 +18,13 @@ class Prefix(str, Enum):
 class Suffix(str, Enum):
     SIGNAL = "SIGNAL"
     MODE = "MODE"
-    USER_COUNTDOWN = "USERCOUNTDN"
-    BEAM_ENERGY = "BEAMENERGY"
+    USERCOUNTDOWN = "USERCOUNTDN"
+    BEAMENERGY = "BEAMENERGY"
     COUNTDOWN = "COUNTDOWN"
-    END_COUNTDOWN = "ENDCOUNTDN"
+    STACOUNTDN = "STACOUNTDN"
+    ENDCOUNTDOWN = "ENDCOUNTDN"
+    TOPUPSTATE = "TOPUPSTATE"
+    FILLPERIOD = "FILLPERIOD"
 
 
 class SynchrotronMode(StrictEnum):
@@ -35,9 +38,17 @@ class SynchrotronMode(StrictEnum):
     UNKNOWN = "Unknown"
 
 
+class TopUpState(StrictEnum):
+    OFF = "Off"
+    RUNNING = "Running"
+    STOPPING = "Stopping"
+    FAILED = "Failed"
+    NO_PROCESS = "No process"
+
+
 class Synchrotron(StandardReadable):
-    """A StandardReadable device that represents a synchrotron facility, providing access to
-    various synchrotron parameters and operational status.
+    """A StandardReadable device that represents a synchrotron facility, providing
+    access to various synchrotron parameters and operational status.
 
     Args:
         signal_prefix (str, optional): Beamline part of PV. Defaults to Prefix.SIGNAL.
@@ -46,14 +57,34 @@ class Synchrotron(StandardReadable):
         name (str, optional): Name of the device. Defaults to an empty string.
 
     Attributes:
-        current (SignalR[float]): Read-only signal for the synchrotron beam current.
-        energy (SignalR[float]): Read-only signal for the beam energy.
-        probe (SignalR[str]): Read-only signal for the probe type. Defaults to ``"x-ray"``.
-        type (SignalR[str]): Read-only signal for the synchrotron type. Defaults to ``"Synchrotron X-ray Source"``.
-        synchrotron_mode (SignalR[SynchrotronMode]): Read-only signal for the current synchrotron operating mode.
-        machine_user_countdown (SignalR[float]): Read-only signal for the machine user countdown timer.
-        top_up_start_countdown (SignalR[float]): Read-only signal for the top-up start countdown timer.
-        top_up_end_countdown (SignalR[float]): Read-only signal for the top-up end countdown timer.
+        current (SignalR[float]): Read-only signal for the synchrotron ring current.
+            Standard optional field in NXsource component of nexus file.
+        energy (SignalR[float]): Read-only signal for the synchrotron ring energy.
+            Standard optional field in NXsource component of nexus file.
+        probe (SignalR[str]): Read-only signal for the probe type. Defaults to
+            ``"x-ray"``. Standard optional field in NXsource component of nexus file.
+        type (SignalR[str]): Read-only signal for the synchrotron type. Defaults to
+            ``"Synchrotron X-ray Source"``. Standard optional field in NXsource
+            component of nexus file.
+        synchrotron_mode (SignalR[SynchrotronMode]): current state of the synchrotron
+            ring. Standard optional field ``"mode"`` in NXsource component of nexus file.
+        machine_user_countdown (SignalR[float]): If current mode in user, counts down
+            the number of seconds until the end of the current period.
+        top_up_start_countdown (SignalR[float]): counter that runs to zero at the start
+            of TopUp and remains there until the fill is complete when it resets to time
+            before next TopUp fill. Used in data acquisition to avoid beam intensity
+            change during topup.
+        top_up_countdown (SignalR[float]): counter that runs to zero at the start
+            of TopUp fill and is reset immediately to the time to next TopUp fill once
+            TopUp starts.
+        top_up_end_countdown (SignalR[float]): counter that runs to zero at the end of
+            TopUp fill and resets immediately to an estimate of the time before the end
+            of the next TopUp fill. Used in data acquisition to avoid beam intensity
+            change during topup.
+        fill_period (signalR[float]): time between TopUp (in minutes).Used in data
+            acquisition to monitor synchrotron state.
+        top_up_state (signalR[TopUpState]): state of the TopUp process. Used in data
+            acquisition to confirm state.
     """
 
     def __init__(
@@ -65,7 +96,7 @@ class Synchrotron(StandardReadable):
     ):
         with self.add_children_as_readables():
             self.current = epics_signal_r(float, signal_prefix + Suffix.SIGNAL)
-            self.energy = epics_signal_r(float, status_prefix + Suffix.BEAM_ENERGY)
+            self.energy = epics_signal_r(float, status_prefix + Suffix.BEAMENERGY)
 
         with self.add_children_as_readables(StandardReadableFormat.CONFIG_SIGNAL):
             self.probe, _ = soft_signal_r_and_setter(str, initial_value="x-ray")
@@ -76,13 +107,15 @@ class Synchrotron(StandardReadable):
                 SynchrotronMode, status_prefix + Suffix.MODE
             )
         self.machine_user_countdown = epics_signal_r(
-            float, status_prefix + Suffix.USER_COUNTDOWN
+            float, status_prefix + Suffix.USERCOUNTDOWN
         )
         self.top_up_start_countdown = epics_signal_r(
             float, topup_prefix + Suffix.COUNTDOWN
         )
+        self.top_up_countdown = epics_signal_r(float, topup_prefix + Suffix.STACOUNTDN)
         self.top_up_end_countdown = epics_signal_r(
-            float, topup_prefix + Suffix.END_COUNTDOWN
+            float, topup_prefix + Suffix.ENDCOUNTDOWN
         )
-
+        self.fill_period = epics_signal_r(float, topup_prefix + Suffix.FILLPERIOD)
+        self.top_up_state = epics_signal_r(TopUpState, topup_prefix + Suffix.TOPUPSTATE)
         super().__init__(name=name)
