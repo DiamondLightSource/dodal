9ed4817538da534adb61a10d3008705353b113ad
Tamoor Shahid <tamoor.shahid@diamond.ac.uk>
2026-02-16 15:09:00 +0000

    Added synchrotron device to i03 supervisor

--- src/dodal/beamlines/i03_supervisor.py
+++ src/dodal/beamlines/i03_supervisor.py
@@ -1,6 +1,7 @@
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
 from dodal.device_manager import DeviceManager
 from dodal.devices.baton import Baton
+from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import BeamlinePrefix, get_beamline_name
 
@@ -17,3 +18,7 @@ devices = DeviceManager()
 @devices.factory()
 def baton() -> Baton:
     return Baton(f"{PREFIX.beamline_prefix}-CS-BATON-01:")
+
+@devices.factory()
+def synchrotron() -> Synchrotron:
+    return Synchrotron()
