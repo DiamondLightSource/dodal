7ab213b79592c5f02df00dd32a389fbc066d7d41
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-18 11:35:01 +0000

    fix lint

--- src/dodal/devices/beamlines/i05/__init__.py
+++ src/dodal/devices/beamlines/i05/__init__.py
@@ -1,12 +1,12 @@
-from .compound_motors import PolynomCompoundMotors
 from dodal.devices.beamlines.i05.enums import (
     Grating,
     M3MJ6Mirror,
     M4M5Mirror,
     Mj7j8Mirror,
 )
-from .i05_motors import I05Goniometer
 
+from .compound_motors import PolynomCompoundMotors
+from .i05_motors import I05Goniometer
 
 __all__ = [
     "Grating",
