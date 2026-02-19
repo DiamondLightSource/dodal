15454837417849017f9ad1dc52d3f89b9e9e16ed
oliwenmandiamond <oliwenmandiamond@users.noreply.github.com>
2026-02-19 10:53:56 +0000

    deploy: 004332fcddad34807a920297e912434233fc8998

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.0.1.dev20+gdffe1f8af',
+    VERSION: '2.0.1.dev21+g004332fcd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
