c81aab5b8836c91a55b86d2fbb425efa71167d28
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-24 17:45:05 +0000

    add tests

--- src/dodal/devices/experimental_shutter.py
+++ src/dodal/devices/experimental_shutter.py
@@ -10,7 +10,6 @@ from ophyd_async.epics.core import epics_signal_r, epics_signal_w
 
 from dodal.devices.hutch_shutter import (
     ShutterDemand,
-    ShutterNotSafeToOperateError,
     ShutterState,
 )
 
@@ -34,8 +33,8 @@ class ExperimentalShutter(StandardReadable, Movable[ShutterDemand]):
     def __init__(
         self, bl_prefix: str, shtr_infix: str = PSS_SHUTTER_1_SUFFIX, name: str = ""
     ) -> None:
+        self.control = epics_signal_w(ShutterDemand, f"{bl_prefix}{shtr_infix}:CON")
         with self.add_children_as_readables():
-            self.control = epics_signal_w(ShutterDemand, f"{bl_prefix}{shtr_infix}:CON")
             self.status = epics_signal_r(ShutterState, f"{bl_prefix}{shtr_infix}:STA")
             self.interlock = epics_signal_r(
                 InterlockState, f"{bl_prefix}{shtr_infix}:ILKSTA"
@@ -45,11 +44,6 @@ class ExperimentalShutter(StandardReadable, Movable[ShutterDemand]):
     @AsyncStatus.wrap
     async def set(self, value: ShutterDemand):
         if value == ShutterDemand.OPEN:
-            interlock_state = await self.interlock.get_value()
-            if not interlock_state:
-                raise ShutterNotSafeToOperateError(
-                    "Interlock state unknown - can not open shutter."
-                )
             await self.control.set(ShutterDemand.RESET, wait=True)
             await self.control.set(ShutterDemand.OPEN, wait=True)
             return await wait_for_value(
--- tests/devices/test_experimental_shutter.py
+++ tests/devices/test_experimental_shutter.py
@@ -1,15 +1,29 @@
+from unittest.mock import call
+
+import bluesky.plan_stubs as bps
 import pytest
-from ophyd_async.core import init_devices
+from bluesky.run_engine import RunEngine
+from ophyd_async.core import (
+    get_mock_put,
+    init_devices,
+    set_mock_value,
+)
 from ophyd_async.testing import assert_reading, partial_reading
 
 from dodal.devices.experimental_shutter import ExperimentalShutter, InterlockState
-from dodal.devices.hutch_shutter import ShutterState
+from dodal.devices.hutch_shutter import ShutterDemand, ShutterState
 
 
 @pytest.fixture
 async def exp_shutter() -> ExperimentalShutter:
     async with init_devices(mock=True):
         exp_shutter = ExperimentalShutter("TEST:")
+
+    def completed():
+        pass
+
+    mock_put = get_mock_put(exp_shutter.control)
+    mock_put.return_value = completed()
     return exp_shutter
 
 
@@ -21,3 +35,28 @@ async def test_shutter_readable(exp_shutter: ExperimentalShutter):
             f"{exp_shutter.name}-status": partial_reading(ShutterState.FAULT),
         },
     )
+
+
+@pytest.mark.parametrize(
+    "demand, expected_calls, expected_state",
+    [
+        (
+            ShutterDemand.OPEN,
+            [ShutterDemand.RESET, ShutterDemand.OPEN],
+            ShutterState.OPEN,
+        ),
+        (ShutterDemand.CLOSE, [ShutterDemand.CLOSE], ShutterState.CLOSED),
+        (ShutterDemand.RESET, [ShutterDemand.RESET], ShutterState.CLOSED),
+    ],
+)
+async def test_shutter_set_open_call_list(
+    exp_shutter: ExperimentalShutter,
+    run_engine: RunEngine,
+    demand: ShutterDemand,
+    expected_calls: list[ShutterDemand],
+    expected_state: ShutterState,
+):
+    set_mock_value(exp_shutter.status, expected_state)
+    run_engine(bps.abs_set(exp_shutter, demand, wait=True))
+    mock_put = get_mock_put(exp_shutter.control)
+    mock_put.assert_has_calls([call(i, wait=True) for i in expected_calls])
