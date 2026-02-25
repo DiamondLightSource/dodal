2414b7a5db82d8558b2a8bba6d1eb44218d26616
eir17846 <victor.rogalev@diamond.ac.uk>
2026-02-25 16:59:06 +0000

    b07 enable back analyser

--- src/dodal/beamlines/b07_1.py
+++ src/dodal/beamlines/b07_1.py
@@ -49,7 +49,7 @@ def energy_source(pgm: PlaneGratingMonochromator) -> EnergySource:
 
 # CAM:IMAGE will fail to connect outside the beamline network,
 # see https://github.com/DiamondLightSource/dodal/issues/1852
-@devices.factory(skip=True)
+@devices.factory()
 def analyser(energy_source: EnergySource) -> SpecsDetector[LensMode, PsuMode]:
     return SpecsDetector[LensMode, PsuMode](
         prefix=f"{C_PREFIX.beamline_prefix}-EA-DET-01:CAM:",
