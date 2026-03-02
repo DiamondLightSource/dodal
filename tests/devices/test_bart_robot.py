43b2c665a43af2115f51d7adb87ddb9aa7d34ad1
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-03-02 17:18:04 +0000

    Initial fix for not waiting for puck/pin value

--- tests/devices/test_bart_robot.py
+++ tests/devices/test_bart_robot.py
@@ -1,12 +1,12 @@
 import asyncio
 import traceback
 from asyncio import Event, create_task
+from collections.abc import Callable
 from functools import partial
 from unittest.mock import ANY, AsyncMock, MagicMock, call, patch
 
 import pytest
 from ophyd_async.core import (
-    AsyncStatus,
     callback_on_mock_put,
     get_mock,
     get_mock_put,
@@ -52,11 +52,13 @@ async def robot_for_unload():
     return device, trigger_complete, drying_complete
 
 
-async def _get_bart_robot() -> BartRobot:
+@pytest.fixture
+async def bart_robot() -> BartRobot:
     device = BartRobot("robot", "-MO-ROBOT-01:")
-    device.LOAD_TIMEOUT = 1  # type: ignore
-    device.NOT_BUSY_TIMEOUT = 1  # type: ignore
+    device.NOT_BUSY_TIMEOUT = 0.3  # type: ignore
+    device.LOAD_TIMEOUT = 0.3  # type: ignore
     await device.connect(mock=True)
+    set_mock_value(device.program_running, False)
     # set_mock_value(device.beamline_disabled, BeamlineStatus.DISABLED.value)
     return device
 
@@ -67,31 +69,23 @@ def _set_fast_robot_timeouts(robot: BartRobot):
     robot.NOT_BUSY_TIMEOUT = 0.01  # type: ignore
 
 
-async def test_bart_robot_can_be_connected_in_sim_mode():
-    device = await _get_bart_robot()
-    await device.connect(mock=True)
-
-
-async def test_given_robot_load_times_out_when_load_called_then_exception_contains_error_info():
-    device = await _get_bart_robot()
-    _set_fast_robot_timeouts(device)
-    device._load_pin_and_puck = AsyncMock(side_effect=TimeoutError)
-
-    set_mock_value(device.prog_error.code, (expected_error_code := 10))
-    set_mock_value(device.prog_error.str, (expected_error_string := "BAD"))
+async def test_given_robot_load_times_out_when_load_called_then_exception_contains_error_info(
+    robot_with_late_error: BartRobot,
+):
+    device = robot_with_late_error
 
     with pytest.raises(RobotLoadError) as e:
         await device.set(SampleLocation(0, 0))
-    assert e.value.error_code == expected_error_code
-    assert e.value.error_string == expected_error_string
-    assert str(e.value) == expected_error_string
+    assert e.value.error_code == EXPECTED_ERROR_CODE
+    assert e.value.error_string == EXPECTED_ERROR_STRING
+    assert str(e.value) == EXPECTED_ERROR_STRING
 
 
 @patch("dodal.devices.robot.LOGGER")
 async def test_given_program_running_when_load_pin_then_logs_the_program_name_and_times_out(
-    patch_logger: MagicMock,
+    patch_logger: MagicMock, bart_robot: BartRobot
 ):
-    device = await _get_bart_robot()
+    device = bart_robot
     _set_fast_robot_timeouts(device)
     program_name = "BAD_PROGRAM"
     set_mock_value(device.program_running, True)
@@ -104,11 +98,10 @@ async def test_given_program_running_when_load_pin_then_logs_the_program_name_an
 
 @patch("dodal.devices.robot.LOGGER")
 async def test_given_program_not_running_but_pin_not_unmounting_when_load_pin_then_timeout(
-    patch_logger: MagicMock,
+    patch_logger: MagicMock, bart_robot: BartRobot
 ):
-    device = await _get_bart_robot()
+    device = bart_robot
     _set_fast_robot_timeouts(device)
-    set_mock_value(device.program_running, False)
     set_mock_value(device.gonio_pin_sensor, PinMounted.PIN_MOUNTED)
     device.load = AsyncMock(side_effect=device.load)
     with pytest.raises(RobotLoadError):
@@ -121,10 +114,10 @@ async def test_given_program_not_running_but_pin_not_unmounting_when_load_pin_th
 @patch("dodal.devices.robot.LOGGER")
 async def test_given_program_not_running_and_pin_unmounting_but_new_pin_not_mounting_when_load_pin_then_timeout(
     patch_logger: MagicMock,
+    bart_robot: BartRobot,
 ):
-    device = await _get_bart_robot()
+    device = bart_robot
     _set_fast_robot_timeouts(device)
-    set_mock_value(device.program_running, False)
     set_mock_value(device.gonio_pin_sensor, PinMounted.NO_PIN_MOUNTED)
     device.load = AsyncMock(side_effect=device.load)
     with pytest.raises(RobotLoadError) as exc_info:
@@ -139,11 +132,29 @@ async def test_given_program_not_running_and_pin_unmounting_but_new_pin_not_moun
         raise
 
 
-def _set_beamline_enabled_on_log_messages(device: BartRobot, msg: str):
+def _beamline_enable(device: BartRobot):
+    set_mock_value(device.beamline_disabled, BeamlineStatus.ENABLED.value)
+
+
+def _beamline_disable(device: BartRobot):
+    set_mock_value(device.beamline_disabled, BeamlineStatus.DISABLED.value)
+
+
+def _set_pin_and_puck(sample_location: SampleLocation, device: BartRobot):
+    set_mock_value(device.current_puck, sample_location.puck)
+    set_mock_value(device.current_pin, sample_location.pin)
+
+
+def _set_beamline_enabled_on_log_messages(
+    device: BartRobot,
+    on_beamline_enable: Callable[[BartRobot], None],
+    on_beamline_disable: Callable[[BartRobot], None],
+    msg: str,
+):
     if msg == WAIT_FOR_BEAMLINE_DISABLE_MSG:
-        set_mock_value(device.beamline_disabled, BeamlineStatus.DISABLED.value)
+        on_beamline_disable(device)
     elif msg == WAIT_FOR_BEAMLINE_ENABLE_MSG:
-        set_mock_value(device.beamline_disabled, BeamlineStatus.ENABLED.value)
+        on_beamline_enable(device)
 
 
 def _error_on_unload_log_messages(device: BartRobot, msg: str):
@@ -153,34 +164,79 @@ def _error_on_unload_log_messages(device: BartRobot, msg: str):
 
 
 # Use log info messages to determine when to set the beamline enable, so we don't have to use any sleeps during testing
-async def set_with_happy_path(
-    device: BartRobot, mock_log_info: MagicMock
-) -> AsyncStatus:
-    """Mocks the logic that the robot would do on a successful load."""
-    mock_log_info.side_effect = partial(_set_beamline_enabled_on_log_messages, device)
-    set_mock_value(device.program_running, False)
-    set_mock_value(device.beamline_disabled, BeamlineStatus.ENABLED.value)
-    status = device.set(SampleLocation(15, 10))
-    return status
-
-
-async def set_with_unhappy_path(
-    device: BartRobot, mock_log_info: MagicMock
-) -> AsyncStatus:
-    """Mocks the logic that the robot would do on a successful load."""
-    mock_log_info.side_effect = partial(_error_on_unload_log_messages, device)
-    set_mock_value(device.program_running, False)
-    set_mock_value(device.beamline_disabled, BeamlineStatus.ENABLED.value)
-    status = device.set(SampleLocation(15, 10))
-    return status
-
-
-@patch("dodal.devices.robot.LOGGER.info")
-async def test_given_program_not_running_and_pin_unmounts_then_mounts_when_load_pin_then_pin_loaded(
-    mock_log_info: MagicMock,
+@pytest.fixture
+def robot_which_succeeds(
+    bart_robot: BartRobot,
 ):
-    device = await _get_bart_robot()
-    status = await set_with_happy_path(device, mock_log_info)
+    """Mocks the logic that the robot would do on a successful load."""
+    sample_location = SampleLocation(15, 10)
+
+    def _enable_beamline_and_update_pin(device: BartRobot):
+        _beamline_enable(device)
+        _set_pin_and_puck(sample_location, device)
+
+    with patch("dodal.devices.robot.LOGGER.info") as mock_log_info:
+        mock_log_info.side_effect = partial(
+            _set_beamline_enabled_on_log_messages,
+            bart_robot,
+            _enable_beamline_and_update_pin,
+            _beamline_disable,
+        )
+        # set_mock_value(bart_robot.beamline_disabled, BeamlineStatus.ENABLED.value)
+        yield bart_robot
+
+
+@pytest.fixture
+def robot_with_early_error(
+    bart_robot: BartRobot,
+):
+    """Mocks the logic that the robot would do on a successful load."""
+    with patch("dodal.devices.robot.LOGGER.info") as mock_log_info:
+        mock_log_info.side_effect = partial(_error_on_unload_log_messages, bart_robot)
+        set_mock_value(bart_robot.beamline_disabled, BeamlineStatus.ENABLED.value)
+        yield bart_robot
+
+
+@pytest.fixture
+def robot_which_never_reports_new_pin(bart_robot: BartRobot):
+    with patch("dodal.devices.robot.LOGGER.info") as mock_log_info:
+        set_mock_value(bart_robot.beamline_disabled, BeamlineStatus.ENABLED.value)
+        mock_log_info.side_effect = partial(
+            _set_beamline_enabled_on_log_messages,
+            bart_robot,
+            _beamline_enable,
+            _beamline_disable,
+        )
+        yield bart_robot
+
+
+EXPECTED_ERROR_CODE = 10
+EXPECTED_ERROR_STRING = "BAD"
+
+
+@pytest.fixture
+def robot_with_late_error(bart_robot: BartRobot):
+    _set_fast_robot_timeouts(bart_robot)
+    bart_robot._load_pin_and_puck = AsyncMock(side_effect=TimeoutError)
+
+    set_mock_value(bart_robot.prog_error.code, EXPECTED_ERROR_CODE)
+    set_mock_value(bart_robot.prog_error.str, EXPECTED_ERROR_STRING)
+    with patch("dodal.devices.robot.LOGGER.info") as mock_log_info:
+        set_mock_value(bart_robot.beamline_disabled, BeamlineStatus.ENABLED.value)
+        mock_log_info.side_effect = partial(
+            _set_beamline_enabled_on_log_messages,
+            bart_robot,
+            _beamline_enable,
+            _beamline_disable,
+        )
+        yield bart_robot
+
+
+async def test_given_program_not_running_and_pin_unmounts_then_mounts_when_load_pin_then_pin_loaded(
+    robot_which_succeeds: BartRobot,
+):
+    device = robot_which_succeeds
+    status = device.set(SampleLocation(15, 10))
     await status
     assert status.success
     assert (await device.next_puck.get_value()) == 15
@@ -188,8 +244,10 @@ async def test_given_program_not_running_and_pin_unmounts_then_mounts_when_load_
     get_mock_put(device.load).assert_called_once()
 
 
-async def test_given_waiting_for_pin_to_mount_when_no_pin_mounted_then_error_raised():
-    device = await _get_bart_robot()
+async def test_given_waiting_for_pin_to_mount_when_no_pin_mounted_then_error_raised(
+    bart_robot: BartRobot,
+):
+    device = bart_robot
     set_mock_value(device.prog_error.code, 25)
     set_mock_value(device.beamline_disabled, BeamlineStatus.DISABLED.value)
     status = device.beamline_status_or_error(BeamlineStatus.ENABLED)
@@ -197,24 +255,38 @@ async def test_given_waiting_for_pin_to_mount_when_no_pin_mounted_then_error_rai
         await status
 
 
-async def test_given_waiting_for_beamline_to_enable_when_beamline_enabled_then_no_error_raised():
-    device = await _get_bart_robot()
+async def test_given_waiting_for_beamline_to_enable_when_beamline_enabled_then_no_error_raised(
+    bart_robot: BartRobot,
+):
+    device = bart_robot
     status = create_task(device.beamline_status_or_error(BeamlineStatus.ENABLED))
     set_mock_value(device.beamline_disabled, BeamlineStatus.ENABLED.value)
     await status
 
 
+async def test_robot_load_fails_if_new_puck_and_pin_not_reported(
+    robot_which_never_reports_new_pin: BartRobot,
+):
+    robot = robot_which_never_reports_new_pin
+    with pytest.raises(RobotLoadError, match="Robot timed out"):
+        await robot.set(SampleLocation(15, 10))
+
+
 @patch("dodal.devices.robot.wait_for")
-async def test_set_waits_for_both_timeouts(mock_wait_for: AsyncMock):
-    device = await _get_bart_robot()
+async def test_set_waits_for_both_timeouts(
+    mock_wait_for: AsyncMock, bart_robot: BartRobot
+):
+    device = bart_robot
     _set_fast_robot_timeouts(device)
     device._load_pin_and_puck = MagicMock()  # type: ignore
     await device.set(SampleLocation(1, 2))
     mock_wait_for.assert_awaited_once_with(ANY, timeout=0.02)
 
 
-async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and_still_throw_if_error_not_cleared():
-    device = await _get_bart_robot()
+async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and_still_throw_if_error_not_cleared(
+    bart_robot: BartRobot,
+):
+    device = bart_robot
     _set_fast_robot_timeouts(device)
     set_mock_value(device.controller_error.code, BartRobot.LIGHT_CURTAIN_TRIPPED)
 
@@ -232,11 +304,10 @@ async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and
     )
 
 
-@patch("dodal.devices.robot.LOGGER.info")
 async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and_continue_if_error_cleared(
-    mock_log_info: MagicMock,
+    robot_which_succeeds: BartRobot,
 ):
-    device = await _get_bart_robot()
+    device = robot_which_succeeds
     set_mock_value(device.controller_error.code, BartRobot.LIGHT_CURTAIN_TRIPPED)
 
     callback_on_mock_put(
@@ -244,7 +315,7 @@ async def test_moving_the_robot_will_reset_error_if_light_curtain_is_tripped_and
         lambda *_, **__: set_mock_value(device.controller_error.code, 0),
     )
 
-    await (await set_with_happy_path(device, mock_log_info))
+    await device.set(SampleLocation(15, 10))
 
     get_mock(device).assert_has_calls(
         [
@@ -294,14 +365,13 @@ async def test_unloading_the_robot_times_out_if_drying_takes_too_long(robot_for_
     assert isinstance(exc_info.value.__cause__, TimeoutError)
 
 
-@patch("dodal.devices.robot.LOGGER.info")
 async def test_moving_the_robot_will_raise_if_error_during_unload(
-    mock_log_info: MagicMock,
+    robot_with_early_error: BartRobot,
 ):
-    device = await _get_bart_robot()
+    device = robot_with_early_error
 
     with pytest.raises(RobotLoadError) as exc_info:
-        await (await set_with_unhappy_path(device, mock_log_info))
+        await device.set(SampleLocation(15, 10))
 
     assert exc_info.value.error_code == 40
     assert exc_info.value.error_string == "Test error"
