3d107b0c335a1e2bb1697ba2ee338e0a9f3a1f00
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-03-04 15:29:12 +0000

    Do not run system tests in CI

--- system_tests/test_puck_detect_system.py
+++ system_tests/test_puck_detect_system.py
@@ -1,8 +1,10 @@
+import pytest
 from ophyd_async.core import init_devices
 
 from dodal.devices.beamlines.i15_1.puck_detector import PuckDetect
 
 
+@pytest.mark.requires(instrument="i15-1")  # connects to the real beamline
 async def test_puck_detect_can_connect_to_real_detection_webpage():
     async with init_devices(mock=True):
         puck_detect = PuckDetect("https://i15-1-cam3-processing.diamond.ac.uk/result")
