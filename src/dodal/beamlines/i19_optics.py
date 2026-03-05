f7ce59b3998e8f36fcac2bb916070f9abda84862
Paul Coe <paul.coe@diamond.ac.uk>
2026-03-05 11:03:24 +0000

    Fixes #1787 Add I19 filter wheel selections
    * Add filter wheel with all six indexed positions

--- src/dodal/beamlines/i19_optics.py
+++ src/dodal/beamlines/i19_optics.py
@@ -1,7 +1,11 @@
+from ophyd_async.epics.motor import Motor
+
 from dodal.common.beamlines.beamline_utils import (
     set_beamline as set_utils_beamline,
 )
 from dodal.device_manager import DeviceManager
+from dodal.devices.attenuator.filter import FilterWheel
+from dodal.devices.attenuator.filter_selections import I19FilterOneSelections
 from dodal.devices.beamlines.i19.access_controlled.hutch_access import (
     ACCESS_DEVICE_NAME,
     HutchAccessControl,
@@ -20,18 +24,54 @@ devices = DeviceManager()
 
 
 @devices.factory()
-def shutter() -> InterlockedHutchShutter:
-    """Real experiment shutter device for I19."""
-    return InterlockedHutchShutter(
-        PREFIX.beamline_prefix, HutchInterlock(PREFIX.beamline_prefix)
-    )
-
-
-@devices.factory()
 def access_control() -> HutchAccessControl:
     """Device to check which hutch is the active hutch on i19."""
     return HutchAccessControl(
         f"{PREFIX.beamline_prefix}-OP-STAT-01:", ACCESS_DEVICE_NAME
+    )
+
+
+@devices.factory()
+def attenuator_x_motor() -> Motor:
+    """Attenuator x motor - drives the resin wedge horizontally orthogonal to the x-ray beam.
+    :return: Device for the attenuator system resin wedge horizontal motion.
+    """
+    return Motor(f"{PREFIX.beamline_prefix}-OP-ATTN-04:X", "ATTENUATOR_X")
+
+
+@devices.factory()
+def attenuator_y_motor() -> Motor:
+    """Attenuator y motor - drives the aluminium / aluminum wedge vertically orthogonal to the x-ray beam.
+    :return: Device for the attenuator system aluminium wedge vertical motion.
+    """
+    return Motor(f"{PREFIX.beamline_prefix}-OP-ATTN-05:Y", "ATTENUATOR_Y")
+
+
+@devices.factory()
+def filter_wheel() -> FilterWheel:
+    """Filter wheel motor - rotates indexed wheel position to filters into/out of x-ray beam.
+    :return: Device for the indexed filter wheel rotation.
+    """
+    return FilterWheel(
+        f"{PREFIX.beamline_prefix}-MO-FILT-01:FILTER",
+        I19FilterOneSelections,
+        "FILTER_W",
+    )
+
+
+@devices.factory()
+def hfm() -> FocusingMirrorWithPiezo:
+    """Get the i19 hfm device, instantiate it if it hasn't already been.
+    If this is called when already instantiated, it will return the existing object.
+    """
+    return FocusingMirrorWithPiezo(f"{PREFIX.beamline_prefix}-OP-HFM-01:")
+
+
+@devices.factory()
+def shutter() -> InterlockedHutchShutter:
+    """Real experiment shutter device for I19."""
+    return InterlockedHutchShutter(
+        PREFIX.beamline_prefix, HutchInterlock(PREFIX.beamline_prefix)
     )
 
 
@@ -41,11 +81,3 @@ def vfm() -> FocusingMirrorWithPiezo:
     If this is called when already instantiated, it will return the existing object.
     """
     return FocusingMirrorWithPiezo(f"{PREFIX.beamline_prefix}-OP-VFM-01:")
-
-
-@devices.factory()
-def hfm() -> FocusingMirrorWithPiezo:
-    """Get the i19 hfm device, instantiate it if it hasn't already been.
-    If this is called when already instantiated, it will return the existing object.
-    """
-    return FocusingMirrorWithPiezo(f"{PREFIX.beamline_prefix}-OP-HFM-01:")
