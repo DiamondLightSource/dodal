60733b124b202d99d785a2ce5635c24b80fbb691
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-03-06 10:21:40 +0000

    Add attenuator for i15-1

---
+++ src/dodal/devices/beamlines/i15_1/attenuator.py
@@ -0,0 +1,55 @@
+from ophyd_async.core import AsyncStatus, StandardReadable, StrictEnum
+
+from dodal.devices.positioner import create_positioner
+
+
+class AttenuatorPositions(StrictEnum):
+    TRANS_100 = "100%"
+    TRANS_50 = "50%"
+    TRANS_10 = "10%"
+    TRANS_1 = "1%"
+    TRANS_0_1 = "0.1%"
+    TRANS_0_01 = "0.01%"
+    TRANS_0_001 = "0.001%"
+
+    @classmethod
+    def from_float(cls, value):
+        string_representation = f"{value}%"
+        try:
+            return AttenuatorPositions(string_representation)
+        except ValueError as e:
+            raise ValueError(
+                f"{string_representation} is not a valid transmission. Options are {', '.join([a.value for a in AttenuatorPositions])}"
+            ) from e
+
+
+class Attenuator(StandardReadable):
+    """A device to change the attenuation of the beam.
+
+    This can be done by doing:
+
+    >>> bps.mv(attenuator, 10)
+    or
+    >>> bps.mv(attenuator, AttenuatorPositions.TRANS_10)
+
+    Where 10 is the transmission in percent.
+
+    There are only a specific set of transmissions that can be selected. The allowed list
+    can be found by doing `list(AttenuatorPositions)`
+    """
+
+    def __init__(self, prefix: str, name: str = "") -> None:
+        self._positioner = create_positioner(
+            AttenuatorPositions, prefix, positioner_pv_suffix="MP1:SELECT"
+        )
+        super().__init__(name)
+
+    @AsyncStatus.wrap
+    async def set(self, value: float | AttenuatorPositions):
+        """Change the transmission to the specified percentage.
+
+        Will raise an error if the percentage is not possible.
+        """
+        if isinstance(value, float) or isinstance(value, int):
+            value = AttenuatorPositions.from_float(value)
+        await self._positioner.set(value)
---
+++ tests/devices/beamlines/i15_1/test_attenuator.py
@@ -0,0 +1,52 @@
+import pytest
+from ophyd_async.core import get_mock_put, init_devices
+
+from dodal.devices.beamlines.i15_1.attenuator import Attenuator, AttenuatorPositions
+
+
+def test_given_valid_position_can_convert_float_into_enum():
+    assert AttenuatorPositions.from_float(0.01) == AttenuatorPositions.TRANS_0_01
+
+
+def test_given_valid_position_can_convert_int_into_enum():
+    assert AttenuatorPositions.from_float(1) == AttenuatorPositions.TRANS_1
+
+
+def test_given_invalid_position_then_sensible_error_on_converting_into_enum():
+    with pytest.raises(ValueError) as e:
+        AttenuatorPositions.from_float(2)
+    assert "100%, 50%," in e.value.args[0]
+
+
+@pytest.fixture
+async def attenuator():
+    with init_devices(mock=True):
+        attenuator = Attenuator("")
+    return attenuator
+
+
+async def test_given_an_attenuator_device_setting_a_valid_position_enum_sets_this_to_the_device(
+    attenuator,
+):
+    await attenuator.set(AttenuatorPositions.TRANS_0_001)
+    get_mock_put(attenuator._positioner.stage_position).assert_called_once_with(
+        AttenuatorPositions.TRANS_0_001
+    )
+
+
+async def test_given_an_attenuator_device_setting_a_valid_float_sets_this_to_the_device(
+    attenuator,
+):
+    await attenuator.set(10)
+    get_mock_put(attenuator._positioner.stage_position).assert_called_once_with(
+        AttenuatorPositions.TRANS_10
+    )
+
+
+async def test_given_an_attenuator_device_setting_an_invalid_float_raises_and_doesnt_set_device(
+    attenuator,
+):
+    with pytest.raises(ValueError) as e:
+        await attenuator.set(2)
+    assert "100%, 50%," in e.value.args[0]
+    get_mock_put(attenuator._positioner.stage_position).assert_not_called()
