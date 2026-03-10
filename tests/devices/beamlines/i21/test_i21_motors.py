08521f74332872e46f6ad6490e0aae6d1f8e1a82
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-03-10 13:56:03 +0000

    Added missing smp read test

--- tests/devices/beamlines/i21/test_i21_motors.py
+++ tests/devices/beamlines/i21/test_i21_motors.py
@@ -1,5 +1,6 @@
 import pytest
 from ophyd_async.core import init_devices
+from ophyd_async.testing import assert_reading, partial_reading
 
 from dodal.devices.beamlines.i21 import I21SampleManipulatorStage
 
@@ -11,5 +12,18 @@ def smp() -> I21SampleManipulatorStage:
     return smp
 
 
-def test_smp_read(smp: I21SampleManipulatorStage) -> None:
-    pass
+async def test_smp_read(smp: I21SampleManipulatorStage) -> None:
+    await assert_reading(
+        smp,
+        {
+            smp.x.name: partial_reading(0),
+            smp.y.name: partial_reading(0),
+            smp.z.name: partial_reading(0),
+            smp.azimuth.name: partial_reading(0),
+            smp.tilt.name: partial_reading(0),
+            smp.polar.name: partial_reading(0),
+            smp.difftth.name: partial_reading(0),
+            smp.para.name: partial_reading(0),
+            smp.perp.name: partial_reading(0),
+        },
+    )
