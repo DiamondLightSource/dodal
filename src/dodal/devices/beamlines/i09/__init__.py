25817771885cdeaac628f64d9d4912d11e269597
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-03-11 15:06:34 +0000

    Correct import

--- src/dodal/devices/beamlines/i09/__init__.py
+++ src/dodal/devices/beamlines/i09/__init__.py
@@ -1,9 +1,3 @@
-from dodal.devices.beamlines.i09.enums import (
-    Grating,
-    IntensityProtection,
-    LensMode,
-    PassEnergy,
-    PsuMode,
-)
+from .enums import Grating, IntensityProtection, LensMode, PassEnergy, PsuMode
 
 __all__ = ["Grating", "IntensityProtection", "LensMode", "PsuMode", "PassEnergy"]
