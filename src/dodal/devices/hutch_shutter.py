e4914d5c5d7cb7dbb5bb0c01c1d774b16ad54149
Paul Coe <119464076+CoePaul@users.noreply.github.com>
2026-03-12 16:33:46 +0000

    Cleave hutch shutter into two classes ( Resolves #1965 ) (#1971)
    * Cleave hutch shutter into two classes ( Resolve#1965 )
    
    * Hutch shutter with interlock now becomes InterlockedHutchShutter
    
    * Hutch shutter falling back on the :ILKSTA default interlock
       does not specify the interlock nor use it internally
    
    * Base class created to abstract out the elements common to both
      types of HutchShutter
    
    * Exist steps within set method defragmented across parent class
       and child classes - keeping the bare minimal differences in
       the children
    
    * Some inevitable method renaming has resulted from repurposing
      the code for the above reasons / purposes
    
    * Apply Additional fixes to hutch shutter coupled elements
    
    * Further tweaks needed to satisfy type checks
       and to correct mistaken use of ShutterDemand.CLOSE
       where ShutterState.CLOSED was required
    
    * Correct snafus in super class constructor call chain
    
    * Calls from child implementations of the base class for shutter
       were passing in name before prefix instead of the other way around
    
    * Additional changes requested in code review

--- src/dodal/devices/hutch_shutter.py
+++ src/dodal/devices/hutch_shutter.py
@@ -95,19 +95,61 @@ class HutchInterlock(BaseHutchInterlock):
         return isclose(float(interlock_state), HUTCH_SAFE_FOR_OPERATIONS, abs_tol=5e-2)
 
 
-class HutchShutter(StandardReadable, Movable[ShutterDemand]):
+class BaseHutchShutter(ABC, StandardReadable, Movable[ShutterDemand]):
     """Device to operate the hutch shutter.
 
-    When a demand is sent and interlock given, the device should first check the hutch
-    status and raise an error if it's not interlocked (searched and locked), meaning
-    it's not safe to operate the shutter. When no interlock specified, the shutter
-    can be operated without checking the hutch status relying on default shutter
-    interlock (:ILKSTA).
+    Base class for HutchShutters - extended by some that do not use an interlock
+    and by others that do.  See child classes for details
+
+    Attributes:
+        control: An writeable EPICS signal to drive the shutter state changes
+        status: A readable EPICS signal to read the present shutter state
+    """
+
+    def __init__(
+        self,
+        bl_shutter_prefix: str,
+        name: str = "",
+    ) -> None:
+        self.control = epics_signal_w(ShutterDemand, f"{bl_shutter_prefix}:CON")
+        with self.add_children_as_readables():
+            self.status = epics_signal_r(ShutterState, f"{bl_shutter_prefix}:STA")
+        super().__init__(name)
+
+    @AsyncStatus.wrap
+    async def set(self, value: ShutterDemand):
+        if TEST_MODE:
+            self._test_mode_set()
+        else:
+            if value == ShutterDemand.OPEN:
+                await self._pre_open_shutter_actions()
+                required_match: ShutterState = ShutterState.OPEN
+            else:
+                required_match: ShutterState = ShutterState.CLOSED
+            await self._shutter_action(value=value, required_match=required_match)
+
+    @abstractmethod
+    async def _pre_open_shutter_actions(self):
+        """Provides internal implementation of pre-requisite steps that support opening of the shutter."""
+
+    async def _shutter_action(self, value: ShutterDemand, required_match: ShutterState):
+        await self.control.set(value)
+        await wait_for_value(self.status, match=required_match, timeout=DEFAULT_TIMEOUT)
+
+    def _test_mode_set(self):
+        LOGGER.warning("Running in test mode, will not operate the experiment shutter.")
+
+
+class HutchShutter(BaseHutchShutter):
+    """Device to operate the hutch shutter.
+
+    When a demand is sent, the shutter can be operated without checking
+    the hutch status, instead relying on default shutter interlock (:ILKSTA).
 
     If the requested shutter position is "Open", the shutter control PV should first
     go to "Reset" and then move to "Open". This is because before opening the hutch
     shutter, the interlock status will show as `failed` until the hutch shutter is
-    reset. This will set the interlock status to `OK`, allowing for shutter operations.
+    reset. The reset will set the interlock status to `OK`, allowing for shutter operations.
     Until this step is done, the hutch shutter can't be opened. The reset is not needed
     for closing the shutter.
     """
@@ -115,41 +157,58 @@ class HutchShutter(StandardReadable, Movable[ShutterDemand]):
     def __init__(
         self,
         bl_prefix: str,
-        interlock: BaseHutchInterlock | None = None,
         shtr_infix: str = EXP_SHUTTER_1_INFIX,
         name: str = "",
     ) -> None:
-        self.control = epics_signal_w(ShutterDemand, f"{bl_prefix}{shtr_infix}:CON")
-        with self.add_children_as_readables():
-            self.status = epics_signal_r(ShutterState, f"{bl_prefix}{shtr_infix}:STA")
-            self.interlock = interlock
-        super().__init__(name)
+        super().__init__(f"{bl_prefix}{shtr_infix}", name)
 
-    @AsyncStatus.wrap
-    async def set(self, value: ShutterDemand):
-        if not TEST_MODE:
-            if value == ShutterDemand.OPEN:
-                await self._check_interlock()
-                await self.control.set(ShutterDemand.RESET)
-                await self.control.set(value)
-                return await wait_for_value(
-                    self.status, match=ShutterState.OPEN, timeout=DEFAULT_TIMEOUT
-                )
-            else:
-                await self.control.set(value)
-                return await wait_for_value(
-                    self.status, match=ShutterState.CLOSED, timeout=DEFAULT_TIMEOUT
-                )
-        else:
-            LOGGER.warning(
-                "Running in test mode, will not operate the experiment shutter."
-            )
+    async def _pre_open_shutter_actions(self):
+        """Required by parent class API - resets the shutter prior to opening."""
+        await self.control.set(ShutterDemand.RESET)
+
+
+class InterlockedHutchShutter(BaseHutchShutter):
+    """Device to operate the hutch shutter.  With an interlock.
+
+    When a demand is sent the device should first check the hutch status and
+    raise an error if it's not interlocked (searched and locked), as not interlocked
+    means it's not safe to operate the shutter.
+
+    If the requested shutter position is "Open", the shutter control PV should first
+    go to "Reset" and then move to "Open". This is because before opening the hutch
+    shutter, the interlock status will show as `failed` until the hutch shutter is
+    reset. The reset will set the interlock status to `OK`, allowing for shutter operations.
+    Until this step is done, the hutch shutter can't be opened. The reset is not needed
+    for closing the shutter.
+
+    Attributes:
+        interlock : Hutch PSS based interlock status checker
+    """
+
+    def __init__(
+        self,
+        bl_prefix: str,
+        interlock: BaseHutchInterlock,
+        shtr_infix: str = EXP_SHUTTER_1_INFIX,
+        name: str = "",
+    ) -> None:
+        with self.add_children_as_readables():
+            self.interlock = interlock
+        super().__init__(f"{bl_prefix}{shtr_infix}", name)
+
+    async def _pre_open_shutter_actions(self):
+        """Required by parent class API - checks interlock, then resets the shutter prior to opening."""
+        await self._check_interlock()
+        await self.control.set(ShutterDemand.RESET)
 
     async def _check_interlock(self):
-        if self.interlock is not None:
-            interlock_state = await self.interlock.shutter_safe_to_operate()
-            if not interlock_state:
-                # If not in test mode, fail. If in test mode, the optics hutch may be open.
-                raise ShutterNotSafeToOperateError(
-                    "The hutch has not been locked, not operating shutter."
-                )
+        """Disrupts shutter opening if the interlock is not in a safe to operate state.
+
+        Raises:
+             ShutterNotSafeToOperateError - whereby an unhappy interlock will veto any attempt to open the shutter.
+        """
+        interlock_state = await self.interlock.shutter_safe_to_operate()
+        if not interlock_state:
+            raise ShutterNotSafeToOperateError(
+                "The hutch has not been locked, not operating shutter."
+            )
