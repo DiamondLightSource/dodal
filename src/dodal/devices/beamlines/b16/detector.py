7e123b401b9ed3268eb125c262c1d713411bd20d
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-03-16 15:12:54 +0000

    Fix b16

--- src/dodal/devices/beamlines/b16/detector.py
+++ src/dodal/devices/beamlines/b16/detector.py
@@ -1,13 +1,30 @@
+from typing import Generic, TypeVar
+
+from ophyd_async.core import DetectorTriggerLogic, SignalDict
 from ophyd_async.epics.adcore import (
     ADArmLogic,
     ADBaseIO,
     ADWriterType,
     AreaDetector,
+    prepare_exposures,
 )
 
 from dodal.common.beamlines.beamline_utils import get_path_provider
 from dodal.common.beamlines.device_helpers import CAM_SUFFIX, TIFF_SUFFIX
-from dodal.devices.controllers import ConfigurableImageModeTriggerLogic
+
+ADBaseIOT = TypeVar("ADBaseIOT", bound=ADBaseIO)
+
+
+class TiffTriggerLogic(DetectorTriggerLogic, Generic[ADBaseIOT]):
+    def __init__(self, driver: ADBaseIOT, deadtime: float):
+        self.driver = driver
+        self.deadtime = deadtime
+
+    async def prepare_internal(self, num: int, livetime: float, deadtime: float):
+        await prepare_exposures(self.driver, num, livetime, deadtime)
+
+    def get_deadtime(self, config_values: SignalDict) -> float:
+        return self.deadtime
 
 
 def software_triggered_tiff_area_detector(prefix: str, deadtime: float = 0.0):
@@ -20,7 +37,7 @@ def software_triggered_tiff_area_detector(prefix: str, deadtime: float = 0.0):
         prefix=prefix,
         driver=driver,
         arm_logic=ADArmLogic(driver),
-        trigger_logic=ConfigurableImageModeTriggerLogic(driver),
+        trigger_logic=TiffTriggerLogic(driver, deadtime),
         path_provider=get_path_provider(),
         writer_type=ADWriterType.TIFF,
         writer_suffix=TIFF_SUFFIX,
--- tests/beamlines/test_b16.py
+++ tests/beamlines/test_b16.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock, patch
 
+from ophyd_async.epics.adcore import ADWriterType
+
 from dodal.common.beamlines.device_helpers import CAM_SUFFIX, TIFF_SUFFIX
 from dodal.devices.beamlines.b16.detector import software_triggered_tiff_area_detector
 
@@ -10,27 +12,25 @@ def test_software_triggered_tiff_area_detector_calls_with_io_correctly():
 
     with (
         patch(
-            "dodal.devices.beamlines.b16.detector.ADTIFFWriter.with_io"
-        ) as mock_writer_with_io,
-        patch(
             "dodal.devices.beamlines.b16.detector.AreaDetector"
         ) as mock_area_detector,
         patch(
-            "dodal.devices.beamlines.b16.detector.ConstantDeadTimeController"
-        ) as mock_controller,
+            "dodal.devices.beamlines.b16.detector.TiffTriggerLogic"
+        ) as mock_tiff_trigger_logic,
         patch(
             "dodal.devices.beamlines.b16.detector.get_path_provider"
         ) as mock_get_path_provider,
         patch("dodal.devices.beamlines.b16.detector.ADBaseIO") as mock_adbase_io,
+        patch("dodal.devices.beamlines.b16.detector.ADArmLogic") as mock_arm_logic,
     ):
-        mock_writer = MagicMock(name="Writer")
-        mock_writer_with_io.return_value = mock_writer
+        mock_arm_logic_instance = MagicMock(name="ADArmLogic")
+        mock_arm_logic.return_value = mock_arm_logic_instance
 
         mock_path_provider = MagicMock(name="PathProvider")
         mock_get_path_provider.return_value = mock_path_provider
 
-        mock_controller_instance = MagicMock(name="Controller")
-        mock_controller.return_value = mock_controller_instance
+        mock_tiff_trigger_logic_instance = MagicMock(name="TriggerLogic")
+        mock_tiff_trigger_logic.return_value = mock_tiff_trigger_logic_instance
 
         mock_driver_instance = MagicMock(name="Driver")
         mock_adbase_io.return_value = mock_driver_instance
@@ -40,26 +40,28 @@ def test_software_triggered_tiff_area_detector_calls_with_io_correctly():
 
         result = software_triggered_tiff_area_detector(prefix)  # default deadtime
 
-        # Assert with_io called with correct arguments
-        mock_writer_with_io.assert_called_once_with(
-            prefix=prefix,
-            path_provider=mock_path_provider,
-            fileio_suffix=TIFF_SUFFIX,
-        )
-
         # Assert ADBaseIO called with correct prefix + suffix
         mock_adbase_io.assert_called_once_with(prefix + CAM_SUFFIX)
 
-        # Assert ConstantDeadTimeController called with driver and deadtime
-        mock_controller.assert_called_once_with(
-            driver=mock_driver_instance,
-            deadtime=default_deadtime,
+        # Assert correct TriggerLogic used.
+        mock_arm_logic.assert_called_once_with(mock_driver_instance)
+
+        # Assert TiffTriggerLogic called with driver and deadtime
+        mock_tiff_trigger_logic.assert_called_once_with(
+            mock_driver_instance,
+            default_deadtime,
         )
 
         # Assert AreaDetector constructed with correct arguments
         mock_area_detector.assert_called_once_with(
-            writer=mock_writer,
-            controller=mock_controller_instance,
+            prefix=prefix,
+            driver=mock_driver_instance,
+            # writer=mock_writer,
+            trigger_logic=mock_tiff_trigger_logic_instance,
+            path_provider=mock_path_provider,
+            arm_logic=mock_arm_logic_instance,
+            writer_type=ADWriterType.TIFF,
+            writer_suffix=TIFF_SUFFIX,
         )
 
         # The function should return the AreaDetector instance
