f2d4e99327ee482b898753ae4a9b54878a51e932
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-04-10 13:05:15 +0000

    Add doc string

--- src/dodal/devices/beamlines/i21/fast_shutter.py
+++ src/dodal/devices/beamlines/i21/fast_shutter.py
@@ -6,6 +6,20 @@ from dodal.devices.fast_shutter import EnumTypesT, FastShutter
 
 
 class FastShutterWithLateralMotor(FastShutter[EnumTypesT], Generic[EnumTypesT]):
+    """Implementation of fast shutter that connects to an epics pv. This pv is an enum
+    that controls the open and close state of the shutter.
+
+    Args:
+        pv (str): The pv to connect to the shutter device.
+        open_state (EnumTypesT): The enum value that corresponds with opening the
+            shutter.
+        close_state (EnumTypesT): The enum value that corresponds with closing the
+            shutter.
+        shutter_suffix (str, optional): Shutter suffix state. Defaults to CON.
+        lateral_suffix (str, optional): Lateral mottor suffix. Defaults to LAT.
+        name (str, optional): The name of the shutter.
+    """
+
     def __init__(
         self,
         prefix: str,
