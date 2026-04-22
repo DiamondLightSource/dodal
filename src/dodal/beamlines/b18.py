042cb62ebf54ed682cd4cec91869e03411020708
Peter Holloway <peter.holloway@diamond.ac.uk>
2026-04-22 14:33:42 +0100

    Remove unused b18 path_provider

--- src/dodal/beamlines/b18.py
+++ src/dodal/beamlines/b18.py
@@ -1,13 +1,4 @@
-from functools import cache
-from pathlib import Path
-
-from ophyd_async.core import PathProvider
-
 from dodal.common.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.common.visit import (
-    LocalDirectoryServiceClient,
-    StaticVisitPathProvider,
-)
 from dodal.device_manager import DeviceManager
 from dodal.devices.synchrotron import Synchrotron
 from dodal.log import set_beamline as set_log_beamline
@@ -19,16 +10,6 @@ set_log_beamline(BL)
 set_utils_beamline(BL)
 
 devices = DeviceManager()
-
-
-@devices.fixture
-@cache
-def path_provider() -> PathProvider:
-    return StaticVisitPathProvider(
-        BL,
-        Path("/dls/b18/data/2025/cm40637-3/bluesky"),
-        client=LocalDirectoryServiceClient(),
-    )
 
 
 @devices.factory()
