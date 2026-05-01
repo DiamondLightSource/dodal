50146077602205943cc519608b854befee0eb457
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-05-01 14:04:08 +0100

    first draft complete

--- src/dodal/common/general_maths/arithmetic_conversions.py
+++ src/dodal/common/general_maths/arithmetic_conversions.py
@@ -1,63 +1,43 @@
-def convert_percentage_to_factor(pc: float):
-    pc = pc / 100
-    return pc
+def calculate(num: float, multiplier: float):
+    return num * multiplier
 
+
+def convert_percentage_to_factor(pc: float):
     # Takes a percentage value and converts it the corresponding multiplication factor
+    return calculate(pc, 1e-2)
 
 
 def convert_factor_to_percentage(f: float):
-    f = f * 100
-    return f
-
     # Takes a multiplication factor and converts it to the corresponding percentage
+    return calculate(f, 1e2)
 
 
-def convert_microns_to_cm(t_um):
-    t_um = t_um / 10000
-    return t_um
-
+def convert_microns_to_cm(t_um: float):
     # Takes the numerical part of a distance in microns and converts this to cm
+    return calculate(t_um, 1e-4)
 
 
-def convert_microns_to_mm(v_um):
-    v_um = v_um / 1000
-    return v_um
-
+def convert_microns_to_mm(v_um: float):
     # Takes the numerical part of a distance in microns and converts this to mm
+    return calculate(v_um, 1e-3)
 
 
-def convert_mm_to_microns(w_mm):
-    w_mm = w_mm * 1000
-    return w_mm
-
+def convert_mm_to_microns(w_mm: float):
     # Takes the numerical part of a distance in mm and converts this to microns
+    return calculate(w_mm, 1e3)
 
 
-def convert_mm_to_cm(x_mm):
-    x_mm = x_mm / 10
-    return x_mm
-
+def convert_mm_to_cm(x_mm: float):
     # Takes the numerical part of a distance in mm and converts this to cm
+    return calculate(x_mm, 1e-1)
 
 
-def convert_cm_to_mm(y_cm):
-    y_cm = y_cm * 10
-    return y_cm
-
+def convert_cm_to_mm(y_cm: float):
     # Takes the numerical part of a distance in cm and converts this to mm
+    return calculate(y_cm, 1e1)
 
 
-def convert_ev_to_kev(energy_ev):
-    energy_ev = energy_ev / 1000
-    return energy_ev
-
-    # Takes the numerical part of an x-ray energy in electron volts and converts this to keV.
-
-    # @staticmethod
-    # def is_within_range(lower_bound, upper_bound, tested_value):
-    #     a = lower_bound + upper_bound + tested_value
-    #     return a
-
-
-# Ensures each argument is a real number and checks last argument is within the range
-# #(inclusive of bound values)
+def convert_ev_to_kev(energy_ev: float):
+    # Takes the numerical part of an x-ray energy in electron volts and converts this to
+    # keV.
+    return calculate(energy_ev, 1e-3)
---
+++ src/dodal/common/general_maths/check_bounds.py
@@ -0,0 +1,26 @@
+def subordinate_range_test(lower_bound: float, upper_bound: float, x: float):
+    outside_range = x < lower_bound or x > upper_bound
+    return not outside_range
+
+
+def is_within_range(lower_bound: float, upper_bound: float, tested_value: float):
+
+    if upper_bound < lower_bound:
+        value_error_message_template = (
+            "Range upper bound %.4f < lower bound %.4f is an invalid condition"
+        )
+        value_error_message = value_error_message_template % (
+            upper_bound,
+            lower_bound,
+        )
+        raise ValueError(value_error_message)
+    else:
+        return subordinate_range_test(
+            lower_bound,
+            upper_bound,
+            tested_value,
+        )
+
+
+# Ensures each argument is a real number and checks last argument is within the range
+# #(inclusive of bound values)
