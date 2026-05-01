7079d5215a1e53faabf49d459c7f9cde81f8253a
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-05-01 11:24:20 +0100

    moved

--- src/dodal/common/general_maths/arithmetic_conversions.py
+++ src/dodal/common/general_maths/arithmetic_conversions.py
@@ -1,54 +1,55 @@
-class Convert:
-    @staticmethod
-    def convert_percentage_to_factor(pc: float):
-        pc = pc / 100
-        return pc
+def convert_percentage_to_factor(pc: float):
+    pc = pc / 100
+    return pc
 
     # Takes a percentage value and converts it the corresponding multiplication factor
-    @staticmethod
-    def convert_factor_to_percentage(f: float):
-        f = f * 100
-        return f
+
+
+def convert_factor_to_percentage(f: float):
+    f = f * 100
+    return f
 
     # Takes a multiplication factor and converts it to the corresponding percentage
-    @staticmethod
-    def convert_microns_to_cm(t_um):
-        t_um = t_um / 10000
-        return t_um
+
+
+def convert_microns_to_cm(t_um):
+    t_um = t_um / 10000
+    return t_um
 
     # Takes the numerical part of a distance in microns and converts this to cm
-    @staticmethod
-    def convert_microns_to_mm(v_um):
-        v_um = v_um * 1000
-        return v_um
+
+
+def convert_microns_to_mm(v_um):
+    v_um = v_um * 1000
+    return v_um
 
     # Takes the numerical part of a distance in microns and converts this to mm
 
-    @staticmethod
-    def convert_mm_to_microns(w_mm):
-        w_mm = w_mm / 1000
-        return w_mm
+
+def convert_mm_to_microns(w_mm):
+    w_mm = w_mm / 1000
+    return w_mm
 
     # Takes the numerical part of a distance in mm and converts this to microns
 
-    @staticmethod
-    def convert_mm_to_cm(x_mm):
-        x_mm = x_mm / 10
-        return x_mm
+
+def convert_mm_to_cm(x_mm):
+    x_mm = x_mm / 10
+    return x_mm
 
     # Takes the numerical part of a distance in mm and converts this to cm
 
-    @staticmethod
-    def convert_cm_to_mm(y_cm):
-        y_cm = y_cm * 10
-        return y_cm
+
+def convert_cm_to_mm(y_cm):
+    y_cm = y_cm * 10
+    return y_cm
 
     # Takes the numerical part of a distance in cm and converts this to mm
 
-    @staticmethod
-    def convert_ev_to_kev(energy_ev):
-        energy_ev = energy_ev / 1000
-        return energy_ev
+
+def convert_ev_to_kev(energy_ev):
+    energy_ev = energy_ev / 1000
+    return energy_ev
 
     # Takes the numerical part of an x-ray energy in electron volts and converts this to keV.
 
--- tests/common/general_maths/test_arithmetic_conversions.py
+++ tests/common/general_maths/test_arithmetic_conversions.py
@@ -1,7 +1,13 @@
 import pytest
-
 from dodal.common.arithmetic_conversions import (
-    Convert,
+    convert_cm_to_mm,
+    convert_ev_to_kev,
+    convert_factor_to_percentage,
+    convert_microns_to_cm,
+    convert_microns_to_mm,
+    convert_mm_to_cm,
+    convert_mm_to_microns,
+    convert_percentage_to_factor,
 )
 
 # expected success tests (the 'Happy Path'):
@@ -9,39 +15,39 @@ from dodal.common.arithmetic_conversions import (
 
 @pytest.mark.parameterize("input,result", [(0.01, 1.0), (1.0, 100)])
 def test_conversion_to_percentage_from_factor(input, result):
-    assert Convert.convert_factor_to_percentage(f=input) == result
+    assert convert_factor_to_percentage(f=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1.0, 0.01), (100, 1.0)])
 def test_conversion_to_factor_from_percentage(input, result):
-    assert Convert.convert_percentage_to_factor(pc=input) == result
+    assert convert_percentage_to_factor(pc=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(10000, 1.0), (1000, 0.1)])
 def test_conversion_from_microns_to_centimetres(input, result):
-    assert Convert.convert_microns_to_cm(t_um=input) == result
+    assert convert_microns_to_cm(t_um=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1000, 1.0), (100, 0.1)])
 def test_conversion_from_microns_to_millimeters(input, result):
-    assert Convert.convert_microns_to_mm(v_um=input) == result
+    assert convert_microns_to_mm(v_um=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1.0, 10000), (0.1, 1000)])
 def test_conversion_from_millimeters_to_microns(input, result):
-    assert Convert.convert_mm_to_microns(w_mm=input) == result
+    assert convert_mm_to_microns(w_mm=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1.0, 1000), (0.1, 100)])
 def test_conversion_from_millimetres_to_centimetres(input, result):
-    assert Convert.convert_mm_to_cm(x_mm=input) == result
+    assert convert_mm_to_cm(x_mm=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1.0, 100), (0.1, 10)])
 def test_conversion_from_centimetres_to_millimetres(input, result):
-    assert Convert.convert_cm_to_mm(y_cm=input) == result
+    assert convert_cm_to_mm(y_cm=input) == result
 
 
 @pytest.mark.parameterize("input,result", [(1000, 1.0), (100, 0.1)])
 def test_conversion_from_electronvolts_to_kiloelectronvolts(input, result):
-    assert Convert.convert_ev_to_kev(energy_ev=input) == result
+    assert convert_ev_to_kev(energy_ev=input) == result
