9bf2010062719e0599a8ec87ddd34dfe1462f19f
Robert Tuck <robert.tuck@diamond.ac.uk>
2026-05-07 16:25:53 +0100

    Fix eiger arming/disarming issues by serialising set operations

--- tests/devices/test_eiger.py
+++ tests/devices/test_eiger.py
@@ -293,7 +293,6 @@ def test_unsuccessful_true_roi_mode_change_results_in_callback_error(
     ]
     with pytest.raises(StatusError):
         run_functions_without_blocking(unwrapped_funcs).wait()
-    LOGGER.error.assert_called()
 
 
 @patch("ophyd.status.Status.__and__")
@@ -355,7 +354,7 @@ def test_stage_runs_successfully(
     fake_eiger.stage()
     fake_eiger.arming_status.wait(1)  # This should complete long before 1s
     # One log message kicking off arming, then one for each of the 13 arming stages
-    assert len(caplog.messages) == 14
+    assert len(caplog.messages) == 18
 
 
 def test_given_stale_parameters_goes_high_before_callbacks_then_stale_parameters_waited_on(
