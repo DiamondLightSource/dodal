4a6ac473a0e6d01b61f25072a1f5d528fbdbb981
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-05-13 12:24:37 +0000

    Fix Mbs AcquisitionMode enums

--- src/dodal/devices/electron_analyser/mbs/mbs_enums.py
+++ src/dodal/devices/electron_analyser/mbs/mbs_enums.py
@@ -2,6 +2,6 @@ from ophyd_async.core import StrictEnum
 
 
 class AcquisitionMode(StrictEnum):
-    FIXED = "FIXED"
-    SWEPT = "SWEPT"
-    DITHER = "DITHER"
+    FIXED = "Fixed"
+    SWEPT = "Swept"
+    DITHER = "Dither"
