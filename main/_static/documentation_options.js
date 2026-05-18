e19e040896b6dfc67a36a61729302806a4b599f2
adaudon <adaudon@users.noreply.github.com>
2026-05-18 16:01:21 +0000

    deploy: abbe6e98c1d24169d65e92bc4dee3be210c0b839

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.3.1.dev12+g5824ccd04',
+    VERSION: '2.3.1.dev13+gabbe6e98c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
