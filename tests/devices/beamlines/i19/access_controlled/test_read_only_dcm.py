55c53b77d2179312fbe5144f5bf83592c780275b
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-05-21 16:48:52 +0100

    updates test to use assert_reading

--- tests/devices/beamlines/i19/access_controlled/test_read_only_dcm.py
+++ tests/devices/beamlines/i19/access_controlled/test_read_only_dcm.py
@@ -1,5 +1,7 @@
 import pytest
+from bluesky.run_engine import RunEngine
 from ophyd_async.core import set_mock_value
+from ophyd_async.testing import assert_reading, partial_reading
 
 from dodal.devices.beamlines.i19.access_controlled.read_only_dcm import ReadOnlyDCM
 
@@ -13,6 +15,13 @@ async def mock_dcm() -> ReadOnlyDCM:
     return mock_dcm
 
 
-async def test_reading(mock_dcm: ReadOnlyDCM):
-    assert await mock_dcm.energy_in_eV.get_value() == 1
-    assert await mock_dcm.wavelength_in_a.get_value() == 100
+async def test_reading(mock_dcm: ReadOnlyDCM, run_engine: RunEngine):
+    prefix = "FOO-MO"
+    await assert_reading(
+        mock_dcm,
+        {
+            f"{prefix}-energy_in_eV": partial_reading(1.0),
+            f"{prefix}-wavelength_in_a": partial_reading(100.0),
+        },
+        False,
+    )
