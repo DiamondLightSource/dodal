6c4a6483f6e7183f73b9ae35c6954ecb3dcf3f2f
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-05-28 13:22:01 +0000

    Add analyser_slits to beamlines

--- src/dodal/beamlines/i05.py
+++ src/dodal/beamlines/i05.py
@@ -4,7 +4,10 @@ from dodal.device_manager import DeviceManager
 from dodal.devices.beamlines.i05 import I05Goniometer
 from dodal.devices.beamlines.i05_shared import LensMode, M4M5Mirror, PassEnergy
 from dodal.devices.common_mirror import XYZSwitchingMirror
-from dodal.devices.electron_analyser.mbs import MbsDetector
+from dodal.devices.electron_analyser.mbs import (
+    EntranceSlitInformationDevice,
+    MbsDetector,
+)
 from dodal.devices.hutch_shutter import HutchShutter
 from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.devices.temperture_controller import Lakeshore336
@@ -51,10 +54,24 @@ def sa() -> I05Goniometer:
 
 
 @devices.factory
-def analyser(pgm: PlaneGratingMonochromator) -> MbsDetector[LensMode, PassEnergy]:
+def analyser_slits() -> EntranceSlitInformationDevice:
+    return EntranceSlitInformationDevice(f"{PREFIX.beamline_prefix}-EA-SLITS-01:POS")
+
+
+@devices.factory
+def analyser(
+    pgm: PlaneGratingMonochromator, analyser_slits: EntranceSlitInformationDevice
+) -> MbsDetector[LensMode, PassEnergy]:
+    config_sigs = (
+        analyser_slits.direction,
+        analyser_slits.size,
+        analyser_slits.shape,
+        analyser_slits.setting,
+    )
     return MbsDetector[LensMode, PassEnergy](
         prefix=f"{PREFIX.beamline_prefix}-EA-DET-02:CAM:",
         lens_mode_type=LensMode,
         pass_energy_type=PassEnergy,
         energy_source=pgm.energy.user_readback,
+        config_sigs=config_sigs,
     )
--- src/dodal/beamlines/i05_1.py
+++ src/dodal/beamlines/i05_1.py
@@ -4,7 +4,10 @@ from dodal.device_manager import DeviceManager
 from dodal.devices.beamlines.i05_1 import XYZAzimuthPolarDefocusStage
 from dodal.devices.beamlines.i05_shared import LensMode, Mj7j8Mirror, PassEnergy
 from dodal.devices.common_mirror import XYZPiezoSwitchingMirror
-from dodal.devices.electron_analyser.mbs import MbsDetector
+from dodal.devices.electron_analyser.mbs import (
+    EntranceSlitInformationDevice,
+    MbsDetector,
+)
 from dodal.devices.hutch_shutter import HutchShutter
 from dodal.devices.pgm import PlaneGratingMonochromator
 from dodal.log import set_beamline as set_log_beamline
@@ -39,11 +42,26 @@ def sm() -> XYZAzimuthPolarDefocusStage:
     return XYZAzimuthPolarDefocusStage(prefix=f"{PREFIX.beamline_prefix}-EA-SM-01:")
 
 
+# Note: Currently fails due to i05-XXX
 @devices.factory
-def analyser(pgm: PlaneGratingMonochromator) -> MbsDetector[LensMode, PassEnergy]:
+def analyser_slits() -> EntranceSlitInformationDevice:
+    return EntranceSlitInformationDevice(f"{PREFIX.beamline_prefix}-EA-SLITS-01:POS")
+
+
+@devices.factory
+def analyser(
+    pgm: PlaneGratingMonochromator, analyser_slits: EntranceSlitInformationDevice
+) -> MbsDetector[LensMode, PassEnergy]:
+    config_sigs = (
+        analyser_slits.direction,
+        analyser_slits.size,
+        analyser_slits.shape,
+        analyser_slits.setting,
+    )
     return MbsDetector[LensMode, PassEnergy](
         prefix=f"{PREFIX.beamline_prefix}-EA-DET-04:CAM:",
         lens_mode_type=LensMode,
         pass_energy_type=PassEnergy,
         energy_source=pgm.energy.user_readback,
+        config_sigs=config_sigs,
     )
