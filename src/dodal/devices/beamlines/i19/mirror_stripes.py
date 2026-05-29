e1056109a73fdba23936e1c1b6dc9d8be125f2bc
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-05-29 18:03:09 +0100

    Get stripe from energy, start idea but needs more work depending on what final input will be

--- src/dodal/devices/beamlines/i19/mirror_stripes.py
+++ src/dodal/devices/beamlines/i19/mirror_stripes.py
@@ -1,3 +1,5 @@
+from enum import StrEnum
+
 from ophyd_async.core import StandardReadable, StrictEnum
 from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
 
@@ -11,6 +13,12 @@ class StripeChoice(StrictEnum):
     EH2_PT = "EH2-Pt"
 
 
+class Stripe(StrEnum):
+    RH = "Rh"
+    SI = "Si"
+    PT = "Pt"
+
+
 class MirrorStripes(StandardReadable):
     """A simple device that can move the mirror stripes and set the voltages on I19."""
 
@@ -19,11 +27,25 @@ class MirrorStripes(StandardReadable):
             # Setting this will trigger the motors move and the voltages change
             self.stripe_choice = epics_signal_rw(StripeChoice, f"{prefix}stripeChoice")
             self.is_busy = epics_signal_r(int, f"{prefix}stripeChange:BUSY")
+            self.error_code = epics_signal_r(
+                int, f"{prefix}stripeVoltageChange:reportErrors.VALB"
+            )
+            self.error_message = epics_signal_r(
+                str, f"{prefix}stripeVoltageChange:reportErrors.VALA"
+            )
         super().__init__(name=name)
 
     # This will need some mapping
-    def select_stripe_based_on_energy(self, energy_in_kev: float):
+    def select_stripe_based_on_energy(self, energy_in_kev: float) -> Stripe:
         # Pt: 20-30 KeV
         # Rh: 10-20 KeV
         # Si: 5-10 KeV
-        pass
+        if 5 <= energy_in_kev < 10:
+            return Stripe.SI
+        elif 10 <= energy_in_kev < 20:
+            return Stripe.RH
+        elif 20 <= energy_in_kev < 30:
+            return Stripe.PT
+        else:
+            raise ValueError("Energy request out of bounds")
+        # TBC - needs improvement
