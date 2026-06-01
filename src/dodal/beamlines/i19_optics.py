49a16cdacdfd79effaf2751b1d8a40cc75bd9936
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2026-06-01 14:57:39 +0100

    Create a device to change mirror stripes in i19-optics (#2078)
    * Create a small device to move the mirror stripes and instantiate it
    
    * Put device in the correct place
    
    * Some notes
    
    * Get stripe from energy, start idea but needs more work depending on what final input will be
    
    * Remove method from device, will be done in access controleld one
    
    * Add a couple of tests
    
    * Pin daq-config-server to main

--- src/dodal/beamlines/i19_optics.py
+++ src/dodal/beamlines/i19_optics.py
@@ -16,6 +16,7 @@ from dodal.devices.beamlines.i19.access_controlled.hutch_access import (
     ACCESS_DEVICE_NAME,
     HutchAccessControl,
 )
+from dodal.devices.beamlines.i19.mirror_stripes import MirrorStripes
 from dodal.devices.common_dcm import (
     DoubleCrystalMonochromatorWithDSpacing,
     PitchAndRollCrystal,
@@ -150,6 +151,11 @@ def hfm() -> FocusingMirrorWithPiezo:
          Focusing mirror piezo device for the HFM.
     """
     return FocusingMirrorWithPiezo(f"{PREFIX.beamline_prefix}-OP-HFM-01:")
+
+
+@devices.factory()
+def mirror_stripes() -> MirrorStripes:
+    return MirrorStripes(f"{PREFIX.beamline_prefix}-MO-IOC-01:")
 
 
 @devices.factory()
