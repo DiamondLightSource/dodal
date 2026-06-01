676f34cfc06bc9a47f8e3242f181d73b2ea24c6c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-06-01 10:51:39 +0100

    Remove method from device, will be done in access controleld one

--- src/dodal/devices/beamlines/i19/mirror_stripes.py
+++ src/dodal/devices/beamlines/i19/mirror_stripes.py
@@ -1,5 +1,3 @@
-from enum import StrEnum
-
 from ophyd_async.core import StandardReadable, StrictEnum
 from ophyd_async.epics.core import epics_signal_r, epics_signal_rw
 
@@ -11,12 +9,6 @@ class StripeChoice(StrictEnum):
     EH2_RH = "EH2-Rh"
     EH2_SI = "EH2-Si"
     EH2_PT = "EH2-Pt"
-
-
-class Stripe(StrEnum):
-    RH = "Rh"
-    SI = "Si"
-    PT = "Pt"
 
 
 class MirrorStripes(StandardReadable):
@@ -34,18 +26,3 @@ class MirrorStripes(StandardReadable):
                 str, f"{prefix}stripeVoltageChange:reportErrors.VALA"
             )
         super().__init__(name=name)
-
-    # This will need some mapping
-    def select_stripe_based_on_energy(self, energy_in_kev: float) -> Stripe:
-        # Pt: 20-30 KeV
-        # Rh: 10-20 KeV
-        # Si: 5-10 KeV
-        if 5 <= energy_in_kev < 10:
-            return Stripe.SI
-        elif 10 <= energy_in_kev < 20:
-            return Stripe.RH
-        elif 20 <= energy_in_kev < 30:
-            return Stripe.PT
-        else:
-            raise ValueError("Energy request out of bounds")
-        # TBC - needs improvement
