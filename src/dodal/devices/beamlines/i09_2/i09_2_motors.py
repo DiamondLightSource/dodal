ec3b250fee3c39fb13c61ec4fde643d1fabfd6d6
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-06-15 15:44:56 +0000

    Add tests for threshold

--- src/dodal/devices/beamlines/i09_2/i09_2_motors.py
+++ src/dodal/devices/beamlines/i09_2/i09_2_motors.py
@@ -9,6 +9,7 @@ from ophyd_async.core import (
     StandardReadable,
     StandardReadableFormat,
     derived_signal_r,
+    set_and_wait_for_other_value,
     soft_signal_rw,
 )
 from ophyd_async.epics.core import epics_signal_r, epics_signal_rw, epics_signal_w
@@ -17,8 +18,17 @@ from ophyd_async.epics.motor import Motor
 
 @dataclass
 class PiezoElectricMovableLogic(MovableLogic):
-    deadband: SignalR[float]
+    within_tolerance: SignalR[bool]
     motor_stop: SignalW[int]
+
+    async def move(self, new_position: float, timeout: float | None) -> None:
+        await set_and_wait_for_other_value(
+            set_signal=self.setpoint,
+            set_value=new_position,
+            match_signal=self.within_tolerance,
+            match_value=True,
+            timeout=timeout,
+        )
 
     async def stop(self) -> None:
         await self.motor_stop.set(1)
@@ -26,35 +36,39 @@ class PiezoElectricMovableLogic(MovableLogic):
     # How do provide calculate_timeout without velocity and acceleration?
 
 
+def _within_threshold_read(setpoint: float, readback: float, deadband: float) -> bool:
+    return abs(setpoint - readback) < deadband
+
+
 class PiezoElectricMotor(StandardMovable[float], StandardReadable):
-    """Motor like device with user_readback and user_setpoint."""
+    """Motor like device with user_readback, user_setpoint, and a stop signals. Has a
+    configurable deadband soft signal to configure the tolerance of when a motor is done
+    moving. For example, if deadband is configured to be 0.5, and the setpoint is 10 and
+    the readback is 9.8, the motor will be done moving and stop blocking for the
+    AsyncStatus.
+    """
 
     def __init__(self, prefix: str, deadband: float = 0.01, name: str = ""):
         with self.add_children_as_readables(StandardReadableFormat.HINTED_SIGNAL):
             self.user_readback = epics_signal_r(float, prefix + ":POS:RD")
-            self.user_setpoint = epics_signal_rw(float, prefix + ":MOV:RD")
-            self.deadband = soft_signal_rw(float, initial_value=deadband)
 
+        self.user_setpoint = epics_signal_rw(float, prefix + ":MOV:RD")
+        self.deadband = soft_signal_rw(float, initial_value=deadband)
         self.motor_stop = epics_signal_w(int, prefix + ":HLT:WR.PROC")
         self.within_threshold = derived_signal_r(
-            self._within_threshold_read,
+            _within_threshold_read,
+            deadband=self.deadband,
             setpoint=self.user_setpoint,
             readback=self.user_readback,
-            deadband=self.deadband,
         )
         super().__init__(name)
-
-    def _within_threshold_read(
-        self, setpoint: float, readback: float, deadband: float
-    ) -> bool:
-        return abs(setpoint - readback) < deadband
 
     @cached_property
     def movable_logic(self) -> PiezoElectricMovableLogic:
         return PiezoElectricMovableLogic(
             readback=self.user_readback,
             setpoint=self.user_setpoint,
-            deadband=self.deadband,
+            within_tolerance=self.within_threshold,
             motor_stop=self.motor_stop,
         )
 
---
+++ tests/devices/beamlines/i09_2/test_i09_2_motors.py
@@ -0,0 +1,63 @@
+import pytest
+from ophyd_async.core import DeviceMock, init_devices, set_mock_value
+from ophyd_async.testing import assert_reading, partial_reading
+
+from dodal.devices.beamlines.i09_2 import (
+    I092SampleManipulator,
+    PiezoElectricMotor,
+)
+
+
+@pytest.fixture
+def sm() -> I092SampleManipulator:
+    with init_devices(mock=True):
+        sm = I092SampleManipulator("TEST:")
+    return sm
+
+
+async def test_sm_read(sm: I092SampleManipulator) -> None:
+    await assert_reading(
+        sm,
+        {
+            "sm-x1": partial_reading(0),
+            "sm-x2": partial_reading(0),
+            "sm-x3": partial_reading(0),
+            "sm-y": partial_reading(0),
+            "sm-z1": partial_reading(0),
+            "sm-z2": partial_reading(0),
+            "sm-xc": partial_reading(0),
+            "sm-zc": partial_reading(0),
+        },
+    )
+
+
+@pytest.fixture
+async def piezo_motor() -> PiezoElectricMotor:
+    piezo_motor = PiezoElectricMotor("TEST:", name="piezo_motor")
+    # Setup mock to not use default as do not want the setpoint and readback to be the
+    # same for tests so we can correctly test threshold signals.
+    await piezo_motor.connect(mock=DeviceMock())
+    return piezo_motor
+
+
+@pytest.mark.parametrize(
+    "deadband, setpoint, readback, expected_within_threshold",
+    (
+        (1, 10, 11.1, False),
+        (2, 10, 7, False),
+        (0.5, 5, 4.9, True),
+        (0.1, 100, 100.01, True),
+    ),
+)
+async def test_piezo_motor_within_threshold(
+    piezo_motor: PiezoElectricMotor,
+    deadband: float,
+    setpoint: float,
+    readback: float,
+    expected_within_threshold: bool,
+) -> None:
+    set_mock_value(piezo_motor.deadband, deadband)
+    set_mock_value(piezo_motor.user_setpoint, setpoint)
+    set_mock_value(piezo_motor.user_readback, readback)
+
+    assert await piezo_motor.within_threshold.get_value() == expected_within_threshold
