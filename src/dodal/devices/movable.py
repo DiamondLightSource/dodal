ab853b8dfd8a78b1417eef7eeb8d1e4682c4263f
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-06-17 13:06:28 +0000

    Revert demo of breaking tests to fix tests again

--- src/dodal/devices/movable.py
+++ src/dodal/devices/movable.py
@@ -9,7 +9,7 @@ from ophyd_async.core import (
     StandardMovable,
     StandardReadable,
     derived_signal_r,
-    set_and_wait_for_other_value,
+    wait_for_value,
 )
 
 
@@ -18,7 +18,6 @@ class MovableWithToleranceLogic(MovableLogic[float]):
     within_tolerance: SignalR[bool]
 
     async def move(self, new_position: float, timeout: float | None) -> None:
-        # await self.setpoint.set(new_position)
         # Don't use set_and_wait_for_other_value until we drop support for Python 3.11.
         # In Python 3.11 there appears to be a timing issue where, if within_tolerance
         # is already True (for example, because the current readback is close to the
@@ -28,14 +27,8 @@ class MovableWithToleranceLogic(MovableLogic[float]):
         #
         # For now, set the setpoint first and then wait for within_tolerance to become
         # True for the newly requested position.
-        # wait_for_value(self.within_tolerance, True, timeout)
-        await set_and_wait_for_other_value(
-            set_signal=self.setpoint,
-            set_value=new_position,
-            match_signal=self.within_tolerance,
-            match_value=True,
-            timeout=timeout,
-        )
+        await self.setpoint.set(new_position)
+        await wait_for_value(self.within_tolerance, True, timeout)
 
 
 def _within_tolerance_read(setpoint: float, readback: float, tolerance: float) -> bool:
