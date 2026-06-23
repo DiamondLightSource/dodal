af89c97d3ba49ab870a2035380eb738f4635b70c
Paul Coe <paul.coe@diamond.ac.uk>
2026-06-23 17:06:55 +0100

    Apply physically realistic absorption bounds (#2103)
    * Tighten permitted absorption coefficients to match physically possible
      materials
    
    * Tidy related unit tests
         where improvements include:
         - ensure only one parameter invalid at one time in a unit test
         - rename test input to explain parameter purpose
         - use named local test variables to match arguments
            ( rather than magic numbers which require reading elsewhere to
              recognise )
         - separate out tests of ineligible values of the correct type
            from those which test incorrect types

--- src/dodal/common/general_maths/material_absorption_maths.py
+++ src/dodal/common/general_maths/material_absorption_maths.py
@@ -14,21 +14,20 @@ from dodal.common.general_maths.transmission_interconversion import (
 
 @validate_call
 def photon_mass_attenuation_per_unit_length(
-    energy_kev: StrictFloat,
-    photon_absorption_factor_per_unit_length: Annotated[StrictFloat, Field(ge=0)],
-    energy_dependence_exponent: Annotated[StrictFloat, Field(le=0)],
+    energy_kev: Annotated[StrictFloat, Field(gt=0)],
+    photon_absorption_factor_per_unit_length: Annotated[StrictFloat, Field(gt=0)],
+    energy_dependence_exponent: Annotated[StrictFloat, Field(lt=0)],
 ) -> float:
     """Calculates mass attenuation per unit length.
 
     Args:
-        energy_kev (StrictFloat): energy
-        photon_absorption_factor_per_unit_length(StrictFloat greater than/equal to 0): p
-        hoton absorption factor per unit length
-        energy_dependence_exponent (StrictFloat less than/equal to 0): energy
-        dependence exponent
+        energy_kev (StrictFloat): X-ray energy in keV (positive values only).
+        photon_absorption_factor_per_unit_length (StrictFloat): Factors of e,
+            in X-ray flux reduction, per unit depth of absorber (positive values only).
+        energy_dependence_exponent (StrictFloat): Roll off of absorption factor (negative values only).
 
     Returns:
-        (float): mass attenuation per unit length.
+        float: Mass attenuation per unit length.
     """
     return photon_absorption_factor_per_unit_length * (
         energy_kev**energy_dependence_exponent
@@ -44,16 +43,17 @@ def attenuation_at_depth_cm(
     0Bn to 1 and 2000 Bn to 1/(e^2).
 
     Args:
-        depth_cm (StrictFloat greater than/equal to 0):  depth of absorption
-        absorption_coefficient_per_cm (StrictFloat greater than/equal to 0): absorption
-        coefficient per cm
+        depth_cm (StrictFloat): Depth of absorber (zero or positive).
+        absorption_coefficient_per_cm (StrictFloat): Modelled absorption,
+            (zero or positive) coefficient per cm,
+            i.e. per cm factors of e reduction in flux.
 
     Raises:
-        ValueError: If either depth_cm or absorption_coefficient are negative, an error
-        is raised
+        ValueError: If either depth_cm or absorption_coefficient are negative,
+                    an error is raised, systems with optical gain are not modelled here.
 
     Returns:
-        (float): attenuation in Barnett units
+        float: Attenuation in Barnett units of a specific depth of absorber.
     """
     ln_t = -(depth_cm * absorption_coefficient_per_cm)
     return attenuation_from_natural_log_of_transmission(ln_t)
@@ -62,23 +62,26 @@ def attenuation_at_depth_cm(
 @validate_call
 def thickness_cm_required_to_attenuate(
     target_attenuation_bn: Annotated[StrictFloat, Field(ge=0)],
-    absorption_coefficient_per_cm: Annotated[StrictFloat, Field(ge=1.0e-14)],
+    absorption_coefficient_per_cm: Annotated[StrictFloat, Field(gt=1.0e-8)],
 ) -> float:
     """Calculates material depth in cm.
 
     Args:
-        target_attenuation_bn (StrictFloat greater than/equal to 0): Target attenuation
-        to meet in Barnett attenuation units.
-        absorption_coefficient_per_cm (StrictFloat greater than/equal to 0):  absorption
-        coefficient per cm
+        target_attenuation_bn (StrictFloat): Target attenuation, (zero or positive),
+            in logarithmic Barnett attenuation units.
+        absorption_coefficient_per_cm (StrictFloat): Factors of e per cm, reduction in flux.
+            N.B. This coefficient is positive (and greater than a lower bound for realism).
 
 
     Raises:
-        ValueError: if attenuation is below zero, or absorption is below the minimum
-        meaningful absorption coefficient, a value error is raised
+        ValueError: if attenuation is below zero,
+            or absorption is less than the lower bound set at the round magnitude 1.0e-8,
+        a value error is raised
+        (Bound set to a round number just below weakest absorption coefficient, of lightest gases,
+         starting around 3.0e-7 per cm for H2),
 
     Returns:
-        (float): material depth in cm.
+        float: material depth in cm.
     """
     ln_target_t = natural_log_of_transmission_from_attenuation(target_attenuation_bn)
     return -(ln_target_t / absorption_coefficient_per_cm)
