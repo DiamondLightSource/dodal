a486444b4dd882c20f6b07522efed033318d3275
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-06-23 13:42:14 +0000

    Update comment

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -1,4 +1,4 @@
-# Add run_engine and mock_config_client to be used in tests
+# Add run_engine and mock_config_client to be used in system tests
 pytest_plugins = [
     "dodal.testing.fixtures.run_engine",
     "dodal.testing.fixtures.config_client",
