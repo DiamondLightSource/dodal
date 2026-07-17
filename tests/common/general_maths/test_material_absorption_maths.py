db980cba9cb22b9d4c603c6f1309ea5a5c419ab8
Paul Coe <119464076+CoePaul@users.noreply.github.com>
2026-07-17 11:23:11 +0100

    Complete general maths functionality for absorbers in i19 transmission/attenuation (#2119)
    * Complete transmission/absorption calculations for attenuation
    
    * Update and extend general math functions for absorbers
    
    * Add missing elements / minor tweaks in pre-existing conversion tests
      -- including new straight line function ( used in wedge absorber geometry )
         which delegates to numpy polynomial (since straight line is a first order polynomial)
    
    * Add foil geometry which handily converts from a set of supported
      thickness units (likely to be natural in the specifications of a real
      flat foil absorber mm, cm or microns ) to absorption friend cm
    
    * Add wedge geometry which not only model the shape, including tip location offset
      but also handily converts between the natural mm for lateral motor poosition
      to thickness in cm
      -- features conversion functions to get
         wedge thickness in cm in x-ray beam for given motor position in mm
         required motor position in mm to achieve requested thickness
    
    * N.B. these geometry models to not restrict range of motor motion,
      but do veto negative thickness
      motor range restrictions have to be imposed on top of the geometry model
      as it is a beamline scientist "operational policy" type decision about which
      parts of the wedge to accept use of - and which to reject
      ( rejection of certain positions can be for shape limitations or bubbles etc )
    
    * Add unit tests for both geometry types

--- tests/common/general_maths/test_material_absorption_maths.py
+++ tests/common/general_maths/test_material_absorption_maths.py
@@ -1,24 +1,101 @@
 import math
+from typing import cast
+from unittest.mock import MagicMock, patch
 
 import pytest
 from pydantic import ValidationError
 
 from dodal.common.general_maths.material_absorption_maths import (
+    AbsorptionCalculator,
+    CompoundAbsorptionCalculator,
+    PolynomialAbsorptionCorrection,
+    SingleRollOffAbsorptionCalculator,
     attenuation_at_depth_cm,
     photon_mass_attenuation_per_unit_length,
     thickness_cm_required_to_attenuate,
 )
 
-
 # happy path
+
+
+# N.B. the mathematical validity of results from a simple absorption calculator
+# are not tested here - since that would duplicate the validatity tests of numerical calculations
+# already checked against the photon_mass_attenuation function
+@patch(
+    "dodal.common.general_maths.material_absorption_maths.photon_mass_attenuation_per_unit_length"
+)
+def test_simple_absorption_calculator_invokes_photon_mass_attenuation_calculator(
+    mock_pma_calc: MagicMock,
+):
+    _material_factor = 3.1415926
+    _roll_off = -2.71828
+    calculator = SingleRollOffAbsorptionCalculator(_material_factor, _roll_off)
+    _x_ray_kev = 15.6302
+    calculator.absorption_coefficient_per_cm(_x_ray_kev)
+    mock_pma_calc.assert_called_once_with(_x_ray_kev, _material_factor, _roll_off)
+
+
+# N.B. the mathematical validity of results from a compound absorption calculator
+# are not tested here - since that would duplicate the validatity tests of numerical calculations
+# already checked against the contributing calculators within
+def test_compound_absorption_calculator_invokes_contributing_calculators():
+    mock_calcs: list[MagicMock] = [
+        MagicMock(spec=AbsorptionCalculator),
+        MagicMock(spec=AbsorptionCalculator),
+    ]
+    calcs = cast(list[AbsorptionCalculator], mock_calcs)
+    compound_calculator = CompoundAbsorptionCalculator(calcs)
+    _x_ray_kev = 12.6039
+    compound_calculator.absorption_coefficient_per_cm(_x_ray_kev)
+    for mocked_calculator in mock_calcs:
+        mocked_calculator.absorption_coefficient_per_cm.assert_called_once_with(
+            _x_ray_kev
+        )
+
+
+@pytest.mark.parametrize(
+    "energy_kev, polynomial_coefficients, expected_absorption_coefficient",
+    [
+        (
+            8.3328,
+            [50.9211, -23.6148, 4.2138, -0.3814, 1.867e-2, -4.709e-4, 4.796e-6],
+            -1.24317951,
+        ),
+        (
+            12.3984,
+            [43.4, -22.8148, 4.138, -0.3714, 1.967e-2, -5.709e-4, 3.796e-6],
+            0.11254471963,
+        ),
+        (
+            11.9187,
+            [50.9211, -23.6148, 4.2138, -0.3814, 1.867e-2, -4.709e-4, 4.796e-6],
+            -0.45286288,
+        ),
+    ],
+)
+def test_corrective_polynomial_absorption_calculator(
+    energy_kev, polynomial_coefficients, expected_absorption_coefficient
+):
+    corrective_calculator = PolynomialAbsorptionCorrection(polynomial_coefficients)
+    assert corrective_calculator.absorption_coefficient_per_cm(
+        energy_kev
+    ) == pytest.approx(expected_absorption_coefficient)
+
+
 @pytest.mark.parametrize(
     "energy_kev, photon_absorption_factor_per_unit_length, energy_dependence_exponent, "
     "result",
     [
-        (5.042, 1.98e2, -2.717, 2.44170544),  # Arbitrary Energy
-        (8.3328, 2.5706e3, -2.83, 6.3708311),  # Nickel
-        (11.9187, 1.48e3, -2.93, 1.03970725),  # Gold-Three
-        (25.514, 6.48e3, -2.41, 2.63778077),  # Silver
+        (5.042, 1.98e2, -2.717, 2.44170544),  # At an arbitrary energy 5.042 keV
+        (8.3328, 2.5706e3, -2.83, 6.3708311),  # Nickel energy 8.3328 keV
+        (11.9187, 1.48e3, -2.93, 1.03970725),  # Gold-Three energy 11.9187 keV
+        (25.514, 6.48e3, -2.41, 2.63778077),  # Silver energy 25.514 keV
+        (
+            20,
+            1201,
+            -2,
+            3.0025,
+        ),  # 20 keV: Test that integers, for input args, are not rejected
     ],
 )
 def test_photon_mass_attenuation_per_unit_length(
@@ -84,6 +161,8 @@ def test_attenuation_at_depth_cm(depth_cm, absorption_coefficient_per_cm, result
 
 
 # inauspicious path
+
+
 @pytest.mark.parametrize("negative_energy", [-0.1, -12.3, -9739.43])
 def test_photon_mass_attenuation_per_unit_length_errors_with_negative_energy(
     negative_energy,
@@ -127,7 +206,7 @@ def test_photon_mass_attenuation_per_unit_length_errors_with_invalid_exponent(
     invalid_roll_off,
 ):
     _energy_kev = 13.819
-    _absorption_coefficient = 24.8
+    _absorption_coefficient = 2.148
     with pytest.raises(ValidationError):
         photon_mass_attenuation_per_unit_length(
             _energy_kev, _absorption_coefficient, invalid_roll_off
@@ -194,7 +273,7 @@ def test_attenuation_at_depth_raises_error_with_invalid_depth(invalid_depth):
 
 
 @pytest.mark.parametrize(
-    "invalid_absorption", ["a", [], None, math.sin, object(), False]
+    "invalid_absorption", ["a", [], None, math.tan, object(), False]
 )
 def test_attenuation_at_depth_raises_error_with_invalid_attenuation(invalid_absorption):
     _depth_cm = 0.425
