fcecddb653ddf31adbc9ffee4f6d3ae231318400
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-07-20 13:36:20 +0000

    add helper for repeats

--- src/dodal/devices/beamlines/i06_1/temperature_controller.py
+++ src/dodal/devices/beamlines/i06_1/temperature_controller.py
@@ -1,4 +1,5 @@
 from ophyd_async.core import (
+    SignalRW,
     StandardReadableFormat,
     StrictEnum,
 )
@@ -10,6 +11,17 @@ from dodal.devices.temperature_controller import (
     BaseTemperatureSensor,
     TemperatureController,
 )
+
+
+def epics_signal_dmd_sts(
+    datatype: type, prefix: str, infix: str, suffix: str
+) -> SignalRW:
+    """Helper to create an epics_signal_rw with DMD and STS paths."""
+    return epics_signal_rw(
+        datatype,
+        write_pv=f"{prefix}DMD:{infix}{suffix}",
+        read_pv=f"{prefix}STS:{infix}{suffix}",
+    )
 
 
 class HeaterRange(StrictEnum):
@@ -35,7 +47,7 @@ class CryoconM32Sensor(BaseTemperatureSensor):
 
         with self.add_children_as_readables(StandardReadableFormat.HINTED_SIGNAL):
             self.sensor = epics_signal_r(float, prefix + "STS:T1")
-            self._sensor2 = epics_signal_r(float, prefix + "STS:T2")
+            self.sensor2 = epics_signal_r(float, prefix + "STS:T2")
         with self.add_children_as_readables(StandardReadableFormat.CONFIG_SIGNAL):
             self._create_config_signals(prefix=prefix + "STS:T1", attr_prefix="sensor")
             self._create_config_signals(prefix=prefix + "STS:T2", attr_prefix="sensor2")
@@ -54,20 +66,12 @@ class CryoconM32Sensor(BaseTemperatureSensor):
 class CryoconM32Heater(BaseHeater):
     def __init__(self, prefix: str, infix: str, name: str = ""):
         with self.add_children_as_readables(StandardReadableFormat.CONFIG_SIGNAL):
-            self.setpoint = epics_signal_rw(
-                float,
-                write_pv=f"{prefix}DMD:{infix}MANUAL",
-                read_pv=f"{prefix}STS:{infix}MANUAL",
+            self.setpoint = epics_signal_dmd_sts(float, prefix, infix, "MANUAL")
+            self.output_range = epics_signal_dmd_sts(
+                HeaterRange, prefix, infix, "RANGE"
             )
-            self.mode = epics_signal_rw(
-                HeaterMode,
-                f"{prefix}DMD:{infix}LOOPTYPE",
-            )
-            self.output_range = epics_signal_rw(
-                HeaterRange,
-                write_pv=f"{prefix}DMD:{infix}RANGE",
-                read_pv=f"{prefix}STS:{infix}RANGE",
-            )
+            self.mode = epics_signal_rw(HeaterMode, f"{prefix}DMD:{infix}LOOPTYPE")
+
         with self.add_children_as_readables(StandardReadableFormat.HINTED_SIGNAL):
             self.output = epics_signal_r(float, f"{prefix}STS:{infix}HTRREAD")
 
@@ -81,19 +85,14 @@ class SCMTemperatureController(TemperatureController):
         infix: str = "LOOP1:",
         name: str = "",
     ):
+
         sensor = CryoconM32Sensor(prefix=prefix)
         heater = CryoconM32Heater(prefix=prefix, infix=infix)
-        setpoint = epics_signal_rw(
-            float,
-            write_pv=f"{prefix}DMD:{infix}SETPOINT",
-            read_pv=f"{prefix}STS:{infix}SETPOINT",
-        )
-        self.ramp_rate = epics_signal_rw(
-            float,
-            write_pv=f"{prefix}DMD:{infix}RAMPRATE",
-            read_pv=f"{prefix}STS:{infix}RAMPRATE",
-        )
-        self.ramp_mode = epics_signal_r(str, f"{prefix}STS:{infix}LOOPRAMP")
+        setpoint = epics_signal_dmd_sts(float, prefix, infix, "SETPOINT")
+
+        with self.add_children_as_readables(StandardReadableFormat.CONFIG_SIGNAL):
+            self.ramp_mode = epics_signal_r(str, f"{prefix}STS:{infix}LOOPRAMP")
+            self.ramp_rate = epics_signal_dmd_sts(float, prefix, infix, "RAMPRATE")
         pid = PID(
             prefix=f"{prefix}DMD:{infix}",
             suffix_p="PGAIN",
