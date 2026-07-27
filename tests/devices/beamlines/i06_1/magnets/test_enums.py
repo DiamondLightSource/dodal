1a2842585ee15793d2cfc66915a0396c28e67c52
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2026-07-27 10:36:49 +0000

    Add tests for enums

---
+++ tests/devices/beamlines/i06_1/magnets/test_enums.py
@@ -0,0 +1,30 @@
+import pytest
+
+from dodal.devices.beamlines.i06_1.magnets import MagnetMode
+
+
+@pytest.mark.parametrize(
+    "mode, expected_alias",
+    [
+        (MagnetMode.UNIAXIAL_X, "x"),
+        (MagnetMode.UNIAXIAL_Y, "y"),
+        (MagnetMode.UNIAXIAL_Z, "z"),
+    ],
+)
+def test_magnet_mode_axis_alias(mode: MagnetMode, expected_alias: str) -> None:
+    assert mode.axis_alias == expected_alias
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        MagnetMode.CUBIC,
+        MagnetMode.PLANAR_XZ,
+        MagnetMode.QUADRANT_XY,
+        MagnetMode.SPHERICAL,
+        MagnetMode.UNDEFINED,
+    ],
+)
+def test_magnet_mode_axis_alias_raises_error_when_not_axis(mode: MagnetMode) -> None:
+    with pytest.raises(ValueError):
+        mode.axis_alias  # noqa: B018
