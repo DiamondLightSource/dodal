025f2484b712bb4a13e0a2d6a12aee6b3b3196ec
Paul Coe <paul.coe@diamond.ac.uk>
2026-08-11 14:57:09 +0100

    Add transmission module for I19 beamline devices
    * Start with adding JSON validation for system specification
      JSON dict-like structure expectations
    
    * Pydantic BaseModel classes map sub-structures ( sub-dicts ) within
      the system description structure here adding
       JSON validation for energy_interval
       [ the range of x-ray energies (in keV ) over which an absorption curve is valid ]
       JSON validation for absorption fit curve parameters
       [ the scaling constant, roll-off vs energy and an optional residuals polynomial ]

---
+++ src/dodal/devices/beamlines/i19/transmission/spec_from_json/energy_interval_spec.py
@@ -0,0 +1,33 @@
+
+from pydantic import BaseModel, ConfigDict, Field, StrictFloat, model_validator
+
+
+class EnergyIntervalSpec(BaseModel):
+    """JSON built sub-dict of one energy range, used in specifying material absorption spectra.
+
+    Note:
+        The range of energies is a closed interval, so inclusive of end values.
+
+    Attributes:
+        units: At present only 'keV' (or 'kiloelectronvolts') are supported.
+        lower: The lower end of the energy interval (range) for specific absorption fit curve.
+        upper: The upper end of the energy interval (range) for specific absorption fit curve.
+    """
+
+    units: str = Field(..., pattern=r"^(keV|kiloelectronvolts)$")
+    lower: StrictFloat
+    upper: StrictFloat
+
+    # Base Model internal setting to make this class immutable
+    model_config = ConfigDict(frozen=True)
+
+    @model_validator(mode="after")
+    def validate_attributes(self) -> "EnergyIntervalSpec":
+        if 0.0 < self.lower < self.upper:
+            return self
+        _msg = (
+            f"Energy interval lower {self.lower} and upper {self.upper} bounds are in wrong order."
+            if self.lower > 0.0
+            else f"Energy bound use {self.lower} to define lower end of x-ray energy interval, must be greater than zero."
+        )
+        raise ValueError(_msg)
