2597ba1e012703d97eeee7289a2eb768106303d3
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-09-02 18:12:00 +0100

    Use PVA

--- src/dodal/devices/beamlines/b21/flame_spectrometer.py
+++ src/dodal/devices/beamlines/b21/flame_spectrometer.py
@@ -4,12 +4,14 @@ from ophyd_async.epics.core import epics_signal_r, epics_signal_rw_rbv, epics_si
 
 class FlameSpectrometer(StandardReadable):
     def __init__(self, prefix: str, name: str = ""):
-        self.filepath = epics_signal_rw_rbv(str, f"{prefix}:FilePath")
-        self.filename = epics_signal_rw_rbv(str, f"{prefix}:FileName")
-        self.exposure_time = epics_signal_rw_rbv(float, f"{prefix}:IntegrationTime")
-        self.capture = epics_signal_rw_rbv(bool, f"{prefix}:Capture")
-        self.single_trigger = epics_signal_x(f"{prefix}:SingleScan")
+        self.filepath = epics_signal_rw_rbv(str, f"pva://{prefix}:FilePath")
+        self.filename = epics_signal_rw_rbv(str, f"pva://{prefix}:FileName")
+        self.exposure_time = epics_signal_rw_rbv(
+            float, f"pva://{prefix}:IntegrationTime"
+        )
+        self.capture = epics_signal_rw_rbv(bool, f"pva://{prefix}:Capture")
+        self.single_trigger = epics_signal_x(f"pva://{prefix}:SingleScan")
 
-        self.scan_in_progress = epics_signal_r(bool, f"{prefix}:ScanInProgress")
+        self.scan_in_progress = epics_signal_r(bool, f"pva://{prefix}:ScanInProgress")
 
         super().__init__(name)
