a208be405c0d61dd2fe9c989525c105708c80a20
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-09-02 17:59:12 +0100

    Add the flame spectrometer

---
+++ src/dodal/devices/beamlines/b21/flame_spectrometer.py
@@ -0,0 +1,15 @@
+from ophyd_async.core import StandardReadable
+from ophyd_async.epics.core import epics_signal_r, epics_signal_rw_rbv, epics_signal_x
+
+
+class FlameSpectrometer(StandardReadable):
+    def __init__(self, prefix: str, name: str = ""):
+        self.filepath = epics_signal_rw_rbv(str, f"{prefix}:FilePath")
+        self.filename = epics_signal_rw_rbv(str, f"{prefix}:FileName")
+        self.exposure_time = epics_signal_rw_rbv(float, f"{prefix}:IntegrationTime")
+        self.capture = epics_signal_rw_rbv(bool, f"{prefix}:Capture")
+        self.single_trigger = epics_signal_x(f"{prefix}:SingleScan")
+
+        self.scan_in_progress = epics_signal_r(bool, f"{prefix}:ScanInProgress")
+
+        super().__init__(name)
