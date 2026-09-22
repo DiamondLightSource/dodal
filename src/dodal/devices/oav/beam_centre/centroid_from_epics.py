ca46de32e5eb19edc5f64c462d13c1fcd185d5fd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-09-22 18:11:36 +0100

    Add some PVs to set up AD plugin chain

--- src/dodal/devices/oav/beam_centre/centroid_from_epics.py
+++ src/dodal/devices/oav/beam_centre/centroid_from_epics.py
@@ -1,18 +1,48 @@
 from ophyd_async.core import (
     StandardReadable,
+    StrictEnum,
 )
-from ophyd_async.epics.core import (
-    epics_signal_r,
-)
+from ophyd_async.epics.core import epics_signal_r, epics_signal_rw, epics_signal_rw_rbv
+
+CC_INFIX = "CC:"
+STAT_INFIX = "STAT:"
+
+
+class ColourMode(StrictEnum):
+    MONO = "Mono"
+    BAYER = "Bayer"
+    RGB1 = "RGB1"
+    RGB2 = "RGB2"
+    RGB3 = "RGB3"
+    YUV444 = "YUV444"
+    YUV422 = "YUV422"
+    YUV421 = "YUV421"
 
 
 class CentroidFromEpics(StandardReadable):
-    """Needs plugn chain CAM -> CC( to make B/W) -> STAT.
+    """Has the PVs to set up the CAM -> CC -> STAT plugin chain and gets the centroid.
 
     Need to spend some time thinking about taking X/Y croping on CAM into account
     """
 
-    def __init__(self, prefix: str, name: str = ""):
-        self.beam_centre_y = epics_signal_r(float, "BL19I-EA-OAV-01:STAT:CentroidY_RBV")
-        self.beam_centre_x = epics_signal_r(float, "BL19I-EA-OAV-01:STAT:CentroidX_RBV")
+    def __init__(
+        self,
+        prefix: str,
+        cc_infix: str = CC_INFIX,
+        stat_infix: str = STAT_INFIX,
+        name: str = "",
+    ):
+        self.stat_array_port = epics_signal_rw_rbv(
+            str, f"{prefix}{stat_infix}NDArrayPort"
+        )
+        self.centroid_threshold = epics_signal_rw(
+            float, f"{prefix}{stat_infix}CentroidThreshold"
+        )
+        self.beam_centre_y = epics_signal_r(float, f"{prefix}{stat_infix}CentroidY_RBV")
+        self.beam_centre_x = epics_signal_r(float, f"{prefix}{stat_infix}CentroidX_RBV")
+
+        self.cc_array_port = epics_signal_rw_rbv(str, f"{prefix}{cc_infix}NDArrayPort")
+        self.colour_mode = epics_signal_rw(
+            ColourMode, f"{prefix}{cc_infix}ColorModeOut"
+        )
         super().__init__(name)
